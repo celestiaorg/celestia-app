3ac7825297c92d66823d155bf681a3447a64f840
evan-forbes <evan.samuel.forbes@gmail.com>
2021-01-29 09:16:42 -0600

    fix protocgen.sh from stopping prematurely
    remove generated files that didn't get deleted
    
    stop protocgen.sh from stopping prematurely

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -25,19 +25,6 @@ Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
 
 done
 
-# command to generate docs using protoc-gen-doc
-buf protoc \
--I "proto" \
--I "third_party/proto" \
---doc_out=./docs/core \
---doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
-$(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
-go mod tidy
-
-# generate codec/testdata proto code
-buf protoc -I "proto" -I "third_party/proto" -I "testutil/testdata" --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. ./testutil/testdata/*.proto
-
 # move proto files to the right places
 cp -r github.com/cosmos/cosmos-sdk/* ./
 rm -rf github.com
