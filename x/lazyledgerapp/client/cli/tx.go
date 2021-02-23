7adb975ec7129d3ea2265898df1434278a84913f
evan-forbes <evan.samuel.forbes@gmail.com>
2021-02-23 09:34:14 -0600

    implement sub command to generate a MsgWirePayForMessage from the cli

--- x/lazyledgerapp/client/cli/tx.go
+++ x/lazyledgerapp/client/cli/tx.go
@@ -20,7 +20,7 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(CmdCreatePayForMessage())
 
 	return cmd
 }
