bdf5d8b22791eebd4ad7ee6db1b854ebc5d5f6c9
evan-forbes <evan.samuel.forbes@gmail.com>
2021-06-10 15:25:48 -0500

    imlement client code for signing transactions

---
+++ x/lazyledgerapp/client/builder.go
@@ -0,0 +1,172 @@
+package client
+
+import (
+	"context"
+
+	sdkclient "github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/lazyledger/lazyledger-app/app/params"
+	"google.golang.org/grpc"
+)
+
+// Builder wraps the standard sdkclient.TxBuilder add a few quality of life methods along
+// with keeping track of the account number to produce transactions
+type Builder struct {
+	sdkclient.TxBuilder
+	encCfg               params.EncodingConfig
+	address              sdktypes.Address
+	chainID              string
+	accountNum, sequence uint64
+}
+
+func NewBuilder(address sdktypes.Address, chainID string) *Builder {
+	encCfg := params.RegisterAccountInterface(params.MakeEncodingConfig())
+	txBuilder := encCfg.TxConfig.NewTxBuilder()
+	return &Builder{
+		TxBuilder: txBuilder,
+		encCfg:    encCfg,
+		address:   address,
+		chainID:   chainID,
+	}
+}
+
+// UpdateAccountNumber queries the applicaiton to find the latest account number and
+// sequence, updating the respective internal fields
+func (b *Builder) UpdateAccountNumber(ctx context.Context, conn *grpc.ClientConn) error {
+	accNum, seqNumb, err := QueryAccount(ctx, conn, b.encCfg, b.address.String())
+	if err != nil {
+		return err
+	}
+	b.accountNum = accNum
+	b.sequence = seqNumb
+	return nil
+}
+
+// BuildSignedTx creates a signed sdk.Tx that contains the provided WirePayForMessage.
+// The internal fees, the account number, and the sequence should be updated before calling this function
+// (see b.UpdateAccountNumber, sdkclient.TxBuilder.SetGasLimit, and sdkclient.TxBuilder.SetFeeAmount)
+func (b *Builder) BuildSignedTx(msg sdktypes.Msg, kr keyring.Keyring) (authsigning.Tx, error) {
+	// set the msg
+	err := b.SetMsgs(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	// lookup account info
+	keyInfo, err := kr.KeyByAddress(b.address)
+	if err != nil {
+		return nil, err
+	}
+
+	// we must first set an empty signature in order generate
+	// the correct sign bytes
+	sigV2 := signing.SignatureV2{
+		PubKey: keyInfo.GetPubKey(),
+		Data: &signing.SingleSignatureData{
+			SignMode:  signing.SignMode_SIGN_MODE_DIRECT,
+			Signature: nil,
+		},
+		Sequence: b.sequence,
+	}
+
+	// set the empty signature
+	err = b.SetSignatures(sigV2)
+	if err != nil {
+		return nil, err
+	}
+
+	// generate the new signing data
+	signerData := authsigning.SignerData{
+		ChainID:       b.chainID,
+		AccountNumber: b.accountNum,
+		Sequence:      b.sequence,
+	}
+
+	// Generate the bytes to be signed.
+	bytesToSign, err := b.encCfg.TxConfig.SignModeHandler().GetSignBytes(
+		signing.SignMode_SIGN_MODE_DIRECT,
+		signerData,
+		b.GetTx(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Sign those bytes using the keyring. we are ignoring the returned public key
+	sigBytes, _, err := kr.SignByAddress(b.address, bytesToSign)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct the SignatureV2 struct, this time including a real signature
+	sigV2 = signing.SignatureV2{
+		PubKey: keyInfo.GetPubKey(),
+		Data: &signing.SingleSignatureData{
+			SignMode:  signing.SignMode_SIGN_MODE_DIRECT,
+			Signature: sigBytes,
+		},
+		Sequence: b.sequence,
+	}
+
+	// set the final signature
+	err = b.SetSignatures(sigV2)
+	if err != nil {
+		return nil, err
+	}
+
+	// return the signed transaction
+	return b.GetTx(), nil
+}
+
+// EncodeTx uses the builder's encoding config to encode the provided sdk transaction
+func (b Builder) EncodeTx(tx sdktypes.Tx) ([]byte, error) {
+	return b.encCfg.TxConfig.TxEncoder()(tx)
+}
+
+// SignerData returns the signer data from the underlying builder
+func (b Builder) SignerData() authsigning.SignerData {
+	return authsigning.SignerData{
+		ChainID:       b.chainID,
+		AccountNumber: b.accountNum,
+		Sequence:      b.sequence,
+	}
+}
+
+// BroadcastTx uses the provided grpc connection to broadcast a signed and encoded transaction
+func BroadcastTx(ctx context.Context, conn *grpc.ClientConn, mode tx.BroadcastMode, txBytes []byte) (*tx.BroadcastTxResponse, error) {
+	txClient := tx.NewServiceClient(conn)
+
+	return txClient.BroadcastTx(
+		ctx,
+		&tx.BroadcastTxRequest{
+			Mode:    mode,
+			TxBytes: txBytes,
+		},
+	)
+}
+
+// QueryAccount fetches the account number and sequence number from the lazyledger-app node.
+func QueryAccount(ctx context.Context, conn *grpc.ClientConn, encCfg params.EncodingConfig, address string) (accNum uint64, seqNum uint64, err error) {
+	qclient := authtypes.NewQueryClient(conn)
+	resp, err := qclient.Account(
+		ctx,
+		&authtypes.QueryAccountRequest{Address: address},
+	)
+	if err != nil {
+		return accNum, seqNum, err
+	}
+
+	var acc authtypes.AccountI
+	err = encCfg.Marshaler.UnpackAny(resp.Account, &acc)
+	if err != nil {
+		return accNum, seqNum, err
+	}
+
+	accNum, seqNum = acc.GetAccountNumber(), acc.GetSequence()
+	return
+}
---
+++ x/lazyledgerapp/client/builder_test.go
@@ -0,0 +1,141 @@
+package client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
+	"github.com/lazyledger/lazyledger-app/x/lazyledgerapp/types"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestBuildSignedPayForMessage(t *testing.T) {
+	testRing := generateKeyring(t, "testAccount")
+
+	info, err := testRing.Key("testAccount")
+	require.NoError(t, err)
+
+	testAddr := info.GetAddress()
+
+	b := NewBuilder(testAddr, "chain-id")
+	require.NoError(t, err)
+
+	namespace := []byte{1, 1, 1, 1, 1, 1, 1, 1}
+	message := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+	msg, err := types.NewMsgWirePayForMessage(
+		namespace,
+		message,
+		info.GetPubKey().Bytes(),
+		&types.TransactionFee{},
+		4, 16, 32,
+	)
+	require.NoError(t, err)
+
+	signedTx, err := b.BuildSignedTx(msg, testRing)
+	require.NoError(t, err)
+
+	sigs, err := signedTx.GetSignaturesV2()
+	require.NoError(t, err)
+
+	err = authsigning.VerifySignature(info.GetPubKey(), b.SignerData(), sigs[0].Data, b.encCfg.TxConfig.SignModeHandler(), signedTx)
+	require.NoError(t, err)
+}
+
+func TestBroadcastPayForMessage(t *testing.T) {
+	t.Skip("no local connection to app and no funds in wallet")
+	testRing := generateKeyring(t, "testAccount")
+
+	info, err := testRing.Key("testAccount")
+	require.NoError(t, err)
+
+	testAddr := info.GetAddress()
+
+	b := NewBuilder(testAddr, "chain-id")
+	require.NoError(t, err)
+
+	RPCAddress := "127.0.0.1:9090"
+
+	rpcClient, err := grpc.Dial(RPCAddress, grpc.WithInsecure())
+	err = b.UpdateAccountNumber(context.TODO(), rpcClient)
+
+	b.SetGasLimit(100000)
+	coin := sdktypes.Coin{
+		Denom:  "token",
+		Amount: sdktypes.NewInt(10),
+	}
+
+	b.SetFeeAmount(sdktypes.NewCoins(coin))
+
+	namespace := []byte{1, 1, 1, 1, 1, 1, 1, 1}
+	message := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+	msg, err := types.NewMsgWirePayForMessage(
+		namespace,
+		message,
+		info.GetPubKey().Bytes(),
+		&types.TransactionFee{},
+		4, 16, 32,
+	)
+	require.NoError(t, err)
+
+	signedTx, err := b.BuildSignedTx(msg, testRing)
+	require.NoError(t, err)
+
+	encodedTx, err := b.EncodeTx(signedTx)
+	require.NoError(t, err)
+
+	resp, err := BroadcastTx(context.TODO(), rpcClient, tx.BroadcastMode_BROADCAST_MODE_SYNC, encodedTx)
+	require.NoError(t, err)
+
+	require.Equal(t, "", resp.TxResponse.Data)
+}
+
+func TestUpdateAccountNumber(t *testing.T) {
+	t.Skip("no local connection to app and no funds in wallet")
+	testRing := generateKeyring(t, "testAccount")
+
+	info, err := testRing.Key("testAccount")
+	require.NoError(t, err)
+
+	testAddr := info.GetAddress()
+
+	b := NewBuilder(testAddr, "test")
+	require.NoError(t, err)
+
+	RPCAddress := "127.0.0.1:9090"
+
+	rpcClient, err := grpc.Dial(RPCAddress, grpc.WithInsecure())
+	err = b.UpdateAccountNumber(context.TODO(), rpcClient)
+	require.NoError(t, err)
+}
+
+func generateKeyring(t *testing.T, accts ...string) keyring.Keyring {
+	t.Helper()
+	kb := keyring.NewInMemory()
+
+	for _, acc := range accts {
+		_, _, err := kb.NewMnemonic(acc, keyring.English, "", hd.Secp256k1)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	_, err := kb.NewAccount(testAccName, testMnemo, "1234", "", hd.Secp256k1)
+	if err != nil {
+		panic(err)
+	}
+
+	return kb
+}
+
+const (
+	// nolint:lll
+	testMnemo   = `ramp soldier connect gadget domain mutual staff unusual first midnight iron good deputy wage vehicle mutual spike unlock rocket delay hundred script tumble choose`
+	testAccName = "test-account"
+)
