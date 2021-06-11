b5c730019f1d033beb20ba9bb13e69ea372a0c80
evan-forbes <evan.samuel.forbes@gmail.com>
2021-06-11 13:18:39 -0500

    add extra docs

--- x/lazyledgerapp/client/builder.go
+++ x/lazyledgerapp/client/builder.go
@@ -35,8 +35,9 @@ func NewBuilder(address sdktypes.Address, chainID string) *Builder {
 	}
 }
 
-// UpdateAccountNumber queries the application to find the latest account number and
-// sequence, updating the respective internal fields
+// UpdateAccountNumber queries the application to find the latest account number
+// and sequence, updating the respective internal fields. This method should be
+// called before building the signed transaction
 func (b *Builder) UpdateAccountNumber(ctx context.Context, conn *grpc.ClientConn) error {
 	accNum, seqNumb, err := QueryAccount(ctx, conn, b.encCfg, b.address.String())
 	if err != nil {
