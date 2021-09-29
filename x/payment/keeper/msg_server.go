f7ae201f4b11923654152f33e2e9c13075f660ec
evan-forbes <evan.samuel.forbes@gmail.com>
2021-09-29 18:37:04 -0500

    regenerate and adjust module boilerplate

---
+++ x/payment/keeper/msg_server.go
@@ -0,0 +1,25 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/celestiaorg/celestia-app/x/payment/types"
+)
+
+var _ types.MsgServer = msgServer{}
+
+// MsgServer is the server API for Msg service.
+type MsgServer interface {
+	// PayForMessage allows the user to post data to made be available.
+	SignedTransactionDataPayForMessage(context.Context, *types.SignedTransactionDataPayForMessage) (*types.SignedTransactionDataPayForMessageResponse, error)
+}
+
+type msgServer struct {
+	Keeper
+}
+
+// NewMsgServerImpl returns an implementation of the payment MsgServer interface
+// for the provided Keeper.
+func NewMsgServerImpl(keeper Keeper) MsgServer {
+	return &msgServer{Keeper: keeper}
+}
