f4668c078cdf763a5465a35879c13a0cfa6cdeb9
evan-forbes <evan.samuel.forbes@gmail.com>
2021-09-29 18:45:35 -0500

    linter

--- x/payment/types/codec.go
+++ x/payment/types/codec.go
@@ -24,6 +24,5 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 }
 
 var (
-	amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
