0d0b88a989b182b3876cbb501291a9b6f798f2d1
evan-forbes <evan.samuel.forbes@gmail.com>
2021-09-30 11:51:22 -0500

    delete unsused files
    Co-authored-by: Nguyen Nhu Viet <braveryandglory@gmail.com>

--- x/payment/keeper/msg_server_test.go
+++
@@ -1,6 +0,0 @@
-package keeper_test
-
-// func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-// 	k, ctx := keepertest.PaymentKeeper(t)
-// 	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
-// }
