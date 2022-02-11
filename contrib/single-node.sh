ad47ab21a4f608d9d542e74300fd64a59ee3b2c2
evan-forbes <evan.samuel.forbes@gmail.com>
2022-02-11 13:10:24 -0600

    revert changes made to script that modify the config

--- contrib/single-node.sh
+++ contrib/single-node.sh
@@ -14,11 +14,10 @@ celestia-appd gentx validator 5000000000uceles --keyring-backend="test" --chain-
 celestia-appd collect-gentxs
 
 # Set proper defaults and change ports
-# might have to use the -i flag 
-sed 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
-sed 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
-sed 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
-sed 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
+sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
+sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
+sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
+sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
 
 # Start the celestia-app
 celestia-appd start
