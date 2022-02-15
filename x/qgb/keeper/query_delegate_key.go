6cc1c608ccf3242c4077497d2bef4c476cd17a6e
sweexordious <chamirachid1@gmail.com>
2022-02-15 15:40:37 +0100

    linter fixes

--- x/qgb/keeper/query_delegate_key.go
+++ x/qgb/keeper/query_delegate_key.go
@@ -1,13 +1,9 @@
 package keeper
 
-import (
-	"context"
-	"github.com/celestiaorg/celestia-app/x/qgb/types"
-)
-
-func (k Keeper) drchestrator(
-	c context.Context,
-	req *types.QueryDelegateKeysByOrchestratorAddress) (*types.QueryDelegateKeysByOrchestratorAddressResponse, error) {
-	// TODO
-	return &types.QueryDelegateKeysByOrchestratorAddressResponse{}, nil
-}
+// Commented for linting
+//func (k Keeper) drchestrator(
+//	c context.Context,
+//	req *types.QueryDelegateKeysByOrchestratorAddress) (*types.QueryDelegateKeysByOrchestratorAddressResponse, error) {
+//	// TODO
+//	return &types.QueryDelegateKeysByOrchestratorAddressResponse{}, nil
+//}
