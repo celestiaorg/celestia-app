e1676f8a46eb600c72de8744e17d1c657373210f
sweexordious <chamirachid1@gmail.com>
2022-02-19 22:19:32 +0100

    add_qgb_orchestrator_address_logic

--- x/qgb/keeper/msg_server.go
+++ x/qgb/keeper/msg_server.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"context"
 	"github.com/celestiaorg/celestia-app/x/qgb/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 type msgServer struct {
@@ -28,6 +30,54 @@ func (k msgServer) DataCommitmentConfirm(context.Context, *types.MsgDataCommitme
 }
 
 func (k msgServer) SetOrchestratorAddress(c context.Context, msg *types.MsgSetOrchestratorAddress) (*types.MsgSetOrchestratorAddressResponse, error) {
-	// TODO
+	// ensure that this passes validation, checks the key validity
+	err := msg.ValidateBasic()
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "Key not valid")
+	}
+
+	ctx := sdk.UnwrapSDKContext(c)
+
+	// check the following, all should be validated in validate basic
+	val, e1 := sdk.ValAddressFromBech32(msg.Validator)
+	orch, e2 := sdk.AccAddressFromBech32(msg.Orchestrator)
+	addr, e3 := types.NewEthAddress(msg.EthAddress)
+	if e1 != nil || e2 != nil || e3 != nil {
+		return nil, sdkerrors.Wrap(err, "Key not valid")
+	}
+
+	// check that the validator does not have an existing key
+	_, foundExistingOrchestratorKey := k.GetOrchestratorValidator(ctx, orch)
+	_, foundExistingEthAddress := k.GetEthAddressByValidator(ctx, val)
+
+	// ensure that the validator exists
+	if foundExistingOrchestratorKey || foundExistingEthAddress {
+		return nil, sdkerrors.Wrap(types.ErrResetDelegateKeys, val.String())
+	}
+
+	// check that neither key is a duplicate
+	delegateKeys := k.GetDelegateKeys(ctx)
+	for i := range delegateKeys {
+		if delegateKeys[i].EthAddress == addr.GetAddress() {
+			return nil, sdkerrors.Wrap(err, "Duplicate Ethereum Key")
+		}
+		if delegateKeys[i].Orchestrator == orch.String() {
+			return nil, sdkerrors.Wrap(err, "Duplicate Orchestrator Key")
+		}
+	}
+
+	// set the orchestrator address
+	k.SetOrchestratorValidator(ctx, val, orch)
+	// set the ethereum address
+	k.SetEthAddressForValidator(ctx, val, *addr)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeySetOperatorAddr, orch.String()),
+		),
+	)
+
 	return &types.MsgSetOrchestratorAddressResponse{}, nil
 }
