85f44148be216817e5b840ec66f653aef6291157
CHAMI Rachid <chamirachid1@gmail.com>
2022-02-21 13:30:59 +0100

    adds remaining valset confirm handler (#219)

--- x/qgb/keeper/msg_server.go
+++ x/qgb/keeper/msg_server.go
@@ -19,7 +19,28 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 
 // ValsetConfirm handles MsgValsetConfirm
 func (k msgServer) ValsetConfirm(c context.Context, msg *types.MsgValsetConfirm) (*types.MsgValsetConfirmResponse, error) {
-	// TODO
+	ctx := sdk.UnwrapSDKContext(c)
+	// TODO check if valset exists when we add the remaining modules
+
+	orchaddr, err := sdk.AccAddressFromBech32(msg.Orchestrator)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "acc address invalid")
+	}
+
+	// persist signature
+	if k.GetValsetConfirm(ctx, msg.Nonce, orchaddr) != nil {
+		return nil, sdkerrors.Wrap(types.ErrDuplicate, "signature duplicate")
+	}
+	key := k.SetValsetConfirm(ctx, *msg)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyValsetConfirmKey, string(key)),
+		),
+	)
+
 	return &types.MsgValsetConfirmResponse{}, nil
 }
 
--- x/qgb/types/events.go
+++ x/qgb/types/events.go
@@ -1,5 +1,6 @@
 package types
 
 const (
+	AttributeKeyValsetConfirmKey = "valset_confirm_key"
 	AttributeKeySetOperatorAddr = "set_operator_address"
 )
