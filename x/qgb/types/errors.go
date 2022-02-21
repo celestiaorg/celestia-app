e20e44d0d019a4f6a395045d8801e0fa797c0d21
CHAMI Rachid <chamirachid1@gmail.com>
2022-02-21 13:30:59 +0100

    adds remaining valset confirm handler (#219)

--- x/qgb/types/errors.go
+++ x/qgb/types/errors.go
@@ -3,5 +3,7 @@ package types
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
+	ErrInvalid   = sdkerrors.Register(ModuleName, 3, "invalid")
+	ErrDuplicate = sdkerrors.Register(ModuleName, 2, "duplicate")
 	ErrResetDelegateKeys = sdkerrors.Register(ModuleName, 10, "can not set orchestrator addresses more than once")
 )
