0bb462a090ee5c790599acc25cec0d0d921efdaf
sweexordious <chamirachid1@gmail.com>
2022-02-22 12:51:40 +0100

    adds data commitment basic functions

--- x/qgb/types/datacommitmentconfirm.go
+++ x/qgb/types/datacommitmentconfirm.go
@@ -1,15 +1,44 @@
 package types
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewMsgDataCommitmentConfirm creates a new NewMsgDataCommitmentConfirm
+func NewMsgDataCommitmentConfirm(
+	commitment string,
+	signature string,
+	validatorSignature string,
+	beginBlock int64,
+	endBlock int64,
+) *MsgDataCommitmentConfirm {
+	return &MsgDataCommitmentConfirm{
+		Commitment:       commitment,
+		Signature:        signature,
+		ValidatorAddress: validatorSignature,
+		BeginBlock:       beginBlock,
+		EndBlock:         endBlock,
+	}
+}
 
 // GetSigners defines whose signature is required
 func (msg *MsgDataCommitmentConfirm) GetSigners() []sdk.AccAddress {
-	// TODO
-	return []sdk.AccAddress{}
+	acc, err := sdk.AccAddressFromBech32(msg.ValidatorAddress)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{acc}
 }
 
-// ValidateBasic
-func (msg *MsgDataCommitmentConfirm) ValidateBasic() error {
-	// TODO
+// ValidateBasic performs stateless checks
+func (msg *MsgDataCommitmentConfirm) ValidateBasic() (err error) {
+	if _, err = sdk.AccAddressFromBech32(msg.ValidatorAddress); err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.ValidatorAddress)
+	}
+	if msg.BeginBlock > msg.EndBlock {
+		return sdkerrors.Wrap(err, "begin block should be less than end block")
+	}
+	// FIXME: add `that the provided validator's bridge address is the one associated with that validator`
 	return nil
 }
