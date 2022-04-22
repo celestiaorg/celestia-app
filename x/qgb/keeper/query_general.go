de19f31a021dfbb9c409ec6129f8ae2c4f9dfefa
rachid <chamirachid1@gmail.com>
2022-04-22 15:28:59 +0200

    adds query last unbonding height

---
+++ x/qgb/keeper/query_general.go
@@ -0,0 +1,16 @@
+package keeper
+
+import (
+	"context"
+	"github.com/celestiaorg/celestia-app/x/qgb/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// LastUnbondingHeight queries the last unbonding height
+func (k Keeper) LastUnbondingHeight(
+	c context.Context,
+	req *types.QueryLastUnbondingHeightRequest) (*types.QueryLastUnbondingHeightResponse, error) {
+	return &types.QueryLastUnbondingHeightResponse{
+		Height: int64(k.GetLastUnBondingBlockHeight(sdk.UnwrapSDKContext(c))),
+	}, nil
+}
