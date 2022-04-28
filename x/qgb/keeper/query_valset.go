20ce81099db27462f51bd37d3271727780c34a73
CHAMI Rachid <chamirachid1@gmail.com>
2022-04-28 18:14:25 +0200

    adds last valset request before height query (#365)
    * adds last valset request before  height query
    
    * regenerating correct query.pb.go
    
    * adds check before querying next valset
    
    * Update x/qgb/keeper/query_valset.go
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>
    
    * adds comment
    
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- x/qgb/keeper/query_valset.go
+++ x/qgb/keeper/query_valset.go
@@ -8,6 +8,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// TODO add unit tests for all of these requests
 // LastValsetRequests queries the LastValsetRequests of the qgb module
 func (k Keeper) LastValsetRequests(
 	c context.Context,
@@ -40,4 +41,29 @@ func (k Keeper) ValsetRequestByNonce(
 	}
 
 	return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "valset request nonce not found")
+}
+
+// LastValsetBeforeHeight queries the last valset request before height
+func (k Keeper) LastValsetBeforeHeight(
+	c context.Context,
+	req *types.QueryLastValsetBeforeHeightRequest) (*types.QueryLastValsetBeforeHeightResponse, error) {
+	valReq := k.GetValsets(sdk.UnwrapSDKContext(c))
+	for _, valset := range valReq {
+		// The first check is correct because we will always have a valset at block 0.
+		// We're creating valsets:
+		//  - If we have no valset
+		//	- We're an unbonding height
+		//	- There was a significant power difference in the validator set
+		// For more information, check qgb/abci.go.EndBlocker:42
+		if !k.HasValsetRequest(sdk.UnwrapSDKContext(c), valset.Nonce+1) ||
+			(valset.Height < req.Height && k.GetValset(sdk.UnwrapSDKContext(c), valset.Nonce+1).Height >= req.Height) {
+			vs, err := types.CopyValset(valset)
+			if err != nil {
+				return nil, err
+			}
+			return &types.QueryLastValsetBeforeHeightResponse{Valset: vs}, nil
+		}
+	}
+
+	return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "last valset request before height not found")
 }
