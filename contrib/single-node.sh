956cac7846e65ea855635bafa1f7a615e41e7ca5
evan-forbes <evan.samuel.forbes@gmail.com>
2022-05-12 16:33:09 -0500

    add a command to change the mode to validator in script

--- contrib/single-node.sh
+++ contrib/single-node.sh
@@ -18,6 +18,7 @@ sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/confi
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
 sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
 sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
+sed -i 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
 
 # Start the celestia-app
 celestia-appd start
