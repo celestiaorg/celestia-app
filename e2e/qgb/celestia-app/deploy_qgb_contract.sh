dff5059c0a7307e1171604288f4d674fbc4d8f0c
rachid <chamirachid1@gmail.com>
2022-05-13 15:52:41 +0200

    fixes docker-compose commands + improves deployment script

--- e2e/qgb/celestia-app/deploy_qgb_contract.sh
+++ e2e/qgb/celestia-app/deploy_qgb_contract.sh
@@ -2,22 +2,44 @@
 
 # This script deploys the QGB contract and outputs the address in `qgb_address.txt` file
 
+# check whether to deploy a new contract or no need
+if [[ "${DEPLOY_NEW_CONTRACT}" != "true" ]]
+then
+  echo "no need to deploy a new QGB contract. exiting..."
+  exit 0
+fi
+
 # check if environment variables are set
 if [[ -z "${EVM_CHAIN_ID}" || -z "${PRIVATE_KEY}" ]] || \
    [[ -z "${TENDERMINT_RPC}" || -z "${CELESTIA_GRPC}" ]] || \
-   [[ -z "${EVM_CHAIN_ID}" || -z "${EVM_ENDPOINT}"]]
+   [[ -z "${EVM_ENDPOINT}" ]]
 then
   echo "Environment not setup correctly. Please set:"
-  echo "EVM_CHAIN_ID, PRIVATE_KEY, TENDERMINT_RPC, CELESTIA_GRPC, EVM_CHAIN_ID, EVM_ENDPOINT variables"
+  echo "EVM_CHAIN_ID, PRIVATE_KEY, TENDERMINT_RPC, CELESTIA_GRPC, EVM_ENDPOINT variables"
   exit -1
 fi
+
+# wait for the node to get up and running
+while true
+do
+  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null core0:26657/status)
+  if [[ "$status_code" -eq 200 ]] ; then
+    break
+  fi
+  echo "Waiting for node to be up..."
+  sleep 5s
+done
+
+echo "deploying QGB contract..."
 
 /bin/celestia-appd deploy \
   -x ${EVM_CHAIN_ID} \
   -d ${PRIVATE_KEY} \
-  --rpc.laddr ${TENDERMINT_RPC} \
+  -t ${TENDERMINT_RPC} \
   -c ${CELESTIA_GRPC} \
   -z ${EVM_CHAIN_ID} \
   -e ${EVM_ENDPOINT} > /opt/output
+
+echo $(cat /opt/output)
 
 cat /opt/output | tail -n 1 | cut -d\  -f 4 > /opt/qgb_address.txt
--- e2e/qgb/celestia-app/start_relayer.sh
+++ e2e/qgb/celestia-app/start_relayer.sh
@@ -6,11 +6,28 @@
 # check if environment variables are set
 if [[ -z "${EVM_CHAIN_ID}" || -z "${PRIVATE_KEY}" ]] || \
    [[ -z "${TENDERMINT_RPC}" || -z "${CELESTIA_GRPC}" ]] || \
-   [[ -z "${EVM_CHAIN_ID}" || -z "${EVM_ENDPOINT}"]]
+   [[ -z "${EVM_ENDPOINT}" ]]
 then
   echo "Environment not setup correctly. Please set:"
-  echo "EVM_CHAIN_ID, PRIVATE_KEY, TENDERMINT_RPC, CELESTIA_GRPC, EVM_CHAIN_ID, EVM_ENDPOINT variables"
+  echo "EVM_CHAIN_ID, PRIVATE_KEY, TENDERMINT_RPC, CELESTIA_GRPC, EVM_ENDPOINT variables"
   exit -1
+fi
+
+# wait for the node to get up and running
+while true
+do
+  status_code=$(curl --write-out '%{http_code}' --silent --output /dev/null core0:26657/status)
+  if [[ "$status_code" -eq 200 ]] ; then
+    break
+  fi
+  echo "Waiting for node to be up..."
+  sleep 5s
+done
+
+# check whether to deploy a new contract or use an existing one
+if [[ "${DEPLOY_NEW_CONTRACT}" == "true" ]]
+then
+  /bin/bash /opt/deploy_qgb_contract.sh
 fi
 
 # get the address from the `qgb_address.txt` file
@@ -19,10 +36,9 @@ fi
 QGB_CONTRACT=$(cat /opt/qgb_address.txt)
 
 /bin/celestia-appd relayer \
-      -x ${EVM_CHAIN_ID} \
-      -d ${PRIVATE_KEY} \
-      --rpc.laddr ${TENDERMINT_RPC} \
-      -c ${CELESTIA_GRPC} \
-      -z ${EVM_CHAIN_ID} \
-      -e ${EVM_ENDPOINT} \
-      -a ${QGB_CONTRACT}
+  -d ${PRIVATE_KEY} \
+  -t ${TENDERMINT_RPC} \
+  -c ${CELESTIA_GRPC} \
+  -z ${EVM_CHAIN_ID} \
+  -e ${EVM_ENDPOINT} \
+  -a ${QGB_CONTRACT}
