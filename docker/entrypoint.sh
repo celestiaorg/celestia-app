0ad24cd13a8670600f6d966f1569cbaf912357b3
rachid <chamirachid1@gmail.com>
2022-05-17 20:38:05 +0200

    adds curly braces to environment variables in entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,10 @@
 # This script creates the necessary files before starting Celestia-appd
 
 # only create the priv_validator_state.json if it doesn't exist and the command is start
-if [[ $3 == "start" && ! -f $CELESTIA_HOME/data/priv_validator_state.json ]]
+if [[ $3 == "start" && ! -f ${CELESTIA_HOME}/data/priv_validator_state.json ]]
 then
-    mkdir $CELESTIA_HOME/data # it is alright if it fails, the script will continue executing
-    cat <<EOF > $CELESTIA_HOME/data/priv_validator_state.json
+    mkdir ${CELESTIA_HOME}/data # it is alright if it fails, the script will continue executing
+    cat <<EOF > ${CELESTIA_HOME}/data/priv_validator_state.json
 {
   "height": "0",
   "round": 0,
