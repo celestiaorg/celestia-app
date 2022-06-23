20dcea42ab97585425708ddee8bbf1eebac1d5c5
CHAMI Rachid <chamirachid1@gmail.com>
2022-06-24 00:41:50 +0100

    Typos x/qgb/orchestrator/errors.go
    Co-authored-by: Evan Forbes <42654277+evan-forbes@users.noreply.github.com>

--- x/qgb/orchestrator/errors.go
+++ x/qgb/orchestrator/errors.go
@@ -5,8 +5,8 @@ import "errors"
 var (
 	ErrValsetNotFound                  = errors.New("valset not found")
 	ErrDataCommitmentNotFound          = errors.New("data commitment not found")
-	ErrUnmarshallValset                = errors.New("coudln't unmarsall valset")
-	ErrUnmarshallDataCommitment        = errors.New("coudln't unmarsall data commitment")
+	ErrUnmarshallValset                = errors.New("couldn't unmarsall valset")
+	ErrUnmarshallDataCommitment        = errors.New("couldn't unmarsall data commitment")
 	ErrConfirmSignatureNotFound        = errors.New("confirm signature not found")
 	ErrNotEnoughValsetConfirms         = errors.New("couldn't find enough valset confirms")
 	ErrNotEnoughDataCommitmentConfirms = errors.New("couldn't find enough data commitment confirms")
