a00b0d3d66533ccf0f19b18ae9f8362b0260830e
CHAMI Rachid <chamirachid1@gmail.com>
2022-07-01 14:38:23 +0100

    Update e2e/qgb/orchestrator_test.go

--- e2e/qgb/orchestrator_test.go
+++ e2e/qgb/orchestrator_test.go
@@ -209,7 +209,7 @@ func TestOrchestratorWithMultipleValidators(t *testing.T) {
 		0,
 		CORE1ACCOUNTADDRESS,
 	)
-	// assert the confirm exist
+	// check the confirm exist
 	require.NoError(t, err)
 	require.NotNil(t, core1DataCommitmentConfirm)
 	// assert that it carries the right eth address
