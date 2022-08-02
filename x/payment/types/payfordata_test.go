38aaafc40b9490562b1560b0d073e6d90f700e88
evan-forbes <evan.samuel.forbes@gmail.com>
2022-08-02 10:32:01 -0500

    fix: compile error

--- x/payment/types/payfordata_test.go
+++ x/payment/types/payfordata_test.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"testing"
 
-	sdkerrors "github.com/cosmos/cosmos-sdk/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/pkg/consts"
