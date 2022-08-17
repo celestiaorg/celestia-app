0ee5a45a0173edfc1489b1bb68a0d37d70438dd1
CHAMI Rachid <chamirachid1@gmail.com>
2022-08-17 13:00:40 +0200

    QGB Use Ecrecover to validate Ethereum signatures (#605)
    * updates ethereum signer implementation
    
    * update test for correct signature
    
    * go.sum
    
    * Update x/qgb/types/ethereum_signer.go
    
    * Update x/qgb/types/ethereum_signer.go

--- x/qgb/types/ethereum_signer.go
+++ x/qgb/types/ethereum_signer.go
@@ -3,9 +3,8 @@ package types
 import (
 	"crypto/ecdsa"
 
-	"github.com/ethereum/go-ethereum/common"
-
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
@@ -18,7 +17,7 @@ func NewEthereumSignature(hash []byte, privateKey *ecdsa.PrivateKey) ([]byte, er
 	if privateKey == nil {
 		return nil, sdkerrors.Wrap(ErrEmpty, "private key")
 	}
-	protectedHash := crypto.Keccak256Hash(append([]uint8(signaturePrefix), hash...))
+	protectedHash := crypto.Keccak256Hash([]uint8(signaturePrefix), hash)
 	return crypto.Sign(protectedHash.Bytes(), privateKey)
 }
 
@@ -26,32 +25,16 @@ func EthAddressFromSignature(hash []byte, signature []byte) (common.Address, err
 	if len(signature) < 65 {
 		return common.Address{}, sdkerrors.Wrap(ErrInvalid, "signature too short")
 	}
-	// To verify signature
-	// - use crypto.SigToPub to get the public key
-	// - use crypto.PubkeyToAddress to get the address
-	// - compare this to the address given.
-
-	// for backwards compatibility reasons  the V value of an Ethereum sig is presented
-	// as 27 or 28, internally though it should be a 0-3 value due to changed formats.
-	// It seems that go-ethereum expects this to be done before sigs actually reach it's
-	// internal validation functions. In order to comply with this requirement we check
-	// the sig an dif it's in standard format we correct it. If it's in go-ethereum's expected
-	// format already we make no changes.
-	//
-	// We could attempt to break or otherwise exit early on obviously invalid values for this
-	// byte, but that's a task best left to go-ethereum
-	if signature[64] == 27 || signature[64] == 28 {
-		signature[64] -= 27
-	}
-
-	protectedHash := crypto.Keccak256Hash(append([]uint8(signaturePrefix), hash...))
-
-	pubkey, err := crypto.SigToPub(protectedHash.Bytes(), signature)
+	protectedHash := crypto.Keccak256Hash([]uint8(signaturePrefix), hash)
+	sigPublicKey, err := crypto.Ecrecover(protectedHash.Bytes(), signature)
 	if err != nil {
-		return common.Address{}, sdkerrors.Wrap(err, "signature to public key")
+		return common.Address{}, sdkerrors.Wrap(err, "ec recover failed")
 	}
-
-	addr := crypto.PubkeyToAddress(*pubkey)
+	pubKey, err := crypto.UnmarshalPubkey(sigPublicKey)
+	if err != nil {
+		return common.Address{}, sdkerrors.Wrap(err, "unmarshalling signature public key failed")
+	}
+	addr := crypto.PubkeyToAddress(*pubKey)
 	return addr, nil
 }
 
