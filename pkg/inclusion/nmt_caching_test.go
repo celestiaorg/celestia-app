9d2fc604c0ad50837d0f24ee7c165d0454feebd8
Evan Forbes <42654277+evan-forbes@users.noreply.github.com>
2022-09-02 13:29:58 -0500

    feat: Cache and traverse nmt sub tree roots (#549)
    * initial sub tree root traversal code
    
    * use nmt wrapper when generating commitments
    
    * typo
    
    * fix doc typos
    
    * remove unused testutil code
    
    * update hardcoded test
    
    * fix docs left <-> right
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * fix docs left <-> right
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * fix typo
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * fix typo
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * chore: move power of two code to an exported util package
    
    * add subtree root code
    
    * Revert "chore: move power of two code to an exported util package"
    
    This reverts commit b335f2e20118dd5d9cf7e4d193097a40bc887574.
    
    * add docs
    
    * move path code to a different PR
    
    * use proper name for nmt node visitor in docs
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * consistent naming
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * consistent name
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * use normal error message formatting and not consts
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    
    * fix comment
    
    * remove comment
    
    * PR feedback
    
    Co-authored-by: CHAMI Rachid <chamirachid1@gmail.com>
    
    Co-authored-by: Rootul Patel <rootulp@gmail.com>
    Co-authored-by: CHAMI Rachid <chamirachid1@gmail.com>

---
+++ pkg/inclusion/nmt_caching_test.go
@@ -0,0 +1,184 @@
+package inclusion
+
+import (
+	"testing"
+
+	"github.com/celestiaorg/celestia-app/testutil/coretestutil"
+	"github.com/celestiaorg/nmt"
+	"github.com/celestiaorg/rsmt2d"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/pkg/consts"
+	"github.com/tendermint/tendermint/pkg/da"
+	"github.com/tendermint/tendermint/pkg/wrapper"
+)
+
+func TestWalkCachedSubTreeRoot(t *testing.T) {
+	// create the first main tree
+	strc := newSubTreeRootCacher()
+	oss := uint64(8)
+	tr := wrapper.NewErasuredNamespacedMerkleTree(oss, nmt.NodeVisitor(strc.Visit))
+	d := []byte{0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8}
+	for i := 0; i < 8; i++ {
+		tr.Push(d, rsmt2d.SquareIndex{
+			Axis: uint(rsmt2d.Row),
+			Cell: uint(i),
+		})
+	}
+	highestRoot := tr.Root()
+
+	// create a short sub tree
+	shortSubTree := wrapper.NewErasuredNamespacedMerkleTree(oss)
+	for i := 0; i < 2; i++ {
+		shortSubTree.Push(d, rsmt2d.SquareIndex{
+			Axis: uint(rsmt2d.Row),
+			Cell: uint(i),
+		})
+	}
+	shortSTR := shortSubTree.Root()
+
+	// create a tall sub tree root
+	tallSubTree := wrapper.NewErasuredNamespacedMerkleTree(oss)
+	for i := 0; i < 4; i++ {
+		tallSubTree.Push(d, rsmt2d.SquareIndex{
+			Axis: uint(rsmt2d.Row),
+			Cell: uint(i),
+		})
+	}
+	tallSTR := tallSubTree.Root()
+
+	type test struct {
+		name          string
+		path          []WalkInstruction
+		subTreeRoot   []byte
+		expectedError string
+	}
+
+	tests := []test{
+		{
+			"left most short sub tree",
+			[]WalkInstruction{WalkLeft, WalkLeft},
+			shortSTR,
+			"",
+		},
+		{
+			"left middle short sub tree",
+			[]WalkInstruction{WalkLeft, WalkRight},
+			shortSTR,
+			"",
+		},
+		{
+			"right middle short sub tree",
+			[]WalkInstruction{WalkRight, WalkLeft},
+			shortSTR,
+			"",
+		},
+		{
+			"right most short sub tree",
+			[]WalkInstruction{WalkRight, WalkRight},
+			shortSTR,
+			"",
+		},
+		{
+			"left most tall sub tree",
+			[]WalkInstruction{WalkLeft},
+			tallSTR,
+			"",
+		},
+		{
+			"right most tall sub tree",
+			[]WalkInstruction{WalkRight},
+			tallSTR,
+			"",
+		},
+		{
+			"right most tall sub tree",
+			[]WalkInstruction{WalkRight, WalkRight, WalkRight, WalkRight},
+			tallSTR,
+			"did not find sub tree root",
+		},
+	}
+
+	for _, tt := range tests {
+		foundSubRoot, err := strc.walk(highestRoot, tt.path)
+		if tt.expectedError != "" {
+			require.Error(t, err, tt.name)
+			assert.Contains(t, err.Error(), tt.expectedError, tt.name)
+			continue
+		}
+
+		require.NoError(t, err)
+		require.Equal(t, tt.subTreeRoot, foundSubRoot, tt.name)
+	}
+}
+
+func TestEDSSubRootCacher(t *testing.T) {
+	oss := uint64(8)
+	d := coretestutil.GenerateRandNamespacedRawData(uint32(oss*oss), consts.NamespaceSize, consts.ShareSize-consts.NamespaceSize)
+	stc := NewCachedSubtreeCacher(oss)
+
+	eds, err := rsmt2d.ComputeExtendedDataSquare(d, consts.DefaultCodec(), stc.Constructor)
+	require.NoError(t, err)
+
+	dah := da.NewDataAvailabilityHeader(eds)
+
+	for i := range dah.RowsRoots[:oss] {
+		expectedSubTreeRoots := calculateSubTreeRoots(eds.Row(uint(i))[:oss], 2)
+		require.NotNil(t, expectedSubTreeRoots)
+		// note: the depth is one greater than expected because we're dividing
+		// the row in half when we calculate the expected roots.
+		result, err := stc.GetSubTreeRoot(dah, i, []WalkInstruction{false, false, false})
+		require.NoError(t, err)
+		assert.Equal(t, expectedSubTreeRoots[0], result)
+	}
+}
+
+// calculateSubTreeRoots generates the subtree roots for a given row. If the
+// selected depth is too deep for the tree, nil is returned. It relies on
+// passing a row whose length is a power of 2 and assumes that the row is
+// **NOT** extended since calculating subtree root for erasure data using the
+// nmt wrapper makes this difficult.
+func calculateSubTreeRoots(row [][]byte, depth int) [][]byte {
+	subLeafRange := len(row)
+	for i := 0; i < depth; i++ {
+		subLeafRange = subLeafRange / 2
+	}
+
+	if subLeafRange == 0 || subLeafRange%2 != 0 {
+		return nil
+	}
+
+	count := len(row) / subLeafRange
+	subTreeRoots := make([][]byte, count)
+	chunks := chunkSlice(row, subLeafRange)
+	for i, rowChunk := range chunks {
+		tr := wrapper.NewErasuredNamespacedMerkleTree(uint64(len(row)))
+		for j, r := range rowChunk {
+			c := (i * subLeafRange) + j
+			tr.Push(r, rsmt2d.SquareIndex{
+				Axis: uint(rsmt2d.Row),
+				Cell: uint(c),
+			})
+		}
+		subTreeRoots[i] = tr.Root()
+	}
+
+	return subTreeRoots
+}
+
+func chunkSlice(slice [][]byte, chunkSize int) [][][]byte {
+	var chunks [][][]byte
+	for i := 0; i < len(slice); i += chunkSize {
+		end := i + chunkSize
+
+		// necessary check to avoid slicing beyond
+		// slice capacity
+		if end > len(slice) {
+			end = len(slice)
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
+}
