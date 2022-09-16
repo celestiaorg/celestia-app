175469ee0897e02525815da9f69127d10449643f
Rootul Patel <rootulp@gmail.com>
2022-09-15 21:16:57 -0400

    extract separate PR for specifying square size in shares_test.go

--- pkg/shares/shares_test.go
+++ pkg/shares/shares_test.go
@@ -275,12 +275,12 @@ func TestFuzz_Merge(t *testing.T) {
 }
 
 // generateRandomBlockData returns randomly generated block data for testing purposes
-func generateRandomBlockData(txCount, evdCount, msgCount, maxSize int) (data coretypes.Data) {
-	data.Txs = generateRandomlySizedCompactShares(txCount, maxSize)
-	data.Evidence = generateIdenticalEvidence(evdCount)
-	data.Messages = generateRandomlySizedMessages(msgCount, maxSize)
-	data.OriginalSquareSize = 16
-	return data
+func generateRandomBlockData(txCount, evdCount, msgCount, maxSize int) coretypes.Data {
+	var out coretypes.Data
+	out.Txs = generateRandomlySizedCompactShares(txCount, maxSize)
+	out.Evidence = generateIdenticalEvidence(evdCount)
+	out.Messages = generateRandomlySizedMessages(msgCount, maxSize)
+	return out
 }
 
 func generateRandomlySizedCompactShares(count, max int) coretypes.Txs {
