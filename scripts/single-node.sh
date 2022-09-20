0d0256cdf5a81073aa9a7f0f6ee7ba063cdce36d
rachid <chamirachid1@gmail.com>
2022-09-20 13:45:41 +0200

    fix: add Ethereum private key for the address defined in the single-node.sh script

--- scripts/single-node.sh
+++ scripts/single-node.sh
@@ -14,6 +14,7 @@ celestia-appd gentx validator 5000000000utia \
   --keyring-backend="test" \
   --chain-id $CHAINID \
   --orchestrator-address $(celestia-appd keys show validator -a --keyring-backend="test") \
+  #da6ed55cb2894ac2c9c10209c09de8e8b9d109b910338d5bf3d747a7e1fc9eb9
   --ethereum-address 0x966e6f22781EF6a6A82BBB4DB3df8E225DfD9488
 
 celestia-appd collect-gentxs
