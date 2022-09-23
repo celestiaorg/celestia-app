88ca98878b1b8352168b1c5a0303102698a36559
Rootul P <rootulp@gmail.com>
2022-09-23 12:30:37 -0600

    fix: compact share `Count` (#778)

---
+++ pkg/shares/split_compact_shares_test.go
@@ -0,0 +1,33 @@
+package shares
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/celestiaorg/celestia-app/pkg/appconsts"
+	coretypes "github.com/tendermint/tendermint/types"
+)
+
+func TestCount(t *testing.T) {
+	type testCase struct {
+		transactions   []coretypes.Tx
+		wantShareCount int
+	}
+	testCases := []testCase{
+		{transactions: []coretypes.Tx{}, wantShareCount: 0},
+		{transactions: []coretypes.Tx{[]byte{0}}, wantShareCount: 1},
+		{transactions: []coretypes.Tx{bytes.Repeat([]byte{0}, 100)}, wantShareCount: 1},
+		{transactions: []coretypes.Tx{bytes.Repeat([]byte{0}, appconsts.CompactShareContentSize+1)}, wantShareCount: 2},
+		{transactions: []coretypes.Tx{bytes.Repeat([]byte{0}, appconsts.CompactShareContentSize*2+1)}, wantShareCount: 3},
+	}
+	for _, tc := range testCases {
+		css := NewCompactShareSplitter(appconsts.TxNamespaceID, appconsts.ShareVersion)
+		for _, transaction := range tc.transactions {
+			css.WriteTx(transaction)
+		}
+		got := css.Count()
+		if got != tc.wantShareCount {
+			t.Errorf("count got %d want %d", got, tc.wantShareCount)
+		}
+	}
+}
