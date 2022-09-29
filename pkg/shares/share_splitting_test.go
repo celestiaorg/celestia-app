c650400eb1999d1a836f1fa6b9f3e36e05133cd3
Rootul Patel <rootulp@gmail.com>
2022-09-29 11:41:42 -0400

    test: transaction splitting (#813)
    Define unit tests for transaction splitting. These should be helpful
    when we tackle https://github.com/celestiaorg/celestia-app/issues/802

--- pkg/shares/share_splitting_test.go
+++ pkg/shares/share_splitting_test.go
@@ -26,12 +26,20 @@ func TestSplitTxs(t *testing.T) {
 			want: [][]uint8{
 				append([]uint8{
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+<<<<<<< HEAD
 					0x1,                // info byte
 					0x2, 0x0, 0x0, 0x0, // 1 byte (unit)  + 1 byte (unit length) = 2 bytes message length
 					0x0, // BUG: reserved byte should be non-zero
 					0x1, // unit length of first transaction
 					0xa, // data of first transaction
 				}, bytes.Repeat([]byte{0}, 240)...), // padding
+=======
+					0x1, // info byte
+					0x0, // reserved byte
+					0x1, // unit length of first transaction
+					0xa, // data of first transaction
+				}, bytes.Repeat([]byte{0}, 244)...), // padding
+>>>>>>> 5dc1fb8 (test: transaction splitting (#813))
 			},
 		},
 		{
@@ -40,18 +48,28 @@ func TestSplitTxs(t *testing.T) {
 			want: [][]uint8{
 				append([]uint8{
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+<<<<<<< HEAD
 					0x1,                // info byte
 					0x4, 0x0, 0x0, 0x0, // 2 bytes (first transaction) + 2 bytes (second transaction) = 4 bytes message length
 					0x0, // BUG: reserved byte should be non-zero
+=======
+					0x1, // info byte
+					0x0, // reserved byte
+>>>>>>> 5dc1fb8 (test: transaction splitting (#813))
 					0x1, // unit length of first transaction
 					0xa, // data of first transaction
 					0x1, // unit length of second transaction
 					0xb, // data of second transaction
+<<<<<<< HEAD
 				}, bytes.Repeat([]byte{0}, 238)...), // padding
+=======
+				}, bytes.Repeat([]byte{0}, 242)...), // padding
+>>>>>>> 5dc1fb8 (test: transaction splitting (#813))
 			},
 		},
 		{
 			name: "one large tx that spans two shares",
+<<<<<<< HEAD
 			txs:  coretypes.Txs{bytes.Repeat([]byte{0xC}, 241)},
 			want: [][]uint8{
 				append([]uint8{
@@ -61,6 +79,16 @@ func TestSplitTxs(t *testing.T) {
 					0x0,    // BUG: reserved byte should be non-zero
 					241, 1, // unit length of first transaction is 241
 				}, bytes.Repeat([]byte{0xc}, 240)...), // data of first transaction
+=======
+			txs:  coretypes.Txs{bytes.Repeat([]byte{0xC}, 245)},
+			want: [][]uint8{
+				append([]uint8{
+					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+					0x1,    // info byte
+					0x0,    // BUG reserved byte should be non-zero see https://github.com/celestiaorg/celestia-app/issues/802
+					245, 1, // unit length of first transaction is 245
+				}, bytes.Repeat([]byte{0xc}, 244)...), // data of first transaction
+>>>>>>> 5dc1fb8 (test: transaction splitting (#813))
 				append([]uint8{
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x0, // info byte
@@ -71,6 +99,7 @@ func TestSplitTxs(t *testing.T) {
 		},
 		{
 			name: "one small tx then one large tx that spans two shares",
+<<<<<<< HEAD
 			txs:  coretypes.Txs{coretypes.Tx{0xd}, bytes.Repeat([]byte{0xe}, 241)},
 			want: [][]uint8{
 				append([]uint8{
@@ -88,6 +117,24 @@ func TestSplitTxs(t *testing.T) {
 					0x0,           // reserved byte
 					0xe, 0xe, 0xe, // continuation data of second transaction
 				}, bytes.Repeat([]byte{0x0}, 243)...), // padding
+=======
+			txs:  coretypes.Txs{coretypes.Tx{0xd}, bytes.Repeat([]byte{0xe}, 243)},
+			want: [][]uint8{
+				append([]uint8{
+					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+					0x1,    // info byte
+					0x0,    // BUG reserved byte should be non-zero see https://github.com/celestiaorg/celestia-app/issues/802
+					1,      // unit length of first transaction
+					0xd,    // data of first transaction
+					243, 1, // unit length of second transaction is 243
+				}, bytes.Repeat([]byte{0xe}, 242)...), // data of first transaction
+				append([]uint8{
+					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+					0x0, // info byte
+					0x0, // reserved byte
+					0xe, // continuation data of second transaction
+				}, bytes.Repeat([]byte{0x0}, 245)...), // padding
+>>>>>>> 5dc1fb8 (test: transaction splitting (#813))
 			},
 		},
 	}
