f2f79d3165690f193377e6e958a8d7107acc2c4b
Rootul Patel <rootulp@gmail.com>
2022-09-29 15:56:22 -0400

    fix: reserved byte for existing test cases
    test: add test case for reserved byte that is not 14

--- pkg/shares/share_splitting_test.go
+++ pkg/shares/share_splitting_test.go
@@ -28,7 +28,7 @@ func TestSplitTxs(t *testing.T) {
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x1,                // info byte
 					0x2, 0x0, 0x0, 0x0, // 1 byte (unit)  + 1 byte (unit length) = 2 bytes message length
-					0x0, // BUG: reserved byte should be non-zero
+					14,  // reserved byte
 					0x1, // unit length of first transaction
 					0xa, // data of first transaction
 				}, bytes.Repeat([]byte{0}, 240)...), // padding
@@ -42,7 +42,7 @@ func TestSplitTxs(t *testing.T) {
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x1,                // info byte
 					0x4, 0x0, 0x0, 0x0, // 2 bytes (first transaction) + 2 bytes (second transaction) = 4 bytes message length
-					0x0, // BUG: reserved byte should be non-zero
+					14,  // reserved byte
 					0x1, // unit length of first transaction
 					0xa, // data of first transaction
 					0x1, // unit length of second transaction
@@ -58,13 +58,13 @@ func TestSplitTxs(t *testing.T) {
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x1,          // info byte
 					243, 1, 0, 0, // 241 (unit) + 2 (unit length) = 243 message length
-					0x0,    // BUG: reserved byte should be non-zero
+					14,     // reserved byte
 					241, 1, // unit length of first transaction is 241
 				}, bytes.Repeat([]byte{0xc}, 240)...), // data of first transaction
 				append([]uint8{
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x0, // info byte
-					0xb, // BUG: reserved byte should be zero
+					0x0, // reserved byte
 					0xc, // continuation data of first transaction
 				}, bytes.Repeat([]byte{0x0}, 245)...), // padding
 			},
@@ -77,7 +77,7 @@ func TestSplitTxs(t *testing.T) {
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x1,          // info byte
 					245, 1, 0, 0, // 2 bytes (first transaction) + 243 bytes (second transaction) = 245 bytes message length
-					0x0,    // BUG: reserved byte should be non-zero
+					14,     // reserved byte
 					1,      // unit length of first transaction
 					0xd,    // data of first transaction
 					241, 1, // unit length of second transaction is 241
@@ -85,8 +85,29 @@ func TestSplitTxs(t *testing.T) {
 				append([]uint8{
 					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
 					0x0,           // info byte
-					0xd,           // BUG: reserved byte should be zero
+					0x0,           // reserved byte
 					0xe, 0xe, 0xe, // continuation data of second transaction
+				}, bytes.Repeat([]byte{0x0}, 243)...), // padding
+			},
+		},
+		{
+			name: "one large tx that spans two shares then one small tx",
+			txs:  coretypes.Txs{bytes.Repeat([]byte{0xe}, 241), coretypes.Tx{0xd}},
+			want: [][]uint8{
+				append([]uint8{
+					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+					0x1,          // info byte
+					245, 1, 0, 0, // 243 bytes (first transaction) + 2 bytes (second transaction) = 245 bytes message length
+					14,     // reserved byte
+					241, 1, // unit length of first transaction is 241
+				}, bytes.Repeat([]byte{0xe}, 240)...), // data of first transaction
+				append([]uint8{
+					0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1, // namespace id
+					0x0, // info byte
+					11,  // reserved byte
+					0xe, // continuation data of first transaction
+					1,   // unit length of second transaction
+					0xd, // data of second transaction
 				}, bytes.Repeat([]byte{0x0}, 243)...), // padding
 			},
 		},
@@ -95,8 +116,8 @@ func TestSplitTxs(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := SplitTxs(tt.txs)
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("SplitTxs(%#v) got %#v, want %#v", tt.txs, got, tt.want)
+				t.Errorf("SplitTxs()\n got %#v\n want %#v", got, tt.want)
 			}
 		})
 	}
-}
+} //
