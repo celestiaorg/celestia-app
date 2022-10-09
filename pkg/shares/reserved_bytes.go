8a6a66bb81294774b915b9763983e75a6559f76c
Rootul Patel <rootulp@gmail.com>
2022-10-09 16:26:26 -0500

    chore: remove named return values

--- pkg/shares/reserved_bytes.go
+++ pkg/shares/reserved_bytes.go
@@ -12,23 +12,23 @@ import (
 // appconsts.CompactShareReservedBytes that contains a varint of the byteIndex
 // of the first unit that starts in a compact share. If no unit starts in the
 // compact share, ReservedBytes is [0, 0].
-func NewReservedBytes(byteIndex uint64) (reservedBytes []byte, err error) {
+func NewReservedBytes(byteIndex uint64) ([]byte, error) {
 	if byteIndex >= appconsts.ShareSize {
 		return []byte{}, fmt.Errorf("byte index %d must be less than share size %d", byteIndex, appconsts.ShareSize)
 	}
-	reservedBytes = make([]byte, appconsts.CompactShareReservedBytes)
+	reservedBytes := make([]byte, appconsts.CompactShareReservedBytes)
 	binary.PutUvarint(reservedBytes, byteIndex)
 	return reservedBytes, nil
 }
 
 // ParseReservedBytes parses a byte slice of length
 // appconsts.CompactShareReservedBytes into a byteIndex.
-func ParseReservedBytes(reservedBytes []byte) (byteIndex uint64, err error) {
+func ParseReservedBytes(reservedBytes []byte) (uint64, error) {
 	if len(reservedBytes) != appconsts.CompactShareReservedBytes {
 		return 0, fmt.Errorf("reserved bytes must be of length %d", appconsts.CompactShareReservedBytes)
 	}
 	reader := bytes.NewReader(reservedBytes)
-	byteIndex, err = binary.ReadUvarint(reader)
+	byteIndex, err := binary.ReadUvarint(reader)
 	if err != nil {
 		return 0, err
 	}
