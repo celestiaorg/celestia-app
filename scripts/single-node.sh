11ab2a6b964495685438d0c7d60661ed37266fab
Rootul Patel <rootulp@gmail.com>
2022-10-20 14:42:31 -0400

    fix: add zero length extension to sed commands
    To resolve errors of the form `sed: -I or -i may not be used with stdin`
    on MacOS. Inspired by this StackOverflow answer:
    https://stackoverflow.com/questions/21242932/sed-i-may-not-be-used-with-stdin-on-mac-os-x/42667816#42667816

--- scripts/single-node.sh
+++ scripts/single-node.sh
@@ -19,11 +19,11 @@ celestia-appd gentx validator 5000000000ucls \
 celestia-appd collect-gentxs
 
 # Set proper defaults and change ports
-sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
-sed -i 's/timeout_commit = "25s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
-sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
-sed -i 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
+sed -i '' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.celestia-app/config/config.toml
+sed -i '' 's/timeout_commit = "25s"/timeout_commit = "1s"/g' ~/.celestia-app/config/config.toml
+sed -i '' 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.celestia-app/config/config.toml
+sed -i '' 's/index_all_keys = false/index_all_keys = true/g' ~/.celestia-app/config/config.toml
+sed -i '' 's/mode = "full"/mode = "validator"/g' ~/.celestia-app/config/config.toml
 
 # Start the celestia-app
 celestia-appd start
