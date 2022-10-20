500a6f5d9753df2200756887fdd6a7a7c05ef502
Rahul Ghangas <rahulghangas0@gmail.com>
2022-10-21 05:03:33 +1100

    chore: define genesis state params

--- x/payment/genesis.go
+++ x/payment/genesis.go
@@ -9,11 +9,13 @@ import (
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	k.SetParams(ctx, genState.Params)
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
+	genesis.Params = k.GetParams(ctx)
 
 	return genesis
 }
