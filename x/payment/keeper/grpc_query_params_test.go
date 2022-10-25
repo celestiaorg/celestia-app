7e670ab439c73a3aeb1093e896c8f17feeac9002
Rahul Ghangas <rahulghangas0@gmail.com>
2022-10-25 11:03:28 +0530

    test: params query test

---
+++ x/payment/keeper/grpc_query_params_test.go
@@ -0,0 +1,21 @@
+package keeper_test
+
+import (
+	"testing"
+
+	testkeeper "github.com/celestiaorg/celestia-app/testutil/keeper"
+	"github.com/celestiaorg/celestia-app/x/payment/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParamsQuery(t *testing.T) {
+	keeper, ctx := testkeeper.PaymentKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	params := types.DefaultParams()
+	keeper.SetParams(ctx, params)
+
+	response, err := keeper.Params(wctx, &types.QueryParamsRequest{})
+	require.NoError(t, err)
+	require.Equal(t, &types.QueryParamsResponse{Params: params}, response)
+}
