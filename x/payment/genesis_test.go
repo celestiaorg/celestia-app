b769d07708e77fa40d58ae92b0518abee826d361
Rahul Ghangas <rahulghangas0@gmail.com>
2022-11-03 13:55:48 +0530

    test: removing fill assertions and unsafe code

--- x/payment/genesis_test.go
+++ x/payment/genesis_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	keepertest "github.com/celestiaorg/celestia-app/testutil/keeper"
-	"github.com/celestiaorg/celestia-app/testutil/nullify"
 	"github.com/celestiaorg/celestia-app/x/payment"
 	"github.com/celestiaorg/celestia-app/x/payment/types"
 	"github.com/stretchr/testify/require"
@@ -19,7 +18,4 @@ func TestGenesis(t *testing.T) {
 	payment.InitGenesis(ctx, *k, genesisState)
 	got := payment.ExportGenesis(ctx, *k)
 	require.NotNil(t, got)
-
-	nullify.Fill(&genesisState)
-	nullify.Fill(got)
 }
