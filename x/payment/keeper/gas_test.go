2e906c10b485bd31eee7069758702d0336d15027
Rootul P <rootulp@gmail.com>
2022-11-07 17:20:40 -0700

    feat: charge gas per message shares (#946)
    Closes https://github.com/celestiaorg/celestia-app/issues/431
    
    Co-authored-by: CHAMI Rachid <chamirachid1@gmail.com>

--- x/payment/keeper/gas_test.go
+++ x/payment/keeper/gas_test.go
@@ -11,17 +11,38 @@ import (
 )
 
 func TestPayForDataGas(t *testing.T) {
-	app := simapp.Setup(t, false)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-	k := Keeper{}
-
-	messageSize := uint64(100)
-
-	msg := types.MsgPayForData{
-		MessageSize: messageSize,
+	type testCase struct {
+		name            string
+		msg             types.MsgPayForData
+		wantGasConsumed uint64
 	}
 
-	_, err := k.PayForData(sdk.WrapSDKContext(ctx), &msg)
-	require.NoError(t, err)
-	require.Equal(t, messageSize, ctx.GasMeter().GasConsumed())
+	testCases := []testCase{
+		{
+			name:            "1 byte message", // occupies 1 share
+			msg:             types.MsgPayForData{MessageSize: 1},
+			wantGasConsumed: uint64(4096), // 1 share * 512 bytes per share * 8 gas per byte = 4096 gas
+		},
+		{
+			name:            "100 byte message", // occupies 1 share
+			msg:             types.MsgPayForData{MessageSize: 100},
+			wantGasConsumed: uint64(4096),
+		},
+		{
+			name:            "1024 byte message", // occupies 3 shares because share prefix (e.g. namespace, info byte)
+			msg:             types.MsgPayForData{MessageSize: 1024},
+			wantGasConsumed: uint64(12288), // 3 shares * 512 bytes per share * 8 gas per byte = 12288 gas
+		},
+	}
+
+	app := simapp.Setup(t, false)
+	for _, tc := range testCases {
+		ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+		k := Keeper{}
+		_, err := k.PayForData(sdk.WrapSDKContext(ctx), &tc.msg)
+		require.NoError(t, err)
+		if tc.wantGasConsumed != ctx.GasMeter().GasConsumed() {
+			t.Errorf("Gas consumed by %s: %d, want: %d", tc.name, ctx.GasMeter().GasConsumed(), tc.wantGasConsumed)
+		}
+	}
 }
