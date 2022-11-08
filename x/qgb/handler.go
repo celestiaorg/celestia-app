e708d28c2ca6cca795c6e26562c03905570d6a8b
rachid <chamirachid1@gmail.com>
2022-11-08 11:25:58 +0100

    feat: remove unnecessary QGB files

--- x/qgb/handler.go
+++ x/qgb/handler.go
@@ -12,17 +12,9 @@ import (
 // Can be deleted after implementing the Orchestrator and Relayer as per QGB ADR-005.
 // NewHandler uses the provided qgb keeper to create an sdk.Handler.
 func NewHandler(k keeper.Keeper) sdk.Handler {
-	msgServer := keeper.NewMsgServerImpl(k)
-
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
-		case *types.MsgValsetConfirm:
-			res, err := msgServer.ValsetConfirm(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
-		case *types.MsgDataCommitmentConfirm:
-			res, err := msgServer.DataCommitmentConfirm(sdk.WrapSDKContext(ctx), msg)
-			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
--- x/qgb/types/codec.go
+++ x/qgb/types/codec.go
@@ -3,28 +3,16 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
-	cdc.RegisterConcrete(&MsgDataCommitmentConfirm{}, "qgb/DataCommitmentConfirm", nil)
-	cdc.RegisterConcrete(&MsgValsetConfirm{}, "qgb/MsgValSetConfirm", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	registry.RegisterImplementations(
-		(*sdk.Msg)(nil),
-		&MsgDataCommitmentConfirm{},
-		&MsgValsetConfirm{},
-	)
-
 	registry.RegisterInterface(
 		"AttestationRequestI",
 		(*AttestationRequestI)(nil),
 		&DataCommitment{},
 		&Valset{},
 	)
-
-	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
