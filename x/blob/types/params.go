3b7f1b68dcfe141e193145699015bcff94ac8bda
Rahul Ghangas <rahulghangas0@gmail.com>
2022-11-17 20:14:11 +0530

    chore: gofumpted

--- x/blob/types/params.go
+++ x/blob/types/params.go
@@ -2,8 +2,8 @@ package types
 
 import (
 	"fmt"
-	"github.com/celestiaorg/celestia-app/pkg/appconsts"
 
+	"github.com/celestiaorg/celestia-app/pkg/appconsts"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
