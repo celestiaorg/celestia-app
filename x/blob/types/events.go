60fb499ca061d2fa591f625e76ce07a6414dd857
Rahul Ghangas <rahulghangas0@gmail.com>
2022-11-18 16:55:44 +0530

    chore: formatting

--- x/blob/types/events.go
+++ x/blob/types/events.go
@@ -2,9 +2,7 @@ package types
 
 import "github.com/cosmos/gogoproto/proto"
 
-var (
-	EventTypePayForBlob = proto.MessageName(&EventPayForBlob{})
-)
+var EventTypePayForBlob = proto.MessageName(&EventPayForBlob{})
 
 // NewPayForBlobEvent constructs a new pay for blob sdk.Event
 func NewPayForBlobEvent(signer string, size uint64) *EventPayForBlob {
