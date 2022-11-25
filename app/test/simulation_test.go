09d7c0bbda13ecbb9dd9487c1fc650691e55a632
Rahul Ghangas <rahulghangas0@gmail.com>
2022-11-25 18:22:35 +0530

    chore: gofumpted

--- app/test/simulation_test.go
+++ app/test/simulation_test.go
@@ -1,6 +1,9 @@
 package app_test
 
 import (
+	"os"
+	"testing"
+
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/app/encoding"
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -12,8 +15,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
-	"os"
-	"testing"
 )
 
 func init() {
