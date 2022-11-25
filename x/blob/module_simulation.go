9d1f5914deaa4bf88027f1e1b533a9b76cc46bd4
Rahul Ghangas <rahulghangas0@gmail.com>
2022-11-25 15:46:15 +0530

    feat: define methods used for module simulation

---
+++ x/blob/module_simulation.go
@@ -0,0 +1,67 @@
+package blob
+
+import (
+	"github.com/cosmos/cosmos-sdk/x/authz/codec"
+	"math/rand"
+
+	"github.com/celestiaorg/celestia-app/testutil/sample"
+	bloblsimulation "github.com/celestiaorg/celestia-app/x/blob/simulation"
+	"github.com/celestiaorg/celestia-app/x/blob/types"
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+	"github.com/cosmos/cosmos-sdk/x/simulation"
+)
+
+// avoid unused import issue
+var (
+	_ = sample.AccAddress
+	_ = bloblsimulation.FindAccount
+	_ = simappparams.StakePerAccount
+	_ = simulation.MsgEntryKind
+	_ = baseapp.Paramspace
+)
+
+// GenerateGenesisState creates a randomized GenState of the module
+func (AppModule) GenerateGenesisState(simState *module.SimulationState) {
+	accs := make([]string, len(simState.Accounts))
+	for i, acc := range simState.Accounts {
+		accs[i] = acc.Address.String()
+	}
+	blobGenesis := types.GenesisState{
+		Params: types.DefaultParams(),
+	}
+	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&blobGenesis)
+}
+
+// ProposalContents doesn't return any content functions for governance proposals
+func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedProposalContent {
+	return nil
+}
+
+// RandomizedParams creates randomized  param changes for the simulator
+func (am AppModule) RandomizedParams(_ *rand.Rand) []simtypes.ParamChange {
+	blobParams := types.DefaultParams()
+	return []simtypes.ParamChange{
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyMinSquareSize), func(r *rand.Rand) string {
+			return string(codec.Amino.MustMarshalJSON(blobParams.MinSquareSize))
+		}),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyMaxSquareSize), func(r *rand.Rand) string {
+			return string(codec.Amino.MustMarshalJSON(blobParams.MaxSquareSize))
+		}),
+		simulation.NewSimParamChange(types.ModuleName, string(types.KeyGasPerBlobByte), func(r *rand.Rand) string {
+			return string(codec.Amino.MustMarshalJSON(blobParams.GasPerBlobByte))
+		}),
+	}
+}
+
+// RegisterStoreDecoder registers a decoder
+func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
+
+// WeightedOperations returns the all the gov module operations with their respective weights.
+func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
+	operations := make([]simtypes.WeightedOperation, 0)
+	return operations
+}
