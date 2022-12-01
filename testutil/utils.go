09e9dee8c2b0fff715ab79285520f501b60c865a
evan-forbes <evan.samuel.forbes@gmail.com>
2022-11-30 19:23:29 -0600

    feat: add testing helpers for MsgPayForBlob

--- testutil/utils.go
+++ testutil/utils.go
@@ -5,7 +5,19 @@ import (
 	"encoding/hex"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
 	rpctypes "github.com/tendermint/tendermint/rpc/core/types"
+)
+
+const (
+	// nolint:lll
+	TestAccName = "test-account"
+	testMnemo   = `ramp soldier connect gadget domain mutual staff unusual first midnight iron good deputy wage vehicle mutual spike unlock rocket delay hundred script tumble choose`
+	testChainID = "test-chain-1"
 )
 
 func QueryWithoutProof(clientCtx client.Context, hashHexStr string) (*rpctypes.ResultTx, error) {
@@ -20,4 +32,37 @@ func QueryWithoutProof(clientCtx client.Context, hashHexStr string) (*rpctypes.R
 	}
 
 	return node.Tx(context.Background(), hash, false)
+}
+
+func GenerateKeyring(accounts ...string) keyring.Keyring {
+	cdc := simapp.MakeTestEncodingConfig().Codec
+	kb := keyring.NewInMemory(cdc)
+
+	for _, acc := range accounts {
+		_, _, err := kb.NewMnemonic(acc, keyring.English, "", "", hd.Secp256k1)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	_, err := kb.NewAccount(TestAccName, testMnemo, "1234", "", hd.Secp256k1)
+	if err != nil {
+		panic(err)
+	}
+
+	return kb
+}
+
+func RandomAddress() sdk.Address {
+	name := tmrand.Str(6)
+	kr := GenerateKeyring(name)
+	rec, err := kr.Key(name)
+	if err != nil {
+		panic(err)
+	}
+	addr, err := rec.GetAddress()
+	if err != nil {
+		panic(err)
+	}
+	return addr
 }
