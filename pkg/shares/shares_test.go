09085d9ac00d2b3868b481c3efde2b83c4d70e49
Rootul P <rootulp@gmail.com>
2022-12-14 08:31:40 -0700

    feat: implement `share.RawData()` (#1096)
    This is the celestia-app side of a fix for
    https://github.com/celestiaorg/celestia-node/issues/1448. It introduces
    `share.RawData()` for usage in celestia-node
    
    Co-authored-by: Callum Waters <cmwaters19@gmail.com>

--- pkg/shares/shares_test.go
+++ pkg/shares/shares_test.go
@@ -98,3 +98,183 @@ func generateRandomBlobOfShareCount(count int) coretypes.Blob {
 func rawBlobSize(totalSize int) int {
 	return totalSize - DelimLen(uint64(totalSize))
 }
+
+func TestSequenceLen(t *testing.T) {
+	type testCase struct {
+		name         string
+		share        Share
+		wantLen      uint64
+		wantNumBytes int
+		wantErr      bool
+	}
+	firstShare := []byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace
+		1,           // info byte
+		10, 0, 0, 0, // sequence len
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // data
+	}
+	firstShareWithLongSequence := []byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace
+		1,            // info byte
+		195, 2, 0, 0, // sequence len
+	}
+	continuationShare := []byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace
+		0,  // info byte
+		10, // sequence len
+	}
+	compactShare := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		1,           // info byte
+		10, 0, 0, 0, // sequence len
+	}
+	noInfoByte := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+	}
+	noSequenceLen := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		1, // info byte
+	}
+	testCases := []testCase{
+		{
+			name:         "first share",
+			share:        firstShare,
+			wantLen:      10,
+			wantNumBytes: 1,
+			wantErr:      false,
+		},
+		{
+			name:         "first share with long sequence",
+			share:        firstShareWithLongSequence,
+			wantLen:      323,
+			wantNumBytes: 2,
+			wantErr:      false,
+		},
+		{
+			name:         "continuation share",
+			share:        continuationShare,
+			wantLen:      0,
+			wantNumBytes: 0,
+			wantErr:      false,
+		},
+		{
+			name:         "compact share",
+			share:        compactShare,
+			wantLen:      10,
+			wantNumBytes: 4,
+			wantErr:      false,
+		},
+		{
+			name:         "no info byte returns error",
+			share:        noInfoByte,
+			wantLen:      0,
+			wantNumBytes: 0,
+			wantErr:      true,
+		},
+		{
+			name:         "no sequence len returns error",
+			share:        noSequenceLen,
+			wantLen:      0,
+			wantNumBytes: 0,
+			wantErr:      true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			len, numBytes, err := tc.share.SequenceLen()
+
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.Equal(t, tc.wantLen, len)
+			assert.Equal(t, tc.wantNumBytes, numBytes)
+		})
+	}
+}
+
+func TestRawData(t *testing.T) {
+	type testCase struct {
+		name    string
+		share   Share
+		want    []byte
+		wantErr bool
+	}
+	firstSparseShare := []byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace
+		1,                             // info byte
+		10,                            // sequence len
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // data
+	}
+	continuationSparseShare := []byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace
+		0,                             // info byte
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // data
+	}
+	firstCompactShare := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		1,           // info byte
+		10, 0, 0, 0, // sequence len
+		15, 0, // reserved bytes
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // data
+	}
+	continuationCompactShare := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		0,    // info byte
+		0, 0, // reserved bytes
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, // data
+	}
+	noSequenceLen := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		1, // info byte
+	}
+	notEnoughSequenceLenBytes := []byte{
+		0, 0, 0, 0, 0, 0, 0, 1, // namespace
+		1,        // info byte
+		10, 0, 0, // sequence len
+	}
+	testCases := []testCase{
+		{
+			name:  "first sparse share",
+			share: firstSparseShare,
+			want:  []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:  "continuation sparse share",
+			share: continuationSparseShare,
+			want:  []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:  "first compact share",
+			share: firstCompactShare,
+			want:  []byte{15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:  "continuation compact share",
+			share: continuationCompactShare,
+			want:  []byte{0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			name:    "no sequence len returns error",
+			share:   noSequenceLen,
+			wantErr: true,
+		},
+		{
+			name:    "not enough sequence len bytes returns error",
+			share:   notEnoughSequenceLenBytes,
+			wantErr: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			rawData, err := tc.share.RawData()
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.Equal(t, tc.want, rawData)
+		})
+	}
+}
