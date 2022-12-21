eba93c51fd6ea634952127eb8d38ff2f3e908133
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-21 21:13:20 +0530

    chore: formatting

--- x/blob/keeper/keeper.go
+++ x/blob/keeper/keeper.go
@@ -5,15 +5,13 @@ import (
 	"fmt"
 
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
-
-	"github.com/tendermint/tendermint/libs/log"
-
 	"github.com/celestiaorg/celestia-app/pkg/shares"
 	"github.com/celestiaorg/celestia-app/x/blob/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 const (
