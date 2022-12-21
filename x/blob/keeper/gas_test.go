765918c6865d1bd42834535eeb1206dca39edc83
Rahul Ghangas <rahulghangas0@gmail.com>
2022-12-21 21:08:33 +0530

    test: move initialization of keeper to helper function

--- x/blob/keeper/gas_test.go
+++ x/blob/keeper/gas_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/celestiaorg/celestia-app/x/blob/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -16,46 +15,7 @@ import (
 	tmdb "github.com/tendermint/tm-db"
 )
 
-func TestPayForBlobGas(t *testing.T) {
-	type testCase struct {
-		name            string
-		msg             types.MsgPayForBlob
-		wantGasConsumed uint64
-	}
-
-	testCases := []testCase{
-		{
-			name:            "1 byte blob", // occupies 1 share
-			msg:             types.MsgPayForBlob{BlobSize: 1},
-			wantGasConsumed: uint64(4096), // 1 share * 512 bytes per share * 8 gas per byte = 4096 gas
-		},
-		{
-			name:            "100 byte blob", // occupies 1 share
-			msg:             types.MsgPayForBlob{BlobSize: 100},
-			wantGasConsumed: uint64(4096),
-		},
-		{
-			name:            "1024 byte blob", // occupies 3 shares because share prefix (e.g. namespace, info byte)
-			msg:             types.MsgPayForBlob{BlobSize: 1024},
-			wantGasConsumed: uint64(12288), // 3 shares * 512 bytes per share * 8 gas per byte = 12288 gas
-		},
-	}
-
-	app := simapp.Setup(t, false)
-	for _, tc := range testCases {
-		ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-		k := Keeper{}
-		_, err := k.PayForBlob(sdk.WrapSDKContext(ctx), &tc.msg)
-		require.NoError(t, err)
-		if tc.wantGasConsumed != ctx.GasMeter().GasConsumed() {
-			t.Errorf("Gas consumed by %s: %d, want: %d", tc.name, ctx.GasMeter().GasConsumed(), tc.wantGasConsumed)
-		}
-	}
-}
-
-func TestChangingGasParam(t *testing.T) {
-	msg := types.MsgPayForBlob{BlobSize: 1024}
-
+func keeper(t *testing.T) (*Keeper, store.CommitMultiStore) {
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
 
@@ -83,6 +43,50 @@ func TestChangingGasParam(t *testing.T) {
 		paramsSubspace,
 	)
 	k.SetParams(tempCtx, types.DefaultParams())
+
+	return k, stateStore
+}
+
+func TestPayForBlobGas(t *testing.T) {
+	type testCase struct {
+		name            string
+		msg             types.MsgPayForBlob
+		wantGasConsumed uint64
+	}
+
+	testCases := []testCase{
+		{
+			name:            "1 byte blob", // occupies 1 share
+			msg:             types.MsgPayForBlob{BlobSize: 1},
+			wantGasConsumed: uint64(4096), // 1 share * 512 bytes per share * 8 gas per byte = 4096 gas
+		},
+		{
+			name:            "100 byte blob", // occupies 1 share
+			msg:             types.MsgPayForBlob{BlobSize: 100},
+			wantGasConsumed: uint64(4096),
+		},
+		{
+			name:            "1024 byte blob", // occupies 3 shares because share prefix (e.g. namespace, info byte)
+			msg:             types.MsgPayForBlob{BlobSize: 1024},
+			wantGasConsumed: uint64(12288), // 3 shares * 512 bytes per share * 8 gas per byte = 12288 gas
+		},
+	}
+
+	for _, tc := range testCases {
+		k, stateStore := keeper(t)
+		ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, nil)
+		_, err := k.PayForBlob(sdk.WrapSDKContext(ctx), &tc.msg)
+		require.NoError(t, err)
+		if tc.wantGasConsumed != ctx.GasMeter().GasConsumed() {
+			t.Errorf("Gas consumed by %s: %d, want: %d", tc.name, ctx.GasMeter().GasConsumed(), tc.wantGasConsumed)
+		}
+	}
+}
+
+func TestChangingGasParam(t *testing.T) {
+	msg := types.MsgPayForBlob{BlobSize: 1024}
+	k, stateStore := keeper(t)
+	tempCtx := sdk.NewContext(stateStore, tmproto.Header{}, false, nil)
 
 	ctx1 := sdk.NewContext(stateStore, tmproto.Header{}, false, nil)
 	_, err := k.PayForBlob(sdk.WrapSDKContext(ctx1), &msg)
