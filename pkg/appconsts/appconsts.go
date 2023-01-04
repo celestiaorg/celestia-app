d29809f5235e441e022586ce5704e964a466c317
Rootul Patel <rootulp@gmail.com>
2023-01-04 11:19:37 -0500

    chore: remove MalleatedTx consts

--- pkg/appconsts/appconsts.go
+++ pkg/appconsts/appconsts.go
@@ -69,16 +69,6 @@ const (
 	// MaxShareVersion is the maximum value a share version can be.
 	MaxShareVersion = 127
 
-	// MalleatedTxBytes is the overhead bytes added to a normal transaction after
-	// malleating it. 32 for the original hash, 4 for the uint32 share_index, and 3
-	// for protobuf
-	MalleatedTxBytes = 32 + 4 + 3
-
-	// MalleatedTxEstimateBuffer is the "magic" number used to ensure that the
-	// estimate of a malleated transaction is at least as big if not larger than
-	// the actual value. TODO: use a more accurate number
-	MalleatedTxEstimateBuffer = 100
-
 	// DefaultGasPerBlobByte is the default gas cost deducted per byte of blob
 	// included in a PayForBlob txn
 	DefaultGasPerBlobByte = 8
