4e3b307051ffbaaa8ba8a08106e4be37b6170d84
Rootul P <rootulp@gmail.com>
2023-01-06 15:54:22 -0700

    feat: emit namespaceId in PayForBlob event (#1188)
    Closes https://github.com/celestiaorg/celestia-app/issues/1169

--- x/blob/types/events.go
+++ x/blob/types/events.go
@@ -5,9 +5,10 @@ import "github.com/cosmos/gogoproto/proto"
 var EventTypePayForBlob = proto.MessageName(&EventPayForBlob{})
 
 // NewPayForBlobEvent returns a new EventPayForBlob
-func NewPayForBlobEvent(signer string, blobSize uint32) *EventPayForBlob {
+func NewPayForBlobEvent(signer string, blobSize uint32, namespaceID []byte) *EventPayForBlob {
 	return &EventPayForBlob{
-		Signer:   signer,
-		BlobSize: blobSize,
+		Signer:      signer,
+		BlobSize:    blobSize,
+		NamespaceId: namespaceID,
 	}
 }
