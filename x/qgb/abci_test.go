e7dbd6da2c5afad99f2bd60d7c1f4e1988f8eb6a
CHAMI Rachid <chamirachid1@gmail.com>
2023-01-06 21:55:23 +0100

    Add more QGB abci test cases (#1048)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    Closes: https://github.com/celestiaorg/celestia-app/issues/768
    <!-- 
    Please provide an explanation of the PR, including the apprioprate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords
    
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- x/qgb/abci_test.go
+++ x/qgb/abci_test.go
@@ -3,54 +3,22 @@ package qgb_test
 import (
 	"testing"
 
+	"github.com/celestiaorg/celestia-app/x/qgb/types"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking/teststaking"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
 	"github.com/celestiaorg/celestia-app/testutil"
 	"github.com/celestiaorg/celestia-app/x/qgb"
-	"github.com/celestiaorg/celestia-app/x/qgb/types"
 	"github.com/cosmos/cosmos-sdk/x/staking"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestAttestationCreationWhenStartingTheChain(t *testing.T) {
-	input, ctx := testutil.SetupFiveValChain(t)
-	pk := input.QgbKeeper
-
-	// EndBlocker should set a new validator set if not available
-	qgb.EndBlocker(ctx, *pk)
-	require.Equal(t, uint64(1), pk.GetLatestAttestationNonce(ctx))
-	attestation, found, err := pk.GetAttestationByNonce(ctx, 1)
-	require.True(t, found)
-	require.Nil(t, err)
-	require.NotNil(t, attestation)
-	require.Equal(t, uint64(1), attestation.GetNonce())
-}
-
-func TestValsetCreationUponUnbonding(t *testing.T) {
-	input, ctx := testutil.SetupFiveValChain(t)
-	pk := input.QgbKeeper
-
-	currentValsetNonce := pk.GetLatestAttestationNonce(ctx)
-	vs, err := pk.GetCurrentValset(ctx)
-	require.Nil(t, err)
-	err = pk.SetAttestationRequest(ctx, &vs)
-	require.Nil(t, err)
-
-	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
-	// begin unbonding
-	msgServer := stakingkeeper.NewMsgServerImpl(input.StakingKeeper)
-	undelegateMsg := testutil.NewTestMsgUnDelegateValidator(testutil.ValAddrs[0], testutil.StakingAmount)
-	_, err = msgServer.Undelegate(input.Context, undelegateMsg)
-	require.NoError(t, err)
-
-	// Run the staking endblocker to ensure valset is set in state
-	staking.EndBlocker(input.Context, input.StakingKeeper)
-	qgb.EndBlocker(input.Context, *pk)
-
-	assert.NotEqual(t, currentValsetNonce, pk.GetLatestAttestationNonce(ctx))
-}
-
-func TestValsetEmission(t *testing.T) {
+func TestFirstAttestationIsValset(t *testing.T) {
 	input, ctx := testutil.SetupFiveValChain(t)
 	pk := input.QgbKeeper
 
@@ -64,14 +32,109 @@ func TestValsetEmission(t *testing.T) {
 	require.NotNil(t, attestation)
 	require.Equal(t, uint64(1), attestation.GetNonce())
 
-	// get the valsets
+	// get the valset
 	require.Equal(t, types.ValsetRequestType, attestation.Type())
 	vs, ok := attestation.(*types.Valset)
 	require.True(t, ok)
 	require.NotNil(t, vs)
 }
 
-func TestValsetSetting(t *testing.T) {
+func TestValsetCreationWhenValidatorUnbonds(t *testing.T) {
+	input, ctx := testutil.SetupFiveValChain(t)
+	pk := input.QgbKeeper
+
+	// run abci methods after chain init
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(ctx, *pk)
+
+	// current attestation expectedNonce should be 1 because a valset has been emitted upon chain init.
+	currentAttestationNonce := pk.GetLatestAttestationNonce(ctx)
+	require.Equal(t, uint64(1), currentAttestationNonce)
+
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
+	msgServer := stakingkeeper.NewMsgServerImpl(input.StakingKeeper)
+
+	undelegateMsg := testutil.NewTestMsgUnDelegateValidator(testutil.ValAddrs[0], testutil.StakingAmount)
+	_, err := msgServer.Undelegate(input.Context, undelegateMsg)
+	require.NoError(t, err)
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(input.Context, *pk)
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 10)
+
+	assert.Equal(t, currentAttestationNonce+1, pk.GetLatestAttestationNonce(ctx))
+}
+
+func TestEndBlockerAfterEditingOrchestratorAddress(t *testing.T) {
+	input, ctx := testutil.SetupFiveValChain(t)
+	pk := input.QgbKeeper
+
+	// run abci methods after chain init
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(ctx, *pk)
+
+	// current attestation expectedNonce should be 1 because a valset has been emitted upon chain init.
+	currentAttestationNonce := pk.GetLatestAttestationNonce(ctx)
+	require.Equal(t, uint64(1), currentAttestationNonce)
+
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
+	msgServer := stakingkeeper.NewMsgServerImpl(input.StakingKeeper)
+
+	newOrchAddr := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	editMsg := stakingtypes.NewMsgEditValidator(
+		testutil.ValAddrs[1],
+		stakingtypes.Description{},
+		nil,
+		nil,
+		&newOrchAddr,
+		nil,
+	)
+	_, err := msgServer.EditValidator(input.Context, editMsg)
+	require.NoError(t, err)
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(input.Context, *pk)
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 10)
+
+	// the nonce shouldn't increase because a change in the orchestrator address shouldn't
+	// cause a change in the validator set, from the QGB perspective.
+	// check x/qgb/types/validator.go for more details.
+	assert.Equal(t, currentAttestationNonce, pk.GetLatestAttestationNonce(ctx))
+}
+
+func TestValsetCreationWhenEditingEVMAddr(t *testing.T) {
+	input, ctx := testutil.SetupFiveValChain(t)
+	pk := input.QgbKeeper
+
+	// run abci methods after chain init
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(ctx, *pk)
+
+	// current attestation expectedNonce should be 1 because a valset has been emitted upon chain init.
+	currentAttestationNonce := pk.GetLatestAttestationNonce(ctx)
+	require.Equal(t, uint64(1), currentAttestationNonce)
+
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 1)
+	msgServer := stakingkeeper.NewMsgServerImpl(input.StakingKeeper)
+
+	newEVMAddr, err := teststaking.RandomEVMAddress()
+	require.NoError(t, err)
+	editMsg := stakingtypes.NewMsgEditValidator(
+		testutil.ValAddrs[1],
+		stakingtypes.Description{},
+		nil,
+		nil,
+		nil,
+		newEVMAddr,
+	)
+	_, err = msgServer.EditValidator(input.Context, editMsg)
+	require.NoError(t, err)
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(input.Context, *pk)
+	input.Context = ctx.WithBlockHeight(ctx.BlockHeight() + 10)
+
+	assert.Equal(t, currentAttestationNonce+1, pk.GetLatestAttestationNonce(ctx))
+}
+
+func TestSetValset(t *testing.T) {
 	input, ctx := testutil.SetupFiveValChain(t)
 	pk := input.QgbKeeper
 
@@ -83,4 +146,36 @@ func TestValsetSetting(t *testing.T) {
 	require.Equal(t, uint64(1), pk.GetLatestAttestationNonce(ctx))
 }
 
-// Add data commitment window tests
+func TestSetDataCommitment(t *testing.T) {
+	input, ctx := testutil.SetupFiveValChain(t)
+	qk := input.QgbKeeper
+
+	input.Context = ctx.WithBlockHeight(int64(qk.GetDataCommitmentWindowParam(ctx)))
+	vs, err := qk.GetCurrentDataCommitment(ctx)
+	require.Nil(t, err)
+	err = qk.SetAttestationRequest(ctx, &vs)
+	require.Nil(t, err)
+
+	require.Equal(t, uint64(1), qk.GetLatestAttestationNonce(ctx))
+}
+
+func TestDataCommitmentCreation(t *testing.T) {
+	input, ctx := testutil.SetupFiveValChain(t)
+	qk := input.QgbKeeper
+
+	// run abci methods after chain init
+	staking.EndBlocker(input.Context, input.StakingKeeper)
+	qgb.EndBlocker(ctx, *qk)
+
+	// current attestation nonce should be 1 because a valset has been emitted upon chain init.
+	currentAttestationNonce := qk.GetLatestAttestationNonce(ctx)
+	require.Equal(t, uint64(1), currentAttestationNonce)
+
+	// increment height to be the same as the data commitment window
+	newHeight := int64(qk.GetDataCommitmentWindowParam(ctx))
+	input.Context = ctx.WithBlockHeight(newHeight)
+	qgb.EndBlocker(input.Context, *qk)
+
+	require.Less(t, newHeight, ctx.BlockHeight())
+	assert.Equal(t, uint64(2), qk.GetLatestAttestationNonce(ctx))
+}
