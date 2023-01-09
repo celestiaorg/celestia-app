76d8f0cc1992a9f201eeb30d0c6b672065c45e50
evan-forbes <evan.samuel.forbes@gmail.com>
2023-01-08 19:02:51 -0600

    feat!: use multisharecommitments instead of a single sharecommitment

--- x/blob/types/blob_tx.go
+++ x/blob/types/blob_tx.go
@@ -96,7 +96,11 @@ func ProcessBlobTx(txcfg client.TxEncodingConfig, bTx tmproto.BlobTx) (Processed
 		}
 
 		// verify that the commitment of the blob matches that of the PFB
-		calculatedCommit, err := CreateCommitment(pfb.NamespaceId, blob, appconsts.ShareVersionZero)
+		calculatedCommit, err := CreateMultiShareCommitment(
+			[][]byte{pfb.NamespaceId},
+			[][]byte{blob},
+			[]uint32{uint32(appconsts.ShareVersionZero)},
+		)
 		if err != nil {
 			return ProcessedBlobTx{}, ErrCalculateCommit
 		}
