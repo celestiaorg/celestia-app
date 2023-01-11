450d8df6bd58a3cde26b3124c9327f5040760817
Callum Waters <cmwaters19@gmail.com>
2023-01-11 16:25:40 +0100

    implement token filter ibc middleware

--- app/app.go
+++ app/app.go
@@ -92,6 +92,7 @@ import (
 	blobmodule "github.com/celestiaorg/celestia-app/x/blob"
 	blobmodulekeeper "github.com/celestiaorg/celestia-app/x/blob/keeper"
 	blobmoduletypes "github.com/celestiaorg/celestia-app/x/blob/types"
+	"github.com/celestiaorg/celestia-app/x/tokenfilter"
 
 	qgbmodule "github.com/celestiaorg/celestia-app/x/qgb"
 	qgbmodulekeeper "github.com/celestiaorg/celestia-app/x/qgb/keeper"
@@ -354,7 +355,13 @@ func New(
 		app.AccountKeeper, app.BankKeeper, scopedTransferKeeper,
 	)
 	transferModule := transfer.NewAppModule(app.TransferKeeper)
-	transferIBCModule := transfer.NewIBCModule(app.TransferKeeper)
+
+	// transfer stack contains (from top to bottom):
+	// - Token Filter
+	// - Transfer
+	var transferStack ibcporttypes.IBCModule
+	transferStack = transfer.NewIBCModule(app.TransferKeeper)
+	transferStack = tokenfilter.NewIBCMiddleware(transferStack, app.IBCKeeper.ChannelKeeper)
 
 	// Create evidence Keeper for to register the IBC light client misbehaviour evidence route
 	evidenceKeeper := evidencekeeper.NewKeeper(
@@ -380,7 +387,7 @@ func New(
 
 	// // Create static IBC router, add transfer route, then set and seal it
 	ibcRouter := ibcporttypes.NewRouter()
-	ibcRouter.AddRoute(ibctransfertypes.ModuleName, transferIBCModule)
+	ibcRouter.AddRoute(ibctransfertypes.ModuleName, transferStack)
 	app.IBCKeeper.SetRouter(ibcRouter)
 
 	/****  Module Options ****/
---
+++ x/tokenfilter/ibc_middleware.go
@@ -0,0 +1,82 @@
+package tokenfilter
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v6/modules/core/04-channel/types"
+	porttypes "github.com/cosmos/ibc-go/v6/modules/core/05-port/types"
+	"github.com/cosmos/ibc-go/v6/modules/core/exported"
+)
+
+var _ porttypes.Middleware = &tokenFilterMiddleware{}
+
+const ModuleName = "tokenfilter"
+
+// tokenFilterMiddleware directly inherits the IBCModule and ICS4Wrapper interfaces.
+// Only with OnRecvPacket, does it wrap the underlying implementation with additional
+// stateless logic for rejecting the inbound transfer of non-native tokens. This
+// middleware is unilateral and no handshake is required. If using this middleware
+// on an existing chain, tokens that have been routed through this chain will still
+// be allowed to unwrap.
+type tokenFilterMiddleware struct {
+	porttypes.IBCModule
+	porttypes.ICS4Wrapper
+}
+
+// NewIBCMiddleware creates a new instance of the token filter middleware for
+// the transfer module.
+func NewIBCMiddleware(ibcModule porttypes.IBCModule, wrapper porttypes.ICS4Wrapper) porttypes.Middleware {
+	return &tokenFilterMiddleware{
+		IBCModule:   ibcModule,
+		ICS4Wrapper: wrapper,
+	}
+}
+
+// OnRecvPacket implements the IBCModule interface. It is called whenever a new packet
+// from another chain is received on this chain. Here, the token filter middleware
+// unmarshals the FungibleTokenPacketData and checks to see if the denomination being
+// transferred to this chain originally came from this chain i.e. is a native token.
+// If not, it returns an ErrorAcknowledgement.
+func (m *tokenFilterMiddleware) OnRecvPacket(
+	ctx sdk.Context,
+	packet channeltypes.Packet,
+	relayer sdk.AccAddress,
+) exported.Acknowledgement {
+
+	var data types.FungibleTokenPacketData
+	if err := types.ModuleCdc.UnmarshalJSON(packet.GetData(), &data); err != nil {
+		// If this happens either a) a user has crafted an invalid packet, b) a
+		// software developer has connected the middleware to a stack that does
+		// not have a transfer module, or c) the transfer module has been modified
+		// to accept other Packets. The best thing we can do here is pass the packet
+		// on down the stack.
+		return m.IBCModule.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	// This checks the first channel and port in the denomination path. If it matches
+	// our channel and port it means that the token was originally sent from this
+	// chain. Note that this firewall prevents routing of other transactions through
+	// the chain so from this logic, the denom has to be a native denom.
+	if types.ReceiverChainIsSource(packet.GetSourcePort(), packet.GetSourceChannel(), data.Denom) {
+		return m.IBCModule.OnRecvPacket(ctx, packet, relayer)
+	}
+
+	ackErr := sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "only native denom transfers accepted, got %s", data.Denom)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			types.EventTypePacket,
+			sdk.NewAttribute(sdk.AttributeKeyModule, ModuleName),
+			sdk.NewAttribute(sdk.AttributeKeySender, data.Sender),
+			sdk.NewAttribute(types.AttributeKeyReceiver, data.Receiver),
+			sdk.NewAttribute(types.AttributeKeyDenom, data.Denom),
+			sdk.NewAttribute(types.AttributeKeyAmount, data.Amount),
+			sdk.NewAttribute(types.AttributeKeyMemo, data.Memo),
+			sdk.NewAttribute(types.AttributeKeyAckSuccess, "false"),
+			sdk.NewAttribute(types.AttributeKeyAckError, ackErr.Error()),
+		),
+	)
+
+	return channeltypes.NewErrorAcknowledgement(ackErr)
+}
