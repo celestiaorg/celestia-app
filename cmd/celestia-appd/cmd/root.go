d56ad9960c41077b3cb8800994935b73c25f0e9f
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2023-01-15 09:50:28 +0200

    Enable snapshots
    Enable snapshots for testnet to enable validators to more quickly deploy nodes/run experiments

--- cmd/celestia-appd/cmd/root.go
+++ cmd/celestia-appd/cmd/root.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"os"
 	"time"
-
+        "path/filepath"
+	
+	
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/app/encoding"
 	"github.com/cosmos/cosmos-sdk/simapp/simd/cmd"
@@ -22,6 +24,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/server"
 	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/cosmos/cosmos-sdk/snapshots"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
@@ -235,6 +238,17 @@ func NewAppServer(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts se
 	if err != nil {
 		panic(err)
 	}
+	
+	// Add snapshots
+	snapshotDir := filepath.Join(cast.ToString(appOpts.Get(flags.FlagHome)), "data", "snapshots")
+        snapshotDB, err := sdk.NewLevelDB("metadata", snapshotDir)
+        if err != nil {
+                panic(err)
+        }
+        snapshotStore, err := snapshots.NewStore(snapshotDB, snapshotDir)
+        if err != nil {
+                panic(err)
+        }
 
 	return app.New(
 		logger, db, traceStore, true, skipUpgradeHeights,
@@ -251,6 +265,9 @@ func NewAppServer(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts se
 		baseapp.SetInterBlockCache(cache),
 		baseapp.SetTrace(cast.ToBool(appOpts.Get(server.FlagTrace))),
 		baseapp.SetIndexEvents(cast.ToStringSlice(appOpts.Get(server.FlagIndexEvents))),
+		baseapp.SetSnapshotStore(snapshotStore),
+		baseapp.SetSnapshotInterval(cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval))),
+                baseapp.SetSnapshotKeepRecent(cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent))),
 	)
 }
 
