287cbcf5a45c74be8da51b8355d04d45d4ee3c7d
Rootul Patel <rootulp@gmail.com>
2023-01-24 11:01:34 -0500

    chore: delete merge

--- pkg/shares/share_merging.go
+++ pkg/shares/share_merging.go
@@ -6,63 +6,8 @@ import (
 
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
 	"github.com/celestiaorg/nmt/namespace"
-	"github.com/celestiaorg/rsmt2d"
 	coretypes "github.com/tendermint/tendermint/types"
 )
-
-// merge extracts block data from an extended data square.
-func merge(eds *rsmt2d.ExtendedDataSquare) (coretypes.Data, error) {
-	squareSize := eds.Width() / 2
-
-	// sort block data shares by namespace
-	var (
-		sortedTxShares   [][]byte
-		sortedBlobShares [][]byte
-	)
-
-	// iterate over each row index
-	for x := uint(0); x < squareSize; x++ {
-		// iterate over each share in the original data square
-		row := eds.Row(x)
-
-		for _, share := range row[:squareSize] {
-			// sort the data of that share types via namespace
-			nid := share[:appconsts.NamespaceSize]
-			switch {
-			case bytes.Equal(appconsts.TxNamespaceID, nid):
-				sortedTxShares = append(sortedTxShares, share)
-
-			case bytes.Equal(appconsts.TailPaddingNamespaceID, nid):
-				continue
-
-			// ignore unused but reserved namespaces
-			case bytes.Compare(nid, appconsts.MaxReservedNamespace) < 1:
-				continue
-
-			// every other namespaceID should be a blob
-			default:
-				sortedBlobShares = append(sortedBlobShares, share)
-			}
-		}
-	}
-
-	// pass the raw share data to their respective parsers
-	txs, err := ParseTxs(sortedTxShares)
-	if err != nil {
-		return coretypes.Data{}, err
-	}
-
-	blobs, err := ParseBlobs(sortedBlobShares)
-	if err != nil {
-		return coretypes.Data{}, err
-	}
-
-	return coretypes.Data{
-		Txs:        txs,
-		Blobs:      blobs,
-		SquareSize: uint64(squareSize),
-	}, nil
-}
 
 // ParseTxs collects all of the transactions from the shares provided
 func ParseTxs(shares [][]byte) (coretypes.Txs, error) {
--- pkg/shares/shares_test.go
+++ pkg/shares/shares_test.go
@@ -2,67 +2,15 @@ package shares
 
 import (
 	"bytes"
-	"context"
 	"testing"
-	"time"
 
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/testutil/testfactory"
-	"github.com/celestiaorg/rsmt2d"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 	coretypes "github.com/tendermint/tendermint/types"
 )
-
-func TestMerge(t *testing.T) {
-	type test struct {
-		name      string
-		txCount   int
-		blobCount int
-		maxSize   int // max size of each tx or blob
-	}
-
-	tests := []test{
-		{"one of each random small size", 1, 1, 40},
-		{"one of each random large size", 1, 1, 400},
-		{"many of each random large size", 10, 10, 40},
-		{"many of each random large size", 10, 10, 400},
-		{"only transactions", 10, 0, 400},
-		{"only blobs", 0, 10, 400},
-	}
-
-	for _, tc := range tests {
-		tc := tc
-
-		t.Run(tc.name, func(t *testing.T) {
-			// generate random data
-			data := generateRandomBlockData(
-				tc.txCount,
-				tc.blobCount,
-				tc.maxSize,
-			)
-
-			shares, err := Split(data, false)
-			require.NoError(t, err)
-			rawShares := ToBytes(shares)
-
-			eds, err := rsmt2d.ComputeExtendedDataSquare(rawShares, appconsts.DefaultCodec(), rsmt2d.NewDefaultTree)
-			if err != nil {
-				t.Error(err)
-			}
-
-			res, err := merge(eds)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			res.SquareSize = data.SquareSize
-
-			assert.Equal(t, data, res)
-		})
-	}
-}
 
 // TestPadFirstIndexedBlob ensures that we are adding padding to the first share
 // instead of calculating the value.
@@ -88,21 +36,6 @@ func TestPadFirstIndexedBlob(t *testing.T) {
 	require.NoError(t, err)
 
 	require.True(t, bytes.Contains(resShare, blob))
-}
-
-func TestFuzz_Merge(t *testing.T) {
-	t.Skip()
-	// run random shares through processCompactShares for a minute
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-			TestMerge(t)
-		}
-	}
 }
 
 // generateRandomBlockData returns randomly generated block data for testing purposes
