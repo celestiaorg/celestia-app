2f45971ec45a7339298ab759b24c1172fe44aa8d
Nguyen Nhu Viet <braveryandglory@gmail.com>
2023-02-03 13:22:45 +0100

    chore!: bump `nmt` from v0.13 to v0.14 and remove the codec from `NewTxInclusionProof` (#1333)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    Latest v0.12.0-rc5 is not compliant with what node will have to upcoming
    v0.7.0-rc`x`, as celestia-node is using nmt `v0.14.0` and dropped
    v0.13.0
    
    This PR fixes this, hence I propose to make a `v0.12.0-rc6` afterwards 🙏
    In addition, this PR cleans up codec from the signature that is not used
    in `NewTxInclusionProof ` anymore
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

--- pkg/proof/proof_test.go
+++ pkg/proof/proof_test.go
@@ -60,7 +60,6 @@ func TestNewTxInclusionProof(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			proof, err := NewTxInclusionProof(
-				appconsts.DefaultCodec(),
 				tt.data,
 				tt.txIndex,
 			)
