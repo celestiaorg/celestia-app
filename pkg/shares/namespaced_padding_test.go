bf8026570fada9b2a638f38a81d1f012bad45c2a
Rootul Patel <rootulp@gmail.com>
2023-02-06 17:27:53 -0500

    test: unit test IsNamespacedPadded

--- pkg/shares/namespaced_padding_test.go
+++ pkg/shares/namespaced_padding_test.go
@@ -35,3 +35,51 @@ func TestNamespacedPaddedShares(t *testing.T) {
 		assert.Equal(t, want, share.ToBytes())
 	}
 }
+
+func TestIsNamespacedPadded(t *testing.T) {
+	type testCase struct {
+		name    string
+		share   Share
+		want    bool
+		wantErr bool
+	}
+	emptyShare := Share{}
+	blobShare, _ := zeroPadIfNecessary([]byte{
+		1, 1, 1, 1, 1, 1, 1, 1, // namespace ID
+		1,          // info byte
+		0, 0, 0, 1, // sequence len
+		0xff, // data
+	}, appconsts.ShareSize)
+
+	testCases := []testCase{
+		{
+			name:  "namespaced padded share",
+			share: NamespacedPaddedShare(namespace.ID{1, 1, 1, 1, 1, 1, 1, 1}),
+			want:  true,
+		},
+		{
+			name:    "empty share",
+			share:   emptyShare,
+			want:    false,
+			wantErr: true,
+		},
+		{
+			name:    "blob share",
+			share:   blobShare,
+			want:    false,
+			wantErr: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := IsNamespacedPadded(tc.share)
+			if tc.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			assert.NoError(t, err)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
