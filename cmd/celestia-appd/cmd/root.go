c5f56540ea5abff3c972f6254ed95d28f1ec1377
Viacheslav Gonkivskyi <viacheslavgonkivskyi@gmail.com>
2023-02-07 13:24:50 +0200

    chore: move init function to the different package

--- cmd/celestia-appd/cmd/root.go
+++ cmd/celestia-appd/cmd/root.go
@@ -44,14 +44,6 @@ import (
 
 const EnvPrefix = "CELESTIA"
 
-func init() {
-	cfg := sdk.GetConfig()
-	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
-	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
-	cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
-	cfg.Seal()
-}
-
 // NewRootCmd creates a new root command for celestia-appd. It is called once in the
 // main function.
 func NewRootCmd() *cobra.Command {
---
+++ cmd/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/celestiaorg/celestia-app/app"
+)
+
+func init() {
+	cfg := sdk.GetConfig()
+	cfg.SetBech32PrefixForAccount(app.Bech32PrefixAccAddr, app.Bech32PrefixAccPub)
+	cfg.SetBech32PrefixForValidator(app.Bech32PrefixValAddr, app.Bech32PrefixValPub)
+	cfg.SetBech32PrefixForConsensusNode(app.Bech32PrefixConsAddr, app.Bech32PrefixConsPub)
+	cfg.Seal()
+}
