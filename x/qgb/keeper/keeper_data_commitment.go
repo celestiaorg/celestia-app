b40a6908954828b90aebd530c5d30592626834f7
CHAMI Rachid <chamirachid1@gmail.com>
2023-02-16 15:59:59 +0100

    feat: data commitment for height support for yet to be committed heights (#1389)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    Closes https://github.com/celestiaorg/celestia-app/issues/1388
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords
    
    ---------
    
    Co-authored-by: Evan Forbes <42654277+evan-forbes@users.noreply.github.com>

--- x/qgb/keeper/keeper_data_commitment.go
+++ x/qgb/keeper/keeper_data_commitment.go
@@ -32,6 +32,17 @@ func (k Keeper) GetDataCommitmentWindowParam(ctx sdk.Context) uint64 {
 
 // GetDataCommitmentForHeight returns the attestation containing the provided height.
 func (k Keeper) GetDataCommitmentForHeight(ctx sdk.Context, height uint64) (types.DataCommitment, error) {
+	lastDC, err := k.GetLastDataCommitment(ctx)
+	if err != nil {
+		return types.DataCommitment{}, err
+	}
+	if lastDC.EndBlock < height {
+		return types.DataCommitment{}, fmt.Errorf(
+			"no data commitment has been generated for the provided height. Last height %d < %d",
+			lastDC.EndBlock,
+			height,
+		)
+	}
 	latestNonce := k.GetLatestAttestationNonce(ctx)
 	for i := uint64(0); i < latestNonce; i++ {
 		// TODO better search
@@ -51,4 +62,24 @@ func (k Keeper) GetDataCommitmentForHeight(ctx sdk.Context, height uint64) (type
 		}
 	}
 	return types.DataCommitment{}, fmt.Errorf("data commitment for height not found")
+}
+
+// GetLastDataCommitment returns the last data commitment.
+func (k Keeper) GetLastDataCommitment(ctx sdk.Context) (types.DataCommitment, error) {
+	latestNonce := k.GetLatestAttestationNonce(ctx)
+	for i := uint64(0); i < latestNonce; i++ {
+		att, found, err := k.GetAttestationByNonce(ctx, latestNonce-i)
+		if err != nil {
+			return types.DataCommitment{}, err
+		}
+		if !found {
+			return types.DataCommitment{}, fmt.Errorf("couldn't find attestation with nonce %d", latestNonce-i)
+		}
+		dcc, ok := att.(*types.DataCommitment)
+		if !ok {
+			continue
+		}
+		return *dcc, nil
+	}
+	return types.DataCommitment{}, fmt.Errorf("last data commitment not found")
 }
