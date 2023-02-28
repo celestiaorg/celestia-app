7781942d6406f963da29329ac7773b61fc96f494
Rootul Patel <rootulp@gmail.com>
2023-02-28 14:02:36 -0500

    feat: json+gzip compression stats
    nidSize=8
    unencoded proof is 336 bytes
    /tmp/proof_8.json is 545 bytes
    /tmp/proof_8.gz is 471 bytes
    
    nidSize=16
    unencoded proof is 448 bytes
    /tmp/proof_16.json is 713 bytes
    /tmp/proof_16.gz is 591 bytes
    
    nidSize=20
    unencoded proof is 504 bytes
    /tmp/proof_20.json is 769 bytes
    /tmp/proof_20.gz is 636 bytes
    
    nidSize=32
    unencoded proof is 672 bytes
    /tmp/proof_32.json is 993 bytes
    /tmp/proof_32.gz is 808 bytes

--- pkg/wrapper/nmt_wrapper_test.go
+++ pkg/wrapper/nmt_wrapper_test.go
@@ -1,10 +1,17 @@
 package wrapper
 
 import (
+	"bufio"
 	"bytes"
+	"compress/gzip"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
@@ -159,4 +166,69 @@ func generateRandNamespacedRawData(total int, nidSize int, leafSize int) [][]byt
 
 func sortByteArrays(src [][]byte) {
 	sort.Slice(src, func(i, j int) bool { return bytes.Compare(src[i], src[j]) < 0 })
+}
+
+func TestErasuredNamespacedMerkleTree_Prove(t *testing.T) {
+	nidSizes := []int{8, 16, 20, 32}
+	for _, nidSize := range nidSizes {
+		data := generateRandNamespacedRawData(appconsts.DefaultMaxSquareSize, nidSize, appconsts.ShareSize-nidSize)
+		tree := NewErasuredNamespacedMerkleTree(appconsts.DefaultMaxSquareSize, 0, nmt.NamespaceIDSize(nidSize))
+		for _, d := range data {
+			tree.Push(d)
+		}
+		proof, err := tree.Prove(0)
+		assert.NoError(t, err)
+		fmt.Printf("nidSize=%v\n", nidSize)
+		fmt.Printf("unencoded proof is %v bytes\n", proofSize(proof))
+
+		publicProof := PublicProof{
+			Start:                   proof.Start(),
+			End:                     proof.End(),
+			Nodes:                   proof.Nodes(),
+			LeafHash:                proof.LeafHash(),
+			IsMaxNamespaceIDIgnored: proof.IsMaxNamespaceIDIgnored(),
+		}
+		b, err := json.Marshal(publicProof)
+		assert.NoError(t, err)
+
+		filename := fmt.Sprintf("/tmp/proof_%v.json", nidSize)
+		err = os.WriteFile(filename, b, 0644)
+
+		f, err := os.Open(filename)
+		assert.NoError(t, err)
+		read := bufio.NewReader(f)
+		d, err := ioutil.ReadAll(read)
+		assert.NoError(t, err)
+		compressedFilename := strings.Replace(filename, ".json", ".gz", -1)
+		f, err = os.Create(compressedFilename)
+		assert.NoError(t, err)
+		w := gzip.NewWriter(f)
+		w.Write(d)
+		w.Close()
+
+		uncompressedFile, err := os.Stat(filename)
+		assert.NoError(t, err)
+		compressedFile, err := os.Stat(compressedFilename)
+		assert.NoError(t, err)
+		fmt.Printf("%v is %v bytes\n", filename, uncompressedFile.Size())
+		fmt.Printf("%v is %v bytes\n", compressedFilename, compressedFile.Size())
+
+		fmt.Println()
+	}
+}
+
+func proofSize(proof nmt.Proof) int {
+	size := 0
+	for _, node := range proof.Nodes() {
+		size += len(node)
+	}
+	return size
+}
+
+type PublicProof struct {
+	Start                   int
+	End                     int
+	Nodes                   [][]byte
+	LeafHash                []byte
+	IsMaxNamespaceIDIgnored bool
 }
