59ed724d6a81c6b3339f66e1ea83116ab9af3ed0
CHAMI Rachid <chamirachid1@gmail.com>
2023-03-22 09:33:57 +0100

    fix: initialize the data commitment range with 1 instead of 0 (#1514)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    Closes https://github.com/celestiaorg/celestia-app/issues/1513
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords

--- x/qgb/keeper/keeper_data_commitment.go
+++ x/qgb/keeper/keeper_data_commitment.go
@@ -13,10 +13,9 @@ import (
 // GetCurrentDataCommitment creates the latest data commitment at current height according to
 // the data commitment window specified
 func (k Keeper) GetCurrentDataCommitment(ctx sdk.Context) (types.DataCommitment, error) {
-	beginBlock := uint64(ctx.BlockHeight()) - k.GetDataCommitmentWindowParam(ctx)
-	// to avoid having overlapped ranges of data commitments such as: 0-400;400-800;800-1200
-	// we will commit to the previous block height so that the ranges are as follows: 0-399;400-799;800-1199
-	endBlock := uint64(ctx.BlockHeight()) - 1
+	beginBlock := uint64(ctx.BlockHeight()) - k.GetDataCommitmentWindowParam(ctx) + 1
+	// for a data commitment window of 400, the ranges will be: 1-400;401-800;801-1200
+	endBlock := uint64(ctx.BlockHeight())
 	nonce := k.GetLatestAttestationNonce(ctx) + 1
 
 	dataCommitment := types.NewDataCommitment(nonce, beginBlock, endBlock)
