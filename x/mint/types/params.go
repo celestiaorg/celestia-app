51f75842916022756406ccc8a8d36cd23dd78848
Mojtaba <mojtaba.esk@gmail.com>
2023-03-31 11:52:09 +0200

    fix: export DefaultBlocksPerYear

--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -17,6 +17,10 @@ var (
 	KeyBlocksPerYear = []byte("BlocksPerYear")
 )
 
+// Default # of blocks per year if not configured in genesis.
+// Assuming 15 second block times
+const DefaultBlocksPerYear = uint64(60 * 60 * 8766 / 15)
+
 // ParamTable for minting module.
 func ParamKeyTable() paramtypes.KeyTable {
 	return paramtypes.NewKeyTable().RegisterParamSet(&Params{})
@@ -35,7 +39,7 @@ func NewParams(
 func DefaultParams() Params {
 	return Params{
 		MintDenom:     sdk.DefaultBondDenom,
-		BlocksPerYear: uint64(60 * 60 * 8766 / 5), // assuming 5 second block times
+		BlocksPerYear: DefaultBlocksPerYear,
 	}
 }
 
