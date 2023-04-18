01ae46da55e36e4e7248cf37ee6e56af029082ba
evan-forbes <evan.samuel.forbes@gmail.com>
2023-04-18 16:57:00 -0500

    fix: docs

--- pkg/appconsts/appconsts.go
+++ pkg/appconsts/appconsts.go
@@ -76,11 +76,7 @@ const (
 	// root height for blobs. If the number of subtree roots used to create a
 	// share commitment surpasses this threshold, then the height is increased.
 	// The rationale for this value is described in more detail in ADR013
-	// (./docs/architecture/adr-013). Note that this value should not drop below
-	// the max square size because that would result in sub tree widths that are
-	// larger than the actual suquare size for that block. Having a value that
-	// is larger than the square is simply wasteful in that the proofs for large
-	// blobs are unnecessarily large.
+	// (./docs/architecture/adr-013).
 	// ADR013 https://github.com/celestiaorg/celestia-app/blob/e905143e8fe138ce6085ae9a5c1af950a2d87638/docs/architecture/adr-013-non-interactive-default-rules-for-zero-padding.md //nolint: lll
 	SubtreeRootThreshold = DefaultMaxSquareSize
 
