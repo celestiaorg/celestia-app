b0dde1f914f2350e02f48711fecd7d51f8477965
evan-forbes <evan.samuel.forbes@gmail.com>
2023-04-20 11:20:56 -0500

    fix: test

--- pkg/shares/non_interactive_defaults_test.go
+++ pkg/shares/non_interactive_defaults_test.go
@@ -475,7 +475,7 @@ func TestSubTreeWidth(t *testing.T) {
 		t.Run(fmt.Sprintf("shareCount %d", tc.shareCount), func(t *testing.T) {
 			got := SubTreeWidth(tc.shareCount)
 			assert.Equal(t, tc.want, got, i)
-			assert.GreaterOrEqual(t, got, BlobMinSquareSize(tc.shareCount))
+			assert.LessOrEqual(t, got, BlobMinSquareSize(tc.shareCount))
 		})
 	}
 }
