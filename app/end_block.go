691cd36c348c55c8f8ba94f3fbf1889fc9198755
evan-forbes <evan.samuel.forbes@gmail.com>
2023-04-21 16:06:11 -0500

    docs: docs

--- app/end_block.go
+++ app/end_block.go
@@ -5,6 +5,8 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
+// EndBlock wraps the BaseApp's end block method. This is done to modify the app
+// version if necessary.
 func (app *App) EndBlock(req abci.RequestEndBlock) (res abci.ResponseEndBlock) {
 	res = app.BaseApp.EndBlock(req)
 	ctx := app.GetContextForDeliverTx([]byte{1})
