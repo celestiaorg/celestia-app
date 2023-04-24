848bed88c263392d9d3119194e4dca611830b9c5
evan-forbes <evan.samuel.forbes@gmail.com>
2023-04-24 08:41:07 -0500

    fix: imports

--- x/upgrade/test/integration_test.go
+++ x/upgrade/test/integration_test.go
@@ -9,7 +9,7 @@ import (
 
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/app/encoding"
-	"github.com/celestiaorg/celestia-app/testutil/testnode"
+	"github.com/celestiaorg/celestia-app/test/util/testnode"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
