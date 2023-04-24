e6a4445bc88dbedf3d66d180b700ec411b2e4f31
evan-forbes <evan.samuel.forbes@gmail.com>
2023-04-24 08:41:19 -0500

    fix:imports

--- x/upgrade/test/removal_test.go
+++ x/upgrade/test/removal_test.go
@@ -3,7 +3,7 @@ package test
 import (
 	"testing"
 
-	"github.com/celestiaorg/celestia-app/testutil"
+	testutil "github.com/celestiaorg/celestia-app/test/util"
 	sdkupgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"github.com/stretchr/testify/require"
 )
