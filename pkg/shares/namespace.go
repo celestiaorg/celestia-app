f5877b570026496fe8ccb136b8c12be26871292a
Callum Waters <cmwaters19@gmail.com>
2023-05-12 11:12:03 +0200

    resolve merge conflicts

--- pkg/shares/namespace.go
+++ pkg/shares/namespace.go
@@ -8,42 +8,42 @@ import (
 
 // GetSharesByNamespace returns all shares that belong to a given namespace.
 // It will return an empty range if the namespace could not be found.
-// This assumes that the slice of shares are lexicographically sorted by 
+// This assumes that the slice of shares are lexicographically sorted by
 // namespace.
-func GetShareRangeForNamespace(shares []Share, ns namespace.Namespace) (ShareRange, error) {
+func GetShareRangeForNamespace(shares []Share, ns namespace.Namespace) (Range, error) {
 	if len(shares) == 0 {
-		return ShareRange{}, nil
+		return EmptyRange(), nil
 	}
 	n0, err := shares[0].Namespace()
 	if err != nil {
-		return ShareRange{}, err
+		return EmptyRange(), err
 	}
 	if ns.IsLessThan(n0) {
-		return ShareRange{}, nil
+		return EmptyRange(), nil
 	}
 	n1, err := shares[len(shares)-1].Namespace()
 	if err != nil {
-		return ShareRange{}, err
+		return EmptyRange(), err
 	}
 	if ns.IsGreaterThan(n1) {
-		return ShareRange{}, nil
+		return EmptyRange(), nil
 	}
 
-	var start = -1
+	start := -1
 	for i, share := range shares {
 		shareNS, err := share.Namespace()
 		if err != nil {
-			return ShareRange{}, fmt.Errorf("failed to get namespace from share %d: %w", i, err)
+			return EmptyRange(), fmt.Errorf("failed to get namespace from share %d: %w", i, err)
 		}
 		if ns.Equals(shareNS) && start == -1 {
 			start = i
 		}
 		if shareNS.IsGreaterThan(ns) && start != -1 {
-			return ShareRange{start, i-1}, nil
+			return Range{start, i - 1}, nil
 		}
 	}
 	if start == -1 {
-		return ShareRange{}, nil
+		return EmptyRange(), nil
 	}
-	return ShareRange{start, len(shares)-1}, nil
+	return Range{start, len(shares) - 1}, nil
 }
