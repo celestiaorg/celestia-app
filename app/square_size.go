a17fba033f8daa6237343eb0c9dadce7dd066be2
evan-forbes <evan.samuel.forbes@gmail.com>
2023-05-14 11:56:24 -0500

    fix: HACK to fix the state not being readable at height 0

--- app/square_size.go
+++ app/square_size.go
@@ -10,6 +10,14 @@ import (
 // using the max bytes value from the consensus params. Governance can change
 // the result of this value by changing the MaxBytes consensus parameter.
 func (app *App) GovMaxSquareSize(ctx sdk.Context) int {
+	// TODO: fix hack that forces the max square size for the first height to
+	// 64. This is due to the sdk not loading state until calling commit for
+	// height 1. This means that even if the actual GovMaxSquareSize is supposed
+	// to be something other than 64, it will be 64 for the first block
+	// prodcuced.
+	if ctx.BlockHeader().Height == 0 {
+		return int(SquareSizeFromMaxBytes(int64(appconsts.DefaultMaxBytes)))
+	}
 	params := app.GetConsensusParams(ctx)
 	return int(SquareSizeFromMaxBytes(params.Block.MaxBytes))
 }
