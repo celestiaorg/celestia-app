4aabe5203bc89fda1ac89abd6489c3a0f4f31c76
Callum Waters <cmwaters19@gmail.com>
2023-05-15 21:48:28 +0200

    test: fuzz share commitments (#1749)
    Closes: #1744, closes: #1747

--- pkg/square/square_benchmark_test.go
+++ pkg/square/square_benchmark_test.go
@@ -12,7 +12,8 @@ import (
 func BenchmarkSquareConstruct(b *testing.B) {
 	for _, txCount := range []int{10, 100, 1000} {
 		b.Run(fmt.Sprintf("txCount=%d", txCount), func(b *testing.B) {
-			txs := generateOrderedTxs(txCount/2, txCount/2, 1024)
+			txs := generateOrderedTxs(txCount/2, txCount/2, 1, 1024)
+			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				_, err := square.Construct(txs, appconsts.DefaultMaxSquareSize)
 				require.NoError(b, err)
@@ -24,7 +25,19 @@ func BenchmarkSquareConstruct(b *testing.B) {
 func BenchmarkSquareBuild(b *testing.B) {
 	for _, txCount := range []int{10, 100, 1000, 10000} {
 		b.Run(fmt.Sprintf("txCount=%d", txCount), func(b *testing.B) {
-			txs := generateMixedTxs(txCount/2, txCount/2, 1024)
+			txs := generateMixedTxs(txCount/2, txCount/2, 1, 1024)
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				_, _, err := square.Build(txs, appconsts.DefaultMaxSquareSize)
+				require.NoError(b, err)
+			}
+		})
+	}
+	const txCount = 10
+	for _, blobSize := range []int{10, 100, 1000, 10000} {
+		b.Run(fmt.Sprintf("blobSize=%d", blobSize), func(b *testing.B) {
+			txs := generateMixedTxs(0, txCount, 1, blobSize)
+			b.ResetTimer()
 			for i := 0; i < b.N; i++ {
 				_, _, err := square.Build(txs, appconsts.DefaultMaxSquareSize)
 				require.NoError(b, err)
