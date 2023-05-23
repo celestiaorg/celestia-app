46e340b59e48e41e3fa9f4f402216e53e2a7b264
CHAMI Rachid <chamirachid1@gmail.com>
2023-05-23 20:36:41 +0200

    Update x/qgb/genesis.go
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- x/qgb/genesis.go
+++ x/qgb/genesis.go
@@ -11,7 +11,7 @@ import (
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	k.SetLatestAttestationNonce(ctx, 0)
 	// The reason we're setting the earliest available nonce to 1 is because at chain startup,
-	// a new valset will be created all the time.
+	// a new valset will always be created.
 	// Also, it's easier to set it here to 1 instead of doing it in abci.EndBlocker and do
 	// the check on every iteration
 	k.SetEarliestAvailableAttestationNonce(ctx, 1)
