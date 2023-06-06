5bed8e0c45706a1fb5733c7b53a62f44f73a843c
sanaz <sanaztaheri@ieee.org>
2023-06-06 16:37:27 -0700

    adds generate Random amount function

--- test/util/blobfactory/test_util.go
+++ test/util/blobfactory/test_util.go
@@ -27,18 +27,6 @@ func GenerateManyRawSendTxs(txConfig client.TxConfig, count int) []coretypes.Tx 
 	return txs
 }
 
-// GenerateManyRawSendTxsWithSpecificSize generates `count` many raw send txs with specific size `amount`.
-func GenerateManyRawSendTxsWithSpecificSize(txConfig client.TxConfig, count int, amount int64) []coretypes.Tx {
-	const acc = "signer"
-	kr := testfactory.GenerateKeyring(acc)
-	signer := blobtypes.NewKeyringSigner(kr, acc, "chainid")
-	txs := make([]coretypes.Tx, count)
-	for i := 0; i < count; i++ {
-		txs[i] = GenerateRawSendTx(txConfig, signer, amount)
-	}
-	return txs
-}
-
 // GenerateRawSendTx creates send transactions meant to help test encoding/prepare/process
 // proposal, they are not meant to actually be executed by the state machine. If
 // we want that, we have to update nonce, and send funds to someone other than
@@ -88,16 +76,24 @@ func CreateRawTx(txConfig client.TxConfig, msg sdk.Msg, signer *blobtypes.Keyrin
 // GenerateRandomSigner generates a random signer for the given chainID.
 func GenerateRandomSigner(chainID string) *blobtypes.KeyringSigner {
 	// generate a variable length string
-	acc := tmrand.Str(tmrand.Int())
+	acc := tmrand.Str(tmrand.Intn(100) + 1)
 	kr := testfactory.GenerateKeyring(acc)
 	signer := blobtypes.NewKeyringSigner(kr, acc, chainID)
 	return signer
 }
 
+func GenerateRandomAmount() int64 {
+	n := tmrand.Int64()
+	if n < 0 {
+		return -n
+	}
+	return n
+}
+
 // GenerateRandomRawSendTx generates a random raw send tx.
 func GenerateRandomRawSendTx(txConfig client.TxConfig) (rawTx []byte) {
 	signer := GenerateRandomSigner("chainID")
-	amount := tmrand.Int64()
+	amount := GenerateRandomAmount()
 	return GenerateRawSendTx(txConfig, signer, amount)
 
 }
