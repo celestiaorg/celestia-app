c610f1a2772e9851877b0883f2c91cdd1581e577
Callum Waters <cmwaters19@gmail.com>
2023-06-16 20:04:29 +0200

    feat: introduce ibc redundant relay decorator (#1934)

---
+++ app/ante.go
@@ -0,0 +1,38 @@
+package app
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+	"github.com/cosmos/cosmos-sdk/x/auth/signing"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	ibcante "github.com/cosmos/ibc-go/v6/modules/core/ante"
+	ibckeeper "github.com/cosmos/ibc-go/v6/modules/core/keeper"
+)
+
+func NewAnteHandler(
+	accountKeeper ante.AccountKeeper,
+	bankKeeper authtypes.BankKeeper,
+	feegrantKeeper ante.FeegrantKeeper,
+	signModeHandler signing.SignModeHandler,
+	sigGasConsumer ante.SignatureVerificationGasConsumer,
+	channelKeeper *ibckeeper.Keeper,
+) sdk.AnteHandler {
+	return sdk.ChainAnteDecorators(
+		ante.NewSetUpContextDecorator(), // outermost AnteDecorator. SetUpContext must be called first
+		// reject all tx extensions
+		ante.NewExtensionOptionsDecorator(nil),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(accountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(accountKeeper),
+		// check that the fee matches the gas and the local minimum gas price
+		// of the validator
+		ante.NewDeductFeeDecorator(accountKeeper, bankKeeper, feegrantKeeper, nil),
+		ante.NewSetPubKeyDecorator(accountKeeper), // SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewValidateSigCountDecorator(accountKeeper),
+		ante.NewSigGasConsumeDecorator(accountKeeper, sigGasConsumer),
+		ante.NewSigVerificationDecorator(accountKeeper, signModeHandler),
+		ante.NewIncrementSequenceDecorator(accountKeeper),
+		ibcante.NewRedundantRelayDecorator(channelKeeper),
+	)
+}
