b45d9161991ef6e455fb404352a7687d0d198171
Rootul P <rootulp@gmail.com>
2023-06-23 15:05:57 -0400

    feat!: remove genesis time from genesis state (#1851) (#1988)
    Cherry-pick https://github.com/celestiaorg/celestia-app/pull/1851 to the
    v1.x release branch
    
    ---------
    
    Co-authored-by: Evan Forbes <42654277+evan-forbes@users.noreply.github.com>

--- x/mint/simulation/decoder.go
+++ x/mint/simulation/decoder.go
@@ -14,11 +14,16 @@ import (
 func NewDecodeStore(cdc codec.Codec) func(kvA, kvB kv.Pair) string {
 	return func(kvA, kvB kv.Pair) string {
 		switch {
-		case bytes.Equal(kvA.Key, types.MintKey):
+		case bytes.Equal(kvA.Key, types.KeyMinter):
 			var minterA, minterB types.Minter
 			cdc.MustUnmarshal(kvA.Value, &minterA)
 			cdc.MustUnmarshal(kvB.Value, &minterB)
 			return fmt.Sprintf("%v\n%v", minterA, minterB)
+		case bytes.Equal(kvA.Key, types.KeyGenesisTime):
+			var genesisTimeA, genesisTimeB types.GenesisTime
+			cdc.MustUnmarshal(kvA.Value, &genesisTimeA)
+			cdc.MustUnmarshal(kvB.Value, &genesisTimeB)
+			return fmt.Sprintf("%v\n%v", genesisTimeA, genesisTimeB)
 		default:
 			panic(fmt.Sprintf("invalid mint key %X", kvA.Key))
 		}
