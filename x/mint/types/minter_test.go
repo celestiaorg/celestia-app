7b52005603d2db1dd49a55eb2f6f0b74c23c576c
Rootul P <rootulp@gmail.com>
2023-06-23 14:00:33 -0400

    feat!: remove genesis time from genesis state (#1851)
    Closes https://github.com/celestiaorg/celestia-app/issues/1757
    
    ## Description
    
    Prior to this PR, the mint module contained a placeholder `genesis_time`
    in genesis state. It ignored the placeholder and used the block time for
    the block height `1` as the "genesis time". This PR removes the
    placeholder and that mechanism.
    
    ```diff
          "minter": {
            "inflation_rate": "0.080000000000000000",
            "annual_provisions": "0.000000000000000000",
    -       "genesis_time": "2023-06-22T18:46:11.873883Z",
            "previous_block_time": null,
            "bond_denom": "utia"
          }
    ```
    
    After this PR, `genesis_time` in the mint module is set to the chain's
    genesis time which is the root level `genesis_time` specified in
    genesis.json. Genesis.json is created on chain initialization (i.e.
    `celestia-appd init chain-id`).
    
    This is breaking b/c it modifies a state transition for the mint module
    state and removes a field from the `Minter` state.
    
    ## Testing
    
    Now there is only one `genesis_time` in genesis.json:
    
    ```shell
    $ ./scripts/build-run-single-node.sh
    
    $ cat /var/folders/y0/dd92_x8x4tlf397xstgwfz_c0000gn/T/celestia_app_XXXXXXXXXXXXX.wYBu5mwm/config/genesis.json | grep genesis_time -A 3 -B 3
    {
      "genesis_time": "2023-06-23T16:13:24.719488Z",
      "chain_id": "private",
      "initial_height": "1",
      "consensus_params": {
    ```
    
    Additionally it gets set correctly in mint module state
    
    ```shell
    $ ./build/celestia-appd query mint genesis-time
    2023-06-23 16:13:24.719488 +0000 UTC
    ```
    
    ---------
    
    Co-authored-by: Evan Forbes <42654277+evan-forbes@users.noreply.github.com>

--- x/mint/types/minter_test.go
+++ x/mint/types/minter_test.go
@@ -15,7 +15,6 @@ import (
 func TestCalculateInflationRate(t *testing.T) {
 	minter := DefaultMinter()
 	genesisTime := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
-	minter.GenesisTime = &genesisTime
 
 	type testCase struct {
 		year int
@@ -70,7 +69,7 @@ func TestCalculateInflationRate(t *testing.T) {
 		years := time.Duration(tc.year * NanosecondsPerYear * int(time.Nanosecond))
 		blockTime := genesisTime.Add(years)
 		ctx := sdk.NewContext(nil, tmproto.Header{}, false, nil).WithBlockTime(blockTime)
-		inflationRate := minter.CalculateInflationRate(ctx)
+		inflationRate := minter.CalculateInflationRate(ctx, genesisTime)
 		got, err := inflationRate.Float64()
 		assert.NoError(t, err)
 		assert.Equal(t, tc.want, got, "want %v got %v year %v blockTime %v", tc.want, got, tc.year, blockTime)
@@ -100,7 +99,7 @@ func TestCalculateBlockProvision(t *testing.T) {
 			current:          current,
 			previous:         current.Add(-blockInterval),
 			// 80 billion utia (annual provisions) * 15 (seconds) / 31,556,952 (seconds per year) = 38026.48620817 which truncates to 38026 utia
-			want: sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(38026)),
+			want: sdk.NewCoin(DefaultBondDenom, sdk.NewInt(38026)),
 		},
 		{
 			name:             "one 30 second block during the first year",
@@ -108,7 +107,7 @@ func TestCalculateBlockProvision(t *testing.T) {
 			current:          current,
 			previous:         current.Add(-2 * blockInterval),
 			// 80 billion utia (annual provisions) * 30 (seconds) / 31,556,952 (seconds per year) = 76052.97241635 which truncates to 76052 utia
-			want: sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(76052)),
+			want: sdk.NewCoin(DefaultBondDenom, sdk.NewInt(76052)),
 		},
 	}
 	for _, tc := range testCases {
@@ -173,10 +172,11 @@ func BenchmarkCalculateBlockProvision(b *testing.B) {
 func BenchmarkCalculateInflationRate(b *testing.B) {
 	b.ReportAllocs()
 	minter := DefaultMinter()
+	genesisTime := time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC)
 
 	for n := 0; n < b.N; n++ {
 		ctx := sdk.NewContext(nil, tmproto.Header{Height: int64(n)}, false, nil)
-		minter.CalculateInflationRate(ctx)
+		minter.CalculateInflationRate(ctx, genesisTime)
 	}
 }
 
