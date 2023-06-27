094528d1be2d6e3d301da6d46215034373c52769
Mojtaba <mojtaba.esk@gmail.com>
2023-06-27 11:22:52 +0330

    test: contVesting acc added + some refactoring

--- app/test/vesting_test.go
+++ app/test/vesting_test.go
@@ -2,7 +2,6 @@ package app_test
 
 import (
 	"context"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"sync"
@@ -16,7 +15,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	vestingtypes "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -24,8 +22,6 @@ import (
 	"github.com/stretchr/testify/suite"
 	tmtime "github.com/tendermint/tendermint/types/time"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestVestingModule(t *testing.T) {
@@ -100,7 +96,8 @@ func (s *VestingModuleTestSuite) TestGenesisDelayedVestingAccounts() {
 		vBals := []banktypes.Balance{}
 		for i, tt := range tests {
 			tests[i].accName = fmt.Sprintf("vesting_%d", i)
-			acc := s.newDelayedVestingAccount(kr, tests[i].accName, tt.endTime)
+			bacc, coins := testfactory.NewBaseAccount(kr, tests[i].accName)
+			acc := vestingtypes.NewDelayedVestingAccount(bacc, coins, tt.endTime.Unix())
 			vAccs = append(vAccs, acc)
 			vBals = append(vBals, banktypes.Balance{
 				Address: acc.GetAddress().String(),
@@ -118,20 +115,17 @@ func (s *VestingModuleTestSuite) TestGenesisDelayedVestingAccounts() {
 		return gs
 	}
 
-	cctx, _, grpcAddr := s.NewNetworkWithGenesisOpt(kr, gsOpt)
+	cctx, _, _ := s.NewNetworkWithGenesisOpt(kr, gsOpt)
 
 	assert.NoError(s.T(), cctx.WaitForNextBlock())
-
-	grpcConn, err := GrpcConnect(grpcAddr, false)
-	assert.NoError(s.T(), err)
 
 	for _, tt := range tests {
 		s.Run(tt.name, func() {
 			accAddress := getAddress(tt.accName, cctx.Keyring).String()
 
 			// Test account details correctness
-			acli := authtypes.NewQueryClient(grpcConn)
-			res, err := acli.Account(context.Background(), &authtypes.QueryAccountRequest{
+			cli := authtypes.NewQueryClient(cctx.GRPCClient)
+			res, err := cli.Account(context.Background(), &authtypes.QueryAccountRequest{
 				Address: accAddress,
 			})
 			assert.NoError(s.T(), err)
@@ -144,10 +138,9 @@ func (s *VestingModuleTestSuite) TestGenesisDelayedVestingAccounts() {
 			assert.Equal(s.T(), accAddress, qAcc.GetAddress().String(), "account address must match")
 			assert.EqualValues(s.T(), tt.endTime.Unix(), qAcc.GetEndTime(), "end time must match")
 
-			bondDenom := qAcc.GetOriginalVesting().GetDenomByIndex(0)
 			assert.EqualValues(s.T(),
 				testfactory.BaseAccountDefaultBalance,
-				qAcc.GetOriginalVesting().AmountOf(bondDenom).Int64(),
+				qAcc.GetOriginalVesting().AmountOf(app.BondDenom).Int64(),
 				"original vesting must match")
 
 			/*--------*/
@@ -155,101 +148,50 @@ func (s *VestingModuleTestSuite) TestGenesisDelayedVestingAccounts() {
 			// Test the locking mechanism
 			// If the end time is already passed, the funds must be unlocked
 			// and we should be able to transfer some of it to another account
-			mustSuceed := tt.endTime.Before(time.Now())
+			mustSucceed := tt.endTime.Before(time.Now())
 
 			assert.NoError(s.T(), cctx.WaitForNextBlock())
 
 			/*--------*/
 
 			// Test available balance
-			bcli := banktypes.NewQueryClient(grpcConn)
-			bres, err := bcli.SpendableBalances(context.Background(), &banktypes.QuerySpendableBalancesRequest{
-				Address: accAddress,
-			})
+			balances, err := GetAccountSpendableBalance(cctx.GRPCClient, accAddress)
 			assert.NoError(s.T(), err)
 
 			expectedSpendableBal := int64(0)
-			if mustSuceed {
+			if mustSucceed {
 				expectedSpendableBal = testfactory.BaseAccountDefaultBalance
 			}
 			expectedSpendableBal += initBalanceForGasFee.Amount.Int64()
 			assert.EqualValues(s.T(),
 				expectedSpendableBal,
-				bres.GetBalances().AmountOf(bondDenom).Int64(),
+				balances.AmountOf(app.BondDenom).Int64(),
 				"spendable balance must match")
 
 			/*--------*/
 
-			// Test transfer ability
-			randomAcc := s.unusedAccount()
-			msgSend := banktypes.NewMsgSend(
-				getAddress(tt.accName, cctx.Keyring),
-				getAddress(randomAcc, cctx.Keyring),
-				sdk.NewCoins(sdk.NewCoin(bondDenom, sdk.NewInt(1+initBalanceForGasFee.Amount.Int64()))),
-			)
-			encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
-			resTx, err := testnode.SignAndBroadcastTx(encCfg, cctx.Context, tt.accName, []sdk.Msg{msgSend}...)
-			assert.NoError(s.T(), err)
+			s.testTransferVestingAmount(cctx, tt.accName, mustSucceed)
 
-			resQ, err := cctx.WaitForTx(resTx.TxHash, 10)
-			assert.NoError(s.T(), err)
-
-			if mustSuceed {
-				assert.EqualValues(s.T(), 0, resQ.TxResult.Code, "the transfer TX must succeed")
-			} else {
-				assert.EqualValues(s.T(), 5, resQ.TxResult.Code, "the transfer TX must fail")
-			}
-
-			/*--------*/
-
-			// Test available balance after transfer
-			bres, err = bcli.SpendableBalances(context.Background(), &banktypes.QuerySpendableBalancesRequest{
-				Address: accAddress,
-			})
+			balancesAfter, err := GetAccountSpendableBalance(cctx.GRPCClient, accAddress)
 			assert.NoError(s.T(), err)
 
 			expectedSpendableBalAfterTx := expectedSpendableBal - 1 // -1utia for gas fee of the tx above
-			if mustSuceed {
+			if mustSucceed {
 				// if the transfer was successful
-				expectedSpendableBalAfterTx -= 1 + initBalanceForGasFee.Amount.Int64()
+				expectedSpendableBalAfterTx -= 1 + initBalanceForGasFee.Amount.Int64() + testfactory.BaseAccountDefaultBalance
 			}
 
 			assert.EqualValues(s.T(), // we pay for gas as well
-				bres.GetBalances().AmountOf(bondDenom).Int64(),
+				balancesAfter.AmountOf(app.BondDenom).Int64(),
 				expectedSpendableBalAfterTx,
 				"spendable balance must be equal")
 
 			/*--------*/
 
-			del, err := GetAccountDelegations(grpcConn, accAddress)
-			assert.NoError(s.T(), err)
-			assert.Empty(s.T(), del, "initial delegation must be empty")
-
-			validators, err := GetValidators(grpcConn)
-			assert.NoError(s.T(), err)
-			assert.NotEmpty(s.T(), validators)
-
-			msgDelg := stakingtypes.NewMsgDelegate(
-				getAddress(tt.accName, cctx.Keyring),
-				validators[0].GetOperator(),
-				sdk.NewCoin(bondDenom, sdk.NewInt(1+initBalanceForGasFee.Amount.Int64())),
-			)
-			// encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
-			resTx, err = testnode.SignAndBroadcastTx(encCfg, cctx.Context, tt.accName, []sdk.Msg{msgDelg}...)
-			assert.NoError(s.T(), err)
-
-			resQ, err = cctx.WaitForTx(resTx.TxHash, 10)
-			assert.NoError(s.T(), err)
-			assert.EqualValues(s.T(), 0, resQ.TxResult.Code, "the delegation TX must succeed")
-
-			// verify the delegations
-			del, err = GetAccountDelegations(grpcConn, accAddress)
-			assert.NoError(s.T(), err)
-			assert.NotEmpty(s.T(), del, "delegations must not be empty")
-			assert.EqualValues(s.T(),
-				1+initBalanceForGasFee.Amount.Int64(),
-				del[0].Balance.Amount.Int64(),
-				"delegation amount must match")
+			// test delegation of only the locked account(s)
+			if !mustSucceed {
+				s.testDelegation(cctx, tt.accName)
+			}
 		})
 	}
 }
@@ -259,16 +201,14 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 	initBalanceForGasFee := sdk.NewCoin(app.BondDenom, sdk.NewInt(10))
 
 	startTime := tmtime.Now()
-	endTime := startTime.Add(24 * time.Second)
-	_ = endTime
 	periods := vestingtypes.Periods{
-		types.Period{Length: int64(10), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
-		types.Period{Length: int64(4), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
-		types.Period{Length: int64(4), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
-		types.Period{Length: int64(6), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
+		vestingtypes.Period{Length: int64(10), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
+		vestingtypes.Period{Length: int64(4), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
+		vestingtypes.Period{Length: int64(4), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
+		vestingtypes.Period{Length: int64(6), Amount: sdk.Coins{sdk.NewInt64Coin(app.BondDenom, 2500)}},
 	}
 
-	accName := "period_vesting_0"
+	const accName = "period_vesting_0"
 	kr := testfactory.GenerateKeyring()
 
 	gsOpt := func(gs map[string]json.RawMessage) map[string]json.RawMessage {
@@ -293,18 +233,15 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 		return gs
 	}
 
-	cctx, _, grpcAddr := s.NewNetworkWithGenesisOpt(kr, gsOpt)
+	cctx, _, _ := s.NewNetworkWithGenesisOpt(kr, gsOpt)
 
 	assert.NoError(s.T(), cctx.WaitForNextBlock())
-
-	grpcConn, err := GrpcConnect(grpcAddr, false)
-	assert.NoError(s.T(), err)
 
 	accAddress := getAddress(accName, cctx.Keyring).String()
 
 	// Test account details correctness
-	acli := authtypes.NewQueryClient(grpcConn)
-	res, err := acli.Account(context.Background(), &authtypes.QueryAccountRequest{
+	cli := authtypes.NewQueryClient(cctx.GRPCClient)
+	res, err := cli.Account(context.Background(), &authtypes.QueryAccountRequest{
 		Address: accAddress,
 	})
 	assert.NoError(s.T(), err)
@@ -327,11 +264,9 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 	// Test available balance
 	for i := 0; i < 2; i++ { // We just let one period to be unlocked to keep some locked amount to test other stuff
 
-		bcli := banktypes.NewQueryClient(grpcConn)
 		currentLen := time.Since(startTime).Seconds()
-		bres, err := bcli.SpendableBalances(context.Background(), &banktypes.QuerySpendableBalancesRequest{
-			Address: accAddress,
-		})
+
+		balances, err := GetAccountSpendableBalance(cctx.GRPCClient, accAddress)
 		assert.NoError(s.T(), err)
 
 		expectedSpendableBal := initBalanceForGasFee.Amount.Int64()
@@ -344,15 +279,103 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 		}
 		assert.EqualValues(s.T(),
 			expectedSpendableBal,
-			bres.GetBalances().AmountOf(app.BondDenom).Int64(),
+			balances.AmountOf(app.BondDenom).Int64(),
 			"spendable balance must match")
 
 		_, err = cctx.WaitForTimestamp(startTime.Add(periods[i].Duration() + 1)) // Wait for the next period to be passed
 		assert.NoError(s.T(), err)
 	}
+
+	s.testTransferVestingAmount(cctx, accName, false)
+	s.testDelegation(cctx, accName)
+}
+
+func (s *VestingModuleTestSuite) TestGenesisContinuesVestingAccounts() {
+	// initial unlocked allocation to pay the gas fees
+	initBalanceForGasFee := sdk.NewCoin(app.BondDenom, sdk.NewInt(10))
+
+	startTime := tmtime.Now().Add(10 * time.Second)
+	endTime := startTime.Add(20 * time.Second)
+
+	const accName = "cont_vesting_0"
+	kr := testfactory.GenerateKeyring()
+
+	bacc, coins := testfactory.NewBaseAccount(kr, accName)
+	cva := vestingtypes.NewContinuousVestingAccount(bacc, coins, startTime.Unix(), endTime.Unix())
+
+	gsOpt := func(gs map[string]json.RawMessage) map[string]json.RawMessage {
+		vAccs := authtypes.GenesisAccounts{cva}
+		vBals := []banktypes.Balance{
+			{
+				Address: cva.GetAddress().String(),
+				Coins:   cva.GetOriginalVesting().Add(initBalanceForGasFee),
+			},
+		}
+
+		var err error
+		gs, err = AddAccountsToGenesisState(gs, vAccs...)
+		assert.NoError(s.T(), err)
+
+		gs, err = AddBalancesToGenesisState(gs, vBals...)
+		assert.NoError(s.T(), err)
+
+		return gs
+	}
+
+	cctx, _, _ := s.NewNetworkWithGenesisOpt(kr, gsOpt)
+
+	assert.NoError(s.T(), cctx.WaitForNextBlock())
+
+	accAddress := getAddress(accName, cctx.Keyring).String()
+
+	// Test account details correctness
+	acli := authtypes.NewQueryClient(cctx.GRPCClient)
+	res, err := acli.Account(context.Background(), &authtypes.QueryAccountRequest{
+		Address: accAddress,
+	})
+	assert.NoError(s.T(), err)
+
+	var qAcc vestingtypes.ContinuousVestingAccount
+	err = qAcc.Unmarshal(res.Account.Value)
+	assert.NoError(s.T(), err)
+
+	// Checking the queried account data
+	assert.Equal(s.T(), accAddress, qAcc.GetAddress().String(), "account address must match")
+	assert.EqualValues(s.T(), startTime.Unix(), qAcc.GetStartTime(), "start time must match")
+	assert.EqualValues(s.T(), endTime.Unix(), qAcc.GetEndTime(), "end time must match")
+	assert.EqualValues(s.T(),
+		testfactory.BaseAccountDefaultBalance,
+		qAcc.GetOriginalVesting().AmountOf(app.BondDenom).Int64(),
+		"original vesting must match")
+
+	fmt.Printf("qAcc.String(): %v\n", qAcc.String())
+
 	/*--------*/
 
-	// Test transfer ability
+	// Test available balance
+	for i := 0; i < 5; i++ {
+		queryTime := tmtime.Now()
+
+		balances, err := GetAccountSpendableBalance(cctx.GRPCClient, accAddress)
+		assert.NoError(s.T(), err)
+
+		minExpectedSpendableBal := cva.GetVestedCoins(queryTime).Add(initBalanceForGasFee)
+
+		assert.LessOrEqual(s.T(),
+			balances.AmountOf(app.BondDenom).Int64(),
+			minExpectedSpendableBal.AmountOf(app.BondDenom).Int64(),
+		)
+
+		_, err = cctx.WaitForTimestamp(startTime.Add(5)) // Wait for a while
+		assert.NoError(s.T(), err)
+	}
+	/*--------*/
+
+	s.testTransferVestingAmount(cctx, accName, false)
+	s.testDelegation(cctx, accName)
+}
+
+func (s *VestingModuleTestSuite) testTransferVestingAmount(cctx testnode.Context, accName string, mustSucceed bool) {
 	randomAcc := s.unusedAccount()
 	msgSend := banktypes.NewMsgSend(
 		getAddress(accName, cctx.Keyring),
@@ -366,15 +389,21 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 	resQ, err := cctx.WaitForTx(resTx.TxHash, 10)
 	assert.NoError(s.T(), err)
 
-	assert.EqualValues(s.T(), 5, resQ.TxResult.Code, "the transfer TX must fail")
+	if mustSucceed {
+		assert.EqualValues(s.T(), 0, resQ.TxResult.Code, "the transfer TX must succeed")
+	} else {
+		assert.EqualValues(s.T(), 5, resQ.TxResult.Code, "the transfer TX must fail")
+	}
+}
 
-	/*--------*/
+func (s *VestingModuleTestSuite) testDelegation(cctx testnode.Context, accName string) {
+	accAddress := getAddress(accName, cctx.Keyring).String()
 
-	del, err := GetAccountDelegations(grpcConn, accAddress)
+	del, err := GetAccountDelegations(cctx.GRPCClient, accAddress)
 	assert.NoError(s.T(), err)
 	assert.Empty(s.T(), del, "initial delegation must be empty")
 
-	validators, err := GetValidators(grpcConn)
+	validators, err := GetValidators(cctx.GRPCClient)
 	assert.NoError(s.T(), err)
 	assert.NotEmpty(s.T(), validators)
 
@@ -383,34 +412,22 @@ func (s *VestingModuleTestSuite) TestGenesisPeriodicVestingAccounts() {
 		validators[0].GetOperator(),
 		sdk.NewCoin(app.BondDenom, sdk.NewInt(testfactory.BaseAccountDefaultBalance)),
 	)
-	resTx, err = testnode.SignAndBroadcastTx(encCfg, cctx.Context, accName, []sdk.Msg{msgDelg}...)
+	encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
+	resTx, err := testnode.SignAndBroadcastTx(encCfg, cctx.Context, accName, []sdk.Msg{msgDelg}...)
 	assert.NoError(s.T(), err)
 
-	resQ, err = cctx.WaitForTx(resTx.TxHash, 10)
+	resQ, err := cctx.WaitForTx(resTx.TxHash, 10)
 	assert.NoError(s.T(), err)
 	assert.EqualValues(s.T(), 0, resQ.TxResult.Code, "the delegation TX must succeed")
 
 	// verify the delegations
-	del, err = GetAccountDelegations(grpcConn, accAddress)
+	del, err = GetAccountDelegations(cctx.GRPCClient, accAddress)
 	assert.NoError(s.T(), err)
 	assert.NotEmpty(s.T(), del, "delegations must not be empty")
 	assert.EqualValues(s.T(),
 		testfactory.BaseAccountDefaultBalance,
 		del[0].Balance.Amount.Int64(),
 		"delegation amount must match")
-}
-
-func (s *VestingModuleTestSuite) newDelayedVestingAccount(kr keyring.Keyring, name string, endTime time.Time) *vestingtypes.DelayedVestingAccount {
-	bacc, coins := testfactory.NewBaseAccount(kr, name)
-	return vestingtypes.NewDelayedVestingAccount(bacc, coins, endTime.Unix())
-}
-
-func GrpcConnect(serverAddr string, tlsEnabled bool) (*grpc.ClientConn, error) {
-	if tlsEnabled {
-		creds := credentials.NewTLS(&tls.Config{})
-		return grpc.Dial(serverAddr, grpc.WithTransportCredentials(creds))
-	}
-	return grpc.Dial(serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 }
 
 func AddAccountsToGenesisState(gs map[string]json.RawMessage, accounts ...authtypes.GenesisAccount) (map[string]json.RawMessage, error) {
@@ -466,4 +483,18 @@ func GetAccountDelegations(grpcConn *grpc.ClientConn, address string) (stakingty
 	}
 
 	return res.DelegationResponses, err
+}
+
+func GetAccountSpendableBalance(grpcConn *grpc.ClientConn, address string) (sdk.Coins, error) {
+	cli := banktypes.NewQueryClient(grpcConn)
+	res, err := cli.SpendableBalances(
+		context.Background(),
+		&banktypes.QuerySpendableBalancesRequest{
+			Address: address,
+		},
+	)
+	if err != nil || res == nil {
+		return nil, err
+	}
+	return res.GetBalances(), nil
 }
