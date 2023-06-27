9c1d220297299212ae2b641c6b3f8979d341756d
Evan Forbes <42654277+evan-forbes@users.noreply.github.com>
2023-06-27 15:25:58 -0500

    chore!: refactor testnode to expose appcreator (#1991)
    ## Overview
    
    This refactor is mainly targeting #1990, but doing so in a way that uses
    common patters to avoid continuing to blow up the api
    
    closes #1990
    
    ## Checklist
    
    - [x] New and updated code has appropriate documentation
    - [x] New and updated code has new and/or updated testing
    - [x] Required CI checks are passing
    - [x] Visual proof for any user facing features like CLI or
    documentation updates
    - [x] Linked issues closed with keywords

--- app/test/std_sdk_test.go
+++ app/test/std_sdk_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	abci "github.com/tendermint/tendermint/abci/types"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
 )
 
 func TestStandardSDKIntegrationTestSuite(t *testing.T) {
@@ -47,8 +48,15 @@ type StandardSDKIntegrationTestSuite struct {
 func (s *StandardSDKIntegrationTestSuite) SetupSuite() {
 	t := s.T()
 	t.Log("setting up integration test suite")
-	accounts, cctx := testnode.DefaultNetwork(t)
-	s.accounts = accounts
+
+	accounts := make([]string, 300)
+	for i := 0; i < len(accounts); i++ {
+		accounts[i] = tmrand.Str(9)
+	}
+
+	cfg := testnode.DefaultConfig().WithAccounts(accounts)
+	cctx, _, _ := testnode.NewNetwork(t, cfg)
+	s.accounts = cfg.Accounts
 	s.ecfg = encoding.MakeConfig(app.ModuleEncodingRegisters...)
 	s.cctx = cctx
 }
