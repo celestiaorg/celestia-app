bc154553db03b5a979c2c0ea3a6b234da0c7ff10
Rootul Patel <rootulp@gmail.com>
2023-08-05 15:27:41 -0400

    chore!: shrink interface of x/mint expected keepers

--- x/mint/types/expected_keepers.go
+++ x/mint/types/expected_keepers.go
@@ -14,14 +14,12 @@ type StakingKeeper interface {
 // AccountKeeper defines the contract required for account APIs.
 type AccountKeeper interface {
 	GetModuleAddress(name string) sdk.AccAddress
-	SetModuleAccount(sdk.Context, types.ModuleAccountI)
 	GetModuleAccount(ctx sdk.Context, moduleName string) types.ModuleAccountI
 }
 
 // BankKeeper defines the contract needed to be fulfilled for banking and supply
 // dependencies.
 type BankKeeper interface {
-	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 }
