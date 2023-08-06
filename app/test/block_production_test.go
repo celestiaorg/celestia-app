d6e03bcb553c4385716a0ebf0bd01d80bf503a1e
sanaz <sanaztaheri@ieee.org>
2023-08-06 14:05:30 +0300

    renames the test suite

--- app/test/block_production_test.go
+++ app/test/block_production_test.go
@@ -10,18 +10,18 @@ import (
 	"time"
 )
 
-func TestIntegrationTestSuite(t *testing.T) {
-	suite.Run(t, new(IntegrationTestSuite))
+func TestBlockProductionestSuite(t *testing.T) {
+	suite.Run(t, new(BlockProductionestSuite))
 }
 
-type IntegrationTestSuite struct {
+type BlockProductionestSuite struct {
 	suite.Suite
 
 	accounts []string
 	cctx     testnode.Context
 }
 
-func (s *IntegrationTestSuite) SetupSuite() {
+func (s *BlockProductionestSuite) SetupSuite() {
 	if testing.Short() {
 		s.T().Skip("skipping full node integration test in short mode.")
 	}
@@ -41,7 +41,7 @@ func (s *IntegrationTestSuite) SetupSuite() {
 	s.accounts = accounts
 }
 
-func (s *IntegrationTestSuite) Test_PostData() {
+func (s *BlockProductionestSuite) Test_PostData() {
 	require := s.Require()
 	_, err := s.cctx.PostData(s.accounts[0], flags.BroadcastBlock, appns.RandomBlobNamespace(), tmrand.Bytes(100000))
 	require.Error(err) // change this to require.NoError(err) to see the exact error message
