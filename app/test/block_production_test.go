585da49673967101d598ccf611eedf0250557af1
sanaz <sanaztaheri@ieee.org>
2023-08-07 15:32:50 +0300

    revises a wrong value in the comments

--- app/test/block_production_test.go
+++ app/test/block_production_test.go
@@ -44,7 +44,7 @@ func (s *BlockProductionestSuite) SetupSuite() {
 func (s *BlockProductionestSuite) Test_PostData() {
 	require := s.Require()
 	_, err := s.cctx.PostData(s.accounts[0], flags.BroadcastBlock, appns.RandomBlobNamespace(), tmrand.Bytes(100000))
-	// since the block production is delayed by 10 seconds, the transactions posted arrive when the node is still at height 1
+	// since the block production is delayed by 5 seconds, the transactions posted arrive when the node is still at height 1
 	// this makes the post data fail with the following error:
 	// rpc error: code = Unknown desc = codespace sdk code 18: invalid request: failed to load state at height 0; no commit info found (latest height: 0)
 	require.Error(err) // change this to require.NoError(err) to see the error message
