05cfa0481dcfac3ae4e50108c0452cf51cead257
Rootul P <rootulp@gmail.com>
2023-08-07 09:06:25 -0400

    Update x/mint/keeper/genesis.go
    Co-authored-by: CHAMI Rachid <chamirachid1@gmail.com>

--- x/mint/keeper/genesis.go
+++ x/mint/keeper/genesis.go
@@ -15,7 +15,7 @@ func (k Keeper) InitGenesis(ctx sdk.Context, ak types.AccountKeeper, data *types
 	}
 	k.SetGenesisTime(ctx, gt)
 	// Although ak.GetModuleAccount appears to be a no-op, it actually creates a
-	// new module account in the x/auth account store. See the x/auth keeper for
+	// new module account in the x/auth account store if it doesn't exist. See the x/auth keeper for
 	// more details.
 	ak.GetModuleAccount(ctx, types.ModuleName)
 }
