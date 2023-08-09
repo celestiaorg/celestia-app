3f41f1d945c78704a9eaac2a6ee1d2ed138f0d5c
Rootul Patel <rootulp@gmail.com>
2023-08-09 13:43:07 -0400

    rename and clarify godoc

--- x/blob/ante/max_blob_size_ante.go
+++ x/blob/ante/max_blob_size_ante.go
@@ -9,31 +9,29 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// MaxBlobSizeDecorator helps to prevent a PFB from being included in a block
+// MaxTotalBlobSizeDecorator helps to prevent a PFB from being included in a block
 // but not fitting in a data square.
-type MaxBlobSizeDecorator struct {
+type MaxTotalBlobSizeDecorator struct {
 	k BlobKeeper
 }
 
-func NewMaxBlobSizeDecorator(k BlobKeeper) MaxBlobSizeDecorator {
-	return MaxBlobSizeDecorator{k}
+func NewMaxBlobSizeDecorator(k BlobKeeper) MaxTotalBlobSizeDecorator {
+	return MaxTotalBlobSizeDecorator{k}
 }
 
-// AnteHandle implements the AnteHandler interface. It returns an error if the
-// tx contains a MsgPayForBlobs where the total blob data size exceeds the upper
-// bound. The upper bound is calculated based on the number of bytes available
-// to blobs in a data square with the maximum square size.
-func (d MaxBlobSizeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+// AnteHandle implements the Cosmos SDK AnteHandler function signature. It
+// returns an error if tx contains a MsgPayForBlobs where the total blob size is
+// greater than the max total blob size.
+func (d MaxTotalBlobSizeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	if !ctx.IsCheckTx() {
 		return next(ctx, tx, simulate)
 	}
 
-	upperBound := d.totalBlobSizeUpperBound(ctx)
+	max := d.maxTotalBlobSize(ctx)
 	for _, m := range tx.GetMsgs() {
 		if pfb, ok := m.(*blobtypes.MsgPayForBlobs); ok {
-			total := getTotal(pfb.BlobSizes)
-			if total > upperBound {
-				return ctx, errors.Wrapf(blobtypes.ErrTotalBlobSizeTooLarge, "total blob size %d exceeds upper bound %d", total, upperBound)
+			if total := getTotal(pfb.BlobSizes); total > max {
+				return ctx, errors.Wrapf(blobtypes.ErrTotalBlobSizeTooLarge, "total blob size %d exceeds max %d", total, max)
 			}
 		}
 	}
@@ -41,22 +39,22 @@ func (d MaxBlobSizeDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bo
 	return next(ctx, tx, simulate)
 }
 
-// totalBlobSizeUpperBound returns an upper bound for the number of bytes available
-// for blob data in a data square based on the max square size. Note it is
-// possible that txs with a total blob size less than this upper bound still
-// fail to be included in a block due to overhead from the PFB tx and/or padding
-// shares. As a result, this upper bound should only be used to reject
-// transactions that are guaranteed to be too large.
-func (d MaxBlobSizeDecorator) totalBlobSizeUpperBound(ctx sdk.Context) int {
+// maxTotalBlobSize returns the max the number of bytes available for blobs in a
+// data square based on the max square size. Note it is possible that txs with a
+// total blob size less than this max still fail to be included in a block due
+// to overhead from the PFB tx and/or padding shares.
+func (d MaxTotalBlobSizeDecorator) maxTotalBlobSize(ctx sdk.Context) int {
 	squareSize := d.getMaxSquareSize(ctx)
 	totalShares := squareSize * squareSize
-	blobShares := totalShares - 1 // subtract 1 to account for the PFB tx share
+	// The PFB tx share must occupy at least one share so the # of blob shares
+	// is at least one less than totalShares.
+	blobShares := totalShares - 1
 	return shares.AvailableBytesFromSparseShares(blobShares)
 }
 
 // getMaxSquareSize returns the maximum square size based on the current values
 // for the relevant governance parameter and the versioned constant.
-func (d MaxBlobSizeDecorator) getMaxSquareSize(ctx sdk.Context) int {
+func (d MaxTotalBlobSizeDecorator) getMaxSquareSize(ctx sdk.Context) int {
 	// TODO: fix hack that forces the max square size for the first height to
 	// 64. This is due to our fork of the sdk not initializing state before
 	// BeginBlock of the first block. This is remedied in versions of the sdk
