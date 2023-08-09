2affc8237ba27be112275c7fb6708b3a4da96320
sanaz <sanaztaheri@ieee.org>
2023-08-09 13:03:16 +0300

    extends the test by verifying that the second block is non-empty

--- app/test/block_production_test.go
+++ app/test/block_production_test.go
@@ -58,18 +58,34 @@ func (s *BlockProductionTestSuite) Test_BlockOneTransactionNonInclusion() {
 	require.ErrorContains(err, "rpc error: code = Unknown desc = codespace sdk code 18: invalid request: failed to load state at height 0; no commit info found (latest height: 0)")
 }
 
-// Test_Empty_Block_One tests that the first block is empty
-func (s *BlockProductionTestSuite) Test_Empty_Block_One() {
+// Test_FirstBlockIsEmpty tests whether the first block is empty.
+func (s *BlockProductionTestSuite) Test_FirstBlockIsEmpty() {
 	require := s.Require()
-	// wait for 2*s.timeoutCommit+1*time.Second to ensure that the node is at height 2
-	_, err := s.cctx.WaitForHeightWithTimeout(2, 2*s.timeoutCommit+1*time.Second)
+	// wait until the node is at height 1
+	time.Sleep(1 * s.timeoutCommit)
+
+	// send some transactions, these should be included in the second block
+	_, err := s.cctx.PostData(s.accounts[0], flags.BroadcastBlock, appns.RandomBlobNamespace(), tmrand.Bytes(100000))
 	require.NoError(err)
-	// fetch block 1
+
+	// wait for 2*s.timeoutCommit+1*time.Second to ensure that the node is at height 2
+	_, err = s.cctx.WaitForHeightWithTimeout(2, 2*s.timeoutCommit+1*time.Second)
+	require.NoError(err)
+
+	// fetch the first block
 	one := int64(1)
-	b, err := s.cctx.Client.Block(s.cctx.GoContext(), &one)
+	b1, err := s.cctx.Client.Block(s.cctx.GoContext(), &one)
 	require.NoError(err)
 	// check whether the first block is empty
-	require.True(b.Block.Header.Height == 1)
-	require.Equal(len(b.Block.Data.Txs), 0)
+	require.True(b1.Block.Header.Height == 1)
+	require.Equal(len(b1.Block.Data.Txs), 0)
+
+	// fetch the second block
+	two := int64(2)
+	b2, err := s.cctx.Client.Block(s.cctx.GoContext(), &two)
+	require.NoError(err)
+	// check whether the second block is non-empty
+	require.True(b2.Block.Header.Height == 2)
+	require.True(len(b2.Block.Data.Txs) == 1)
 
 }
