7d7108580ce3edf417f825b26b203a5952e0be69
Rootul Patel <rootulp@gmail.com>
2023-08-09 15:43:02 -0400

    extract integ test to new file

---
+++ app/test/max_total_blob_size_test.go
@@ -0,0 +1,117 @@
+package app_test
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/celestiaorg/celestia-app/app"
+	"github.com/celestiaorg/celestia-app/app/encoding"
+	"github.com/celestiaorg/celestia-app/pkg/appconsts"
+	appns "github.com/celestiaorg/celestia-app/pkg/namespace"
+	"github.com/celestiaorg/celestia-app/test/util/testnode"
+	"github.com/celestiaorg/celestia-app/x/blob"
+	blobtypes "github.com/celestiaorg/celestia-app/x/blob/types"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmrand "github.com/tendermint/tendermint/libs/rand"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+const (
+	mebibyte = 1_048_576 // one mebibyte in bytes
+)
+
+func TestMaxTotalBlobSizeSuite(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping max total blob size suite in short mode.")
+	}
+	suite.Run(t, &MaxTotalBlobSizeSuite{})
+}
+
+type MaxTotalBlobSizeSuite struct {
+	suite.Suite
+
+	ecfg     encoding.Config
+	accounts []string
+	cctx     testnode.Context
+}
+
+func (s *MaxTotalBlobSizeSuite) SetupSuite() {
+	t := s.T()
+
+	s.accounts = makeAccounts(1)
+	cfg := testnode.DefaultConfig().WithAccounts(s.accounts)
+
+	cctx, _, _ := testnode.NewNetwork(t, cfg)
+	s.cctx = cctx
+	s.ecfg = encoding.MakeConfig(app.ModuleEncodingRegisters...)
+
+	require.NoError(t, cctx.WaitForNextBlock())
+
+	for _, account := range s.accounts {
+		signer := blobtypes.NewKeyringSigner(s.cctx.Keyring, account, s.cctx.ChainID)
+		err := signer.QueryAccountNumber(s.cctx.GoContext(), s.cctx.GRPCClient)
+		require.NoError(t, err)
+	}
+}
+
+// TestSubmitPayForBlob_blobSizes verifies the tx response ABCI code when
+// SubmitPayForBlob is invoked with different blob sizes.
+func (s *MaxTotalBlobSizeSuite) TestSubmitPayForBlob_blobSizes() {
+	t := s.T()
+	require.NoError(t, s.cctx.WaitForBlocks(3))
+
+	type testCase struct {
+		name string
+		blob *tmproto.Blob
+		// txResponseCode is the expected tx response ABCI code.
+		txResponseCode uint32
+	}
+	testCases := []testCase{
+		{
+			name:           "1 byte blob",
+			blob:           mustNewBlob(t, 1),
+			txResponseCode: abci.CodeTypeOK,
+		},
+		{
+			name:           "1 mebibyte blob",
+			blob:           mustNewBlob(t, mebibyte),
+			txResponseCode: abci.CodeTypeOK,
+		},
+		// {
+		// 	name:           "2 mebibyte blob",
+		// 	blob:           mustNewBlob(t, 2*mebibyte),
+		// 	txResponseCode: types.ErrTotalBlobSizeTooLarge.ABCICode(),
+		// },
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			signer := blobtypes.NewKeyringSigner(s.cctx.Keyring, s.accounts[0], s.cctx.ChainID)
+			options := []blobtypes.TxBuilderOption{blobtypes.SetGasLimit(1_000_000_000)}
+			res, err := blob.SubmitPayForBlob(context.TODO(), signer, s.cctx.GRPCClient, []*blobtypes.Blob{tc.blob}, options...)
+
+			require.NoError(t, err)
+			require.NotNil(t, res)
+			require.Equal(t, tc.txResponseCode, res.Code, res.Logs)
+		})
+	}
+}
+
+func mustNewBlob(t *testing.T, blobSize int) *tmproto.Blob {
+	ns1 := appns.MustNewV0(bytes.Repeat([]byte{1}, appns.NamespaceVersionZeroIDSize))
+	data := tmrand.Bytes(blobSize)
+	result, err := blobtypes.NewBlob(ns1, data, appconsts.ShareVersionZero)
+	require.NoError(t, err)
+	return result
+}
+
+func makeAccounts(count int) []string {
+	accounts := make([]string, count)
+	for i := 0; i < count; i++ {
+		accounts[i] = tmrand.Str(20)
+	}
+	return accounts
+}
