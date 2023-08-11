9b999ec72b4db9414d35f1f923cf3d8ed200f91e
Callum Waters <cmwaters19@gmail.com>
2023-08-11 16:28:50 +0200

    chore: move BondDenom to appconsts (#2246)
    This moves BondDemon to the appconsts package. This is a commonly used
    constant especially for clients submitting messages. They shouldn't need
    to import the entire app package just to use this constant. By doing
    this we also remove a duplicate bondDenom that was created to avoid
    import cycles

--- test/util/blobfactory/payforblob_factory.go
+++ test/util/blobfactory/payforblob_factory.go
@@ -99,7 +99,7 @@ func RandBlobTxsRandomlySized(enc sdk.TxEncoder, rand *tmrand.Rand, count, maxSi
 	}
 
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 
@@ -155,7 +155,7 @@ func RandBlobTxsWithAccounts(
 	accounts []string,
 ) []coretypes.Tx {
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 
@@ -229,7 +229,7 @@ func RandBlobTxs(enc sdk.TxEncoder, rand *tmrand.Rand, count, blobsPerTx, size i
 	}
 
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 
@@ -358,7 +358,7 @@ func MultiBlobTx(
 	require.NoError(t, err)
 
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 	opts := []blobtypes.TxBuilderOption{
@@ -399,7 +399,7 @@ func IndexWrappedTxWithInvalidNamespace(
 	require.NoError(t, err)
 
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 	opts := []blobtypes.TxBuilderOption{
@@ -441,7 +441,7 @@ func RandBlobTxsWithNamespacesAndSigner(
 	}
 
 	coin := sdk.Coin{
-		Denom:  bondDenom,
+		Denom:  appconsts.BondDenom,
 		Amount: sdk.NewInt(10),
 	}
 
@@ -480,7 +480,7 @@ func ComplexBlobTxWithOtherMsgs(t *testing.T, rand *tmrand.Rand, kr keyring.Keyr
 	pfb, blobs := RandMsgPayForBlobsWithSigner(rand, signerAddr.String(), 100, 1)
 
 	opts := []blobtypes.TxBuilderOption{
-		blobtypes.SetFeeAmount(sdk.NewCoins(sdk.NewCoin(bondDenom, sdk.NewInt(10)))),
+		blobtypes.SetFeeAmount(sdk.NewCoins(sdk.NewCoin(appconsts.BondDenom, sdk.NewInt(10)))),
 		blobtypes.SetGasLimit(100000000000000),
 	}
 
