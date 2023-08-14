176a587dd17ea72c18a40afd56fd53fcfc48d497
Mojtaba <mojtaba-esk@users.noreply.github.com>
2023-08-14 16:24:09 +0200

    test: genesis state tests for vesting accounts (#2004)
    closes #1806 
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    Overview
    
    This PR is proposing a test suit to test the vesting accounts where
    introduced at genesis state. It tests the scenarios that we are intend
    to use for our mainnet.
    
    **Note:** THIS PR is a copy of the PR #1993 as having the refactors of
    the main rebased into it to have easier review.
    
    The suggested changes are applied to the tests.
    Thank you @cmwaters and @evan-forbes and special thanks to @rootulp for
    his detailed points and for his great proposal to make the tests more
    fine grains and more special.
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    ## Checklist
    
    <!-- 
    Please complete the checklist to ensure that the PR is ready to be
    reviewed.
    
    IMPORTANT:
    PRs should be left in Draft until the below checklist is completed.
    -->
    
    - [ ] New and updated code has appropriate documentation
    - [ ] New and updated code has new and/or updated testing
    - [ ] Required CI checks are passing
    - [ ] Visual proof for any user facing features like CLI or
    documentation updates
    - [ ] Linked issues closed with keywords
    
    ---------
    
    Co-authored-by: Rootul P <rootulp@gmail.com>
    Co-authored-by: CHAMI Rachid <chamirachid1@gmail.com>

--- test/util/testfactory/utils.go
+++ test/util/testfactory/utils.go
@@ -11,15 +11,18 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 	rpctypes "github.com/tendermint/tendermint/rpc/core/types"
+	"google.golang.org/grpc"
 )
 
 const (
 	// nolint:lll
-	TestAccName  = "test-account"
-	TestAccMnemo = `ramp soldier connect gadget domain mutual staff unusual first midnight iron good deputy wage vehicle mutual spike unlock rocket delay hundred script tumble choose`
-	bondDenom    = "utia"
+	TestAccName               = "test-account"
+	TestAccMnemo              = `ramp soldier connect gadget domain mutual staff unusual first midnight iron good deputy wage vehicle mutual spike unlock rocket delay hundred script tumble choose`
+	bondDenom                 = "utia"
+	BaseAccountDefaultBalance = int64(10000)
 )
 
 func QueryWithoutProof(clientCtx client.Context, hashHexStr string) (*rpctypes.ResultTx, error) {
@@ -101,4 +104,71 @@ func GenerateAccounts(count int) []string {
 		accs[i] = tmrand.Str(20)
 	}
 	return accs
+}
+
+// NewBaseAccount creates a new base account.
+// If an empty string is passed as a name, a random one will be generated and used.
+//
+// It takes a keyring and a name as its parameters.
+// It returns a BaseAccount and a slice of sdk Coins with the default bond denom.
+func NewBaseAccount(kr keyring.Keyring, name string) (*authtypes.BaseAccount, sdk.Coins) {
+	if name == "" {
+		name = tmrand.Str(6)
+	}
+	rec, _, err := kr.NewMnemonic(name, keyring.English, "", "", hd.Secp256k1)
+	if err != nil {
+		panic(err)
+	}
+	addr, err := rec.GetAddress()
+	if err != nil {
+		panic(err)
+	}
+	origCoins := sdk.Coins{sdk.NewInt64Coin(bondDenom, BaseAccountDefaultBalance)}
+	bacc := authtypes.NewBaseAccountWithAddress(addr)
+	return bacc, origCoins
+}
+
+func GetValidators(grpcConn *grpc.ClientConn) (stakingtypes.Validators, error) {
+	scli := stakingtypes.NewQueryClient(grpcConn)
+	vres, err := scli.Validators(context.Background(), &stakingtypes.QueryValidatorsRequest{})
+	if err != nil {
+		return stakingtypes.Validators{}, err
+	}
+	return vres.Validators, nil
+}
+
+func GetAccountDelegations(grpcConn *grpc.ClientConn, address string) (stakingtypes.DelegationResponses, error) {
+	cli := stakingtypes.NewQueryClient(grpcConn)
+	res, err := cli.DelegatorDelegations(context.Background(),
+		&stakingtypes.QueryDelegatorDelegationsRequest{DelegatorAddr: address})
+	if err != nil {
+		return nil, err
+	}
+
+	return res.DelegationResponses, nil
+}
+
+func GetAccountSpendableBalance(grpcConn *grpc.ClientConn, address string) (balances sdk.Coins, err error) {
+	cli := banktypes.NewQueryClient(grpcConn)
+	res, err := cli.SpendableBalances(
+		context.Background(),
+		&banktypes.QuerySpendableBalancesRequest{
+			Address: address,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return res.GetBalances(), nil
+}
+
+func GetRawAccountInfo(grpcConn *grpc.ClientConn, address string) ([]byte, error) {
+	cli := authtypes.NewQueryClient(grpcConn)
+	res, err := cli.Account(context.Background(), &authtypes.QueryAccountRequest{
+		Address: address,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res.Account.Value, nil
 }
