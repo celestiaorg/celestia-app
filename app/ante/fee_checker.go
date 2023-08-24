c806365191f905fac29459bf76fd7e34dc612d81
Evan Forbes <42654277+evan-forbes@users.noreply.github.com>
2023-08-24 08:06:38 -0500

    Update app/ante/fee_checker.go
    Co-authored-by: Callum Waters <cmwaters19@gmail.com>

--- app/ante/fee_checker.go
+++ app/ante/fee_checker.go
@@ -52,13 +52,13 @@ func checkTxFeeWithValidatorMinGasPrices(ctx sdk.Context, tx sdk.Tx) (sdk.Coins,
 func getTxPriority(fee sdk.Coins, gas int64) int64 {
 	var priority int64
 	for _, c := range fee {
-		p := int64(math.MaxInt64)
-		gasPrice := c.Amount.Mul(sdk.NewInt(1000000)).QuoRaw(gas)
-		if gasPrice.IsInt64() {
-			p = gasPrice.Int64()
+		p := c.Amount.Mul(sdk.NewInt(1000000)).QuoRaw(gas)
+		if !p.IsInt64() {
+			continue
 		}
-		if priority == 0 || p < priority {
-			priority = p
+		// take the lowest priority as the tx priority
+		if priority == 0 || p.Int64() < priority {
+			priority = p.Int64()
 		}
 	}
 
