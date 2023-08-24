0b8c2adbfce99f444c9f036688f707157d761e79
evan-forbes <evan.samuel.forbes@gmail.com>
2023-08-24 07:22:02 -0500

    chore: add extra test case

--- app/ante/fee_checker_test.go
+++ app/ante/fee_checker_test.go
@@ -21,16 +21,22 @@ func TestGetTxPriority(t *testing.T) {
 			expectedPri: 1000000,
 		},
 		{
-			name:        "1 utia gas small gas",
+			name:        "1 utia fee small gas",
 			fee:         sdk.NewCoins(sdk.NewInt64Coin("utia", 1)),
 			gas:         1,
 			expectedPri: 1000000,
 		},
 		{
-			name:        "2 utia gas price small gas",
+			name:        "2 utia fee small gas",
 			fee:         sdk.NewCoins(sdk.NewInt64Coin("utia", 2)),
 			gas:         1,
 			expectedPri: 2000000,
+		},
+		{
+			name:        "1_000_000 TIA fee normal gas tx",
+			fee:         sdk.NewCoins(sdk.NewInt64Coin("utia", 1_000_000_000_000)),
+			gas:         75000,
+			expectedPri: 13333333333333,
 		},
 	}
 
