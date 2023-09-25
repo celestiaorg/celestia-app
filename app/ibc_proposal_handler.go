f7cc6c94a286b874493b109d4b98bc2bc06f8d7b
Callum Waters <cmwaters19@gmail.com>
2023-09-25 15:11:57 +0200

    feat!: disable ibc upgrade proposal handler

---
+++ app/ibc_proposal_handler.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+
+	"github.com/cosmos/ibc-go/v6/modules/core/02-client/keeper"
+	"github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
+)
+
+// NewClientProposalHandler defines the 02-client proposal handler. It disables the
+// UpgradeProposalType. Handling of updating the IBC Client will be done in v2 of the
+// app.
+func NewClientProposalHandler(k keeper.Keeper) govtypes.Handler {
+	return func(ctx sdk.Context, content govtypes.Content) error {
+		switch c := content.(type) {
+		case *types.ClientUpdateProposal:
+			return k.ClientUpdateProposal(ctx, c)
+		case *types.UpgradeProposal:
+			return errors.Wrap(sdkerrors.ErrInvalidRequest, "ibc upgrade proposal not supported")
+
+		default:
+			return errors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized ibc proposal content type: %T", c)
+		}
+	}
+}
