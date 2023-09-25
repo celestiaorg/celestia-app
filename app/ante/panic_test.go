825be70f67c108adb2264d75000e0e968adf47a1
Callum Waters <cmwaters19@gmail.com>
2023-09-25 11:12:50 +0200

    fix backport

--- app/ante/panic_test.go
+++ app/ante/panic_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/app/ante"
 	"github.com/celestiaorg/celestia-app/app/encoding"
-	"github.com/celestiaorg/celestia-app/test/util/testnode"
+	"github.com/celestiaorg/celestia-app/test/util/testfactory"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
@@ -19,7 +19,7 @@ func TestPanicHandlerDecorator(t *testing.T) {
 	ctx := sdk.Context{}
 	encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
 	builder := encCfg.TxConfig.NewTxBuilder()
-	err := builder.SetMsgs(banktypes.NewMsgSend(testnode.RandomAddress().(sdk.AccAddress), testnode.RandomAddress().(sdk.AccAddress), sdk.NewCoins(sdk.NewInt64Coin(app.BondDenom, 10))))
+	err := builder.SetMsgs(banktypes.NewMsgSend(testfactory.RandomAddress().(sdk.AccAddress), testfactory.RandomAddress().(sdk.AccAddress), sdk.NewCoins(sdk.NewInt64Coin(app.BondDenom, 10))))
 	require.NoError(t, err)
 	tx := builder.GetTx()
 	defer func() {
