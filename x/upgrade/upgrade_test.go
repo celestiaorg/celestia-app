390112b5f87f1f4085412ba41cf292572a27a5ca
Callum Waters <cmwaters19@gmail.com>
2023-10-05 16:17:43 +0200

    add more tests

--- x/upgrade/upgrade_test.go
+++ x/upgrade/upgrade_test.go
@@ -7,6 +7,10 @@ import (
 
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/app/encoding"
+	"github.com/celestiaorg/celestia-app/pkg/appconsts"
+	"github.com/celestiaorg/celestia-app/pkg/da"
+	"github.com/celestiaorg/celestia-app/pkg/shares"
+	"github.com/celestiaorg/celestia-app/pkg/square"
 	"github.com/celestiaorg/celestia-app/test/util"
 	"github.com/celestiaorg/celestia-app/x/upgrade"
 	"github.com/stretchr/testify/require"
@@ -17,20 +21,152 @@ import (
 )
 
 func TestUpgradeAppVersion(t *testing.T) {
+	testApp := setupTestApp(t, upgrade.NewSchedule(upgrade.NewPlan(3, 5, 2)))
+
+	upgradeTx, err := upgrade.NewMsgVersionChange(testApp.GetTxConfig(), 3)
+	require.NoError(t, err)
+	respCheckTx := testApp.CheckTx(abci.RequestCheckTx{Tx: upgradeTx})
+	// we expect that a new msg version change should always be rejected
+	// by checkTx
+	require.EqualValues(t, 15, respCheckTx.Code, respCheckTx.Log)
+
+	resp := testApp.PrepareProposal(abci.RequestPrepareProposal{
+		Height:        2,
+		ChainId:       testApp.GetChainID(),
+		BlockData:     &tmproto.Data{},
+		BlockDataSize: 1e6,
+	})
+
+	// At the height before the first height in the upgrade plan, the
+	// node should prepend a signal upgrade message.
+	require.Len(t, resp.BlockData.Txs, 1)
+	tx, err := testApp.GetTxConfig().TxDecoder()(resp.BlockData.Txs[0])
+	require.NoError(t, err)
+	require.Len(t, tx.GetMsgs(), 1)
+	msg, ok := tx.GetMsgs()[0].(*upgrade.MsgVersionChange)
+	require.True(t, ok)
+	require.EqualValues(t, 2, msg.Version)
+
+	{
+		// the same thing should happen if we run prepare proposal
+		// at height 4 as it is within the range
+		resp := testApp.PrepareProposal(abci.RequestPrepareProposal{
+			Height:        4,
+			ChainId:       testApp.GetChainID(),
+			BlockData:     &tmproto.Data{},
+			BlockDataSize: 1e6,
+		})
+
+		require.Len(t, resp.BlockData.Txs, 1)
+		tx, err := testApp.GetTxConfig().TxDecoder()(resp.BlockData.Txs[0])
+		require.NoError(t, err)
+		require.Len(t, tx.GetMsgs(), 1)
+		msg, ok := tx.GetMsgs()[0].(*upgrade.MsgVersionChange)
+		require.True(t, ok)
+		require.EqualValues(t, 2, msg.Version)
+	}
+
+	{
+		// Height 5 however is outside the range and thus the upgrade
+		// message should not be prepended
+		resp := testApp.PrepareProposal(abci.RequestPrepareProposal{
+			Height:        5,
+			ChainId:       testApp.GetChainID(),
+			BlockData:     &tmproto.Data{},
+			BlockDataSize: 1e6,
+		})
+		require.Len(t, resp.BlockData.Txs, 0)
+	}
+
+	// We should accept this proposal as valid
+	processProposalResp := testApp.ProcessProposal(abci.RequestProcessProposal{
+		Header: tmproto.Header{
+			Height:   2,
+			DataHash: resp.BlockData.Hash,
+		},
+		BlockData: resp.BlockData,
+	})
+	require.True(t, processProposalResp.IsOK())
+
+	{
+		// to assert that the upgrade tx must be the first tx
+		// we insert a tx before the upgrade tx. To get the hash
+		// we need to build the square and data availability header
+		txs := [][]byte{[]byte("hello world"), upgradeTx}
+		dataSquare, txs, err := square.Build(txs, appconsts.LatestVersion, appconsts.DefaultGovMaxSquareSize)
+		require.NoError(t, err)
+		eds, err := da.ExtendShares(shares.ToBytes(dataSquare))
+		require.NoError(t, err)
+		dah, err := da.NewDataAvailabilityHeader(eds)
+		require.NoError(t, err)
+		blockData := &tmproto.Data{
+			Txs:        txs,
+			SquareSize: uint64(dataSquare.Size()),
+			Hash:       dah.Hash(),
+		}
+
+		processProposalResp := testApp.ProcessProposal(abci.RequestProcessProposal{
+			Header: tmproto.Header{
+				Height:   2,
+				DataHash: blockData.Hash,
+			},
+			BlockData: blockData,
+		})
+		require.True(t, processProposalResp.IsRejected())
+	}
+
+	testApp.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{Height: 2}})
+	respDeliverTx := testApp.DeliverTx(abci.RequestDeliverTx{Tx: resp.BlockData.Txs[0]})
+	require.EqualValues(t, 0, respDeliverTx.Code, respDeliverTx.Log)
+	// app version should not have changed yet
+	require.EqualValues(t, 1, testApp.AppVersion())
+	respEndBlock := testApp.EndBlock(abci.RequestEndBlock{Height: 2})
+	// now the app version changes
+	require.EqualValues(t, 2, respEndBlock.ConsensusParamUpdates.Version.AppVersion)
+	require.EqualValues(t, 2, testApp.AppVersion())
+
+	_ = testApp.Commit()
+
+	// If another node proposes a block with a version change that is
+	// not supported by the nodes own state machine then the node
+	// rejects the proposed block
+	respProcessProposal := testApp.ProcessProposal(abci.RequestProcessProposal{
+		Header: tmproto.Header{
+			Height: 3,
+		},
+		BlockData: &tmproto.Data{
+			Txs: [][]byte{upgradeTx},
+		},
+	})
+	require.True(t, respProcessProposal.IsRejected())
+
+	// if we ask the application to prepare another proposal
+	// it will not add the upgrade signal message even though
+	// its within the range of the plan because the application
+	// has already upgraded to that height
+	respPrepareProposal := testApp.PrepareProposal(abci.RequestPrepareProposal{
+		Height:        3,
+		ChainId:       testApp.GetChainID(),
+		BlockData:     &tmproto.Data{},
+		BlockDataSize: 1e6,
+	})
+	require.Len(t, respPrepareProposal.BlockData.Txs, 0)
+}
+
+func setupTestApp(t *testing.T, schedule upgrade.Schedule) *app.App {
+	t.Helper()
+
 	db := dbm.NewMemDB()
 	chainID := "test_chain"
 	encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
 	upgradeSchedule := make(map[string]upgrade.Schedule)
-	upgradeSchedule[chainID] = upgrade.NewSchedule(upgrade.NewPlan(3, 5, 2))
+	upgradeSchedule[chainID] = schedule
 	testApp := app.New(log.NewNopLogger(), db, nil, true, 0, encCfg, upgradeSchedule, util.EmptyAppOptions{})
 
 	genesisState, _, _ := util.GenesisStateWithSingleValidator(testApp)
 
 	stateBytes, err := json.MarshalIndent(genesisState, "", " ")
-	if err != nil {
-		panic(err)
-	}
-
+	require.NoError(t, err)
 	require.EqualValues(t, 0, testApp.GetBaseApp().AppVersion())
 
 	cp := app.DefaultConsensusParams()
@@ -58,69 +194,5 @@ func TestUpgradeAppVersion(t *testing.T) {
 	require.EqualValues(t, app.DefaultConsensusParams().Version.AppVersion, testApp.GetBaseApp().AppVersion())
 
 	_ = testApp.Commit()
-
-	resp := testApp.PrepareProposal(abci.RequestPrepareProposal{
-		Height:        2,
-		ChainId:       chainID,
-		BlockData:     &tmproto.Data{},
-		BlockDataSize: 1e6,
-	})
-
-	// At the height before the first height in the upgrade plan, the
-	// node should prepend a signal upgrade message.
-	require.Len(t, resp.BlockData.Txs, 1)
-	tx, err := encCfg.TxConfig.TxDecoder()(resp.BlockData.Txs[0])
-	require.NoError(t, err)
-	require.Len(t, tx.GetMsgs(), 1)
-	msg, ok := tx.GetMsgs()[0].(*upgrade.MsgVersionChange)
-	require.True(t, ok)
-	require.EqualValues(t, 2, msg.Version)
-
-	processProposalResp := testApp.ProcessProposal(abci.RequestProcessProposal{
-		Header: tmproto.Header{
-			Height:   2,
-			DataHash: resp.BlockData.Hash,
-		},
-		BlockData: resp.BlockData,
-	})
-	require.True(t, processProposalResp.IsOK())
-
-	testApp.BeginBlock(abci.RequestBeginBlock{Header: tmproto.Header{Height: 2}})
-	respDeliverTx := testApp.DeliverTx(abci.RequestDeliverTx{Tx: resp.BlockData.Txs[0]})
-	require.EqualValues(t, 0, respDeliverTx.Code, respDeliverTx.Log)
-	// app version should not have changed yet
-	require.EqualValues(t, 1, testApp.AppVersion())
-	respEndBlock := testApp.EndBlock(abci.RequestEndBlock{Height: 2})
-	// now the app version changes
-	require.EqualValues(t, 2, respEndBlock.ConsensusParamUpdates.Version.AppVersion)
-	require.EqualValues(t, 2, testApp.AppVersion())
-
-	_ = testApp.Commit()
-
-	// If another node proposes a block with a version change that is
-	// not supported by the nodes own state machine then the node
-	// rejects the proposed block
-	upgradeTx, err := upgrade.NewMsgVersionChange(encCfg.TxConfig, 3)
-	require.NoError(t, err)
-	respProcessProposal := testApp.ProcessProposal(abci.RequestProcessProposal{
-		Header: tmproto.Header{
-			Height: 3,
-		},
-		BlockData: &tmproto.Data{
-			Txs: [][]byte{upgradeTx},
-		},
-	})
-	require.True(t, respProcessProposal.IsRejected())
-
-	// if we ask the application to prepare another proposal
-	// it will not add the upgrade signal message even though
-	// its within the range of the plan because the application
-	// has already upgraded to that height
-	respPrepareProposal := testApp.PrepareProposal(abci.RequestPrepareProposal{
-		Height:        3,
-		ChainId:       chainID,
-		BlockData:     &tmproto.Data{},
-		BlockDataSize: 1e6,
-	})
-	require.Len(t, respPrepareProposal.BlockData.Txs, 0)
+	return testApp
 }
