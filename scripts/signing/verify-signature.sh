374a97e83bb1b855a076987927ed3a9fdc46fd57
Rootul Patel <rootulp@gmail.com>
2023-10-05 18:04:05 -0400

    debug: test signing locally

--- scripts/signing/verify-signature.sh
+++ scripts/signing/verify-signature.sh
@@ -2,7 +2,7 @@
 
 KEYS_DIR="./keys"
 
-echo "Importing the public keys in $KEYS_DIR..."
+echo "Importing the public keys in $KEYS_DIR"
 
 # Loop over all keys in the keys directory
 for key in "$KEYS_DIR"/*; do
@@ -14,5 +14,13 @@ for key in "$KEYS_DIR"/*; do
     fi
 done
 
-echo "Verifying the signature of "$1" with "$2"..."
+# Check if the number of arguments is not 2
+if [[ $# -ne 2 ]]; then
+    echo "Error: Exactly two arguments are required."
+    echo "Example usage:"
+    echo "  ./verify-signature.sh <signature-file> <file-to-verify>"
+    exit 1
+fi
+
+echo "Verifying the signature of "$1" with "$2""
 gpg --verify $1 $2
