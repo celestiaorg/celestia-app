8dc374e144817b3de3c23867d5a9ded04aa8e3c7
Callum Waters <cmwaters19@gmail.com>
2023-10-23 12:22:59 +0200

    checkpoint

--- node/node.go
+++ node/node.go
@@ -3,18 +3,25 @@ package node
 import (
 	"context"
 
+	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/rsmt2d"
+	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
+	tmconfig "github.com/tendermint/tendermint/config"
 	"github.com/tendermint/tendermint/types"
 )
 
 type PublishFn func(context.Context, *types.Header, *types.Commit, *types.ValidatorSet, *rsmt2d.ExtendedDataSquare) error
 
 type Node struct {
-	
+	appConfig       *serverconfig.Config
+	consensusConfig *tmconfig.Config
 }
 
-func Init(dir string) *Node {
-	return &Node{}
+func Init(dir string) error {
+	cmd := genutilcli.InitCmd(app.ModuleBasics, dir)
+	cmd.SetArgs([]string{"node"})
+	return cmd.Execute()
 }
 
 func Load(dir string) *Node {
@@ -24,5 +31,3 @@ func Load(dir string) *Node {
 func (n *Node) Run(ctx context.Context, publishFn PublishFn) error {
 	return nil
 }
-
-func (n *Node) Save() error
