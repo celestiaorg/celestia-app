4090022f303ea2cf1bfff9de3a140874e20fde0f
Rootul Patel <rootulp@gmail.com>
2023-10-27 15:59:30 -0400

    refactor: table driven test

--- test/ledger/ledger_test.go
+++ test/ledger/ledger_test.go
@@ -11,44 +11,48 @@ import (
 )
 
 func TestLedgerSupport(t *testing.T) {
-	t.Run("build with Ledger support", func(t *testing.T) {
-		// Generate the binary with Ledger support
-		cmd := exec.Command("go", "build", "-tags", "ledger", "-o", "celestia-appd", "../../cmd/celestia-appd")
-		err := cmd.Run()
-		require.NoError(t, err)
+	type testCase struct {
+		name   string
+		ledger bool
+		want   string
+	}
 
-		// Clean up the binary
-		// defer os.Remove("celestia-appd")
+	testCases := []testCase{
+		{
+			name:   "ledger support enabled",
+			ledger: true,
+			want:   "Error: failed to generate ledger key: failed to retrieve device: ledger nano S: LedgerHID device (idx 0) not found. Ledger LOCKED OR Other Program/Web Browser may have control of device.\n",
+		},
+		{
+			name:   "ledger support disabled",
+			ledger: false,
+			want:   "Error: failed to generate ledger key: failed to retrieve device: ledger nano S: support for ledger devices is not available in this executable\n",
+		},
+	}
 
-		// Run the binary
-		cmd = exec.Command("./celestia-appd", "keys", "add", "test-key-name", "--ledger")
-		var out bytes.Buffer
-		cmd.Stderr = &out
-		err = cmd.Run()
-		require.Error(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var cmd *exec.Cmd
+			if tc.ledger {
+				// Generate the binary with Ledger support
+				cmd = exec.Command("go", "build", "-tags", "ledger", "-o", "celestia-appd", "../../cmd/celestia-appd")
+			} else {
+				// Generate the binary without Ledger support
+				cmd = exec.Command("go", "build", "-o", "celestia-appd", "../../cmd/celestia-appd")
+			}
+			err := cmd.Run()
+			require.NoError(t, err)
 
-		// Verify the output contains an error message
-		want := "Error: failed to generate ledger key: failed to retrieve device: ledger nano S: LedgerHID device (idx 0) not found. Ledger LOCKED OR Other Program/Web Browser may have control of device.\n"
-		assert.Equal(t, want, out.String())
-	})
-	t.Run("build without Ledger support", func(t *testing.T) {
-		// Generate the binary without Ledger support
-		cmd := exec.Command("go", "build", "-o", "celestia-appd", "../../cmd/celestia-appd")
-		err := cmd.Run()
-		require.NoError(t, err)
+			// Clean up the binary
+			defer os.Remove("celestia-appd")
 
-		// Clean up the binary
-		defer os.Remove("celestia-appd")
-
-		// Run the binary
-		cmd = exec.Command("./celestia-appd", "keys", "add", "test-key-name", "--ledger")
-		var out bytes.Buffer
-		cmd.Stderr = &out
-		err = cmd.Run()
-		require.Error(t, err)
-
-		// Verify the output contains an error message
-		want := "Error: failed to generate ledger key: failed to retrieve device: ledger nano S: support for ledger devices is not available in this executable\n"
-		assert.Equal(t, want, out.String())
-	})
+			// Run the binary
+			cmd = exec.Command("./celestia-appd", "keys", "add", "test-key-name", "--ledger")
+			var out bytes.Buffer
+			cmd.Stderr = &out
+			err = cmd.Run()
+			require.Error(t, err)
+			assert.Equal(t, tc.want, out.String())
+		})
+	}
 }
