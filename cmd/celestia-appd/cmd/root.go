c105756fce75a5558e1ea51c0c655f6848f98df7
Rootul P <rootulp@gmail.com>
2023-11-01 20:55:25 +0100

    feat: backport addrbook command (backport #2623 and #2783) (#2796)
    Backports https://github.com/celestiaorg/celestia-app/pull/2623 and
    https://github.com/celestiaorg/celestia-app/pull/2783
    
    ## Testing
    
    ```
    $ ./build/celestia-appd addrbook ~/git/rootulp/celestia/networks/celestia/peers.txt ~/Downloads/addrbook.json
    Error adding b751ffe713e3e5d80e8f45c3bd5e640ee9a91cb9@10.129.127.95:26656: Cannot add non-routable address b751ffe713e3e5d80e8f45c3bd5e640ee9a91cb9@10.129.127.95:26656
    Converted /Users/rootulp/git/rootulp/celestia/networks/celestia/peers.txt into /Users/rootulp/Downloads/addrbook.json
    
    $ head -n 10  /Users/rootulp/Downloads/addrbook.json
    {
    	"key": "8957397890a66cd0f716cbc9",
    	"addrs": [
    		{
    			"addr": {
    				"id": "a41b4bc451b1b71d537aa1018226e08d7fa7e44e",
    				"ip": "5.255.77.44",
    				"port": 26656
    			},
    			"src": {
    ```

--- cmd/celestia-appd/cmd/root.go
+++ cmd/celestia-appd/cmd/root.go
@@ -128,6 +128,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig encoding.Config) {
 		debugCmd,
 		config.Cmd(),
 		commands.CompactGoLevelDBCmd,
+		addrbookCommand(),
 		downloadGenesisCommand(),
 	)
 
