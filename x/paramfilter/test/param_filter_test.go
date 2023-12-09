f966d2f5f3f3e67a89919e026c3c24ca3a17619a
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-12-09 04:01:19 -0500

    remove invalid param test

--- x/paramfilter/test/param_filter_test.go
+++ x/paramfilter/test/param_filter_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/celestiaorg/celestia-app/x/paramfilter"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/params/types/proposal"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/stretchr/testify/require"
 	tmlog "github.com/tendermint/tendermint/libs/log"
 	"github.com/tendermint/tendermint/proto/tendermint/types"
@@ -33,30 +32,6 @@ func TestParamFilter(t *testing.T) {
 		err := handler(ctx, p)
 		require.Error(t, err)
 		require.Contains(t, err.Error(), "parameter can not be modified")
-	}
-
-	// ensure that we are not filtering out valid proposals
-	validChange := proposal.NewParamChange(stakingtypes.ModuleName, string(stakingtypes.KeyMaxValidators), "1")
-	p := testProposal(validChange)
-	err := handler(ctx, p)
-	require.NoError(t, err)
-
-	ps := app.StakingKeeper.GetParams(ctx)
-	require.Equal(t, ps.MaxValidators, uint32(1))
-
-	// ensure that we're throwing out entire proposals if any of the changes are blocked
-	for _, p := range app.BlockedParams() {
-		// try to set the max validators to 2, unlike above this should fail
-		validChange := proposal.NewParamChange(stakingtypes.ModuleName, string(stakingtypes.KeyMaxValidators), "2")
-		invalidChange := proposal.NewParamChange(p[0], p[1], "value")
-		p := testProposal(validChange, invalidChange)
-		err := handler(ctx, p)
-		require.Error(t, err)
-		require.Contains(t, err.Error(), "parameter can not be modified")
-
-		ps := app.StakingKeeper.GetParams(ctx)
-		require.Equal(t, ps.MaxValidators, uint32(1))
-
 	}
 }
 
