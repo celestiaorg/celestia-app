7c0adde6d04cacf36f42193b4d4f6905f3309387
Rootul Patel <rootulp@gmail.com>
2023-12-12 23:23:57 -0500

    test: put database in distinct dir

--- test/util/testnode/full_node.go
+++ test/util/testnode/full_node.go
@@ -19,7 +19,9 @@ import (
 // already initialized and saved to the baseDir.
 func NewCometNode(baseDir string, cfg *UniversalTestingConfig) (*node.Node, srvtypes.Application, error) {
 	logger := newLogger(cfg)
-	dbPath := filepath.Join(cfg.TmConfig.RootDir, "data")
+	// Put the database in a new directory called "database" instead of "data"
+	// because "data" will be managed by CometBFT.
+	dbPath := filepath.Join(cfg.TmConfig.RootDir, "database")
 	db, err := dbm.NewGoLevelDB("application", dbPath)
 	if err != nil {
 		return nil, nil, err
