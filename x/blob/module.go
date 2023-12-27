d750e4227a0e822ee45991d60a7451d0bc3dc9a3
Rootul P <rootulp@gmail.com>
2023-12-26 20:14:54 -0500

    refactor: remove `RegisterCodec` (#2961)
    Closes https://github.com/celestiaorg/celestia-app/issues/2953

--- x/blob/module.go
+++ x/blob/module.go
@@ -44,10 +44,6 @@ func (AppModuleBasic) Name() string {
 	return types.ModuleName
 }
 
-func (AppModuleBasic) RegisterCodec(cdc *codec.LegacyAmino) {
-	types.RegisterLegacyAminoCodec(cdc)
-}
-
 func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	types.RegisterLegacyAminoCodec(cdc)
 }
