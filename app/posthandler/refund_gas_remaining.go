7ad46276c13b6f3a9cb740c366b122741e944661
Rootul Patel <rootulp@gmail.com>
2023-12-27 16:14:42 -0500

    refactor: use short receiver name

--- app/posthandler/refund_gas_remaining.go
+++ app/posthandler/refund_gas_remaining.go
@@ -44,16 +44,16 @@ func NewRefundGasRemainingDecorator(ak authkeeper.AccountKeeper, bk types.BankKe
 
 // AnteHandle implements the cosmos-sdk AnteHandler interface. Note: the
 // AnteHandler interface is also used for post-handlers.
-func (rgrd RefundGasRemainingDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+func (d RefundGasRemainingDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	// TODO: How to handle the simulate flag?
-	if err := rgrd.maybeRefund(ctx, tx, simulate); err != nil {
+	if err := d.maybeRefund(ctx, tx, simulate); err != nil {
 		return ctx, err
 	}
 	return next(ctx, tx, simulate)
 }
 
 // maybeRefund conditionally refunds a portion of the tx fee to the fee payer.
-func (rgrd RefundGasRemainingDecorator) maybeRefund(ctx sdk.Context, tx sdk.Tx, simulate bool) error {
+func (d RefundGasRemainingDecorator) maybeRefund(ctx sdk.Context, tx sdk.Tx, simulate bool) error {
 	// TODO: How to handle the simulate flag?
 
 	// Replace the context's gas meter with an infinite gas meter so that this
@@ -76,8 +76,30 @@ func (rgrd RefundGasRemainingDecorator) maybeRefund(ctx sdk.Context, tx sdk.Tx, 
 	coinsToRefund := getCoinsToRefund(gasMeter, feeTx)
 	recipient := getRefundRecipient(feeTx)
 
-	if err := rgrd.processRefund(ctx, coinsToRefund, recipient); err != nil {
+	if err := d.processRefund(ctx, coinsToRefund, recipient); err != nil {
 		return err
+	}
+
+	return nil
+}
+
+// processRefund sends amountToRefund from the fee collector module account to the recipient.
+func (d RefundGasRemainingDecorator) processRefund(ctx sdk.Context, amountToRefund sdk.Coins, recipient sdk.AccAddress) error {
+	from := d.accountKeeper.GetModuleAddress(types.FeeCollectorName)
+	if from == nil {
+		return fmt.Errorf("fee collector module account (%s) has not been set", types.FeeCollectorName)
+	}
+
+	if recipientAccount := d.accountKeeper.GetAccount(ctx, recipient); recipientAccount == nil {
+		return errors.Wrapf(sdkerrors.ErrUnknownAddress, "recipient address: %s does not exist", recipientAccount)
+	}
+
+	if !amountToRefund.IsValid() {
+		return fmt.Errorf("invalid amount to refund: %s", amountToRefund)
+	}
+
+	if err := d.bankKeeper.SendCoins(ctx, from, recipient, amountToRefund); err != nil {
+		return errors.Wrapf(err, "error refunding %s from fee collector module account to %s", amountToRefund, recipient)
 	}
 
 	return nil
@@ -91,28 +113,6 @@ func getCoinsToRefund(gasMeter sdk.GasMeter, feeTx sdk.FeeTx) sdk.Coins {
 	amountToRefund := minimum(toRefund, maxToRefund)
 	coinsToRefund := sdk.NewCoins(sdk.NewCoin(appconsts.BondDenom, amountToRefund))
 	return coinsToRefund
-}
-
-// processRefund sends amountToRefund from the fee collector module account to the recipient.
-func (frd RefundGasRemainingDecorator) processRefund(ctx sdk.Context, amountToRefund sdk.Coins, recipient sdk.AccAddress) error {
-	from := frd.accountKeeper.GetModuleAddress(types.FeeCollectorName)
-	if from == nil {
-		return fmt.Errorf("fee collector module account (%s) has not been set", types.FeeCollectorName)
-	}
-
-	if recipientAccount := frd.accountKeeper.GetAccount(ctx, recipient); recipientAccount == nil {
-		return errors.Wrapf(sdkerrors.ErrUnknownAddress, "recipient address: %s does not exist", recipientAccount)
-	}
-
-	if !amountToRefund.IsValid() {
-		return fmt.Errorf("invalid amount to refund: %s", amountToRefund)
-	}
-
-	if err := frd.bankKeeper.SendCoins(ctx, from, recipient, amountToRefund); err != nil {
-		return errors.Wrapf(err, "error refunding %s from fee collector module account to %s", amountToRefund, recipient)
-	}
-
-	return nil
 }
 
 // getRefundRecipient returns the address that should receive the refund.
