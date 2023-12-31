d8ea95e95f42152b7fe577e29219b6fda52aab0f
fahimahmedx <f92ahmed@uwaterloo.ca>
2023-12-31 02:40:12 -0500

    add auth, blob, blobstream, and consensus governance param tests

--- x/paramfilter/test/governance_params_test.go
+++ x/paramfilter/test/governance_params_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/celestiaorg/celestia-app/app"
 	"github.com/celestiaorg/celestia-app/x/paramfilter"
@@ -11,6 +12,7 @@ import (
 
 	testutil "github.com/celestiaorg/celestia-app/test/util"
 	blobtypes "github.com/celestiaorg/celestia-app/x/blob/types"
+	bsmoduletypes "github.com/celestiaorg/celestia-app/x/blobstream/types"
 	minttypes "github.com/celestiaorg/celestia-app/x/mint/types"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -53,6 +55,32 @@ func (suite *TestSuite) TestModifiableParameters() {
 		subTest  func()
 	}{
 		{
+			"auth.MaxMemoCharacters",
+			testProposal(proposal.ParamChange{
+				Subspace: authtypes.ModuleName,
+				Key:      string(authtypes.KeyMaxMemoCharacters),
+				Value:    `"2"`,
+			}),
+			func() {
+				gotMaxMemoCharacters := suite.app.AccountKeeper.GetParams(suite.ctx).MaxMemoCharacters
+				wantMaxMemoCharacters := uint64(2)
+				suite.Require().Equal(wantMaxMemoCharacters, gotMaxMemoCharacters)
+			},
+		},
+		{
+			"auth.SigVerifyCostED25519",
+			testProposal(proposal.ParamChange{
+				Subspace: authtypes.ModuleName,
+				Key:      string(authtypes.KeySigVerifyCostED25519),
+				Value:    `"2"`,
+			}),
+			func() {
+				gotSigVerifyCostED25519 := suite.app.AccountKeeper.GetParams(suite.ctx).SigVerifyCostED25519
+				wantSigVerifyCostED25519 := uint64(2)
+				suite.Require().Equal(wantSigVerifyCostED25519, gotSigVerifyCostED25519)
+			},
+		},
+		{
 			"auth.SigVerifyCostSecp256k1",
 			testProposal(proposal.ParamChange{
 				Subspace: authtypes.ModuleName,
@@ -63,6 +91,19 @@ func (suite *TestSuite) TestModifiableParameters() {
 				gotSigVerifyCostSecp256k1 := suite.app.AccountKeeper.GetParams(suite.ctx).SigVerifyCostSecp256k1
 				wantSigVerifyCostSecp256k1 := uint64(2)
 				suite.Require().Equal(wantSigVerifyCostSecp256k1, gotSigVerifyCostSecp256k1)
+			},
+		},
+		{
+			"auth.TxSigLimit",
+			testProposal(proposal.ParamChange{
+				Subspace: authtypes.ModuleName,
+				Key:      string(authtypes.KeyTxSigLimit),
+				Value:    `"2"`,
+			}),
+			func() {
+				gotTxSigLimit := suite.app.AccountKeeper.GetParams(suite.ctx).TxSigLimit
+				wantTxSigLimit := uint64(2)
+				suite.Require().Equal(wantTxSigLimit, gotTxSigLimit)
 			},
 		},
 		{
@@ -94,18 +135,81 @@ func (suite *TestSuite) TestModifiableParameters() {
 			},
 		},
 		{
-			"consensus.block.MaxGas",
+			"blob.GovMaxSquareSize",
+			testProposal(proposal.ParamChange{
+				Subspace: blobtypes.ModuleName,
+				Key:      string(blobtypes.KeyGovMaxSquareSize),
+				Value:    `"2"`,
+			}),
+			func() {
+				gotGovMaxSquareSize := suite.app.BlobKeeper.GetParams(suite.ctx).GovMaxSquareSize
+				wantGovMaxSquareSize := uint64(2)
+				suite.Require().Equal(
+					wantGovMaxSquareSize,
+					gotGovMaxSquareSize)
+			},
+		},
+		{
+			"blobstream.DataCommitmentWindow",
+			testProposal(proposal.ParamChange{
+				Subspace: bsmoduletypes.ModuleName,
+				Key:      string(bsmoduletypes.ParamsStoreKeyDataCommitmentWindow),
+				Value:    `"100"`,
+			}),
+			func() {
+				gotDataCommitmentWindow := suite.app.BlobstreamKeeper.GetParams(suite.ctx).DataCommitmentWindow
+				wantDataCommitmentWindow := uint64(100)
+				suite.Require().Equal(
+					wantDataCommitmentWindow,
+					gotDataCommitmentWindow)
+			},
+		},
+		{
+			"consensus.block",
 			testProposal(proposal.ParamChange{
 				Subspace: baseapp.Paramspace,
 				Key:      string(baseapp.ParamStoreKeyBlockParams),
 				Value:    `{"max_bytes": "1", "max_gas": "2"}`,
 			}),
 			func() {
+				gotMaxBytes := suite.app.BaseApp.GetConsensusParams(suite.ctx).Block.MaxBytes
+				wantMaxBytes := int64(1)
+				suite.Require().Equal(
+					wantMaxBytes,
+					gotMaxBytes)
+
 				gotMaxGas := suite.app.BaseApp.GetConsensusParams(suite.ctx).Block.MaxGas
 				wantMaxGas := int64(2)
 				suite.Require().Equal(
 					wantMaxGas,
 					gotMaxGas)
+			},
+		},
+		{
+			"consensus.evidence",
+			testProposal(proposal.ParamChange{
+				Subspace: baseapp.Paramspace,
+				Key:      string(baseapp.ParamStoreKeyEvidenceParams),
+				Value:    `{"max_age_duration": "1", "max_age_num_blocks": "2", "max_bytes": "3"}`,
+			}),
+			func() {
+				gotMaxAgeDuration := suite.app.BaseApp.GetConsensusParams(suite.ctx).Evidence.MaxAgeDuration
+				wantMaxAgeDuration := time.Duration(1)
+				suite.Require().Equal(
+					wantMaxAgeDuration,
+					gotMaxAgeDuration)
+
+				gotMaxAgeNumBlocks := suite.app.BaseApp.GetConsensusParams(suite.ctx).Evidence.MaxAgeNumBlocks
+				wantMaxAgeNumBlocks := int64(2)
+				suite.Require().Equal(
+					wantMaxAgeNumBlocks,
+					gotMaxAgeNumBlocks)
+
+				gotMaxBytes := suite.app.BaseApp.GetConsensusParams(suite.ctx).Evidence.MaxBytes
+				wantMaxBytes := int64(3)
+				suite.Require().Equal(
+					wantMaxBytes,
+					gotMaxBytes)
 			},
 		},
 	}
