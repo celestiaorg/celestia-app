c188ac0067673cbf1e66e783e045c7c3e69eb547
Håvard Anda Estensen <haavard.ae@gmail.com>
2024-01-04 21:47:55 +0100

    chore: unslice slice expressions

--- pkg/square/square.go
+++ pkg/square/square.go
@@ -262,7 +262,7 @@ func WriteSquare(
 	}
 
 	square := make([]shares.Share, totalShares)
-	copy(square[:], txShares)
+	copy(square, txShares)
 	copy(square[pfbStartIndex:], pfbShares)
 	if blobWriter.Count() > 0 {
 		copy(square[paddingStartIndex:], padding)
