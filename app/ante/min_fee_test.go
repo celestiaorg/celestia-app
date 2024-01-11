e7593702300d4b83eb8afacb153d881480c1f754
Nina Barbakadze <barbakadzeninaa@gmail.com>
2024-01-11 15:22:12 +0100

    feat: add version control

--- app/ante/min_fee_test.go
+++ app/ante/min_fee_test.go
@@ -8,13 +8,14 @@ import (
 	"github.com/celestiaorg/celestia-app/app/encoding"
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/test/util/testnode"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	version "github.com/tendermint/tendermint/proto/tendermint/version"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCheckTxFeeWithGlobalMinGasPrices(t *testing.T) {
-	// try and build a tx
 	encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
 
 	builder := encCfg.TxConfig.NewTxBuilder()
@@ -25,39 +26,57 @@ func TestCheckTxFeeWithGlobalMinGasPrices(t *testing.T) {
 	)
 	require.NoError(t, err)
 
-	feeAmount := int64(1000) // Set the desired fee amount here
+	feeAmount := int64(1000)
 
 	gasLimit := uint64(float64(feeAmount) / appconsts.GlobalMinGasPrice)
 	builder.SetGasLimit(gasLimit)
 
+	ctx := sdk.Context{}
+	
+
 	testCases := []struct {
 		name   string
 		fee    sdk.Coins
+		appVersion uint64
 		expErr bool
 	}{
 		{
 			name:   "bad tx; fee below required minimum",
 			fee:    sdk.NewCoins(sdk.NewInt64Coin(appconsts.BondDenom, feeAmount-1)),
+			appVersion: uint64(2),
 			expErr: true,
 		},
 		{
 			name:   "good tx; fee above required minimum",
 			fee:    sdk.NewCoins(sdk.NewInt64Coin(appconsts.BondDenom, feeAmount+1)),
+			appVersion: uint64(2),
 			expErr: false,
 		},
 		{
 			name:   "good tx; fee equal to required minimum",
 			fee:    sdk.NewCoins(sdk.NewInt64Coin(appconsts.BondDenom, feeAmount)),
+			appVersion: uint64(2),
+			expErr: false,
+		},
+		{
+			name:   "good tx; with no fee",
+			fee:    sdk.NewCoins(sdk.NewInt64Coin(appconsts.BondDenom, 0)),
+			appVersion: uint64(1),
 			expErr: false,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			// set the fee and gas before calling checktx
 			builder.SetFeeAmount(tc.fee)
 			tx := builder.GetTx()
-			_, _, err := ante.CheckTxFeeWithGlobalMinGasPrices(sdk.Context{}, tx)
+
+			ctx = ctx.WithBlockHeader(tmproto.Header{
+				Version: version.Consensus{
+					App: tc.appVersion,
+				},
+			})
+			_, _, err := ante.CheckTxFeeWithGlobalMinGasPrices(ctx, tx)
 			if tc.expErr {
 				require.Error(t, err)
 			} else {
