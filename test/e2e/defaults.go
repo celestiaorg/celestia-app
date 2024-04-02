2441b836d1aefb3b378337d5c4abf6dd58a5f464
sanaz <sanaztaheri@ieee.org>
2024-04-02 14:29:02 -0700

    adds some defaults for max resources for each instance type

--- test/e2e/defaults.go
+++ test/e2e/defaults.go
@@ -6,3 +6,17 @@ var defaultResources = Resources{
 	cpu:           "300m",
 	volume:        "1Gi",
 }
+
+var maxValidatorResources = Resources{
+	memoryRequest: "10Gi",
+	memoryLimit:   "12Gi",
+	cpu:           "6",
+	volume:        "1Gi",
+}
+
+var maxTxSimResources = Resources{
+	memoryRequest: "1Gi",
+	memoryLimit:   "1Gi",
+	cpu:           "2",
+	volume:        "1Gi",
+}
