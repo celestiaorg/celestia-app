e6527c05d88064cfe95aacee2e7ceffd9b9ab2ec
sanaz <sanaztaheri@ieee.org>
2024-04-02 14:29:15 -0700

    removes plotting utils

--- test/e2e/throughput_test.go
+++ test/e2e/throughput_test.go
@@ -2,21 +2,13 @@ package e2e
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"testing"
 	"time"
 
 	"github.com/celestiaorg/celestia-app/pkg/appconsts"
-	v1 "github.com/celestiaorg/celestia-app/pkg/appconsts/v1"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
-
 	"github.com/celestiaorg/celestia-app/test/util/testnode"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/types"
 )
 
 func TestE2EThroughput(t *testing.T) {
@@ -47,12 +39,6 @@ func TestE2EThroughput(t *testing.T) {
 	t.Cleanup(testnet.Cleanup)
 
 	// add 2 validators
-	//maxResources := Resources{
-	//	memoryRequest: "10Gi",
-	//	memoryLimit:   "12Gi",
-	//	cpu:           "6",
-	//	volume:        "1Gi",
-	//}
 	require.NoError(t, testnet.CreateGenesisNodes(2, latestVersion, 10000000,
 		0, defaultResources))
 
@@ -67,16 +53,10 @@ func TestE2EThroughput(t *testing.T) {
 
 	t.Log("Creating txsim nodes")
 	// create txsim nodes and point them to the validators
-	txsimVersion := "a954bc1" // old: "cee9cd4" // "65c1a8e" // TODO pull the
-	// latest version of txsim if possible
+	txsimVersion := "a954bc1"
 
-	err = testnet.CreateAndSetupTxSimNodes(txsimVersion, seed, 10,
-		"99000-99000", 3, Resources{
-			memoryRequest: "1Gi",
-			memoryLimit:   "1Gi",
-			cpu:           "2",
-			volume:        "1Gi",
-		},
+	err = testnet.CreateAndSetupTxSimNodes(txsimVersion, seed, 1,
+		"10000-10000", 3, defaultResources,
 		gRPCEndpoints[:], rPCEndPoints[:])
 	require.NoError(t, err)
 
@@ -98,79 +78,10 @@ func TestE2EThroughput(t *testing.T) {
 	blockchain, err := testnode.ReadBlockchain(context.Background(), testnet.Node(0).AddressRPC())
 	require.NoError(t, err)
 
-	blockTimes, blockSizes, thputs, blockTimesNano := throughput(blockchain)
-	t.Log("blockTimes", blockTimes)
-	t.Log("blockTimesNano", blockTimesNano)
-	t.Log("blockSizes", blockSizes)
-	t.Log("thputs", thputs)
-	plotData(blockSizes, fmt.Sprintf("blocksizes-%d.png", appconsts.DefaultGovMaxSquareSize),
-		"Block Size", "Height",
-		"Block Size")
-	plotData(blockTimes, fmt.Sprintf("blocktimes-%d.png",
-		appconsts.DefaultGovMaxSquareSize), "Block Time in seconds", "Height",
-		"Block Time in seconds")
-	plotData(thputs, fmt.Sprintf("throughputs-%d.png",
-		appconsts.DefaultGovMaxSquareSize), "Throughput",
-		"Height", "Throughput")
-
 	totalTxs := 0
 	for _, block := range blockchain {
-		require.Equal(t, v1.Version, block.Version.App)
+		require.Equal(t, appconsts.LatestVersion, block.Version.App)
 		totalTxs += len(block.Data.Txs)
 	}
 	require.Greater(t, totalTxs, 10)
-}
-
-func throughput(blockchain []*types.Block) ([]float64, []float64, []float64,
-	[]float64) {
-	blockTimes := make([]float64, 0, len(blockchain)-1)
-	blockTimesNano := make([]float64, 0, len(blockchain)-1)
-	blockSizes := make([]float64, 0, len(blockchain)-1)
-	throughputs := make([]float64, 0, len(blockchain)-1)
-	// timestamp of the last processed block
-	lastBlockTS := blockchain[0].Header.Time
-
-	for _, block := range blockchain[1:] {
-		blockTimeNano := float64(block.Header.Time.Sub(lastBlockTS))
-		blockTime := float64(block.Header.Time.Sub(lastBlockTS) / 1e9) // Convert time from nanoseconds to seconds
-		blockSize := float64(block.Size() / (1024))                    // Convert size from bytes to KiB
-		thput := blockSize / blockTime
-
-		blockTimesNano = append(blockTimesNano, blockTimeNano)
-		blockTimes = append(blockTimes, blockTime)
-		blockSizes = append(blockSizes, blockSize)
-		throughputs = append(throughputs, thput)
-
-		lastBlockTS = block.Header.Time // update lastBlockTS for the next block
-	}
-	return blockTimes, blockSizes, throughputs, blockTimesNano
-}
-
-func plotData(data []float64, fileName string, title, xLabel, yLabel string) {
-	if len(data) == 0 {
-		return
-	}
-	pts := make(plotter.XYs, len(data))
-	for i := range data {
-		pts[i].X = float64(i)
-		pts[i].Y = data[i]
-	}
-
-	p, err := plot.New()
-	if err != nil {
-		panic(err)
-	}
-	p.Title.Text = title
-	p.X.Label.Text = xLabel
-	p.Y.Label.Text = yLabel
-
-	err = plotutil.AddLinePoints(p, yLabel, pts)
-	if err != nil {
-		panic(err)
-	}
-
-	// save the plot
-	if err := p.Save(10*vg.Inch, 5*vg.Inch, fileName); err != nil {
-		panic(err)
-	}
 }
