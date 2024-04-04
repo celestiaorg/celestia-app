47be5f41c8787c6e65a59a0e2f21ddf059d345b1
Callum Waters <cmwaters19@gmail.com>
2024-04-04 13:48:23 +0200

    add fix

--- pkg/user/e2e_test.go
+++ pkg/user/e2e_test.go
@@ -71,36 +71,6 @@ func TestConcurrentTxSubmission(t *testing.T) {
 	}
 }
 
-func TestSignerTwins(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping in short mode")
-	}
-
-	// Setup network
-	tmConfig := testnode.DefaultTendermintConfig()
-	tmConfig.Consensus.TimeoutCommit = 10 * time.Second
-	ctx, _, _ := testnode.NewNetwork(t, testnode.DefaultConfig().WithTendermintConfig(tmConfig))
-	_, err := ctx.WaitForHeight(1)
-	require.NoError(t, err)
-
-	signer1, err := newSingleSignerFromContext(ctx)
-	require.NoError(t, err)
-	signer2, err := newSingleSignerFromContext(ctx)
-	require.NoError(t, err)
-
-	blobs := blobfactory.ManyRandBlobs(t, tmrand.NewRand(), blobfactory.Repeat(2048, 8)...)
-
-	_, err = signer1.SubmitPayForBlob(ctx.GoContext(), blobs[:1], user.SetGasLimitAndFee(500_000, appconsts.DefaultMinGasPrice))
-	require.NoError(t, err)
-
-	_, err = signer2.SubmitPayForBlob(ctx.GoContext(), blobs[1:3], user.SetGasLimitAndFee(500_000, appconsts.DefaultMinGasPrice))
-	require.NoError(t, err)
-
-	signer1.ForceSetSequence(4)
-	_, err = signer1.SubmitPayForBlob(ctx.GoContext(), blobs[3:5], user.SetGasLimitAndFee(500_000, appconsts.DefaultMinGasPrice))
-	require.NoError(t, err)
-}
-
 func newSingleSignerFromContext(ctx testnode.Context) (*user.Signer, error) {
 	encCfg := encoding.MakeConfig(app.ModuleEncodingRegisters...)
 	record, err := ctx.Keyring.Key("validator")
