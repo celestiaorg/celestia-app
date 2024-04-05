d231bdca4ba84b79dddfd146967be96515d9e176
sanaz <sanaztaheri@ieee.org>
2024-04-05 12:32:25 -0700

    reverts GovMaxSquareSize value

--- pkg/appconsts/initial_consts.go
+++ pkg/appconsts/initial_consts.go
@@ -7,7 +7,7 @@ import "time"
 const (
 	// DefaultGovMaxSquareSize is the default value for the governance modifiable
 	// max square size.
-	DefaultGovMaxSquareSize = 512
+	DefaultGovMaxSquareSize = 64
 
 	// DefaultMaxBytes is the default value for the governance modifiable
 	// maximum number of bytes allowed in a valid block.
