9813aa3ca4a7e4991cf25947ac81370b87d0e1ef
sanaz <sanaztaheri@ieee.org>
2024-04-05 14:10:11 -0700

    increases default max square size for all versions

--- pkg/appconsts/v2/app_consts.go
+++ pkg/appconsts/v2/app_consts.go
@@ -2,7 +2,7 @@ package v2
 
 const (
 	Version              uint64 = 2
-	SquareSizeUpperBound int    = 128
+	SquareSizeUpperBound int    = 512
 	SubtreeRootThreshold int    = 64
 	// GlobalMinGasPrice is used by x/minfee to prevent transactions from being
 	// included in a block if they specify a gas price lower than this
