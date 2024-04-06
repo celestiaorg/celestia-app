9bd8bdb7d6356c6b000e677344403d5f23e41849
evan-forbes <evan.samuel.forbes@gmail.com>
2024-04-06 16:24:41 -0500

    chore: bump version of core with config fix

--- test/testground/network/params.go
+++ test/testground/network/params.go
@@ -185,7 +185,7 @@ func StandardCometConfig(params *Params) *tmconfig.Config {
 	cmtcfg.Mempool.TTLDuration = 40 * time.Minute
 	cmtcfg.Mempool.MaxGossipDelay = 20 * time.Second
 	cmtcfg.Instrumentation.TraceType = "local"
-	cmtcfg.Instrumentation.TracePushConfig = "trace_config.json"
+	cmtcfg.Instrumentation.TracePushConfig = "s3.json"
 	cmtcfg.Instrumentation.TraceBufferSize = 1000
 	cmtcfg.Instrumentation.TracePushConfig = ""
 	return cmtcfg
