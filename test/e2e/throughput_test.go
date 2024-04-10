ec7f02d6cf2172d6cc5a811f07c0ecc0a6ff2a75
sanaz <sanaztaheri@ieee.org>
2024-04-10 09:39:47 -0700

    saves block data as scv

--- test/e2e/throughput_test.go
+++ test/e2e/throughput_test.go
@@ -4,8 +4,6 @@ import (
 	"bufio"
 	"context"
 	"encoding/csv"
-	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"testing"
@@ -15,10 +13,6 @@ import (
 	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
 	"github.com/stretchr/testify/require"
 	"github.com/tendermint/tendermint/types"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
 )
 
 func TestE2EThroughput(t *testing.T) {
@@ -114,31 +108,9 @@ func TestE2EThroughput(t *testing.T) {
 	blockchain, err := testnode.ReadBlockchain(context.Background(), testnet.Node(0).AddressRPC())
 	require.NoError(t, err)
 
-	blockTimes, blockSizes, thputs, blockTimesNano := throughput(blockchain)
-	t.Log("blockTimes", blockTimes)
-	t.Log("blockTimesNano", blockTimesNano)
-	t.Log("blockSizes", blockSizes)
-	t.Log("thputs", thputs)
-
-	// save the data to a CSV file
-	err = SaveFloatsToCSV(blockTimes, "blockTimes.csv")
-	require.NoError(t, err)
-	err = SaveFloatsToCSV(blockTimesNano, "blockTimesNano.csv")
-	require.NoError(t, err)
-	err = SaveFloatsToCSV(blockSizes, "blockSizes.csv")
-	require.NoError(t, err)
-	err = SaveFloatsToCSV(thputs, "throughputs.csv")
-	require.NoError(t, err)
-
-	plotData(blockSizes, fmt.Sprintf("blocksizes-%d.png", appconsts.DefaultGovMaxSquareSize),
-		"Block Size", "Height",
-		"Block Size")
-	plotData(blockTimes, fmt.Sprintf("blocktimes-%d.png",
-		appconsts.DefaultGovMaxSquareSize), "Block Time in seconds", "Height",
-		"Block Time in seconds")
-	plotData(thputs, fmt.Sprintf("throughputs-%d.png",
-		appconsts.DefaultGovMaxSquareSize), "Throughput",
-		"Height", "Throughput")
+	// save the blockchain data to a CSV file
+	blockData := readBlockData(blockchain)
+	err = SaveToCSV(blockData, "./results/blockData.csv")
 
 	totalTxs := 0
 	for _, block := range blockchain {
@@ -148,94 +120,54 @@ func TestE2EThroughput(t *testing.T) {
 	require.Greater(t, totalTxs, 10)
 }
 
-func throughput(blockchain []*types.Block) ([]float64, []float64, []float64,
-	[]float64) {
-	blockTimes := make([]float64, 0, len(blockchain)-1)
-	blockTimesNano := make([]float64, 0, len(blockchain)-1)
-	blockSizes := make([]float64, 0, len(blockchain)-1)
-	throughputs := make([]float64, 0, len(blockchain)-1)
-	// timestamp of the last processed block
-	lastBlockTS := blockchain[0].Header.Time
-
-	for _, block := range blockchain[1:] {
-		blockTimeNano := float64(block.Header.Time.Sub(lastBlockTS))
-		blockTime := float64(block.Header.Time.Sub(lastBlockTS) / 1e9) // Convert time from nanoseconds to seconds
-
-		blockTimesNano = append(blockTimesNano, blockTimeNano)
-		blockTimes = append(blockTimes, blockTime)
-
-		lastBlockTS = block.Header.Time // update lastBlockTS for the next block
+func readBlockData(blockchain []*types.Block) []BlockData {
+	blockData := make([]BlockData, 0, len(blockchain))
+	for _, block := range blockchain {
+		blockData = append(blockData, BlockData{
+			Time:   block.Header.Time,
+			Size:   float64(block.Size()),
+			Height: int(block.Height),
+		})
 	}
-	for i, block := range blockchain[:len(blockchain)-1] {
-		blockSize := float64(block.Size() / (1024)) // Convert size from bytes to KiB
-		thput := blockSize / blockTimes[i]
+	return blockData
 
-		blockSizes = append(blockSizes, blockSize)
-		throughputs = append(throughputs, thput)
-	}
-	return blockTimes, blockSizes, throughputs, blockTimesNano
 }
 
-func plotData(data []float64, fileName string, title, xLabel, yLabel string) {
-	if len(data) == 0 {
-		return
-	}
-	pts := make(plotter.XYs, len(data))
-	maxValue := math.Inf(-1) // Start with the smallest possible float64 value
-	for i := range data {
-		pts[i].X = float64(i)
-		pts[i].Y = data[i]
-		if data[i] > maxValue {
-			maxValue = data[i] // Update maxValue if the current value is greater
-		}
-	}
-
-	p, err := plot.New()
-	if err != nil {
-		panic(err)
-	}
-	p.Title.Text = title
-	p.X.Label.Text = xLabel
-	p.Y.Label.Text = yLabel
-
-	// Set the Max field of the Y-axis
-	p.Y.Max = maxValue
-
-	err = plotutil.AddLinePoints(p, yLabel, pts)
-	if err != nil {
-		panic(err)
-	}
-
-	// save the plot
-	if err := p.Save(10*vg.Inch, 5*vg.Inch, fileName); err != nil {
-		panic(err)
-	}
+type BlockData struct {
+	Time   time.Time // Use time.Time for the Time field
+	Size   float64   // in bytes
+	Height int
 }
 
-// SaveFloatsToCSV saves a slice of float values to a CSV file with the given filename.
-func SaveFloatsToCSV(floats []float64, fileName string) error {
-	// create or open the CSV file
-	file, err := os.Create(fileName)
+// Function to save slice of BlockData to CSV
+func SaveToCSV(blockData []BlockData, filePath string) error {
+	// Create a new file
+	file, err := os.Create(filePath)
 	if err != nil {
-		return fmt.Errorf("error creating file: %w", err)
+		return err
 	}
 	defer file.Close()
 
-	// create a CSV writer
+	// Create a CSV writer
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	// convert float values to strings and prepare for CSV writing
-	var record []string
-	for _, value := range floats {
-		// Convert each float value to a string
-		strValue := strconv.FormatFloat(value, 'f', -1, 64)
-		record = append(record, strValue)
+	// Write the header
+	header := []string{"Time", "Size", "Height"}
+	if err := writer.Write(header); err != nil {
+		return err
 	}
 
-	// write the record
-	if err := writer.Write(record); err != nil {
-		return fmt.Errorf("error writing record to csv: %w", err)
+	// Iterate over the slice and write each element as a row in the CSV
+	for _, data := range blockData {
+		row := []string{
+			data.Time.Format(time.RFC3339), // Format the time using RFC3339 standard
+			strconv.FormatFloat(data.Size, 'f', -1, 64),
+			strconv.Itoa(data.Height),
+		}
+		if err := writer.Write(row); err != nil {
+			return err
+		}
 	}
 
 	return nil
