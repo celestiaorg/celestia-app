40a5db5f874cb069dee0060e9dd25add8ade3602
Rootul Patel <rootulp@gmail.com>
2024-04-11 15:57:08 -0400

    doc: improve godoc

--- app/module/types.go
+++ app/module/types.go
@@ -7,9 +7,10 @@ import (
 
 type VersionedModule struct {
 	Module sdkmodule.AppModule
-	// fromVersion and toVersion indicate the continuous range of app versions that the particular
-	// module is part of. The range is inclusive. `fromVersion` should not be smaller than `toVersion`
-	// 0 is not a valid app version
+	// FromVersion and ToVersion indicate the continuous range of app versions
+	// that this particular module is part of. The range is inclusive.
+	// FromVersion should not be smaller than ToVersion. 0 is not a valid app
+	// version.
 	FromVersion, ToVersion uint64
 }
 
