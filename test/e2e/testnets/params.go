f00a92879fde1ff5d38f1efdc1429c9034241671
sanaz <sanaztaheri@ieee.org>
2024-04-22 14:10:50 -0400

    explains the purpose of each field in the testnet setting

--- test/e2e/testnets/params.go
+++ test/e2e/testnets/params.go
@@ -8,26 +8,32 @@ import (
 	"github.com/tendermint/tendermint/config"
 )
 
+// TestnetSetting defines the parameters for a testnet.
 type TestnetSetting struct {
 	ChainID           string
-	Validators        int
-	ValidatorResource Resources
-	SelfDelegation    int64
-	Version           string
-	FullNodes         int
-	BlobsPerSeq       int
-	BlobSequences     int
-	BlobSizes         int
-	PerPeerBandwidth  int64
-	UpgradeHeight     int64
-	TimeoutCommit     time.Duration
-	TimeoutPropose    time.Duration
-	Mempool           string
-	BroadcastTxs      bool
-	Prometheus        bool
-
-	GovMaxSquareSize int
-	MaxBlockBytes    int64
+	Validators        int       // Number of validators in the testnet
+	ValidatorResource Resources // Resource requirements for a validator node
+	SelfDelegation    int64     // Self-delegation amount for validators
+	Version           string    // Version of the app to use for validators
+	// tx client settings
+	BlobsPerSeq   int // Number of blobs per sequence
+	BlobSequences int // Number of blob sequences
+	BlobSizes     int // Size of blobs in bytes
+	// p2p configs
+	PerPeerBandwidth int64 // Bandwidth per peer in bytes per second
+	// consensus configs
+	TimeoutCommit  time.Duration
+	TimeoutPropose time.Duration
+	// mempool configs
+	Mempool      string // Mempool version
+	BroadcastTxs bool
+	// prometheus configs
+	Prometheus bool
+	// consensus params
+	MaxBlockBytes int64
+	// other configs
+	UpgradeHeight    int64 // Upgrade height
+	GovMaxSquareSize int64
 }
 
 func GetTestnetDefaultSetting() TestnetSetting {
@@ -42,16 +48,15 @@ func GetTestnetDefaultSetting() TestnetSetting {
 		BlobsPerSeq:       1,
 		BlobSequences:     1,
 		BlobSizes:         10 * 1024,
-		//GovMaxSquareSize:
-		PerPeerBandwidth: cfg.P2P.SendRate,
-		UpgradeHeight:    0,
-		TimeoutCommit:    cfg.Consensus.TimeoutCommit,
-		TimeoutPropose:   cfg.Consensus.TimeoutPropose,
-		Mempool:          cfg.Mempool.Version,
-		BroadcastTxs:     cfg.Mempool.Broadcast,
-		Prometheus:       cfg.Instrumentation.Prometheus,
-		GovMaxSquareSize: appconsts.DefaultGovMaxSquareSize,
-		MaxBlockBytes:    appParams.Block.MaxBytes,
+		PerPeerBandwidth:  cfg.P2P.SendRate,
+		UpgradeHeight:     0,
+		TimeoutCommit:     cfg.Consensus.TimeoutCommit,
+		TimeoutPropose:    cfg.Consensus.TimeoutPropose,
+		Mempool:           cfg.Mempool.Version,
+		BroadcastTxs:      cfg.Mempool.Broadcast,
+		Prometheus:        cfg.Instrumentation.Prometheus,
+		GovMaxSquareSize:  appconsts.DefaultGovMaxSquareSize,
+		MaxBlockBytes:     appParams.Block.MaxBytes,
 	}
 	return defaultParams
 }
