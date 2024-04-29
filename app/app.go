29c9ce154d9ecfc1a7a9b5a699e6a856723ff4ca
Rootul P <rootulp@gmail.com>
2024-04-29 17:15:49 +0400

    Update app/app.go

--- app/app.go
+++ app/app.go
@@ -512,7 +512,6 @@ func (app *App) Info(req abci.RequestInfo) abci.ResponseInfo {
 	}
 
 	resp := app.BaseApp.Info(req)
-	fmt.Println("info called", resp.AppVersion, app.IsSealed())
 	// mount the stores for the provided app version
 	if resp.AppVersion > 0 && !app.IsSealed() {
 		fmt.Println("mounting stored")
