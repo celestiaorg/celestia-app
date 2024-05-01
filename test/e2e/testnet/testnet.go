61b938dc875a6e138cdb9e860c4a5b7b9244d765
sanaz <sanaztaheri@ieee.org>
2024-05-01 15:45:03 -0700

    adds keyring after commit

--- test/e2e/testnet/testnet.go
+++ test/e2e/testnet/testnet.go
@@ -142,6 +142,14 @@ func (t *Testnet) CreateTxClient(name,
 		return err
 	}
 
+	err = txsim.Instance.Commit()
+	if err != nil {
+		log.Err(err).
+			Str("name", name).
+			Msg("error committing txsim")
+		return err
+	}
+
 	// copy over the keyring directory to the txsim instance
 	err = txsim.Instance.AddFolder(txsimKeyringDir, txsimRootDir, "10001:10001")
 	if err != nil {
@@ -149,13 +157,6 @@ func (t *Testnet) CreateTxClient(name,
 			Str("directory", txsimKeyringDir).
 			Str("name", name).
 			Msg("error adding keyring dir to txsim")
-		return err
-	}
-	err = txsim.Instance.Commit()
-	if err != nil {
-		log.Err(err).
-			Str("name", name).
-			Msg("error committing txsim")
 		return err
 	}
 
