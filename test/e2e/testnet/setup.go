f6e7ae19d8a03310f07819e3e49b19ee12cc8956
sanaz <sanaztaheri@ieee.org>
2024-05-07 13:36:13 -0700

    uses default app config

--- test/e2e/testnet/setup.go
+++ test/e2e/testnet/setup.go
@@ -17,8 +17,8 @@ import (
 )
 
 func MakeConfig(node *Node, opts ...Option) (*config.Config, error) {
-	cfg := config.DefaultConfig() //
-	//app.DefaultConsensusConfig() //
+	//cfg := config.DefaultConfig() //
+	cfg := app.DefaultConsensusConfig()
 	cfg.TxIndex.Indexer = "kv"
 	cfg.Mempool.MaxTxsBytes = 1024 * 1024 * 1024
 	cfg.Mempool.MaxTxBytes = 8 * 1024 * 1024
