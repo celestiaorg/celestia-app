0cce7c6034cb3e741c23a6fa00a02b188149c51c
Rootul Patel <rootulp@gmail.com>
2024-05-09 16:01:23 -0400

    fix: lint

--- test/e2e/check_upgrades.go
+++ test/e2e/check_upgrades.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"os/exec"
+	"slices"
 	"strings"
 	"time"
 
@@ -25,7 +26,7 @@ func MinorVersionCompatibility(logger *log.Logger) error {
 	testnet.NoError("failed to get versions", err)
 	versions1 := testnet.ParseVersions(versionStr).FilterMajor(v1.Version).FilterOutReleaseCandidates()
 	versions2 := testnet.ParseVersions(versionStr).FilterMajor(v2.Version) // include release candidates for v2 because there isn't an official release yet.
-	versions := append(versions1, versions2...)
+	versions := slices.Concat(versions1, versions2)
 
 	if len(versions) == 0 {
 		logger.Fatal("no versions to test")
