bc0e91c951ac9398a372a0a83f0e7fb8ea892681
sanaz <sanaztaheri@ieee.org>
2024-05-21 13:07:55 -0700

    adapts the new architecture using BenchmarkTest struct and methods

--- test/e2e/benchmark/benchmark.go
+++ test/e2e/benchmark/benchmark.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/celestiaorg/celestia-app/v2/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/v2/test/e2e/testnet"
+	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
+	"github.com/tendermint/tendermint/pkg/trace"
 )
 
 type BenchmarkTest struct {
@@ -47,13 +51,13 @@ func (b *BenchmarkTest) SetupNodes() error {
 	gRPCEndpoints, err := b.RemoteGRPCEndpoints()
 	testnet.NoError("failed to get validators GRPC endpoints", err)
 	log.Println("validators GRPC endpoints", gRPCEndpoints)
-
 	// create tx clients and point them to the validators
 	log.Println("Creating tx clients")
 
 	err = b.CreateTxClients(b.manifest.TxClientVersion,
 		b.manifest.BlobSequences,
 		b.manifest.BlobSizes,
+		b.manifest.BlobsPerSeq,
 		b.manifest.TxClientsResource, gRPCEndpoints)
 	testnet.NoError("failed to create tx clients", err)
 
@@ -63,7 +67,27 @@ func (b *BenchmarkTest) SetupNodes() error {
 		testnet.WithTimeoutPropose(b.manifest.TimeoutPropose),
 		testnet.WithTimeoutCommit(b.manifest.TimeoutCommit),
 		testnet.WithPrometheus(b.manifest.Prometheus),
+		testnet.WithLocalTracing(b.manifest.LocalTracingType),
 	))
+
+	if b.manifest.PushTrace {
+		log.Println("reading trace push config")
+		if pushConfig, err := trace.GetPushConfigFromEnv(); err == nil {
+			log.Print("Setting up trace push config")
+			for _, node := range b.Nodes() {
+				testnet.NoError("failed to set TRACE_PUSH_BUCKET_NAME",
+					node.Instance.SetEnvironmentVariable(trace.PushBucketName, pushConfig.BucketName))
+				testnet.NoError("failed to set TRACE_PUSH_REGION",
+					node.Instance.SetEnvironmentVariable(trace.PushRegion, pushConfig.Region))
+				testnet.NoError("failed to set TRACE_PUSH_ACCESS_KEY",
+					node.Instance.SetEnvironmentVariable(trace.PushAccessKey, pushConfig.AccessKey))
+				testnet.NoError("failed to set TRACE_PUSH_SECRET_KEY",
+					node.Instance.SetEnvironmentVariable(trace.PushKey, pushConfig.SecretKey))
+				testnet.NoError("failed to set TRACE_PUSH_DELAY",
+					node.Instance.SetEnvironmentVariable(trace.PushDelay, fmt.Sprintf("%d", pushConfig.PushDelay)))
+			}
+		}
+	}
 	return nil
 }
 
@@ -98,5 +122,48 @@ func (b *BenchmarkTest) Run() error {
 	// wait some time for the tx clients to submit transactions
 	time.Sleep(b.manifest.TestDuration)
 
+	return nil
+}
+
+func (b *BenchmarkTest) CheckResults() error {
+	log.Println("Checking results")
+
+	// If pulling traced data is enabled, pull the data and return an error if it fails
+	if true {
+		_, err := b.Node(0).PullRoundStateTraces()
+		return fmt.Errorf("failed to pull round state traces: %w", err)
+
+		_, err = b.Node(0).PullReceivedBytes()
+		return fmt.Errorf("failed to pull received bytes traces: %w", err)
+	}
+
+	// check if any tx has been submitted
+	log.Println("Reading blockchain")
+	blockchain, err := testnode.ReadBlockHeights(context.Background(),
+		b.Node(0).AddressRPC(), 1, 5)
+	if err != nil {
+		return fmt.Errorf("failed to read blockchain: %w", err)
+
+	}
+
+	totalTxs := 0
+	for _, block := range blockchain {
+		if appconsts.LatestVersion != block.Version.App {
+			return fmt.Errorf("expected app version %d, got %d", appconsts.LatestVersion, block.Version.App)
+		}
+		totalTxs += len(block.Data.Txs)
+	}
+	if totalTxs < 10 {
+		return fmt.Errorf("expected at least 10 transactions, got %d", totalTxs)
+	}
+
+	// save the blockchain headers to a CSV file
+	err = SaveToCSV(extractHeaders(blockchain),
+		fmt.Sprintf("./blockchain_%s.csv", b.manifest.TestnetName))
+	if err != nil {
+		log.Println("failed to save blockchain headers to a CSV file", err)
+	}
+
+	log.Println("--- PASS ✅: E2EThroughput")
 	return nil
 }
--- test/e2e/benchmark/throughput.go
+++ test/e2e/benchmark/throughput.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"time"
 
 	"github.com/celestiaorg/celestia-app/v2/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/v2/test/e2e/testnet"
-	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
-	"github.com/tendermint/tendermint/pkg/trace"
 )
 
 const (
@@ -55,108 +51,174 @@ var bigBlockManifest = Manifest{
 	PushTrace:          true,
 }
 
-func Run(manifest Manifest) error {
+//
+//func Run(manifest Manifest) error {
+//
+//	log.Printf("version:%s", manifest.CelestiaAppVersion)
+//	// create a new testnet
+//	testNet, err := testnet.New(manifest.TestnetName, seed,
+//		testnet.GetGrafanaInfoFromEnvVar(), manifest.ChainID,
+//		manifest.GetGenesisModifiers()...)
+//	testnet.NoError("failed to create testnet", err)
+//
+//	testNet.SetConsensusParams(manifest.GetConsensusParams())
+//
+//	defer func() {
+//		log.Print("Cleaning up testnet")
+//		benchTest.Cleanup()
+//	}()
+//
+//	testnet.NoError("failed to create genesis nodes",
+//		testNet.CreateGenesisNodes(manifest.Validators,
+//			manifest.CelestiaAppVersion, manifest.SelfDelegation,
+//			manifest.UpgradeHeight, manifest.ValidatorResource))
+//
+//	if manifest.PushTrace {
+//		log.Println("reading trace push config")
+//		if pushConfig, err := trace.GetPushConfigFromEnv(); err == nil {
+//			log.Print("Setting up trace push config")
+//			for _, node := range testNet.Nodes() {
+//				testnet.NoError("failed to set TRACE_PUSH_BUCKET_NAME",
+//					node.Instance.SetEnvironmentVariable(trace.PushBucketName, pushConfig.BucketName))
+//				testnet.NoError("failed to set TRACE_PUSH_REGION",
+//					node.Instance.SetEnvironmentVariable(trace.PushRegion, pushConfig.Region))
+//				testnet.NoError("failed to set TRACE_PUSH_ACCESS_KEY",
+//					node.Instance.SetEnvironmentVariable(trace.PushAccessKey, pushConfig.AccessKey))
+//				testnet.NoError("failed to set TRACE_PUSH_SECRET_KEY",
+//					node.Instance.SetEnvironmentVariable(trace.PushKey, pushConfig.SecretKey))
+//				testnet.NoError("failed to set TRACE_PUSH_DELAY",
+//					node.Instance.SetEnvironmentVariable(trace.PushDelay, fmt.Sprintf("%d", pushConfig.PushDelay)))
+//			}
+//		}
+//	}
+//
+//	// obtain the GRPC endpoints of the validators
+//	gRPCEndpoints, err := testNet.RemoteGRPCEndpoints()
+//	testnet.NoError("failed to get validators GRPC endpoints", err)
+//	log.Println("validators GRPC endpoints", gRPCEndpoints[:manifest.TxClients])
+//
+//	// create tx clients and point them to the validators
+//	log.Println("Creating tx clients")
+//
+//	err = testNet.CreateTxClients(manifest.TxClientVersion, manifest.BlobSequences,
+//		manifest.BlobSizes, manifest.BlobsPerSeq,
+//		manifest.TxClientsResource, gRPCEndpoints[:manifest.TxClients])
+//	testnet.NoError("failed to create tx clients", err)
+//
+//	// start the testnet
+//	log.Println("Setting up testnet")
+//	testnet.NoError("failed to setup testnet", testNet.Setup(
+//		testnet.WithPerPeerBandwidth(manifest.PerPeerBandwidth),
+//		testnet.WithTimeoutPropose(manifest.TimeoutPropose),
+//		testnet.WithTimeoutCommit(manifest.TimeoutCommit),
+//		testnet.WithPrometheus(manifest.Prometheus),
+//		testnet.WithLocalTracing(manifest.LocalTracingType),
+//	))
+//	log.Println("Starting testnet")
+//	testnet.NoError("failed to start testnet", testNet.Start())
+//
+//	// once the testnet is up, start the tx clients
+//	log.Println("Starting tx clients")
+//	testnet.NoError("failed to start tx clients", testNet.StartTxClients())
+//
+//	// wait some time for the tx clients to submit transactions
+//	time.Sleep(manifest.TestDuration)
+//
+//	// pull some traced tables from the nodes
+//	_, err = testNet.Node(0).PullRoundStateTraces()
+//	testnet.NoError("failed to pull round state traces", err)
+//
+//	_, err = testNet.Node(0).PullReceivedBytes()
+//	testnet.NoError("failed to pull received bytes traces", err)
+//
+//	log.Println("Reading blockchain")
+//	blockchain, err := testnode.ReadBlockHeights(context.Background(),
+//		testNet.Node(0).AddressRPC(), 1, 5)
+//	testnet.NoError("failed to read blockchain", err)
+//
+//	err = SaveToCSV(extractHeaders(blockchain),
+//		fmt.Sprintf("./blockchain_%s.csv", manifest.TestnetName))
+//	if err != nil {
+//		log.Println("failed to save blockchain headers to a CSV file", err)
+//	}
+//
+//	totalTxs := 0
+//	for _, block := range blockchain {
+//		if appconsts.LatestVersion != block.Version.App {
+//			return fmt.Errorf("expected app version %d, got %d", appconsts.LatestVersion, block.Version.App)
+//		}
+//		totalTxs += len(block.Data.Txs)
+//	}
+//	if totalTxs < 10 {
+//		return fmt.Errorf("expected at least 10 transactions, got %d", totalTxs)
+//	}
+//	return nil
+//}
+//func E2EThroughput() error {
+//	latestVersion, err := testnet.GetLatestVersion()
+//	testnet.NoError("failed to get latest version", err)
+//
+//	log.Println("=== RUN E2EThroughput", "version:", latestVersion)
+//
+//	manifest := Manifest{
+//		ChainID:            "test-e2e-throughput",
+//		Validators:         2,
+//		ValidatorResource:  testnet.DefaultResources,
+//		TxClientsResource:  testnet.DefaultResources,
+//		SelfDelegation:     10000000,
+//		CelestiaAppVersion: latestVersion,
+//		TxClientVersion:    testnet.TxsimVersion,
+//		EnableLatency:      true,
+//		LatencyParams:      LatencyParams{100, 10}, // in  milliseconds
+//		BlobsPerSeq:        1,
+//		BlobSequences:      1,
+//		BlobSizes:          "10000-10000",
+//		PerPeerBandwidth:   5 * 1024 * 1024,
+//		UpgradeHeight:      0,
+//		TimeoutCommit:      1 * time.Second,
+//		TimeoutPropose:     1 * time.Second,
+//		Mempool:            "v1",
+//		BroadcastTxs:       true,
+//		Prometheus:         true,
+//		GovMaxSquareSize:   appconsts.DefaultGovMaxSquareSize,
+//		MaxBlockBytes:      appconsts.DefaultMaxBytes,
+//		TestDuration:       30 * time.Second,
+//		TxClients:          2,
+//	}
+//
+//	benchTest, err := NewBenchmarkTest("E2EThroughput", &manifest)
+//	testnet.NoError("failed to create benchmark test", err)
+//
+//	defer func() {
+//		log.Print("Cleaning up testnet")
+//		benchTest.Cleanup()
+//	}()
+//
+//	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+//
+//	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+//
+//	// post test data collection and validation
+//	log.Println("Reading blockchain")
+//	blockchain, err := testnode.ReadBlockchain(context.Background(),
+//		benchTest.Node(0).AddressRPC())
+//	testnet.NoError("failed to read blockchain", err)
+//
+//	totalTxs := 0
+//	for _, block := range blockchain {
+//		if appconsts.LatestVersion != block.Version.App {
+//			return fmt.Errorf("expected app version %d, got %d", appconsts.LatestVersion, block.Version.App)
+//		}
+//		totalTxs += len(block.Data.Txs)
+//	}
+//	if totalTxs < 10 {
+//		return fmt.Errorf("expected at least 10 transactions, got %d", totalTxs)
+//	}
+//
+//	log.Println("--- PASS ✅: E2EThroughput")
+//	return nil
+//}
 
-	log.Printf("version:%s", manifest.CelestiaAppVersion)
-	// create a new testnet
-	testNet, err := testnet.New(manifest.TestnetName, seed,
-		testnet.GetGrafanaInfoFromEnvVar(), manifest.ChainID,
-		manifest.GetGenesisModifiers()...)
-	testnet.NoError("failed to create testnet", err)
-
-	testNet.SetConsensusParams(manifest.GetConsensusParams())
-
-	defer func() {
-		log.Print("Cleaning up testnet")
-		benchTest.Cleanup()
-	}()
-
-	testnet.NoError("failed to create genesis nodes",
-		testNet.CreateGenesisNodes(manifest.Validators,
-			manifest.CelestiaAppVersion, manifest.SelfDelegation,
-			manifest.UpgradeHeight, manifest.ValidatorResource))
-
-	if manifest.PushTrace {
-		log.Println("reading trace push config")
-		if pushConfig, err := trace.GetPushConfigFromEnv(); err == nil {
-			log.Print("Setting up trace push config")
-			for _, node := range testNet.Nodes() {
-				testnet.NoError("failed to set TRACE_PUSH_BUCKET_NAME",
-					node.Instance.SetEnvironmentVariable(trace.PushBucketName, pushConfig.BucketName))
-				testnet.NoError("failed to set TRACE_PUSH_REGION",
-					node.Instance.SetEnvironmentVariable(trace.PushRegion, pushConfig.Region))
-				testnet.NoError("failed to set TRACE_PUSH_ACCESS_KEY",
-					node.Instance.SetEnvironmentVariable(trace.PushAccessKey, pushConfig.AccessKey))
-				testnet.NoError("failed to set TRACE_PUSH_SECRET_KEY",
-					node.Instance.SetEnvironmentVariable(trace.PushKey, pushConfig.SecretKey))
-				testnet.NoError("failed to set TRACE_PUSH_DELAY",
-					node.Instance.SetEnvironmentVariable(trace.PushDelay, fmt.Sprintf("%d", pushConfig.PushDelay)))
-			}
-		}
-	}
-
-	// obtain the GRPC endpoints of the validators
-	gRPCEndpoints, err := testNet.RemoteGRPCEndpoints()
-	testnet.NoError("failed to get validators GRPC endpoints", err)
-	log.Println("validators GRPC endpoints", gRPCEndpoints[:manifest.TxClients])
-
-	// create tx clients and point them to the validators
-	log.Println("Creating tx clients")
-
-	err = testNet.CreateTxClients(manifest.TxClientVersion, manifest.BlobSequences,
-		manifest.BlobSizes, manifest.BlobsPerSeq,
-		manifest.TxClientsResource, gRPCEndpoints[:manifest.TxClients])
-	testnet.NoError("failed to create tx clients", err)
-
-	// start the testnet
-	log.Println("Setting up testnet")
-	testnet.NoError("failed to setup testnet", testNet.Setup(
-		testnet.WithPerPeerBandwidth(manifest.PerPeerBandwidth),
-		testnet.WithTimeoutPropose(manifest.TimeoutPropose),
-		testnet.WithTimeoutCommit(manifest.TimeoutCommit),
-		testnet.WithPrometheus(manifest.Prometheus),
-		testnet.WithLocalTracing(manifest.LocalTracingType),
-	))
-	log.Println("Starting testnet")
-	testnet.NoError("failed to start testnet", testNet.Start())
-
-	// once the testnet is up, start the tx clients
-	log.Println("Starting tx clients")
-	testnet.NoError("failed to start tx clients", testNet.StartTxClients())
-
-	// wait some time for the tx clients to submit transactions
-	time.Sleep(manifest.TestDuration)
-
-	// pull some traced tables from the nodes
-	_, err = testNet.Node(0).PullRoundStateTraces()
-	testnet.NoError("failed to pull round state traces", err)
-
-	_, err = testNet.Node(0).PullReceivedBytes()
-	testnet.NoError("failed to pull received bytes traces", err)
-
-	log.Println("Reading blockchain")
-	blockchain, err := testnode.ReadBlockHeights(context.Background(),
-		testNet.Node(0).AddressRPC(), 1, 5)
-	testnet.NoError("failed to read blockchain", err)
-
-	err = SaveToCSV(extractHeaders(blockchain),
-		fmt.Sprintf("./blockchain_%s.csv", manifest.TestnetName))
-	if err != nil {
-		log.Println("failed to save blockchain headers to a CSV file", err)
-	}
-
-	totalTxs := 0
-	for _, block := range blockchain {
-		if appconsts.LatestVersion != block.Version.App {
-			return fmt.Errorf("expected app version %d, got %d", appconsts.LatestVersion, block.Version.App)
-		}
-		totalTxs += len(block.Data.Txs)
-	}
-	if totalTxs < 10 {
-		return fmt.Errorf("expected at least 10 transactions, got %d", totalTxs)
-	}
-	return nil
-}
 func TwoNodeSimple(_ *log.Logger) error {
 	manifest := Manifest{
 		TestnetName:        "TwoNodeSimple",
@@ -185,7 +247,22 @@ func TwoNodeSimple(_ *log.Logger) error {
 		PushTrace:          false,
 	}
 
-	return Run(manifest)
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: E2EThroughput")
+	return nil
 }
 
 func TwoNodeBigBlock_8MiB(logger *log.Logger) error {
@@ -194,7 +271,21 @@ func TwoNodeBigBlock_8MiB(logger *log.Logger) error {
 	manifest.TestnetName = "TwoNodeBigBlock_8MiB"
 	manifest.ChainID = "two-node-big-block-8mib"
 	manifest.MaxBlockBytes = 8 * toMiB
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: TwoNodeBigBlock_8MiB")
+	return nil
 }
 
 func TwoNodeBigBlock_32MiB(logger *log.Logger) error {
@@ -203,7 +294,21 @@ func TwoNodeBigBlock_32MiB(logger *log.Logger) error {
 	manifest.TestnetName = "TwoNodeBigBlock_32MiB"
 	manifest.ChainID = "two-node-big-block-32mib"
 	manifest.MaxBlockBytes = 32 * toMiB
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: TwoNodeBigBlock_32MiB")
+	return nil
 }
 
 func TwoNodeBigBlock_64MiB(logger *log.Logger) error {
@@ -212,7 +317,21 @@ func TwoNodeBigBlock_64MiB(logger *log.Logger) error {
 	manifest.TestnetName = "TwoNodeBigBlock_64MiB"
 	manifest.ChainID = "two-node-big-block-64mib"
 	manifest.MaxBlockBytes = 64 * toMiB
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: TwoNodeBigBlock_64MiB")
+	return nil
 }
 
 func LargeNetwork_BigBlock_8MiB(logger *log.Logger) error {
@@ -224,7 +343,21 @@ func LargeNetwork_BigBlock_8MiB(logger *log.Logger) error {
 	manifest.Validators = 50
 	manifest.TxClients = 50
 	manifest.BlobsPerSeq = 20
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: LargeNetwork_BigBlock_8MiB")
+	return nil
 }
 
 func LargeNetwork_BigBlock_32MiB(logger *log.Logger) error {
@@ -236,7 +369,21 @@ func LargeNetwork_BigBlock_32MiB(logger *log.Logger) error {
 	manifest.Validators = 50
 	manifest.TxClients = 50
 	manifest.BlobsPerSeq = 20
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: LargeNetwork_BigBlock_32MiB")
+	return nil
 }
 
 func LargeNetwork_BigBlock_64MiB(logger *log.Logger) error {
@@ -248,5 +395,20 @@ func LargeNetwork_BigBlock_64MiB(logger *log.Logger) error {
 	manifest.Validators = 50
 	manifest.TxClients = 50
 	manifest.BlobsPerSeq = 20
-	return Run(manifest)
+
+	benchTest, err := NewBenchmarkTest("TwoNodeSimple", &manifest)
+	testnet.NoError("failed to create benchmark test", err)
+
+	defer func() {
+		log.Print("Cleaning up testnet")
+		benchTest.Cleanup()
+	}()
+
+	testnet.NoError("failed to setup nodes", benchTest.SetupNodes())
+	testnet.NoError("failed to run the benchmark test", benchTest.Run())
+	testnet.NoError("failed to check results", benchTest.CheckResults())
+
+	log.Println("--- PASS ✅: LargeNetwork_BigBlock_64MiB")
+	return nil
+
 }
