2df5bf382a3660f3ffe8c30a63d4aafabc4ba2f6
sanaz <sanaztaheri@ieee.org>
2024-05-30 11:18:12 -0700

    addresses first round of comments

--- test/e2e/testnet/node.go
+++ test/e2e/testnet/node.go
@@ -50,9 +50,9 @@ type Node struct {
 }
 
 // PullRoundStateTraces retrieves the round state traces from a node.
-// it will save it to the provided path.
+// It will save them to the provided path.
 func (n *Node) PullRoundStateTraces(path string) ([]trace.Event[schema.
-	RoundState], error,
+RoundState], error,
 ) {
 	addr := n.AddressTracing()
 	log.Info().Str("Address", addr).Msg("Pulling round state traces")
