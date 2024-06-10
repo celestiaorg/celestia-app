6ab994aa5aaa4ce87f079fab65563d2797163036
Rootul P <rootulp@gmail.com>
2024-06-10 17:24:59 -0600

    docs(x/signal): replace upgrade with signal in comments (#3547)
    Follow up to https://github.com/celestiaorg/celestia-app/pull/3290 which
    fixes a few comments that still say "upgrade" when they should say
    "signal".

--- x/signal/keeper.go
+++ x/signal/keeper.go
@@ -30,9 +30,8 @@ func Threshold(_ uint64) sdk.Dec {
 }
 
 type Keeper struct {
-	// storeKey uses the same key as the Cosmos SDK x/upgrade module so that
-	// existing IBC client state can safely be ported over without any
-	// migration.
+	// storeKey is key that is used to fetch the signal store from the multi
+	// store.
 	storeKey storetypes.StoreKey
 
 	// quorumVersion is the version that has received a quorum of validators
@@ -45,7 +44,7 @@ type Keeper struct {
 	stakingKeeper StakingKeeper
 }
 
-// NewKeeper returns an upgrade keeper.
+// NewKeeper returns a signal keeper.
 func NewKeeper(
 	storeKey storetypes.StoreKey,
 	stakingKeeper StakingKeeper,
