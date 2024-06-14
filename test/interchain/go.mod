a681bc0a4564c4bad58031aa18d4c7047a206bc9
Nina Barbakadze <barbakadzeninaa@gmail.com>
2024-06-14 16:20:48 +0200

    refactor: cleanup

--- test/interchain/go.mod
+++ test/interchain/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/strangelove-ventures/interchaintest/v6 v6.0.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.24.0
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
@@ -46,7 +47,6 @@ require (
 	github.com/cosmos/go-bip39 v1.0.0 // indirect
 	github.com/cosmos/gorocksdb v1.2.0 // indirect
 	github.com/cosmos/iavl v0.19.6 // indirect
-	github.com/cosmos/ibc-go/v6 v6.2.2 // indirect
 	github.com/cosmos/ledger-cosmos-go v0.13.2 // indirect
 	github.com/danieljoos/wincred v1.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
--- test/interchain/packet_forward_test.go
+++ test/interchain/packet_forward_test.go
@@ -9,6 +9,7 @@ import (
 
 	"cosmossdk.io/math"
 	"github.com/celestiaorg/celestia-app/test/interchain/chainspec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	transfertypes "github.com/cosmos/ibc-go/v6/modules/apps/transfer/types"
 	"github.com/strangelove-ventures/interchaintest/v6"
 	"github.com/strangelove-ventures/interchaintest/v6/chain/cosmos"
@@ -16,6 +17,7 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v6/testreporter"
 	"github.com/strangelove-ventures/interchaintest/v6/testutil"
 	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
 )
 
 type PacketMetadata struct {
@@ -32,6 +34,13 @@ type ForwardMetadata struct {
 	RefundSequence *uint64       `json:"refund_sequence,omitempty"`
 }
 
+type QueryCelestiaBalResponse struct {
+	Balances []struct {
+		Amount string `yaml:"amount"`
+		Denom  string `yaml:"denom"`
+	} `yaml:"balances"`
+}
+
 // TestPacketForwardMiddleware verifies that Packet Forward Middleware (PFM) works as expected by sending a transfer from chain A to chain C through chain B Celestia tht has .
 func TestPFM(t *testing.T) {
 	if testing.Short() {
@@ -44,10 +53,11 @@ func TestPFM(t *testing.T) {
 	cosmosHub2 := chainspec.GetCosmosHub2(t)
 	relayer := getRelayerFactory(t).Build(t, client, network)
 
-	// set up paths for the chains
+	// Set up paths for the chains
 	pathCosmosHubCelestia := fmt.Sprintf("%s%s", cosmosHub.Config().ChainID, celestia.Config().ChainID)
 	pathCelestiaCosmosHub2 := fmt.Sprintf("%s%s", celestia.Config().ChainID, cosmosHub2.Config().ChainID)
-
+    
+	// Create the interchain
 	interchain := interchaintest.NewInterchain().
 		AddChain(cosmosHub).
 		AddChain(celestia).
@@ -75,41 +85,41 @@ func TestPFM(t *testing.T) {
 	})
 	require.NoError(t, err)
 	t.Cleanup(func() { _ = interchain.Close() })
-
+ 
+	// Start the relayer
 	err = relayer.StartRelayer(ctx, reporter, pathCosmosHubCelestia, pathCelestiaCosmosHub2)
 	require.NoError(t, err)
 
 	err = testutil.WaitForBlocks(ctx, 2, celestia, cosmosHub)
 	require.NoError(t, err)
-
+   
+	// Get the connections
 	celestiaConnections, err := relayer.GetConnections(ctx, reporter, celestia.Config().ChainID)
 	require.NoError(t, err)
 	require.Len(t, celestiaConnections, 2)
 
 	cosmosHubConnections, err := relayer.GetConnections(ctx, reporter, cosmosHub.Config().ChainID)
 	require.NoError(t, err)
-	require.Len(t, cosmosHubConnections, 2) // 2 connections: the first is connection-0 and the second is connection-localhost.
+	require.Len(t, cosmosHubConnections, 2)
 
-	// TODO check what these two connections are
 	cosmosHub2Connections, err := relayer.GetConnections(ctx, reporter, cosmosHub2.Config().ChainID)
 	require.NoError(t, err)
-	fmt.Println(cosmosHub2Connections, "COSMOS CONNECTIONS 2")
-	require.Len(t, cosmosHub2Connections, 2) // 2 connections: the first is connection-0 and the second is connection-localhost.
+	require.Len(t, cosmosHub2Connections, 2)
 
-	// fund the users
+	// Fund the users
 	initialBalance := math.NewInt(10_000_000_000)
 	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), initialBalance, celestia, cosmosHub, cosmosHub2)
 	err = testutil.WaitForBlocks(ctx, 2, celestia, cosmosHub, cosmosHub2)
 	require.NoError(t, err)
 
-	// get the addresses of the users on the 3 chains
+	// Get the user addresses
 	celestiaUser, cosmosUser, cosmosUser2 := users[0], users[1], users[2]
 	celestiaAddr := celestiaUser.(*cosmos.CosmosWallet).FormattedAddressWithPrefix(celestia.Config().Bech32Prefix)
 	cosmosAddr := cosmosUser.(*cosmos.CosmosWallet).FormattedAddressWithPrefix(cosmosHub.Config().Bech32Prefix)
 	cosmosAddr2 := cosmosUser2.(*cosmos.CosmosWallet).FormattedAddressWithPrefix(cosmosHub2.Config().Bech32Prefix)
 	fmt.Printf("celestiaAddr: %s, cosmosAddr: %v, cosmosAddr2: %v\n", celestiaAddr, cosmosAddr, cosmosAddr2)
 
-	// create channels
+	// Create the channels
 	cosmosHubCelestiaChan, err := ibc.GetTransferChannel(ctx, relayer, reporter, cosmosHub.Config().ChainID, celestia.Config().ChainID)
 	require.NoError(t, err)
 	celestiaCosmosHub2Chan, err := ibc.GetTransferChannel(ctx, relayer, reporter, celestia.Config().ChainID, cosmosHub2.Config().ChainID)
@@ -118,20 +128,17 @@ func TestPFM(t *testing.T) {
 	transferAmount := math.NewInt(100_000)
 
 	firstHopDenom := transfertypes.GetPrefixedDenom(cosmosHubCelestiaChan.PortID, cosmosHubCelestiaChan.ChannelID, celestia.Config().Denom)
-	secondHopDenom := transfertypes.GetPrefixedDenom(celestiaCosmosHub2Chan.PortID, celestiaCosmosHub2Chan.ChannelID, firstHopDenom)
 
 	firstHopDenomTrace := transfertypes.ParseDenomTrace(firstHopDenom)
-	secondHopDenomTrace := transfertypes.ParseDenomTrace(secondHopDenom)
-	fmt.Println(secondHopDenomTrace, "SECOND HOP DENOM TRACE")
 
-	// first send some tia to cosmos
+	// Send some tia to cosmos
 	transfer := ibc.WalletAmount{
 		Address: cosmosAddr,
 		Denom:   celestia.Config().Denom,
 		Amount:  transferAmount,
 	}
 
-	// celestia should forward the packet to cosmos2
+	// Celestia should forward the packet to cosmos2
 	secondHopMetadata := &PacketMetadata{
 		Forward: &ForwardMetadata{
 			Receiver: cosmosAddr2,
@@ -143,7 +150,7 @@ func TestPFM(t *testing.T) {
 	require.NoError(t, err)
 	next := string(nextBz)
 
-	// send tia back to celestia for it to forward to cosmos2
+	// Send tia back to celestia for it to forward to cosmos2
 	firstHopMetadata := &PacketMetadata{
 		Forward: &ForwardMetadata{
 			Receiver: celestiaAddr,
@@ -152,21 +159,24 @@ func TestPFM(t *testing.T) {
 			Next:     &next,
 		},
 	}
-
+	// Marshal the metadata
 	memo, err := json.Marshal(firstHopMetadata)
 	require.NoError(t, err)
 
 	celHeight, err := celestia.Height(ctx)
 	require.NoError(t, err)
 
+	// Send the transfer
 	transferTx, err := celestia.SendIBCTransfer(ctx, cosmosHubCelestiaChan.ChannelID, celestiaUser.KeyName(), transfer, ibc.TransferOptions{Memo: string(memo)})
 	require.NoError(t, err)
 
+	// Wait for the packet to be acknowledged
 	_, err = testutil.PollForAck(ctx, celestia, celHeight, celHeight+30, transferTx.Packet)
 	require.NoError(t, err)
 	err = testutil.WaitForBlocks(ctx, 3, celestia, cosmosHub, cosmosHub2)
 	require.NoError(t, err)
 
+	// Query the celestia balance
 	queryCelestiaBal := []string{
 		celestia.Config().Bin, "query", "bank", "balances", celestiaAddr,
 		"--chain-id", celestia.Config().ChainID,
@@ -176,19 +186,26 @@ func TestPFM(t *testing.T) {
 	stdout, stderr, err := celestia.Exec(ctx, queryCelestiaBal, nil)
 	require.NoError(t, err)
 	require.Empty(t, stderr)
-	fmt.Println(string(stdout), "STDOUT")
-	t.Logf("stdout %v\n", string(stdout))
 
+	// Parse the query response
+	var response QueryCelestiaBalResponse
+	err = yaml.Unmarshal(stdout, &response)
+	require.NoError(t, err)
+
+	// Cosmos user balance should be 0
 	cosmosUserBalance, err := cosmosHub.GetBalance(ctx, cosmosAddr, firstHopDenomTrace.IBCDenom())
 	require.NoError(t, err)
 	require.True(t, cosmosUserBalance.Equal(math.NewInt(0)))
-	fmt.Println(cosmosUserBalance, "COSMOS BALANCE")
 
-	// factoring in the network fee on celestia
-	// assert celestia balance
-	// celestiaNetworkFee := 0.002
-	// require.True(t, celestiaBalance.Equal(initialBalance.Sub(transferAmount).Sub(math.NewInt(int64(celestiaNetworkFee)))))
-	
+	// Parse the gas prices from the celestia config
+	gasPrices, _ := sdk.ParseDecCoin(celestia.Config().GasPrices)
+
+	// Fee is calculated as (gas spent * gas prices)
+	fee := sdk.NewDecFromInt(math.NewInt(transferTx.GasSpent)).Mul(gasPrices.Amount)
+	celBalanceAfter := initialBalance.Sub(transferAmount).Sub(fee.TruncateInt())
+	require.Equal(t, response.Balances[0].Amount, celBalanceAfter.String())
+
+	// Cosmos 2 user balance should be the transfer amount
 	cosmosUser2Balance, err := cosmosHub2.GetBalance(ctx, cosmosAddr2, firstHopDenomTrace.IBCDenom())
 	require.NoError(t, err)
 	require.True(t, cosmosUser2Balance.Equal(transferAmount))
