a30c2feecaa6e7adbd183f917ddaf04aa0b35f43
sanaz <sanaztaheri@ieee.org>
2024-06-24 13:15:31 -0700

    runs one test at a time

--- test/e2e/benchmark/main.go
+++ test/e2e/benchmark/main.go
@@ -14,26 +14,20 @@ func main() {
 	}
 
 	// check the test name passed as an argument and run it
-	specificTestFound := false
-	for _, arg := range os.Args[1:] {
-		for _, test := range tests {
-			if test.Name == arg {
-				runTest(logger, test)
-				specificTestFound = true
-				break
-			}
+	if len(os.Args) < 2 {
+		logger.Println("Usage: go run ./test/e2e/benchmark <test_name>")
+		logger.Printf("Valid tests are: %s\n\n", getTestNames(tests))
+		return
+
+	}
+	testName := os.Args[1]
+	for _, test := range tests {
+		if test.Name == testName {
+			runTest(logger, test)
+			break
 		}
 	}
 
-	if !specificTestFound {
-		logger.Println("No particular test specified. Running all tests.")
-		logger.Println("go run ./test/e2e/benchmark <test_name> to run a specific test")
-		logger.Printf("Valid tests are: %s\n\n", getTestNames(tests))
-		// if no specific test is passed, run all tests
-		for _, test := range tests {
-			runTest(logger, test)
-		}
-	}
 }
 
 type TestFunc func(*log.Logger) error
