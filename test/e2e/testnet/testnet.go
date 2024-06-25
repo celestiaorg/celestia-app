95d89d9049396aa64a7a00e3741cb11e6a33d0f1
Smuu <18609909+Smuu@users.noreply.github.com>
2024-06-25 09:39:22 +0200

    fix: txsim commit
    Signed-off-by: Smuu <18609909+Smuu@users.noreply.github.com>

--- test/e2e/testnet/testnet.go
+++ test/e2e/testnet/testnet.go
@@ -151,14 +151,6 @@ func (t *Testnet) CreateTxClient(name,
 		return err
 	}
 
-	err = txsim.Instance.Commit()
-	if err != nil {
-		log.Err(err).
-			Str("name", name).
-			Msg("error committing txsim")
-		return err
-	}
-
 	// copy over the keyring directory to the txsim instance
 	err = txsim.Instance.AddFolder(txsimKeyringDir, txsimRootDir, "10001:10001")
 	if err != nil {
