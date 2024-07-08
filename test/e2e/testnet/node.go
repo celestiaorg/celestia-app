34cd4b34d99e27c5e8611abab6aaecc550499045
Sanaz Taheri <35961250+staheri14@users.noreply.github.com>
2024-07-08 11:51:56 -0700

    test: adding big block tests to the main branch (#3612)
    Part of #3557
    Closes https://github.com/celestiaorg/celestia-app/issues/3614
    Closes #3638 
    Closes https://github.com/celestiaorg/celestia-app/issues/3371

--- test/e2e/testnet/node.go
+++ test/e2e/testnet/node.go
@@ -63,6 +63,20 @@ func (n *Node) PullRoundStateTraces(path string) ([]trace.Event[schema.RoundStat
 	return nil, nil
 }
 
+// PullBlockSummaryTraces retrieves the block summary traces from a node.
+// It will save them to the provided path.
+func (n *Node) PullBlockSummaryTraces(path string) ([]trace.Event[schema.BlockSummary], error,
+) {
+	addr := n.AddressTracing()
+	log.Info().Str("Address", addr).Msg("Pulling block summary traces")
+
+	err := trace.GetTable(addr, schema.BlockSummary{}.Table(), path)
+	if err != nil {
+		return nil, fmt.Errorf("getting table: %w", err)
+	}
+	return nil, nil
+}
+
 // Resources defines the resource requirements for a Node.
 type Resources struct {
 	// MemoryRequest specifies the initial memory allocation for the Node.
