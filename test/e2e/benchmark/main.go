e4e53332a00fb07db34bf9e5b707af99390a4e6c
sanaz <sanaztaheri@ieee.org>
2024-07-09 10:20:12 -0700

    includes LargeNetworkBigBlock64MBLatency in the list of available tests

--- test/e2e/benchmark/main.go
+++ test/e2e/benchmark/main.go
@@ -18,6 +18,7 @@ func main() {
 		{"LargeNetworkBigBlock8MB", LargeNetworkBigBlock8MB},
 		{"LargeNetworkBigBlock32MB", LargeNetworkBigBlock32MB},
 		{"LargeNetworkBigBlock64MB", LargeNetworkBigBlock64MB},
+		{"LargeNetworkBigBlock64MBLatency", LargeNetworkBigBlock64MBLatency},
 	}
 
 	// check the test name passed as an argument and run it
