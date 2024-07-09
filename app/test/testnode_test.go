c31ac1400858d6fec9d56d4e87ea127bcc436477
Rootul Patel <rootulp@gmail.com>
2024-07-09 13:59:35 -0400

    test: testnode with custom chainID

--- app/test/testnode_test.go
+++ app/test/testnode_test.go
@@ -3,16 +3,28 @@ package app_test
 import (
 	"testing"
 
+	"github.com/celestiaorg/celestia-app/v2/test/util/genesis"
 	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
 )
 
 func Test_testnode(t *testing.T) {
-	t.Run("testnode can start a network with default config", func(t *testing.T) {
+	t.Run("testnode can start a network with default chain ID", func(t *testing.T) {
 		testnode.NewNetwork(t, testnode.DefaultConfig())
 	})
 	t.Run("testnode can start a network with a custom chain ID", func(t *testing.T) {
+		chainID := "custom-chain-id"
+
+		// Set the chain ID on genesis. If this isn't done, the default chain ID
+		// is used for the validator which results in a "signature verification
+		// failed" error.
+		genesis := genesis.NewDefaultGenesis().
+			WithChainID(chainID).
+			WithValidators(genesis.NewDefaultValidator(testnode.DefaultValidatorAccountName)).
+			WithConsensusParams(testnode.DefaultConsensusParams())
+
 		config := testnode.DefaultConfig()
-		config.Genesis.ChainID = "foo"
+		config.WithChainID(chainID)
+		config.WithGenesis(genesis)
 		testnode.NewNetwork(t, config)
 	})
 }
