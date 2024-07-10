7c671a2fed5616a3bacb7e3a5a7e895897e3a88e
Rootul Patel <rootulp@gmail.com>
2024-07-10 17:48:21 +0200

    refactor: clean up

--- app/test/testnode_test.go
+++ app/test/testnode_test.go
@@ -2,6 +2,7 @@ package app_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/celestiaorg/celestia-app/app"
@@ -18,27 +19,29 @@ func Test_testnode(t *testing.T) {
 		testnode.NewNetwork(t, testnode.DefaultConfig())
 	})
 	t.Run("testnode can start with a custom MinGasPrice", func(t *testing.T) {
-		want := "0.003utia"
+		wantMinGasPrice := float64(0.003)
 		appConfig := testnode.DefaultAppConfig()
-		appConfig.MinGasPrices = want
+		appConfig.MinGasPrices = fmt.Sprintf("%v%s", wantMinGasPrice, app.BondDenom)
 		config := testnode.DefaultConfig().WithAppConfig(appConfig)
 		cctx, _, _ := testnode.NewNetwork(t, config)
+
 		got, err := queryMinimumGasPrice(cctx.GoContext(), cctx.GRPCClient)
 		require.NoError(t, err)
-		assert.Equal(t, float64(0.003), got)
+		assert.Equal(t, wantMinGasPrice, got)
 	})
 }
 
 func queryMinimumGasPrice(ctx context.Context, grpcConn *grpc.ClientConn) (float64, error) {
-	cfgRsp, err := nodeservice.NewServiceClient(grpcConn).Config(ctx, &nodeservice.ConfigRequest{})
+	resp, err := nodeservice.NewServiceClient(grpcConn).Config(ctx, &nodeservice.ConfigRequest{})
 	if err != nil {
 		return 0, err
 	}
 
-	localMinCoins, err := sdktypes.ParseDecCoins(cfgRsp.MinimumGasPrice)
+	minGasCoins, err := sdktypes.ParseDecCoins(resp.MinimumGasPrice)
 	if err != nil {
 		return 0, err
 	}
-	localMinPrice := localMinCoins.AmountOf(app.BondDenom).MustFloat64()
-	return localMinPrice, nil
+
+	minGasPrice := minGasCoins.AmountOf(app.BondDenom).MustFloat64()
+	return minGasPrice, nil
 }
