2a7a9ff7d8ecab6dd82ebf36a66388a6493cdc5a
Rootul Patel <rootulp@gmail.com>
2024-07-14 09:18:22 +0200

    test: implement query test

--- app/test/testnode_test.go
+++ app/test/testnode_test.go
@@ -1,71 +1,50 @@
 package app_test
 
 import (
-	"context"
-	"fmt"
 	"testing"
 
-	"github.com/celestiaorg/celestia-app/v2/app"
-	"github.com/celestiaorg/celestia-app/v2/pkg/user"
+	"github.com/celestiaorg/celestia-app/v2/pkg/appconsts"
+	v2 "github.com/celestiaorg/celestia-app/v2/pkg/appconsts/v2"
 	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
 	"github.com/celestiaorg/celestia-app/v2/x/minfee"
 	nodeservice "github.com/cosmos/cosmos-sdk/client/grpc/node"
-	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
 )
 
 func Test_testnode(t *testing.T) {
 	t.Run("testnode can start a network with default chain ID", func(t *testing.T) {
 		testnode.NewNetwork(t, testnode.DefaultConfig())
 	})
-	t.Run("testnode can query GRPC", func(t *testing.T) {
+	t.Run("testnode can query network min gas price", func(t *testing.T) {
 		config := testnode.DefaultConfig()
 		cctx, _, _ := testnode.NewNetwork(t, config)
 
 		queryClient := minfee.NewQueryClient(cctx.GRPCClient)
-		got, err := queryClient.NetworkMinGasPrice(cctx.GoContext(), &minfee.QueryNetworkMinGasPrice{})
+		resp, err := queryClient.NetworkMinGasPrice(cctx.GoContext(), &minfee.QueryNetworkMinGasPrice{})
 		require.NoError(t, err)
-		fmt.Printf("got %v\n", got)
-
-		cctx.WaitForHeight(1)
-		// serviceClient := nodeservice.NewServiceClient(cctx.GRPCClient)
-		// // TODO: need to figure out the correct contex tto pass here
-		// foo, err := serviceClient.Config(cctx.GoContext(), &nodeservice.ConfigRequest{})
-		// // got, err = queryServer.Config(cctx.GoContext(), &nodeservice.ConfigRequest{})
-		// require.NoError(t, err)
-		// fmt.Printf("foo %v\n", foo.MinimumGasPrice)
-
-		bar, err := user.QueryMinimumGasPrice(cctx.GoContext(), cctx.GRPCClient)
+		got, err := resp.NetworkMinGasPrice.Float64()
 		require.NoError(t, err)
-		fmt.Printf("bar %v\n", bar)
-
+		assert.Equal(t, v2.NetworkMinGasPrice, got)
 	})
-	t.Run("testnode can start with a custom MinGasPrice", func(t *testing.T) {
-		wantMinGasPrice := float64(0.003)
-		appConfig := testnode.DefaultAppConfig()
-		appConfig.MinGasPrices = fmt.Sprintf("%v%s", wantMinGasPrice, app.BondDenom)
-		config := testnode.DefaultConfig().WithAppConfig(appConfig)
+	t.Run("testnode can query local min gas price", func(t *testing.T) {
+		config := testnode.DefaultConfig()
 		cctx, _, _ := testnode.NewNetwork(t, config)
 
-		got, err := queryMinimumGasPrice(cctx.GoContext(), cctx.GRPCClient)
+		serviceClient := nodeservice.NewServiceClient(cctx.GRPCClient)
+		resp, err := serviceClient.Config(cctx.GoContext(), &nodeservice.ConfigRequest{})
 		require.NoError(t, err)
-		assert.Equal(t, wantMinGasPrice, got)
+		assert.Equal(t, appconsts.DefaultMinGasPrice, resp.MinimumGasPrice)
 	})
-}
+	// t.Run("testnode can start with a custom MinGasPrice", func(t *testing.T) {
+	// 	wantMinGasPrice := float64(0.003)
+	// 	appConfig := testnode.DefaultAppConfig()
+	// 	appConfig.MinGasPrices = fmt.Sprintf("%v%s", wantMinGasPrice, app.BondDenom)
+	// 	config := testnode.DefaultConfig().WithAppConfig(appConfig)
+	// 	cctx, _, _ := testnode.NewNetwork(t, config)
 
-func queryMinimumGasPrice(ctx context.Context, grpcConn *grpc.ClientConn) (float64, error) {
-	resp, err := nodeservice.NewServiceClient(grpcConn).Config(ctx, &nodeservice.ConfigRequest{})
-	if err != nil {
-		return 0, err
-	}
-
-	minGasCoins, err := sdktypes.ParseDecCoins(resp.MinimumGasPrice)
-	if err != nil {
-		return 0, err
-	}
-
-	minGasPrice := minGasCoins.AmountOf(app.BondDenom).MustFloat64()
-	return minGasPrice, nil
+	// 	got, err := queryMinimumGasPrice(cctx.GoContext(), cctx.GRPCClient)
+	// 	require.NoError(t, err)
+	// 	assert.Equal(t, wantMinGasPrice, got)
+	// })
 }
