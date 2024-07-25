71cfbd6849db241f8377cbed4a893d4302b652eb
sanaz <sanaztaheri@ieee.org>
2024-07-25 16:04:25 -0700

    uses custom app version for validators

--- test/e2e/benchmark/throughput.go
+++ test/e2e/benchmark/throughput.go
@@ -109,7 +109,7 @@ func TwoNodeSimple(logger *log.Logger) error {
 
 func runBenchmarkTest(logger *log.Logger, testName string, manifest Manifest) error {
 	logger.Println("Running", testName)
-	manifest.ChainID = manifest.summary()
+	manifest.ChainID = "new-" + manifest.summary()
 	log.Println("ChainID: ", manifest.ChainID)
 	benchTest, err := NewBenchmarkTest(testName, &manifest)
 	testnet.NoError("failed to create benchmark test", err)
@@ -199,5 +199,6 @@ func LargeNetworkBigBlock8MBLatency(logger *log.Logger) error {
 	manifest.EnableLatency = true
 	manifest.LatencyParams = LatencyParams{70, 0}
 	manifest.TestDuration = 10 * time.Minute
+	manifest.CelestiaAppVersion = "pr-3737"
 	return runBenchmarkTest(logger, "LargeNetworkBigBlock8MBLatency", manifest)
 }
