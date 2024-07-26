6c75aa77dda310417641ea740ddeee65a6628582
sanaz <sanaztaheri@ieee.org>
2024-07-25 18:03:20 -0700

    adds a prefix to test chainid

--- test/e2e/benchmark/throughput.go
+++ test/e2e/benchmark/throughput.go
@@ -109,7 +109,7 @@ func TwoNodeSimple(logger *log.Logger) error {
 
 func runBenchmarkTest(logger *log.Logger, testName string, manifest Manifest) error {
 	logger.Println("Running", testName)
-	manifest.ChainID = "new-" + manifest.summary()
+	manifest.ChainID = "del-" + manifest.summary()
 	log.Println("ChainID: ", manifest.ChainID)
 	benchTest, err := NewBenchmarkTest(testName, &manifest)
 	testnet.NoError("failed to create benchmark test", err)
