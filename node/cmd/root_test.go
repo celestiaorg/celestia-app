057ebcddae99c5d45562253f3256dc51bb95e5ce
Rootul Patel <rootulp@gmail.com>
2024-08-07 15:01:51 -0400

    Revert "refactor: remove unnecessary rootDir"
    This reverts commit 3ed43103335153147b17e31a3983f04b40185ddf.

--- node/cmd/root_test.go
+++ node/cmd/root_test.go
@@ -13,20 +13,24 @@ func TestRun(t *testing.T) {
 	currentAppVersion := uint64(1)
 	apps := utils.GetApps()
 	multiplexer := utils.NewMultiplexer(currentAppVersion, apps)
-
 	config := utils.GetConfig()
+
+	tempDir := t.TempDir()
 	fmt.Printf("rootDir %v\n", config.TmConfig.RootDir)
 	fmt.Printf("dbPath %v\n", config.TmConfig.DBPath)
 	fmt.Printf("dbDir %v\n", config.TmConfig.DBDir())
+	fmt.Printf("tempDir %v\n", tempDir)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	cctx, err := utils.StartNode(ctx, config, multiplexer)
+	cctx, err := utils.StartNode(ctx, config, multiplexer, tempDir)
 	require.NoError(t, err)
 	fmt.Printf("chainID %v\n", cctx.ChainID)
 
 	latestHeight, err := cctx.LatestHeight()
 	require.NoError(t, err)
 	fmt.Printf("latestHeight %v\n", latestHeight)
+
+	err = cctx.WaitForNextBlock()
+	require.NoError(t, err)
 }
