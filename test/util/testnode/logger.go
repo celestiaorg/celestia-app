fc371181dc1e13fc6e5ceea575b1639197319e79
Rootul Patel <rootulp@gmail.com>
2024-08-07 11:49:19 -0400

    fix: stop filtering to just errors

--- test/util/testnode/logger.go
+++ test/util/testnode/logger.go
@@ -6,11 +6,9 @@ import (
 	"github.com/tendermint/tendermint/libs/log"
 )
 
-func newLogger(cfg *UniversalTestingConfig) log.Logger {
-	if cfg.SuppressLogs {
+func newLogger(config *UniversalTestingConfig) log.Logger {
+	if config.SuppressLogs {
 		return log.NewNopLogger()
 	}
-	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	logger = log.NewFilter(logger, log.AllowError())
-	return logger
+	return log.NewTMLogger(log.NewSyncWriter(os.Stdout))
 }
