55a4c3d9d9691cf967f604ed980e4200520e224d
Rootul Patel <rootulp@gmail.com>
2024-08-09 17:10:46 -0400

    fix: start v1 state machine with v1 consensus param

--- node/utils/config.go
+++ node/utils/config.go
@@ -1,6 +1,9 @@
 package utils
 
-import "github.com/celestiaorg/celestia-app/v2/test/util/testnode"
+import (
+	v1 "github.com/celestiaorg/celestia-app/v2/pkg/appconsts/v1"
+	"github.com/celestiaorg/celestia-app/v2/test/util/testnode"
+)
 
 func GetConfig() *testnode.Config {
 	tmConfig := testnode.DefaultTendermintConfig()
@@ -8,5 +11,12 @@ func GetConfig() *testnode.Config {
 	tmConfig.P2P.ListenAddress = "tcp://127.0.0.1:26656"
 	tmConfig.RPC.ListenAddress = "tcp://127.0.0.1:26657"
 	tmConfig.RPC.GRPCListenAddress = "tcp://127.0.0.1:26658"
-	return testnode.DefaultConfig().WithTendermintConfig(tmConfig).WithSuppressLogs(false)
+
+	consensusParams := testnode.DefaultConsensusParams()
+	consensusParams.Version.AppVersion = v1.Version
+
+	return testnode.DefaultConfig().
+		WithTendermintConfig(tmConfig).
+		WithConsensusParams(consensusParams).
+		WithSuppressLogs(false)
 }
