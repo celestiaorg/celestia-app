87e9e81181cc090b997025712691a035be249568
Rootul Patel <rootulp@gmail.com>
2024-08-13 12:36:03 -0400

    clean up method order

--- node/utils/multiplexer.go
+++ node/utils/multiplexer.go
@@ -11,9 +11,6 @@ const (
 	initialAppVersion = v1.Version
 )
 
-// TODO: modify v1 state machine to contain an upgrade height and have an EndBlocker that returns with ConsensusParamsUpdates app version 2
-// TODO: extend the abci.Application interface to include a method called "RunMigration"
-
 // Multiplexer implements the abci.Application interface
 var _ abci.Application = (*Multiplexer)(nil)
 
@@ -21,9 +18,12 @@ var _ abci.Application = (*Multiplexer)(nil)
 type Multiplexer struct {
 	// applications is a map from appVersion to application
 	applications map[uint64]AppWithMigrations
-	// currentAppVersion is the version of the application that is currently running
+	// currentAppVersion is the version of the application that is currently
+	// running.
 	currentAppVersion uint64
-	// nextAppVersion is the version of the application that should be upgraded to. Usually this value is the same as currentAppVersion except if the current height is an upgrade height.
+	// nextAppVersion is the version of the application that should be upgraded
+	// to. This value only differs from currentAppVersion if the current height
+	// is an upgrade height.
 	nextAppVersion uint64
 }
 
@@ -35,52 +35,45 @@ func NewMultiplexer() *Multiplexer {
 	}
 }
 
-func (m *Multiplexer) getCurrentApp() AppWithMigrations {
-	return m.applications[m.currentAppVersion]
+//
+// #region Consensus
+//
+
+func (m *Multiplexer) InitChain(request abci.RequestInitChain) abci.ResponseInitChain {
+	fmt.Printf("Multiplexer InitChain invoked with current app version %v request app version %v\n", m.currentAppVersion, request.ConsensusParams.Version.AppVersion)
+	m.currentAppVersion = request.ConsensusParams.Version.AppVersion
+	m.nextAppVersion = request.ConsensusParams.Version.AppVersion
+	app := m.getCurrentApp()
+	return app.InitChain(request)
 }
 
-func (m *Multiplexer) ApplySnapshotChunk(request abci.RequestApplySnapshotChunk) abci.ResponseApplySnapshotChunk {
+func (m *Multiplexer) PrepareProposal(request abci.RequestPrepareProposal) abci.ResponsePrepareProposal {
+	fmt.Printf("Multiplexer PrepareProposal invoked with current app version %v\n", m.currentAppVersion)
 	app := m.getCurrentApp()
-	return app.ApplySnapshotChunk(request)
+	return app.PrepareProposal(request)
+}
+
+func (m *Multiplexer) ProcessProposal(request abci.RequestProcessProposal) abci.ResponseProcessProposal {
+	fmt.Printf("Multiplexer ProcessProposal invoked with current app version %v\n", m.currentAppVersion)
+	app := m.getCurrentApp()
+	return app.ProcessProposal(request)
 }
 
 func (m *Multiplexer) BeginBlock(request abci.RequestBeginBlock) abci.ResponseBeginBlock {
+	fmt.Printf("Multiplexer BeginBlock invoked with current app version %v\n", m.currentAppVersion)
 	app := m.getCurrentApp()
-	got := app.Info(abci.RequestInfo{})
-	if got.AppVersion != m.currentAppVersion {
-		panic(fmt.Sprintf("current app version %v does not match app version from current app %v\n", m.currentAppVersion, got.AppVersion))
-	}
 	return app.BeginBlock(request)
 }
 
-func (m *Multiplexer) CheckTx(request abci.RequestCheckTx) abci.ResponseCheckTx {
-	app := m.getCurrentApp()
-	return app.CheckTx(request)
-}
-
-func (m *Multiplexer) Commit() abci.ResponseCommit {
-	// Note: the application can create or delete stores in this method
-	app := m.getCurrentApp()
-	got := app.Commit()
-
-	if m.isUpgradePending() {
-		fmt.Printf("Multiplexer upgrade is pending from %v to %v\n", m.currentAppVersion, m.nextAppVersion)
-		m.currentAppVersion = m.nextAppVersion
-		fmt.Printf("Multiplexer upgrade completed to %v\n", m.currentAppVersion)
-		appHash := m.RunMigrations()
-		got.Data = appHash
-		return got
-	}
-	return got
-}
-
 func (m *Multiplexer) DeliverTx(request abci.RequestDeliverTx) abci.ResponseDeliverTx {
+	fmt.Printf("Multiplexer DeliverTx invoked with current app version %v\n", m.currentAppVersion)
 	app := m.getCurrentApp()
 	return app.DeliverTx(request)
 }
 
 func (m *Multiplexer) EndBlock(request abci.RequestEndBlock) abci.ResponseEndBlock {
-	fmt.Printf("Multiplexer EndBlock height %v invoked with current app version %v\n", request.Height, m.currentAppVersion)
+	fmt.Printf("Multiplexer EndBlock invoked with current app version %v\n", m.currentAppVersion)
+
 	// Note: the application can't create or delete stores in this method
 	// because it is operating on a branch of state.
 	app := m.getCurrentApp()
@@ -99,42 +92,40 @@ func (m *Multiplexer) EndBlock(request abci.RequestEndBlock) abci.ResponseEndBlo
 	return got
 }
 
+func (m *Multiplexer) Commit() abci.ResponseCommit {
+	fmt.Printf("Multiplexer Commit invoked with current app version %v\n", m.currentAppVersion)
+
+	// Note: the application can create or delete stores in this method
+	app := m.getCurrentApp()
+	got := app.Commit()
+
+	if m.isUpgradePending() {
+		fmt.Printf("Multiplexer upgrade is pending from %v to %v\n", m.currentAppVersion, m.nextAppVersion)
+		m.currentAppVersion = m.nextAppVersion
+		fmt.Printf("Multiplexer upgrade completed to %v\n", m.currentAppVersion)
+		appHash := m.RunMigrations()
+		got.Data = appHash
+		return got
+	}
+	return got
+}
+
+//
+// #region Mempool
+//
+
+func (m *Multiplexer) CheckTx(request abci.RequestCheckTx) abci.ResponseCheckTx {
+	app := m.getCurrentApp()
+	return app.CheckTx(request)
+}
+
+//
+// #region Info
+//
+
 func (m *Multiplexer) Info(request abci.RequestInfo) abci.ResponseInfo {
 	app := m.getCurrentApp()
 	return app.Info(request)
-}
-
-func (m *Multiplexer) InitChain(request abci.RequestInitChain) abci.ResponseInitChain {
-	// TODO consider getting app version from request.ConsensusParams.Version.AppVersion
-	fmt.Printf("Multiplexer InitChain invoked with current app version %v request %v\n", m.currentAppVersion, request.ConsensusParams.Version.AppVersion)
-	app := m.getCurrentApp()
-	return app.InitChain(request)
-}
-
-func (m *Multiplexer) ListSnapshots(request abci.RequestListSnapshots) abci.ResponseListSnapshots {
-	app := m.getCurrentApp()
-	return app.ListSnapshots(request)
-}
-
-func (m *Multiplexer) LoadSnapshotChunk(request abci.RequestLoadSnapshotChunk) abci.ResponseLoadSnapshotChunk {
-	app := m.getCurrentApp()
-	return app.LoadSnapshotChunk(request)
-}
-
-func (m *Multiplexer) OfferSnapshot(request abci.RequestOfferSnapshot) abci.ResponseOfferSnapshot {
-	app := m.getCurrentApp()
-	return app.OfferSnapshot(request)
-}
-
-func (m *Multiplexer) PrepareProposal(request abci.RequestPrepareProposal) abci.ResponsePrepareProposal {
-	fmt.Printf("Multiplexer PrepareProposal invoked with current app version %v\n", m.currentAppVersion)
-	app := m.getCurrentApp()
-	return app.PrepareProposal(request)
-}
-
-func (m *Multiplexer) ProcessProposal(request abci.RequestProcessProposal) abci.ResponseProcessProposal {
-	app := m.getCurrentApp()
-	return app.ProcessProposal(request)
 }
 
 func (m *Multiplexer) Query(request abci.RequestQuery) abci.ResponseQuery {
@@ -142,17 +133,50 @@ func (m *Multiplexer) Query(request abci.RequestQuery) abci.ResponseQuery {
 	return app.Query(request)
 }
 
+//
+// #region Snapshot
+//
+
+func (m *Multiplexer) ApplySnapshotChunk(request abci.RequestApplySnapshotChunk) abci.ResponseApplySnapshotChunk {
+	app := m.getCurrentApp()
+	return app.ApplySnapshotChunk(request)
+}
+func (m *Multiplexer) ListSnapshots(request abci.RequestListSnapshots) abci.ResponseListSnapshots {
+	app := m.getCurrentApp()
+	return app.ListSnapshots(request)
+}
+func (m *Multiplexer) LoadSnapshotChunk(request abci.RequestLoadSnapshotChunk) abci.ResponseLoadSnapshotChunk {
+	app := m.getCurrentApp()
+	return app.LoadSnapshotChunk(request)
+}
+func (m *Multiplexer) OfferSnapshot(request abci.RequestOfferSnapshot) abci.ResponseOfferSnapshot {
+	app := m.getCurrentApp()
+	return app.OfferSnapshot(request)
+}
+
+//
+// #region Other
+//
+
 func (m *Multiplexer) SetOption(request abci.RequestSetOption) abci.ResponseSetOption {
 	app := m.getCurrentApp()
 	return app.SetOption(request)
-}
-
-func (m *Multiplexer) isUpgradePending() bool {
-	return m.currentAppVersion != m.nextAppVersion
 }
 
 func (m *Multiplexer) RunMigrations() []byte {
 	fmt.Printf("Multiplexer RunMigrations invoked with current app version %v\n", m.currentAppVersion)
 	app := m.getCurrentApp()
 	return app.RunMigrations()
+}
+
+//
+// #region Private
+//
+
+func (m *Multiplexer) isUpgradePending() bool {
+	return m.currentAppVersion != m.nextAppVersion
+}
+
+func (m *Multiplexer) getCurrentApp() AppWithMigrations {
+	return m.applications[m.currentAppVersion]
 }
