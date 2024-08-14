3f787ce030a3fd20544e865fa2008f84c888daec
Rootul Patel <rootulp@gmail.com>
2024-08-14 11:30:08 -0400

    store does not exist for key: capability

--- node/utils/multiplexer.go
+++ node/utils/multiplexer.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	v1 "github.com/celestiaorg/celestia-app/v2/pkg/appconsts/v1"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	capabilitytypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	tmdb "github.com/tendermint/tm-db"
 )
@@ -68,6 +70,10 @@ func (m *Multiplexer) ProcessProposal(request abci.RequestProcessProposal) abci.
 func (m *Multiplexer) BeginBlock(request abci.RequestBeginBlock) abci.ResponseBeginBlock {
 	fmt.Printf("Multiplexer BeginBlock invoked with current app version %v\n", m.currentAppVersion)
 	app := m.getCurrentApp()
+	capabilityKey := storetypes.NewKVStoreKey(capabilitytypes.StoreKey)
+	kvStore := app.GetCommitMultiStore().GetKVStore(capabilityKey)
+	fmt.Printf("kvStore %v\n", kvStore)
+
 	return app.BeginBlock(request)
 }
 
