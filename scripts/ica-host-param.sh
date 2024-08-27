2e8684d17dcb1f34b0684b15ca014637efe46bb8
Rootul Patel <rootulp@gmail.com>
2024-08-27 17:33:58 -0400

    wip

---
+++ scripts/ica-host-param.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script submits a governance proposal to modify the ICA host allow
+# messages param.
+
+# Stop script execution if an error is encountered
+set -o errexit
+# Stop script execution if an undefined variable is used
+set -o nounset
+
+FROM="validator"
+
+cat << 'EOF' > proposal.json
+{
+  "title": "Modify ICA host allow messages",
+  "description": "Modify ICA host allow messages",
+  "changes": [
+    {
+      "subspace": "icahost",
+      "key": "AllowMessages",
+      "value": '["/ibc.applications.transfer.v1.MsgTransfer","/cosmos.bank.v1beta1.MsgSend","/cosmos.staking.v1beta1.MsgDelegate","/cosmos.staking.v1beta1.MsgBeginRedelegate","/cosmos.staking.v1beta1.MsgUndelegate","/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation","/cosmos.distribution.v1beta1.MsgSetWithdrawAddress","/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward","/cosmos.distribution.v1beta1.MsgFundCommunityPool","/cosmos.gov.v1.MsgVote","/cosmos.feegrant.v1beta1.MsgGrantAllowance","/cosmos.feegrant.v1beta1.MsgRevokeAllowance"]'
+    }
+  ],
+  "deposit": "10000tia"
+}
+EOF
+
+celestia-appd tx gov submit-proposal param-change proposal.json --from $FROM
