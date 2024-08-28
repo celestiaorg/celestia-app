46c5799f97ea33a3af298db6767325eeedfb9fd5
sanaz <sanaztaheri@ieee.org>
2024-08-28 13:41:02 -0700

    makes disableBBR parameter ineffective in the Node's Upgrade

--- test/e2e/testnet/node.go
+++ test/e2e/testnet/node.go
@@ -375,12 +375,14 @@ func (n *Node) Upgrade(version string, disableBBR bool) error {
 		return err
 	}
 
-	if disableBBR {
-		//if !contains(n.args, "--force-no-bbr") {
-		n.args = append(n.args, "--force-no-bbr")
-		n.Instance.SetArgs(n.args...)
-		//}
-	}
+	//if disableBBR {
+	//	// FIXME: This does not work currently since the node is not in state 'Preparing' or 'Committed'
+	//	//n.args = append(n.args, "--force-no-bbr")
+	//	//err := n.Instance.SetArgs(n.args...)
+	//	//if err != nil {
+	//	//	return fmt.Errorf("cannot pass --force-no-bbr flag: %w", err)
+	//	//}
+	//}
 
 	return n.Instance.WaitInstanceIsRunning()
 }
