f521d8645171312603a883399741735a6b3033ee
Rootul Patel <rootulp@gmail.com>
2024-08-28 15:03:29 -0400

    error invalid proposal content: no handler exists for proposal type

--- scripts/ica-host-param.sh
+++ scripts/ica-host-param.sh
@@ -18,11 +18,11 @@ cat << 'EOF' > proposal.json
     {
       "subspace": "icahost",
       "key": "AllowMessages",
-      "value": '["/ibc.applications.transfer.v1.MsgTransfer","/cosmos.bank.v1beta1.MsgSend","/cosmos.staking.v1beta1.MsgDelegate","/cosmos.staking.v1beta1.MsgBeginRedelegate","/cosmos.staking.v1beta1.MsgUndelegate","/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation","/cosmos.distribution.v1beta1.MsgSetWithdrawAddress","/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward","/cosmos.distribution.v1beta1.MsgFundCommunityPool","/cosmos.gov.v1.MsgVote","/cosmos.feegrant.v1beta1.MsgGrantAllowance","/cosmos.feegrant.v1beta1.MsgRevokeAllowance"]'
+      "value": "[\"/ibc.applications.transfer.v1.MsgTransfer\",\"/cosmos.bank.v1beta1.MsgSend\",\"/cosmos.staking.v1beta1.MsgDelegate\",\"/cosmos.staking.v1beta1.MsgBeginRedelegate\",\"/cosmos.staking.v1beta1.MsgUndelegate\",\"/cosmos.staking.v1beta1.MsgCancelUnbondingDelegation\",\"/cosmos.distribution.v1beta1.MsgSetWithdrawAddress\",\"/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward\",\"/cosmos.distribution.v1beta1.MsgFundCommunityPool\",\"/cosmos.gov.v1.MsgVote\",\"/cosmos.feegrant.v1beta1.MsgGrantAllowance\",\"/cosmos.feegrant.v1beta1.MsgRevokeAllowance\"]"
     }
   ],
   "deposit": "10000tia"
 }
 EOF
 
-celestia-appd tx gov submit-proposal param-change proposal.json --from $FROM
+celestia-appd tx gov submit-legacy-proposal param-change proposal.json --from $FROM --fees 21000utia
