dc127dabf175e710660d2b837f6ba0fceafa2a64
sanaz <sanaztaheri@ieee.org>
2024-09-19 15:56:33 -0700

    displaying the timeout propose in the logs

--- cmd/celestia-appd/cmd/start.go
+++ cmd/celestia-appd/cmd/start.go
@@ -113,6 +113,8 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 			}
 
 			serverCtx := server.GetServerContextFromCmd(cmd)
+			serverCtx.Logger.Info("starting node with timeout_propose: ",
+				serverCtx.Config.Consensus.TimeoutPropose.Seconds())
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
