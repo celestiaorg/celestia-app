a878a203d0ab16d678845bf6b3d1fb1570a9cf2f
sanaz <sanaztaheri@ieee.org>
2024-09-19 15:55:28 -0700

    sets timeout propose of v3 to 10

--- pkg/appconsts/v3/app_consts.go
+++ pkg/appconsts/v3/app_consts.go
@@ -6,6 +6,6 @@ const (
 	Version              uint64 = 3
 	SquareSizeUpperBound int    = 128
 	SubtreeRootThreshold int    = 64
-	TimeoutPropose              = time.Second * 10
+	TimeoutPropose              = time.Second * 11
 	TimeoutCommit               = time.Second * 11
 )
