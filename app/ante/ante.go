0e89e6da9d1d3a5ab12e80891a5307b27c9f46b1
evan-forbes <evan.samuel.forbes@gmail.com>
2024-09-30 03:36:18 -0500

    chore: fix core finally

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -1,14 +1,12 @@
 package ante
 
 import (
-	blobante "github.com/celestiaorg/celestia-app/v3/x/blob/ante"
 	blob "github.com/celestiaorg/celestia-app/v3/x/blob/keeper"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 	"github.com/cosmos/cosmos-sdk/x/auth/signing"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	paramkeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
-	ibcante "github.com/cosmos/ibc-go/v6/modules/core/ante"
 	ibckeeper "github.com/cosmos/ibc-go/v6/modules/core/keeper"
 )
 
@@ -33,16 +31,16 @@ func NewAnteHandler(
 		// Must be called before gas consumption occurs in any other decorator.
 		ante.NewSetUpContextDecorator(),
 		// Ensure the tx does not contain any extension options.
-		ante.NewExtensionOptionsDecorator(nil),
+		// ante.NewExtensionOptionsDecorator(nil),
 		// Ensure the tx passes ValidateBasic.
-		ante.NewValidateBasicDecorator(),
+		// ante.NewValidateBasicDecorator(),
 		// Ensure the tx has not reached a height timeout.
-		ante.NewTxTimeoutHeightDecorator(),
+		// ante.NewTxTimeoutHeightDecorator(),
 		// Ensure the tx memo <= max memo characters.
-		ante.NewValidateMemoDecorator(accountKeeper),
+		// ante.NewValidateMemoDecorator(accountKeeper),
 		// Ensure the tx's gas limit is > the gas consumed based on the tx size.
 		// Side effect: consumes gas from the gas meter.
-		ante.NewConsumeGasForTxSizeDecorator(accountKeeper),
+		// ante.NewConsumeGasForTxSizeDecorator(accountKeeper),
 		// Ensure the feepayer (fee granter or first signer) has enough funds to pay for the tx.
 		// Ensure the gas price >= network min gas price if app version >= 2.
 		// Side effect: deducts fees from the fee payer. Sets the tx priority in context.
@@ -51,33 +49,33 @@ func NewAnteHandler(
 		// Contract: must be called before all signature verification decorators.
 		ante.NewSetPubKeyDecorator(accountKeeper),
 		// Ensure that the tx's count of signatures is <= the tx signature limit.
-		ante.NewValidateSigCountDecorator(accountKeeper),
+		// ante.NewValidateSigCountDecorator(accountKeeper),
 		// Ensure that the tx's gas limit is > the gas consumed based on signature verification.
 		// Side effect: consumes gas from the gas meter.
-		ante.NewSigGasConsumeDecorator(accountKeeper, sigGasConsumer),
+		// ante.NewSigGasConsumeDecorator(accountKeeper, sigGasConsumer),
 		// Ensure that the tx's signatures are valid. For each signature, ensure
 		// that the signature's sequence number (a.k.a nonce) matches the
 		// account sequence number of the signer.
 		// Note: does not consume gas from the gas meter.
-		ante.NewSigVerificationDecorator(accountKeeper, signModeHandler),
+		// ante.NewSigVerificationDecorator(accountKeeper, signModeHandler),
 		// Ensure that the tx's gas limit is > the gas consumed based on the blob size(s).
 		// Contract: must be called after all decorators that consume gas.
 		// Note: does not consume gas from the gas meter.
-		blobante.NewMinGasPFBDecorator(blobKeeper),
+		// blobante.NewMinGasPFBDecorator(blobKeeper),
 		// Ensure that the tx's total blob size is <= the max blob size.
 		// Only applies to app version == 1.
-		blobante.NewMaxTotalBlobSizeDecorator(blobKeeper),
+		// blobante.NewMaxTotalBlobSizeDecorator(blobKeeper),
 		// Ensure that the blob shares occupied by the tx <= the max shares
 		// available to blob data in a data square. Only applies to app version
 		// >= 2.
-		blobante.NewBlobShareDecorator(blobKeeper),
+		// blobante.NewBlobShareDecorator(blobKeeper),
 		// Ensure that tx's with a MsgSubmitProposal have at least one proposal
 		// message.
-		NewGovProposalDecorator(),
+		// NewGovProposalDecorator(),
 		// Side effect: increment the nonce for all tx signers.
 		ante.NewIncrementSequenceDecorator(accountKeeper),
 		// Ensure that the tx is not a IBC packet or update message that has already been processed.
-		ibcante.NewRedundantRelayDecorator(channelKeeper),
+		// ibcante.NewRedundantRelayDecorator(channelKeeper),
 	)
 }
 
