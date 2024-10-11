d7605a339b37de8fb61da52e5049da22a66dd924
CHAMI Rachid <chamirachid1@gmail.com>
2024-10-12 00:12:48 +0200

    Update pkg/appconsts/v3/app_consts.go
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- pkg/appconsts/v3/app_consts.go
+++ pkg/appconsts/v3/app_consts.go
@@ -7,9 +7,9 @@ const (
 	TxSizeCostPerByte    uint64 = 10
 	GasPerBlobByte       uint32 = 8
 	MaxTxSize            int    = 2097152 // 2 MiB in bytes
-	// SdkMsgTransactionCap maximum number of sdk messages, aside from PFBs, that a block can contain.
+	// SdkMsgTransactionCap is the maximum number of SDK messages, aside from PFBs, that a block can contain.
 	SdkMsgTransactionCap = 200
 
-	// PFBTransactionCap maximum number of PFB messages a block can contain.
+	// PFBTransactionCap is maximum number of PFB messages a block can contain.
 	PFBTransactionCap = 600
 )
