528fa8cceeeefdf5db695e3c32f4fe0bdbc374ed
evan-forbes <evan.samuel.forbes@gmail.com>
2024-10-16 13:51:28 -0500

    chore: delete appconsts.Timeouts

--- pkg/appconsts/consensus_consts.go
+++ pkg/appconsts/consensus_consts.go
@@ -3,11 +3,9 @@ package appconsts
 import "time"
 
 const (
-	TimeoutPropose = time.Second * 10
-	TimeoutCommit  = time.Second * 11
 	// GoalBlockTime is the target time interval between blocks. Since the block
 	// interval isn't enforced at consensus, the real block interval isn't
 	// guaranteed to exactly match GoalBlockTime. GoalBlockTime is currently targeted
 	// through static timeouts (i.e. TimeoutPropose, TimeoutCommit).
-	GoalBlockTime = time.Second * 15
+	GoalBlockTime = time.Second * 6
 )
