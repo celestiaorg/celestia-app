17b3a727e698c298658f329362ab77d5fb045684
Rootul Patel <rootulp@gmail.com>
2024-10-16 14:50:56 -0400

    refactor: rename limits from tx to msg

--- pkg/appconsts/global_consts.go
+++ pkg/appconsts/global_consts.go
@@ -68,11 +68,13 @@ func HashLength() int {
 	return hashLength
 }
 
-// The following consts are not consensus breaking and will be applied straight after this binary is started.
+// The following consts are not consensus breaking and will be applied straight
+// after this binary is started.
 const (
-	// NonPFBTransactionCap is the maximum number of SDK messages, aside from PFBs, that a block can contain.
-	NonPFBTransactionCap = 200
+	// MaxNonPFBMessages is the maximum number of SDK messages, aside from
+	// PFBs, that a block can contain.
+	MaxNonPFBMessages = 200
 
-	// PFBTransactionCap is the maximum number of PFB messages a block can contain.
-	PFBTransactionCap = 600
+	// MaxPFBMessages is the maximum number of PFB messages a block can contain.
+	MaxPFBMessages = 600
 )
