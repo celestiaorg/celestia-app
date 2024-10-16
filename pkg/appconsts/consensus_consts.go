fbbd9e613628225edae690d91a2525ba8f50c15c
evan-forbes <evan.samuel.forbes@gmail.com>
2024-10-16 14:24:41 -0500

    fix: don't update the goal block time for now

--- pkg/appconsts/consensus_consts.go
+++ pkg/appconsts/consensus_consts.go
@@ -7,5 +7,5 @@ const (
 	// interval isn't enforced at consensus, the real block interval isn't
 	// guaranteed to exactly match GoalBlockTime. GoalBlockTime is currently targeted
 	// through static timeouts (i.e. TimeoutPropose, TimeoutCommit).
-	GoalBlockTime = time.Second * 6
+	GoalBlockTime = time.Second * 15
 )
