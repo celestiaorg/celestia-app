9bfc49a61101de762f8fb9bd21734fd5a3c3f673
Nina Barbakadze <barbakadzeninaa@gmail.com>
2024-10-23 17:10:00 -0400

    chore: uncomment

--- ibc/mpt/mpt.go
+++ ibc/mpt/mpt.go
@@ -1,22 +1,21 @@
-// package mpt
+package mpt
 
-// import (
-// 	"fmt"
+import (
+	"fmt"
 
-// 	"github.com/ethereum/go-ethereum/common"
-// 	"github.com/ethereum/go-ethereum/core/rawdb"
-// 	"github.com/ethereum/go-ethereum/rlp"
-// 	"github.com/ethereum/go-ethereum/trie"
-// )
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/trie"
+)
 
-// func VerifyMerklePatriciaTrieProof(stateRoot []byte, key string, proof []byte) (value []byte, err error) {
-// 	rootHash := common.BytesToHash(stateRoot)
-// 	keyBytes := common.FromHex(key)
-// 	proofDB := rawdb.NewMemoryDatabase()
-// 	err = rlp.DecodeBytes(proof, proofDB)
-// 	if err != nil {
-		// return nil, fmt.Errorf("failed to decode proof: %w", err)
-	// }
-	// return trie.VerifyProof(rootHash, keyBytes, proofDB)
-// }
-// 
+func VerifyMerklePatriciaTrieProof(stateRoot []byte, key string, proof []byte) (value []byte, err error) {
+	rootHash := common.BytesToHash(stateRoot)
+	keyBytes := common.FromHex(key)
+	proofDB := rawdb.NewMemoryDatabase()
+	err = rlp.DecodeBytes(proof, proofDB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode proof: %w", err)
+	}
+	return trie.VerifyProof(rootHash, keyBytes, proofDB)
+}
--- ibc/mpt/mpt_test.go
+++ ibc/mpt/mpt_test.go
@@ -1,108 +1,108 @@
-// package mpt_test
+package mpt_test
 
-// import (
-// 	"bytes"
-// 	mrand "math/rand"
-// 	"testing"
+import (
+	"bytes"
+	mrand "math/rand"
+	"testing"
 
-// 	"github.com/celestiaorg/celestia-app/v3/ibc/mpt"
-// 	"github.com/ethereum/go-ethereum/common"
-// 	"github.com/ethereum/go-ethereum/core/rawdb"
-// 	"github.com/ethereum/go-ethereum/crypto"
-// 	"github.com/ethereum/go-ethereum/ethdb/memorydb"
-// 	"github.com/ethereum/go-ethereum/rlp"
-// 	gethtrie "github.com/ethereum/go-ethereum/trie"
-// 	"github.com/stretchr/testify/require"
+	"github.com/celestiaorg/celestia-app/v3/ibc/mpt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethdb/memorydb"
+	"github.com/ethereum/go-ethereum/rlp"
+	gethtrie "github.com/ethereum/go-ethereum/trie"
+	"github.com/stretchr/testify/require"
 
-// 	crand "crypto/rand"
-// 	"encoding/binary"
-// 	"fmt"
-// )
+	crand "crypto/rand"
+	"encoding/binary"
+	"fmt"
+)
 
-// type kv struct {
-// 	k, v []byte
-// 	t    bool
-// }
+type kv struct {
+	k, v []byte
+	t    bool
+}
 
-// var prng = initRnd()
+var prng = initRnd()
 
-// func TestVerifyMerklePatriciaTrieProof(t *testing.T) {
-// 	trie, vals := randomTrie(500)
-// 	root := trie.Hash()
-// 	for i, prover := range makeProvers(trie) {
+func TestVerifyMerklePatriciaTrieProof(t *testing.T) {
+	trie, vals := randomTrie(500)
+	root := trie.Hash()
+	for i, prover := range makeProvers(trie) {
 
-// 		for _, kv := range vals {
-// 			proof := prover(kv.k)
-// 			if proof == nil {
-// 				t.Fatalf("prover %d: missing key %x while constructing proof", i, kv.k)
-// 			}
-// 			var proofBuf bytes.Buffer
-// 			err := rlp.Encode(&proofBuf, proof)
-// 			require.NoError(t, err)
-// 			val, err := mpt.VerifyMerklePatriciaTrieProof(root.Bytes(), string(kv.k), proofBuf.Bytes())
-// 			if err != nil {
-// 				t.Fatalf("prover %d: failed to verify proof for key %x: %v\nraw proof: %x", i, kv.k, err, proof)
-// 			}
-// 			if !bytes.Equal(val, kv.v) {
-// 				t.Fatalf("prover %d: verified value mismatch for key %x: have %x, want %x", i, kv.k, val, kv.v)
-// 			}
-// 		}
-// 	}
-// }
+		for _, kv := range vals {
+			proof := prover(kv.k)
+			if proof == nil {
+				t.Fatalf("prover %d: missing key %x while constructing proof", i, kv.k)
+			}
+			var proofBuf bytes.Buffer
+			err := rlp.Encode(&proofBuf, proof)
+			require.NoError(t, err)
+			val, err := mpt.VerifyMerklePatriciaTrieProof(root.Bytes(), string(kv.k), proofBuf.Bytes())
+			if err != nil {
+				t.Fatalf("prover %d: failed to verify proof for key %x: %v\nraw proof: %x", i, kv.k, err, proof)
+			}
+			if !bytes.Equal(val, kv.v) {
+				t.Fatalf("prover %d: verified value mismatch for key %x: have %x, want %x", i, kv.k, val, kv.v)
+			}
+		}
+	}
+}
 
-// func randomTrie(n int) (*gethtrie.Trie, map[string]*kv) {
-// 	trie := gethtrie.NewEmpty(newTestDatabase(rawdb.NewMemoryDatabase(), rawdb.HashScheme))
-// 	vals := make(map[string]*kv)
-// 	for i := byte(0); i < 100; i++ {
-// 		value := &kv{common.LeftPadBytes([]byte{i}, 32), []byte{i}, false}
-// 		value2 := &kv{common.LeftPadBytes([]byte{i + 10}, 32), []byte{i}, false}
-// 		trie.MustUpdate(value.k, value.v)
-// 		trie.MustUpdate(value2.k, value2.v)
-// 		vals[string(value.k)] = value
-// 		vals[string(value2.k)] = value2
-// 	}
-// 	for i := 0; i < n; i++ {
-// 		value := &kv{randBytes(32), randBytes(20), false}
-// 		trie.MustUpdate(value.k, value.v)
-// 		vals[string(value.k)] = value
-// 	}
-// 	return trie, vals
-// }
+func randomTrie(n int) (*gethtrie.Trie, map[string]*kv) {
+	trie := gethtrie.NewEmpty(newTestDatabase(rawdb.NewMemoryDatabase(), rawdb.HashScheme))
+	vals := make(map[string]*kv)
+	for i := byte(0); i < 100; i++ {
+		value := &kv{common.LeftPadBytes([]byte{i}, 32), []byte{i}, false}
+		value2 := &kv{common.LeftPadBytes([]byte{i + 10}, 32), []byte{i}, false}
+		trie.MustUpdate(value.k, value.v)
+		trie.MustUpdate(value2.k, value2.v)
+		vals[string(value.k)] = value
+		vals[string(value2.k)] = value2
+	}
+	for i := 0; i < n; i++ {
+		value := &kv{randBytes(32), randBytes(20), false}
+		trie.MustUpdate(value.k, value.v)
+		vals[string(value.k)] = value
+	}
+	return trie, vals
+}
 
-// func randBytes(n int) []byte {
-// 	r := make([]byte, n)
-// 	prng.Read(r)
-// 	return r
-// }
+func randBytes(n int) []byte {
+	r := make([]byte, n)
+	prng.Read(r)
+	return r
+}
 
-// func initRnd() *mrand.Rand {
-// 	var seed [8]byte
-// 	crand.Read(seed[:])
-// 	rnd := mrand.New(mrand.NewSource(int64(binary.LittleEndian.Uint64(seed[:]))))
-// 	fmt.Printf("Seed: %x\n", seed)
-// 	return rnd
-// }
+func initRnd() *mrand.Rand {
+	var seed [8]byte
+	crand.Read(seed[:])
+	rnd := mrand.New(mrand.NewSource(int64(binary.LittleEndian.Uint64(seed[:]))))
+	fmt.Printf("Seed: %x\n", seed)
+	return rnd
+}
 
-// // makeProvers creates Merkle trie provers based on different implementations to
-// // test all variations.
-// func makeProvers(trie *gethtrie.Trie) []func(key []byte) *memorydb.Database {
-// 	var provers []func(key []byte) *memorydb.Database
+// makeProvers creates Merkle trie provers based on different implementations to
+// test all variations.
+func makeProvers(trie *gethtrie.Trie) []func(key []byte) *memorydb.Database {
+	var provers []func(key []byte) *memorydb.Database
 
-// 	// Create a direct trie based Merkle prover
-// 	provers = append(provers, func(key []byte) *memorydb.Database {
-// 		proof := memorydb.New()
-// 		trie.Prove(key, proof)
-// 		return proof
-// 	})
-// 	// Create a leaf iterator based Merkle prover
-// 	provers = append(provers, func(key []byte) *memorydb.Database {
-// 		proof := memorydb.New()
-// 		if it := gethtrie.NewIterator(trie.MustNodeIterator(key)); it.Next() && bytes.Equal(key, it.Key) {
-// 			for _, p := range it.Prove() {
-// 				proof.Put(crypto.Keccak256(p), p)
-// 			}
-// 		}
-// 		return proof
-// 	})
-// 	return provers
-// }
+	// Create a direct trie based Merkle prover
+	provers = append(provers, func(key []byte) *memorydb.Database {
+		proof := memorydb.New()
+		trie.Prove(key, proof)
+		return proof
+	})
+	// Create a leaf iterator based Merkle prover
+	provers = append(provers, func(key []byte) *memorydb.Database {
+		proof := memorydb.New()
+		if it := gethtrie.NewIterator(trie.MustNodeIterator(key)); it.Next() && bytes.Equal(key, it.Key) {
+			for _, p := range it.Prove() {
+				proof.Put(crypto.Keccak256(p), p)
+			}
+		}
+		return proof
+	})
+	return provers
+}
