39f26441c099ffefcc3144b5d35914506e127105
Rootul Patel <rootulp@gmail.com>
2024-11-26 16:45:18 -0500

    improve comments

--- pkg/appconsts/v2/app_consts.go
+++ pkg/appconsts/v2/app_consts.go
@@ -6,11 +6,12 @@ const (
 	Version              uint64 = 2
 	SquareSizeUpperBound int    = 128
 	SubtreeRootThreshold int    = 64
-	// Note: TimeoutPropose and TimeoutCommit weren't actually constants in v1,
-	// they were defaults for user-configurable timeouts. TODO: consider
-	// removing them from this package.
+	// TimeoutPropose wasn't a constant in v2, it was the default for a
+	// user-configurable timeout.
 	TimeoutPropose = time.Second * 10
-	TimeoutCommit  = time.Second * 11
+	// TimeoutCommit wasn't a constant in v2, it was the default for a
+	// user-configurable timeout.
+	TimeoutCommit = time.Second * 11
 	// UpgradeHeightDelay is the number of blocks after a quorum has been
 	// reached that the chain should upgrade to the new version. Assuming a block
 	// interval of 12 seconds, this is 7 days.
