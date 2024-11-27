c5a916d4806e881803d1183d2a639835f619d430
Rootul Patel <rootulp@gmail.com>
2024-11-27 09:56:14 -0500

    deprecate consts

--- pkg/appconsts/v2/app_consts.go
+++ pkg/appconsts/v2/app_consts.go
@@ -6,11 +6,11 @@ const (
 	Version              uint64 = 2
 	SquareSizeUpperBound int    = 128
 	SubtreeRootThreshold int    = 64
-	// TimeoutPropose wasn't a constant in v2, it was the default for a
-	// user-configurable timeout.
+	// TimeoutPropose is deprecated because it was not a constant
+	// in v2, it was the default for a user-configurable timeout.
 	TimeoutPropose = time.Second * 10
-	// TimeoutCommit wasn't a constant in v2, it was the default for a
-	// user-configurable timeout.
+	// TimeoutCommit is deprecated because it was not a constant
+	// in v2, it was the default for a user-configurable timeout.
 	TimeoutCommit = time.Second * 11
 	// UpgradeHeightDelay is the number of blocks after a quorum has been
 	// reached that the chain should upgrade to the new version. Assuming a block
