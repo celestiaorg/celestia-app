572af0e32aff52428c1b913a21e139bafa7a4cba
DevEloperate <dmitriy.i3.jp@gmail.com>
2024-12-03 20:54:11 +0300

    typo max_total_blob_size_ante.go

--- x/blob/ante/max_total_blob_size_ante.go
+++ x/blob/ante/max_total_blob_size_ante.go
@@ -62,7 +62,7 @@ func (d MaxTotalBlobSizeDecorator) getMaxSquareSize(ctx sdk.Context) int {
 	// TODO: fix hack that forces the max square size for the first height to
 	// 64. This is due to our fork of the sdk not initializing state before
 	// BeginBlock of the first block. This is remedied in versions of the sdk
-	// and comet that have full support of PreparePropsoal, although
+	// and comet that have full support of PrepareProposal, although
 	// celestia-app does not currently use those. see this PR for more details
 	// https://github.com/cosmos/cosmos-sdk/pull/14505
 	if ctx.BlockHeader().Height <= 1 {
