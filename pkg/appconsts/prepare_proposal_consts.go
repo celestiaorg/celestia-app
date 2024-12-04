3f2b12791d3442516e4e05b7d6ac4cd9c2860cb5
Callum Waters <cmwaters19@gmail.com>
2024-12-04 12:14:27 +0100

    fix comment around max number of messages

--- pkg/appconsts/prepare_proposal_consts.go
+++ pkg/appconsts/prepare_proposal_consts.go
@@ -7,9 +7,9 @@ package appconsts
 // These numbers softly constrain the processing time of blocks to 0.25sec.
 // The benchmarks used to find these limits can be found in `app/benchmarks`.
 const (
-	// MaxPFBMessages is the maximum number of SDK messages, aside from PFBs, that a block can contain.
+	// MaxPFBMessages is the maximum number of PFB messages a block can contain.
 	MaxPFBMessages = 200
 
-	// MaxNonPFBMessages is the maximum number of PFB messages a block can contain.
+	// MaxNonPFBMessages is the maximum number of SDK messages, aside from PFBs, that a block can contain.
 	MaxNonPFBMessages = 600
 )
