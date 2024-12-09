818026ac446570605d3db582f2d261fb392e1973
Rootul P <rootulp@gmail.com>
2024-12-09 09:26:57 -0500

    feat: set defaults for `--v2-upgrade-height` (#4091)
    Closes https://github.com/celestiaorg/celestia-app/issues/3995
    
    If a user specifies the `--v2-upgrade-height` flag then it takes
    precedence over any defaults. If the user doesn't specify a
    `--v2-upgrade-height` flag and the chainID is `arabica-11`, `mocha-4`,
    or `celestia` then the v2 upgrade height is set to the block height that
    was coordinated on those chains.
    
    See:
    https://docs.celestia.org/how-to-guides/network-upgrade-process#lemongrass-network-upgrade
    
    ## Testing
    
    1. `./scripts/single-node.sh` doesn't set a v2 upgrade height
    2. `./arabica.sh` if we remove the `--v2-upgrade-height` flag in the
    script, gets defaulted correctly
    3. `./arabica.sh` if we override the `--v2-upgrade-height` flag in the
    script, that gets used

--- cmd/celestia-appd/cmd/start.go
+++ cmd/celestia-appd/cmd/start.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/celestiaorg/celestia-app/v3/pkg/appconsts"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -115,6 +116,20 @@ is performed. Note, when enabled, gRPC will also be automatically enabled.
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
+			}
+
+			switch clientCtx.ChainID {
+			case appconsts.ArabicaChainID:
+				serverCtx.Logger.Info(fmt.Sprintf("Since the chainID is %v, configuring the default v2 upgrade height to %v", appconsts.ArabicaChainID, appconsts.ArabicaUpgradeHeightV2))
+				serverCtx.Viper.SetDefault(UpgradeHeightFlag, appconsts.ArabicaUpgradeHeightV2)
+			case appconsts.MochaChainID:
+				serverCtx.Logger.Info(fmt.Sprintf("Since the chainID is %v, configuring the default v2 upgrade height to %v", appconsts.MochaChainID, appconsts.MochaUpgradeHeightV2))
+				serverCtx.Viper.SetDefault(UpgradeHeightFlag, appconsts.MochaUpgradeHeightV2)
+			case appconsts.MainnetChainID:
+				serverCtx.Logger.Info(fmt.Sprintf("Since the chainID is %v, configuring the default v2 upgrade height to %v", appconsts.MainnetChainID, appconsts.MainnetUpgradeHeightV2))
+				serverCtx.Viper.SetDefault(UpgradeHeightFlag, appconsts.MainnetUpgradeHeightV2)
+			default:
+				serverCtx.Logger.Info(fmt.Sprintf("No default value exists for the v2 upgrade height when the chainID is %v", clientCtx.ChainID))
 			}
 
 			withTM, _ := cmd.Flags().GetBool(flagWithTendermint)
