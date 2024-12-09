2b519cceadbdef76b2c669babc89ee177ef5c550
Rootul P <rootulp@gmail.com>
2024-12-09 09:26:57 -0500

    feat: set defaults for `--v2-upgrade-height` (#4091)
    Closes https://github.com/celestiaorg/celestia-app/issues/3995
    
    If a user specifies the `--v2-upgrade-height` flag then it takes
    precedence over any defaults. If the user doesn't specify a
    `--v2-upgrade-height` flag and the chainID is `arabica-11`, `mocha-4`,
    or `celestia` then the v2 upgrade height is set to the block height that
    was coordinated on those chains.
    
    See:
    https://docs.celestia.org/how-to-guides/network-upgrade-process#lemongrass-network-upgrade
    
    ## Testing
    
    1. `./scripts/single-node.sh` doesn't set a v2 upgrade height
    2. `./arabica.sh` if we remove the `--v2-upgrade-height` flag in the
    script, gets defaulted correctly
    3. `./arabica.sh` if we override the `--v2-upgrade-height` flag in the
    script, that gets used
    
    (cherry picked from commit 818026ac446570605d3db582f2d261fb392e1973)
    
    # Conflicts:
    #	cmd/celestia-appd/cmd/start.go
    #	pkg/appconsts/chain_ids.go

--- pkg/appconsts/chain_ids.go
+++ pkg/appconsts/chain_ids.go
@@ -4,8 +4,11 @@ const (
 	ArabicaChainID = "arabica-11"
 	MochaChainID   = "mocha-4"
 	MainnetChainID = "celestia"
+<<<<<<< HEAD
 	// TestChainID is the chain ID used for testing.
 	TestChainID = "test"
+=======
+>>>>>>> 818026a (feat: set defaults for `--v2-upgrade-height` (#4091))
 )
 
 var PublicNetworks = []string{ArabicaChainID, MochaChainID, MainnetChainID}
