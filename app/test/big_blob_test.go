3751aacd3a0d54038761dd33df99eca462536609
nina / ნინა <barbakadzeninaa@gmail.com>
2024-12-11 18:38:23 +0100

    fix: reject BlobTxs larger than 2 MiB (#4084)
    ## Overview
    
    Directly checks transaction sizes even before they're decoded and
    removes them if they exceed configured threshold.
    
    We should add MaxTxSize constraint in ProcessProposal directly and
    consider removing the AnteHandler in v4.
    Issue tracking this work:
    https://github.com/celestiaorg/celestia-app/issues/4087
    
    ## Testing
    
    - Check tx test asserts with logs that the expected error gets hit
    - Getting logs from prepare proposal was more challenging so i'm
    inserting a screenshot of application logs when the tests are run.
    <img width="887" alt="Screenshot 2024-12-06 at 12 27 03"
    src="https://github.com/user-attachments/assets/bb701834-5a3d-4eef-85f2-07074ae18a27">
    
    <img width="837" alt="Screenshot 2024-12-06 at 12 27 20"
    src="https://github.com/user-attachments/assets/651d9b87-3d65-43f4-a1a0-3874e03db455">
    
    
    ## Proposal for improving robustness of our test suites
    
    - [ ] Open an issue to assert all logs in our integration tests.
    
    ---------
    
    Co-authored-by: Rootul P <rootulp@gmail.com>

--- app/test/big_blob_test.go
+++ app/test/big_blob_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/celestiaorg/celestia-app/v3/app"
 	"github.com/celestiaorg/celestia-app/v3/app/encoding"
+	apperrors "github.com/celestiaorg/celestia-app/v3/app/errors"
 	"github.com/celestiaorg/celestia-app/v3/pkg/appconsts"
 	"github.com/celestiaorg/celestia-app/v3/pkg/user"
 	"github.com/celestiaorg/celestia-app/v3/test/util/testfactory"
@@ -55,7 +56,7 @@ func (s *BigBlobSuite) SetupSuite() {
 	require.NoError(t, cctx.WaitForNextBlock())
 }
 
-// TestErrBlobsTooLarge verifies that submitting a 2 MiB blob hits ErrBlobsTooLarge.
+// TestErrBlobsTooLarge verifies that submitting a ~1.9 MiB blob hits ErrBlobsTooLarge.
 func (s *BigBlobSuite) TestErrBlobsTooLarge() {
 	t := s.T()
 
@@ -67,8 +68,8 @@ func (s *BigBlobSuite) TestErrBlobsTooLarge() {
 	}
 	testCases := []testCase{
 		{
-			name: "2 mebibyte blob",
-			blob: newBlobWithSize(2 * mebibyte),
+			name: "~ 1.9 MiB blob",
+			blob: newBlobWithSize(2_000_000),
 			want: blobtypes.ErrBlobsTooLarge.ABCICode(),
 		},
 	}
@@ -85,6 +86,41 @@ func (s *BigBlobSuite) TestErrBlobsTooLarge() {
 			require.Nil(t, res)
 			code := err.(*user.BroadcastTxError).Code
 			require.Equal(t, tc.want, code, err.Error())
+		})
+	}
+}
+
+// TestBlobExceedsMaxTxSize verifies that submitting a 2 MiB blob hits ErrTxExceedsMaxSize.
+func (s *BigBlobSuite) TestBlobExceedsMaxTxSize() {
+	t := s.T()
+
+	type testCase struct {
+		name         string
+		blob         *share.Blob
+		expectedCode uint32
+		expectedErr  string
+	}
+	testCases := []testCase{
+		{
+			name:         "2 MiB blob",
+			blob:         newBlobWithSize(2097152),
+			expectedCode: apperrors.ErrTxExceedsMaxSize.ABCICode(),
+			expectedErr:  apperrors.ErrTxExceedsMaxSize.Error(),
+		},
+	}
+
+	txClient, err := testnode.NewTxClientFromContext(s.cctx)
+	require.NoError(t, err)
+
+	for _, tc := range testCases {
+		s.Run(tc.name, func() {
+			subCtx, cancel := context.WithTimeout(s.cctx.GoContext(), 30*time.Second)
+			defer cancel()
+			res, err := txClient.SubmitPayForBlob(subCtx, []*share.Blob{tc.blob}, user.SetGasLimitAndGasPrice(1e9, appconsts.DefaultMinGasPrice))
+			require.Error(t, err)
+			require.Nil(t, res)
+			code := err.(*user.BroadcastTxError).Code
+			require.Equal(t, tc.expectedCode, code, err.Error(), tc.expectedErr)
 		})
 	}
 }
