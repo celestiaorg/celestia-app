b1924edfe1f7d8ad745eb9831b10c78847d95898
Callum Waters <cmwaters19@gmail.com>
2025-01-02 15:45:24 -0500

    test across all mempool versions

--- pkg/user/e2e_test.go
+++ pkg/user/e2e_test.go
@@ -3,6 +3,7 @@ package user_test
 import (
 	"context"
 	"errors"
+	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -13,6 +14,7 @@ import (
 	"github.com/celestiaorg/celestia-app/v3/test/util/testnode"
 	"github.com/celestiaorg/go-square/v2/share"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/config"
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 )
 
@@ -21,49 +23,56 @@ func TestConcurrentTxSubmission(t *testing.T) {
 		t.Skip("skipping in short mode")
 	}
 
-	// Setup network
-	tmConfig := testnode.DefaultTendermintConfig()
-	tmConfig.Consensus.TimeoutCommit = 10 * time.Second
-	ctx, _, _ := testnode.NewNetwork(t, testnode.DefaultConfig().WithTendermintConfig(tmConfig))
-	_, err := ctx.WaitForHeight(1)
-	require.NoError(t, err)
+	// Iterate over all mempool versions
+	mempools := []string{config.MempoolV0, config.MempoolV1, config.MempoolV2}
+	for _, mempool := range mempools {
+		t.Run(fmt.Sprintf("mempool %s", mempool), func(t *testing.T) {
+			// Setup network
+			tmConfig := testnode.DefaultTendermintConfig()
+			tmConfig.Mempool.Version = mempool
+			tmConfig.Consensus.TimeoutCommit = 10 * time.Second
+			ctx, _, _ := testnode.NewNetwork(t, testnode.DefaultConfig().WithTendermintConfig(tmConfig))
+			_, err := ctx.WaitForHeight(1)
+			require.NoError(t, err)
 
-	// Setup signer
-	txClient, err := testnode.NewTxClientFromContext(ctx)
-	require.NoError(t, err)
+			// Setup signer
+			txClient, err := testnode.NewTxClientFromContext(ctx)
+			require.NoError(t, err)
 
-	// Pregenerate all the blobs
-	numTxs := 100
-	blobs := blobfactory.ManyRandBlobs(tmrand.NewRand(), blobfactory.Repeat(2048, numTxs)...)
+			// Pregenerate all the blobs
+			numTxs := 100
+			blobs := blobfactory.ManyRandBlobs(tmrand.NewRand(), blobfactory.Repeat(2048, numTxs)...)
 
-	// Prepare transactions
-	var (
-		wg    sync.WaitGroup
-		errCh = make(chan error, 1)
-	)
+			// Prepare transactions
+			var (
+				wg    sync.WaitGroup
+				errCh = make(chan error, 1)
+			)
 
-	subCtx, cancel := context.WithCancel(ctx.GoContext())
-	defer cancel()
-	time.AfterFunc(time.Minute, cancel)
-	for i := 0; i < numTxs; i++ {
-		wg.Add(1)
-		go func(b *share.Blob) {
-			defer wg.Done()
-			_, err := txClient.SubmitPayForBlob(subCtx, []*share.Blob{b}, user.SetGasLimitAndGasPrice(500_000, appconsts.DefaultMinGasPrice))
-			if err != nil && !errors.Is(err, context.Canceled) {
-				// only catch the first error
-				select {
-				case errCh <- err:
-					cancel()
-				default:
-				}
+			subCtx, cancel := context.WithCancel(ctx.GoContext())
+			defer cancel()
+			time.AfterFunc(time.Minute, cancel)
+			for i := 0; i < numTxs; i++ {
+				wg.Add(1)
+				go func(b *share.Blob) {
+					defer wg.Done()
+					_, err := txClient.SubmitPayForBlob(subCtx, []*share.Blob{b}, user.SetGasLimitAndGasPrice(500_000, appconsts.DefaultMinGasPrice))
+					if err != nil && !errors.Is(err, context.Canceled) {
+						// only catch the first error
+						select {
+						case errCh <- err:
+							cancel()
+						default:
+						}
+					}
+				}(blobs[i])
 			}
-		}(blobs[i])
-	}
-	wg.Wait()
-	select {
-	case err := <-errCh:
-		require.NoError(t, err)
-	default:
+			wg.Wait()
+			select {
+			case err := <-errCh:
+				require.NoError(t, err)
+			default:
+			}
+		})
 	}
 }
