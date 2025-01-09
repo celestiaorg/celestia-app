fb7cca374341cb1d804a4b116dad899421b909ef
Bilog WEB3 <155262265+Bilogweb3@users.noreply.github.com>
2025-01-09 18:34:52 +0100

    docs: fix typo (#4199)
    OutOfOrderNamesapceConfig - OutOfOrderNamespaceConfig
    
    thanks for your work

--- test/util/malicious/test_app.go
+++ test/util/malicious/test_app.go
@@ -22,7 +22,7 @@ import (
 	dbm "github.com/tendermint/tm-db"
 )
 
-// OutOfOrderNamesapceConfig returns a testnode config that will start producing
+// OutOfOrderNamespaceConfig returns a testnode config that will start producing
 // blocks with out of order namespaces at the provided height.
 //
 // Note: per the OutOfOrder go docs, the first two blobs with different
