20d783977d4249129b22400c51349e50f2522c58
Rootul Patel <rootulp@gmail.com>
2025-01-10 13:33:19 -0500

    feat: increase max-recv-msg-size default to 20 MiB

--- app/default_overrides_test.go
+++ app/default_overrides_test.go
@@ -64,6 +64,9 @@ func TestDefaultAppConfig(t *testing.T) {
 	assert.Equal(t, uint64(1500), cfg.StateSync.SnapshotInterval)
 	assert.Equal(t, uint32(2), cfg.StateSync.SnapshotKeepRecent)
 	assert.Equal(t, "0.002utia", cfg.MinGasPrices)
+
+	mebibyte := 1048576
+	assert.Equal(t, 20*mebibyte, cfg.GRPC.MaxRecvMsgSize)
 }
 
 func TestDefaultConsensusConfig(t *testing.T) {
