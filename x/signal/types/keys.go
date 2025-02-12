2b8e3168fadfde31fa95184035fd889c51f213f6
pepes <155114519+dennsikl@users.noreply.github.com>
2025-02-12 18:10:03 +0100

    Update keys.go

--- x/signal/types/keys.go
+++ x/signal/types/keys.go
@@ -8,5 +8,5 @@ var (
 	// FirstSignalKey is used as a divider to separate the UpgradeKey from all
 	// the keys associated with signals from validators. In practice, this key
 	// isn't expected to be set or retrieved.
-	FirstSignalKey = []byte{0x000}
+	FirstSignalKey = []byte{0x01}
 )
