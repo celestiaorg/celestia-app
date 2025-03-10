76dcff316655109e23016c2f297b16a9e3d60aee
Tristav <124001124+Pricstas@users.noreply.github.com>
2025-03-10 12:46:02 +0100

    fix insufficient_gas_price.go

--- app/errors/insufficient_gas_price.go
+++ app/errors/insufficient_gas_price.go
@@ -21,7 +21,7 @@ var (
 // the node should accept.
 // If the error is not due to the gas price being too low, it returns 0, nil
 func ParseInsufficientMinGasPrice(err error, gasPrice float64, gasLimit uint64) (float64, error) {
-	// first work out if the error is ErrInsufficientFunds
+	// first check if the error is ErrInsufficientFee
 	if err == nil || !sdkerrors.ErrInsufficientFee.Is(err) {
 		return 0, nil
 	}
@@ -69,7 +69,7 @@ func ParseInsufficientMinGasPrice(err error, gasPrice float64, gasLimit uint64) 
 
 // IsInsufficientMinGasPrice checks if the error is due to the gas price being too low.
 func IsInsufficientMinGasPrice(err error) bool {
-	// first work out if the error is ErrInsufficientFunds
+	// first check if the error is ErrInsufficientFee
 	if err == nil || !sdkerrors.ErrInsufficientFee.Is(err) {
 		return false
 	}
