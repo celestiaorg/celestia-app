de821192aa2933afa0219b5a50c1bcac2aaf0705
sweexordious <chamirachid1@gmail.com>
2025-03-13 00:09:43 +0400

    feat: initial gas estimation update algorithm

--- app/grpc/gasestimation/gas_estimator.go
+++ app/grpc/gasestimation/gas_estimator.go
@@ -2,23 +2,17 @@ package gasestimation
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"math"
-	"strconv"
-	"strings"
+	"github.com/tendermint/tendermint/types"
+	"sort"
 
 	"github.com/celestiaorg/celestia-app/v3/pkg/appconsts"
-	"github.com/celestiaorg/celestia-app/v3/x/minfee"
 	blobtx "github.com/celestiaorg/go-square/v2/tx"
 	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	gogogrpc "github.com/gogo/protobuf/grpc"
-	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 )
-
-// EstimationZScore is the z-score corresponding to 10% and 90% of the gas prices distribution.
-// More information can be found in: https://en.wikipedia.org/wiki/Standard_normal_table#Cumulative_(less_than_Z)
-const EstimationZScore = 1.28
 
 // baseAppSimulateFn is the signature of the Baseapp#Simulate function.
 type baseAppSimulateFn func(txBytes []byte) (sdk.GasInfo, *sdk.Result, error)
@@ -36,6 +30,7 @@ var _ GasEstimatorServer = &gasEstimatorServer{}
 type gasEstimatorServer struct {
 	clientCtx  client.Context
 	simulateFn baseAppSimulateFn
+	txDecoder  sdk.TxDecoder
 }
 
 func NewGasEstimatorServer(clientCtx client.Context, simulateFn baseAppSimulateFn) GasEstimatorServer {
@@ -44,21 +39,6 @@ func NewGasEstimatorServer(clientCtx client.Context, simulateFn baseAppSimulateF
 		simulateFn: simulateFn,
 	}
 }
-
-// lastFiveBlocksTransactionsQuery transaction search query to get all the transactions in the last five blocks.
-// the latestHeight param represents the chain's tip height.
-func lastFiveBlocksTransactionsQuery(latestHeight int64) string {
-	startHeight := latestHeight - 5
-	if startHeight < 0 {
-		startHeight = 0
-	}
-	return fmt.Sprintf("tx.height>%d AND tx.height<=%d", startHeight, latestHeight)
-}
-
-// numberOfTransactionsPerPage the number of transactions to return per page in the transaction search
-// endpoint.
-// Note: the maximum number of transactions per page the endpoint allows is 100.
-var numberOfTransactionsPerPage = 100
 
 func (s *gasEstimatorServer) EstimateGasPrice(ctx context.Context, request *EstimateGasPriceRequest) (*EstimateGasPriceResponse, error) {
 	gasPrice, err := s.estimateGasPrice(ctx, request.TxPriority)
@@ -105,59 +85,27 @@ func (s *gasEstimatorServer) EstimateGasPriceAndUsage(ctx context.Context, reque
 	}, nil
 }
 
+// gasPriceEstimationThreshold the threshold of mempool transactions to
+// estimate the gas price.
+var gasPriceEstimationThreshold = 0.70
+
 // estimateGasPrice takes a transaction priority and estimates the gas price based
-// on the gas prices of the transactions in the last five blocks.
-// If no transaction is found in the last five blocks, return the network
-// min gas price.
-// It's up to the light client to set the gas price in this case
-// to the minimum gas price set by that node.
+// on the gas prices of the transactions in the mempool.
+// If the mempool transactions can't fill more than 70% of the block, the min gas
+// price is returned.
 func (s *gasEstimatorServer) estimateGasPrice(ctx context.Context, priority TxPriority) (float64, error) {
-	status, err := s.clientCtx.Client.Status(ctx)
+	// using -1 to return all the transactions.
+	limit := -1
+	txsResp, err := s.clientCtx.Client.UnconfirmedTxs(ctx, &limit)
 	if err != nil {
 		return 0, err
 	}
-	latestHeight := status.SyncInfo.LatestBlockHeight
-	page := 1
-	txSearchResult, err := s.clientCtx.Client.TxSearch(
-		ctx,
-		lastFiveBlocksTransactionsQuery(latestHeight),
-		false,
-		&page,
-		&numberOfTransactionsPerPage,
-		"asc",
-	)
+	if txsResp.TotalBytes < int64(appconsts.DefaultMaxBytes*gasPriceEstimationThreshold) {
+		return appconsts.DefaultMinGasPrice, nil
+	}
+	gasPrices, err := sortAndExtractGasPrices(s.txDecoder, txsResp.Txs, int64(appconsts.DefaultUpperBoundMaxBytes))
 	if err != nil {
 		return 0, err
-	}
-
-	totalNumberOfTransactions := txSearchResult.TotalCount
-	if totalNumberOfTransactions == 0 {
-		// return the min gas price if no transaction found in the last 5 blocks
-		return minfee.DefaultNetworkMinGasPrice.MustFloat64(), nil
-	}
-
-	gasPrices := make([]float64, 0)
-	for {
-		currentPageGasPrices, err := extractGasPriceFromTransactions(txSearchResult.Txs)
-		if err != nil {
-			return 0, err
-		}
-		gasPrices = append(gasPrices, currentPageGasPrices...)
-		if len(gasPrices) >= totalNumberOfTransactions {
-			break
-		}
-		page++
-		txSearchResult, err = s.clientCtx.Client.TxSearch(
-			ctx,
-			lastFiveBlocksTransactionsQuery(latestHeight),
-			false,
-			&page,
-			&numberOfTransactionsPerPage,
-			"asc",
-		)
-		if err != nil {
-			return 0, err
-		}
 	}
 	return estimateGasPriceForTransactions(gasPrices, priority)
 }
@@ -165,84 +113,85 @@ func (s *gasEstimatorServer) estimateGasPrice(ctx context.Context, priority TxPr
 // estimateGasPriceForTransactions takes a list of transactions and priority
 // and returns a gas price estimation.
 // The priority sets the estimation as follows:
+// TODO update
 // - High Priority: The gas price is the price at the start of the top 10% of transactions’ gas prices from the last five blocks.
 // - Medium Priority: The gas price is the mean of all gas prices from the last five blocks.
 // - Low Priority: The gas price is the value at the end of the lowest 10% of gas prices from the last five blocks.
 // - Unspecified Priority (default): This is equivalent to the Medium priority, using the mean of all gas prices from the last five blocks.
 // More information can be found in ADR-023.
 func estimateGasPriceForTransactions(gasPrices []float64, priority TxPriority) (float64, error) {
-	meanGasPrice := Mean(gasPrices)
 	switch priority {
 	case TxPriority_TX_PRIORITY_UNSPECIFIED:
-		return meanGasPrice, nil
+		return Median(gasPrices)
 	case TxPriority_TX_PRIORITY_LOW:
-		stDev := StandardDeviation(meanGasPrice, gasPrices)
-		return meanGasPrice - EstimationZScore*stDev, nil
+		return Median(gasPrices[:len(gasPrices)*10/100])
 	case TxPriority_TX_PRIORITY_MEDIUM:
-		return meanGasPrice, nil
+		return Median(gasPrices)
 	case TxPriority_TX_PRIORITY_HIGH:
-		stDev := StandardDeviation(meanGasPrice, gasPrices)
-		return meanGasPrice + EstimationZScore*stDev, nil
+		return Median(gasPrices[len(gasPrices)*90/100:])
 	default:
 		return 0, fmt.Errorf("unknown priority: %d", priority)
 	}
 }
 
-// extractGasPriceFromTransactions takes a list of transaction results
+// sortAndExtractGasPrices takes a list of transaction results
 // and returns their corresponding gas prices.
-func extractGasPriceFromTransactions(txs []*coretypes.ResultTx) ([]float64, error) {
-	gasPrices := make([]float64, 0)
-	for _, tx := range txs {
-		var feeWithDenom string
-		for _, event := range tx.TxResult.Events {
-			if event.GetType() == "tx" {
-				for _, attr := range event.Attributes {
-					if string(attr.Key) == "fee" {
-						feeWithDenom = string(attr.Value)
-					}
-				}
+// The total size of the returned transactions won't exceed the max byte parameter.
+func sortAndExtractGasPrices(txDecoder sdk.TxDecoder, txs []types.Tx, maxBytes int64) ([]float64, error) {
+	type gasPriceAndSize struct {
+		gasPrice float64
+		size     int64
+	}
+	gasPriceAndSizes := make([]gasPriceAndSize, len(txs))
+	for index, rawTx := range txs {
+		txBytes := rawTx
+		bTx, isBlob, err := blobtx.UnmarshalBlobTx(rawTx)
+		if isBlob {
+			if err != nil {
+				panic(err)
 			}
+			txBytes = bTx.Tx
 		}
-		if feeWithDenom == "" {
-			return nil, fmt.Errorf("couldn't find fee for transaction %s", tx.Hash)
-		}
-		feeWithoutDenom, found := strings.CutSuffix(feeWithDenom, appconsts.BondDenom)
-		if !found {
-			return nil, fmt.Errorf("couldn't find fee denom for transaction %s: %s", tx.Hash, feeWithDenom)
-		}
-		fee, err := strconv.ParseFloat(feeWithoutDenom, 64)
+		sdkTx, err := txDecoder(txBytes)
 		if err != nil {
-			return nil, fmt.Errorf("couldn't parse fee for transaction %s: %w", tx.Hash, err)
+			return nil, err
 		}
-		if tx.TxResult.GasWanted == 0 {
-			return nil, fmt.Errorf("zero gas wanted for transaction %s", tx.Hash)
+		feeTx := sdkTx.(sdk.FeeTx)
+		gasPrice := float64(feeTx.GetFee().AmountOf(appconsts.BondDenom).Uint64()) / float64(feeTx.GetGas())
+		gasPriceAndSizes[index] = gasPriceAndSize{
+			size:     int64(len(rawTx)),
+			gasPrice: gasPrice,
 		}
-		gasPrices = append(gasPrices, fee/float64(tx.TxResult.GasWanted))
+	}
+
+	// sort the gas prices in descending order
+	sort.Slice(gasPriceAndSizes, func(i, j int) bool {
+		return gasPriceAndSizes[i].gasPrice > gasPriceAndSizes[j].gasPrice
+	})
+
+	gasPrices := make([]float64, 0)
+	totalSize := int64(0)
+	for _, tx := range gasPriceAndSizes {
+		if tx.size+totalSize > maxBytes {
+			break
+		}
+		gasPrices = append(gasPrices, tx.gasPrice)
 	}
 	return gasPrices, nil
 }
 
-// Mean calculates the mean value of the provided gas prices.
-func Mean(gasPrices []float64) float64 {
-	if len(gasPrices) == 0 {
-		return 0
+// Median calculates the median value of the provided gas prices.
+func Median(gasPrices []float64) (float64, error) {
+	n := len(gasPrices)
+	if n == 0 {
+		return 0, errors.New("cannot compute median of an empty slice")
 	}
-	sum := 0.0
-	for _, gasPrice := range gasPrices {
-		sum += gasPrice
-	}
-	return sum / float64(len(gasPrices))
-}
+	sort.Float64s(gasPrices)
 
-// StandardDeviation calculates the standard deviation of the provided gas prices.
-func StandardDeviation(meanGasPrice float64, gasPrices []float64) float64 {
-	if len(gasPrices) < 2 {
-		return 0
+	if n%2 == 1 {
+		return gasPrices[n/2], nil
 	}
-	var variance float64
-	for _, gasPrice := range gasPrices {
-		variance += math.Pow(gasPrice-meanGasPrice, 2)
-	}
-	variance /= float64(len(gasPrices))
-	return math.Sqrt(variance)
+	mid1 := gasPrices[n/2-1]
+	mid2 := gasPrices[n/2]
+	return (mid1 + mid2) / 2.0, nil
 }
