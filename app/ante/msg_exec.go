934b5b0516e6cdd2257d944817b2cbdad80e3402
Rootul P <rootulp@gmail.com>
2025-03-28 13:00:24 -0400

    feat(ante): reject MsgExec if it contains a MsgExec or MsgPayForBlobs (#4470)
    Closes https://github.com/celestiaorg/celestia-app/issues/3609 by adding
    a new ante handler that rejects a tx if it contains a MsgExec which
    contains a MsgExec or MsgPayForBlobs.
    
    (cherry picked from commit caa3f1d4136fdb14ec46be4eb2a23c393ed48977)

---
+++ app/ante/msg_exec.go
@@ -0,0 +1,38 @@
+package ante
+
+import (
+	blobtypes "github.com/celestiaorg/celestia-app/v3/x/blob/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+)
+
+var _ sdk.AnteDecorator = MsgExecDecorator{}
+
+// MsgExecDecorator ensures that the tx does not contain a MsgExec with a
+// nested MsgExec or MsgPayForBlobs.
+type MsgExecDecorator struct{}
+
+func NewMsgExecDecorator() *MsgExecDecorator {
+	return &MsgExecDecorator{}
+}
+
+func (mgk MsgExecDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	for _, msg := range tx.GetMsgs() {
+		if msgExec, ok := msg.(*authz.MsgExec); ok {
+			nestedMsgs, err := msgExec.GetMessages()
+			if err != nil {
+				return ctx, err
+			}
+			for _, nestedMsg := range nestedMsgs {
+				if _, ok := nestedMsg.(*authz.MsgExec); ok {
+					return ctx, sdkerrors.ErrNotSupported.Wrapf("MsgExec inside MsgExec is not supported")
+				}
+				if _, ok := nestedMsg.(*blobtypes.MsgPayForBlobs); ok {
+					return ctx, sdkerrors.ErrNotSupported.Wrapf("MsgPayForBlobs inside MsgExec is not supported")
+				}
+			}
+		}
+	}
+	return next(ctx, tx, simulate)
+}
