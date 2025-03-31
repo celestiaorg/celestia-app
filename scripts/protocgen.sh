fe3bdc3e489495b9d7ce7fee73f113ae58cfbd42
wizard <112275929+famouswizard@users.noreply.github.com>
2025-03-31 12:58:03 +0300

    chore: fix `find` issue with `-prune` in proto search

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -12,7 +12,7 @@ protoc_gen_gocosmos() {
 protoc_gen_gocosmos
 
 cd proto
-proto_dirs=$(find . -path ./cosmos -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find . ! -path "./cosmos/*" -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
