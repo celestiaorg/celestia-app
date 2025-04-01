482f385d410df588151f75de3fac410ef51449e7
Nina Barbakadze <barbakadzeninaa@gmail.com>
2025-04-01 19:29:26 +0200

    chore: make the error message less verbose'

--- app/errors/errors.go
+++ app/errors/errors.go
@@ -11,5 +11,5 @@ const AppErrorsCodespace = "app"
 // Application error codes start from 11000 to avoid conflicts with other modules
 var (
 	// ErrTxExceedsMaxSize is returned when a transaction size exceeds the maximum allowed limit
-	ErrTxExceedsMaxSize = errors.Register(AppErrorsCodespace, 11142, "transaction size exceeds maximum allowed limit")
+	ErrTxExceedsMaxSize = errors.Register(AppErrorsCodespace, 11142, "tx too large")
 )
