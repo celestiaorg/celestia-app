08abf659519b93759ca2801ce84a1d000dca0976
Rootul Patel <rootulp@gmail.com>
2025-04-11 12:41:46 -0400

    chore: resolve TODO for block height 1

--- app/square_size.go
+++ app/square_size.go
@@ -8,18 +8,7 @@ import (
 
 // MaxEffectiveSquareSize returns the max effective square size.
 func (app *App) MaxEffectiveSquareSize(ctx sdk.Context) int {
-	// TODO: fix hack that forces the max square size for the first height to
-	// 64. This is due to our fork of the sdk not initializing state before
-	// BeginBlock of the first block. This is remedied in versions of the sdk
-	// and comet that have full support of PrepareProposal, although
-	// celestia-app does not currently use those. see this PR for more details
-	// https://github.com/cosmos/cosmos-sdk/pull/14505
-	if ctx.BlockHeader().Height <= 1 {
-		return int(appconsts.DefaultGovMaxSquareSize)
-	}
-
 	govMax := int(app.BlobKeeper.GetParams(ctx).GovMaxSquareSize)
-
 	hardMax := appconsts.DefaultSquareSizeUpperBound
 	return min(govMax, hardMax)
 }
