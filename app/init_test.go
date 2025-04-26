669a75d55077d02d1984c971d80994ca9a16b54d
John Adler <john.adler@protonmail.ch>
2025-04-26 17:33:34 -0400

    Fix test.

--- app/init_test.go
+++ app/init_test.go
@@ -8,42 +8,47 @@ import (
 
 func Test_getDefaultNodeHome(t *testing.T) {
 	type testCase struct {
-		name         string
-		userHome     string
-		celestiaHome string
-		want         string
+		name            string
+		userHome        string
+		celestiaHome    string
+		celestiaHomeOld string
+		want            string
 	}
 
 	testCases := []testCase{
 		{
-			name:         "want .celestia-app when userHome is empty and celestiaHome is empty",
-			userHome:     "",
-			celestiaHome: "",
-			want:         ".celestia-app",
+			name:            "want .celestia-app when userHome is empty and celestiaHomeOld is empty",
+			userHome:        "",
+			celestiaHome:    "",
+			celestiaHomeOld: "",
+			want:            ".celestia-app",
 		},
 		{
-			name:         "want celestia-home/.celestia-app when userHome is empty and celestiaHome is not empty",
-			userHome:     "",
-			celestiaHome: "celestia-home",
-			want:         "celestia-home/.celestia-app",
+			name:            "want celestia-home/.celestia-app when userHome is empty and celestiaHomeOld is not empty",
+			userHome:        "",
+			celestiaHome:    "",
+			celestiaHomeOld: "celestia-home",
+			want:            "celestia-home/.celestia-app",
 		},
 		{
-			name:         "want user-home/.celestia-app when userHome is not empty and celestiaHome is empty",
-			userHome:     "user-home",
-			celestiaHome: "",
-			want:         "user-home/.celestia-app",
+			name:            "want user-home/.celestia-app when userHome is not empty and celestiaHomeOld is empty",
+			userHome:        "user-home",
+			celestiaHome:    "",
+			celestiaHomeOld: "",
+			want:            "user-home/.celestia-app",
 		},
 		{
-			name:         "want celestiaHome to take precedence if both are not empty",
-			userHome:     "user-home",
-			celestiaHome: "celestia-home",
-			want:         "celestia-home/.celestia-app",
+			name:            "want celestiaHomeOld to take precedence if both are not empty",
+			userHome:        "user-home",
+			celestiaHome:    "",
+			celestiaHomeOld: "celestia-home",
+			want:            "celestia-home/.celestia-app",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := getDefaultNodeHome(tc.userHome, tc.celestiaHome, tc.celestiaHome)
+			got := getDefaultNodeHome(tc.userHome, tc.celestiaHome, tc.celestiaHomeOld)
 			assert.Equal(t, tc.want, got)
 		})
 	}
