cc96936aded68ffe74964309adad29586ade61f3
John Adler <john.adler@protonmail.ch>
2025-04-26 21:24:03 -0400

    Add tests.

--- app/init_test.go
+++ app/init_test.go
@@ -17,7 +17,7 @@ func Test_getDefaultNodeHome(t *testing.T) {
 
 	testCases := []testCase{
 		{
-			name:            "want .celestia-app when userHome is empty and celestiaHomeOld is empty",
+			name:            "want .celestia-app when userHome is empty and celestiaHome/Old are empty",
 			userHome:        "",
 			celestiaHome:    "",
 			celestiaHomeOld: "",
@@ -31,18 +31,46 @@ func Test_getDefaultNodeHome(t *testing.T) {
 			want:            "celestia-home/.celestia-app",
 		},
 		{
-			name:            "want user-home/.celestia-app when userHome is not empty and celestiaHomeOld is empty",
+			name:            "want user-home/.celestia-app when userHome is not empty and celestiaHome/Old are empty",
 			userHome:        "user-home",
 			celestiaHome:    "",
 			celestiaHomeOld: "",
 			want:            "user-home/.celestia-app",
 		},
 		{
-			name:            "want celestiaHomeOld to take precedence if both are not empty",
+			name:            "want celestiaHomeOld to take precedence if both it and userHome are not empty",
 			userHome:        "user-home",
 			celestiaHome:    "",
 			celestiaHomeOld: "celestia-home",
 			want:            "celestia-home/.celestia-app",
+		},
+		{
+			name:            "want celestia-home when userHome is empty and celestiaHome is not empty",
+			userHome:        "",
+			celestiaHome:    "celestia-home",
+			celestiaHomeOld: "",
+			want:            "celestia-home",
+		},
+		{
+			name:            "want celestiaHome to take precedence if both it and userHome are not empty",
+			userHome:        "user-home",
+			celestiaHome:    "celestia-home",
+			celestiaHomeOld: "",
+			want:            "celestia-home",
+		},
+		{
+			name:            "want celestiaHome to take precedence if both it and celestiaHomeOld are not empty",
+			userHome:        "",
+			celestiaHome:    "celestia-home",
+			celestiaHomeOld: "celestia-home/.celestia-appd",
+			want:            "celestia-home",
+		},
+		{
+			name:            "want celestiaHome to take precedence if all are not empty",
+			userHome:        "user-home",
+			celestiaHome:    "celestia-home",
+			celestiaHomeOld: "celestia-home/.celestia-appd",
+			want:            "celestia-home",
 		},
 	}
 
