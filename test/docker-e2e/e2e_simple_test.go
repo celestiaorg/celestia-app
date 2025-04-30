aeaf38c661fe405085e9968fcefcae216f066137
chatton <github.qpeyb@simplelogin.fr>
2025-04-30 14:34:28 +0100

    test: converted to test suite

--- test/docker-e2e/e2e_simple_test.go
+++ test/docker-e2e/e2e_simple_test.go
@@ -2,65 +2,26 @@ package docker_e2e
 
 import (
 	"context"
-	"github.com/celestiaorg/celestia-app/v4/app"
 	"github.com/celestiaorg/celestia-app/v4/test/util/testnode"
-	"github.com/chatton/interchaintest"
 	"github.com/chatton/interchaintest/chain/cosmos"
-	"github.com/chatton/interchaintest/ibc"
-	maps "github.com/chatton/interchaintest/testutil/maps"
-	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/stretchr/testify/require"
-	"go.uber.org/zap/zaptest"
+	"github.com/stretchr/testify/suite"
 	"testing"
 	"time"
 )
 
-func TestE2ESimple(t *testing.T) {
+func TestCelestiaTestSuite(t *testing.T) {
+	suite.Run(t, new(CelestiaTestSuite))
+}
+
+func (s *CelestiaTestSuite) TestE2ESimple() {
+	t := s.T()
 	if testing.Short() {
 		t.Skip("skipping in short mode")
 	}
 
-	// Create a new logger for the test
-	logger := zaptest.NewLogger(t)
-
-	// Setup Docker client and network
-	client, network := interchaintest.DockerSetup(t)
-
-	// Define the number of validators
-	numValidators := 4
-	numFullNodes := 0
-
-	enc := testutil.MakeTestEncodingConfig(app.ModuleEncodingRegisters...)
-	// Create a single Celestia chain directly
-	celestia, err := interchaintest.NewChain(logger, t.Name(), client, network, &interchaintest.ChainSpec{
-		Name:          "celestia",
-		ChainName:     "celestia",
-		Version:       "v4.0.0-rc1",
-		NumValidators: &numValidators,
-		NumFullNodes:  &numFullNodes,
-		Config: ibc.Config{
-			ModifyGenesis: func(config ibc.Config, bytes []byte) ([]byte, error) {
-				return maps.SetField(bytes, "consensus.params.version.app", "4")
-			},
-			EncodingConfig:      &enc,
-			AdditionalStartArgs: []string{"--force-no-bbr", "--grpc.enable", "--grpc.address", "0.0.0.0:9090", "--rpc.grpc_laddr=tcp://0.0.0.0:9099"},
-			Type:                "cosmos",
-			ChainID:             "celestia",
-			Images: []ibc.DockerImage{
-				{
-					Repository: "ghcr.io/celestiaorg/celestia-app-multiplexer",
-					Version:    "v4.0.0-rc1",
-					UIDGID:     "10001:10001",
-				},
-			},
-			Bin:           "celestia-appd",
-			Bech32Prefix:  "celestia",
-			Denom:         "utia",
-			GasPrices:     "0.025utia",
-			GasAdjustment: 1.3,
-		},
-	})
-	require.NoError(t, err)
+	celestia, err := s.CreateCelestiaChain("v4.0.0-rc1", "4")
+	s.Require().NoError(err)
 
 	// Start the chain
 	ctx := context.Background()
@@ -83,12 +44,7 @@ func TestE2ESimple(t *testing.T) {
 	cosmosChain, ok := celestia.(*cosmos.Chain)
 	require.True(t, ok, "expected celestia to be a cosmos.Chain")
 
-	// Verify we have the expected number of validators
-	require.Equal(t, numValidators, len(cosmosChain.Validators))
-
-	t.Logf("Successfully started Celestia chain with %d validators", numValidators)
-
-	createTxSim(t, err, ctx, client, network, logger, cosmosChain)
+	createTxSim(t, err, ctx, s.client, s.network, s.logger, cosmosChain)
 
 	// Wait for a short time to allow txsim to start
 	time.Sleep(10 * time.Second)
