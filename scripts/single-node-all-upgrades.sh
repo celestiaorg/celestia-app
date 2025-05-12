d47d65302e7666abfd92a3a7302833ac8097d782
Rootul Patel <rootulp@gmail.com>
2025-05-11 21:56:10 -0400

    feat(scripts): test all upgrades

---
+++ scripts/single-node-all-upgrades.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# This script starts a local single node testnet on app version 1 and then upgrades to app version 2, 3, and 4.
+
+# Stop script execution if an error is encountered
+set -o errexit
+# Stop script execution if an undefined variable is used
+set -o nounset
+
+if ! [ -x "$(command -v celestia-appd)" ]
+then
+    echo "celestia-appd could not be found. Please install the celestia-appd binary using 'make install' and make sure the PATH contains the directory where the binary exists. By default, go will install the binary under '~/go/bin'"
+    exit 1
+fi
+
+# Constants
+CHAIN_ID="test"
+KEY_NAME="validator"
+KEYRING_BACKEND="test"
+FEES="500utia"
+BROADCAST_MODE="sync"
+
+# Use argument as home directory if provided, else default to ~/.celestia-app
+if [ $# -ge 1 ]; then
+  APP_HOME="$1"
+else
+  APP_HOME="${HOME}/.celestia-app"
+fi
+
+VERSION=$(celestia-appd version 2>&1)
+GENESIS_FILE="${APP_HOME}/config/genesis.json"
+
+echo "celestia-app version: ${VERSION}"
+echo "celestia-app home: ${APP_HOME}"
+echo "celestia-app genesis file: ${GENESIS_FILE}"
+echo ""
+
+createGenesis() {
+    echo "Initializing validator and node config files..."
+    celestia-appd passthrough 1 init ${CHAIN_ID} \
+      --chain-id ${CHAIN_ID} \
+      --home "${APP_HOME}" \
+      > /dev/null 2>&1 # Hide output to reduce terminal noise
+
+    echo "Adding a new key to the keyring..."
+    celestia-appd keys add ${KEY_NAME} \
+      --keyring-backend=${KEYRING_BACKEND} \
+      --home "${APP_HOME}" \
+      > /dev/null 2>&1 # Hide output to reduce terminal noise
+
+    echo "Adding genesis account..."
+    celestia-appd passthrough 1 add-genesis-account \
+      "$(celestia-appd keys show ${KEY_NAME} -a --keyring-backend=${KEYRING_BACKEND} --home "${APP_HOME}")" \
+      "1000000000000000utia" \
+      --home "${APP_HOME}"
+
+    echo "Creating a genesis tx..."
+    celestia-appd passthrough 1 gentx ${KEY_NAME} 5000000000utia \
+      --fees ${FEES} \
+      --keyring-backend=${KEYRING_BACKEND} \
+      --chain-id ${CHAIN_ID} \
+      --home "${APP_HOME}" \
+      > /dev/null 2>&1 # Hide output to reduce terminal noise
+
+    echo "Collecting genesis txs..."
+    celestia-appd passthrough 1 collect-gentxs \
+      --home "${APP_HOME}" \
+        > /dev/null 2>&1 # Hide output to reduce terminal noise
+
+    # Override the default RPC server listening address
+    sed -i'.bak' 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' "${APP_HOME}"/config/config.toml
+
+    # Enable transaction indexing
+    sed -i'.bak' 's#"null"#"kv"#g' "${APP_HOME}"/config/config.toml
+
+    # Persist ABCI responses
+    sed -i'.bak' 's#discard_abci_responses = true#discard_abci_responses = false#g' "${APP_HOME}"/config/config.toml
+
+    # Override the VotingPeriod from 1 week to 1 minute
+    sed -i'.bak' 's#"604800s"#"60s"#g' "${APP_HOME}"/config/genesis.json
+
+    # Override the log level to debug
+    sed -i'.bak' 's#log_level = "info"#log_level = "debug"#g' "${APP_HOME}"/config/config.toml
+}
+
+deleteCelestiaAppHome() {
+    echo "Deleting $APP_HOME..."
+    rm -r "$APP_HOME"
+}
+
+startCelestiaApp() {
+  echo "Starting celestia-app..."
+  celestia-appd start \
+    --home "${APP_HOME}" \
+    --api.enable \
+    --grpc.enable \
+    --grpc-web.enable \
+    --timeout-commit 1s \
+    --force-no-bbr \
+    --v2-upgrade-height 3
+}
+
+startCelestiaApp # Start celestia-app in the foreground.
