e4532fdce7400abbbec9e9075186afc86b875ead
Damian Nolan <damiannolan@gmail.com>
2025-05-26 12:27:54 +0200

    chore: update doc.go

--- x/zkism/doc.go
+++ x/zkism/doc.go
@@ -1,3 +1,5 @@
 // The x/zkism module is an implementation of a Hyperlane Interchain Security Module.
-// The purpose of this module is to provide a means for proving execution of a remote chain through a standardised interface, the ISM.
+// This module is a custom implement of an ISM (Interchain Security Module). It's purpose is to provide a means for proving execution
+// of a remote chain through a standardised interface, the ISM. Thus, we leverage the transport semantics of the Hyperlane messaging
+// protocol, while providing a custom security mechanism.
 package zkism
