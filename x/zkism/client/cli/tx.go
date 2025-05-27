fdfb501106c1cf4380408a632204c64c40809e3d
Damian Nolan <damiannolan@gmail.com>
2025-05-27 10:25:17 +0200

    chore: address feedback from review

--- x/zkism/client/cli/tx.go
+++ x/zkism/client/cli/tx.go
@@ -20,8 +20,8 @@ func NewCreateZKExecutionIsmCmd() *cobra.Command {
 		Use:   "create [state-transition-key-hex] [state-membership-key-hex]",
 		Short: "Create a new ZK Execution ISM for use with the Hyperlane messaging protocol.",
 		Long: strings.TrimSpace(`Create a new ZK Execution Interchain Security Module (ISM) for use with the Hyperlane messaging protocol.
-This CLI command requires both the StateTransitionVerifierKey and the StateMembershipVerifierKey to be provided a hex-encoded byte string arguments.`),
-		Example: fmt.Sprintf("%s tx %s create 4f2c7b...3f d18e6c7...3a", version.AppName, types.ModuleName),
+This CLI command requires both the StateTransitionVerifierKey and the StateMembershipVerifierKey to be provided as hex-encoded byte string arguments.`),
+		Example: fmt.Sprintf("%s tx %s create 3f8a8f3be3cd62e2f9b742de9e4b2c1f5a62a7e0e52a29b4bb4d7a6a2fcaf9c2 2c9fafc2a6a7d4bb4b92a2e5e0a7625a1f2c4b9ede42b7f9e262cde3b8f8a3f", version.AppName, types.ModuleName),
 		Args:    cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
