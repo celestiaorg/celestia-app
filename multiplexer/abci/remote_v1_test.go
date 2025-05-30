9209bf6625a3eebb30dbb9d9c05007bc68a2790e
Rootul Patel <rootulp@gmail.com>
2025-05-30 11:36:59 +0200

    fix(multiplexer): propagate TimeoutInfo for remote v1

--- multiplexer/abci/remote_v1_test.go
+++ multiplexer/abci/remote_v1_test.go
@@ -3,7 +3,9 @@ package abci
 import (
 	"testing"
 
+	abciv2 "github.com/cometbft/cometbft/abci/types"
 	"github.com/stretchr/testify/assert"
+	abciv1 "github.com/tendermint/tendermint/abci/types"
 )
 
 func TestConsensusParamsV1ToV2(t *testing.T) {
@@ -11,4 +13,17 @@ func TestConsensusParamsV1ToV2(t *testing.T) {
 		got := consensusParamsV1ToV2(nil)
 		assert.Nil(t, got)
 	})
+}
+
+func Test_timeoutInfoV1ToV2(t *testing.T) {
+	info := abciv1.TimeoutsInfo{
+		TimeoutPropose: 1,
+		TimeoutCommit:  2,
+	}
+	want := abciv2.TimeoutInfo{
+		TimeoutPropose: 1,
+		TimeoutCommit:  2,
+	}
+	got := timeoutInfoV1ToV2(info)
+	assert.Equal(t, want, got)
 }
