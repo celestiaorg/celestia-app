daa7ffd9ea580a8cac362f27b3a2275142674056
Rootul Patel <rootulp@gmail.com>
2025-05-30 12:38:26 +0200

    test: unit test Info

--- multiplexer/abci/remote_v1_test.go
+++ multiplexer/abci/remote_v1_test.go
@@ -1,11 +1,14 @@
 package abci
 
 import (
+	"context"
 	"testing"
 
 	abciv2 "github.com/cometbft/cometbft/abci/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	abciv1 "github.com/tendermint/tendermint/abci/types"
+	"google.golang.org/grpc"
 )
 
 func TestConsensusParamsV1ToV2(t *testing.T) {
@@ -15,7 +18,7 @@ func TestConsensusParamsV1ToV2(t *testing.T) {
 	})
 }
 
-func Test_timeoutInfoV1ToV2(t *testing.T) {
+func TestTimeoutInfoV1ToV2(t *testing.T) {
 	info := abciv1.TimeoutsInfo{
 		TimeoutPropose: 1,
 		TimeoutCommit:  2,
@@ -26,4 +29,41 @@ func Test_timeoutInfoV1ToV2(t *testing.T) {
 	}
 	got := timeoutInfoV1ToV2(info)
 	assert.Equal(t, want, got)
+}
+
+func TestInfo(t *testing.T) {
+	// This test verifies the regression in
+	// https://github.com/celestiaorg/celestia-app/issues/4859
+	t.Run("should convert TimeoutInfo from the abciv1 client to an abciv2 response", func(t *testing.T) {
+		mockClient := &mockABCIApplicationClient{
+			infoResp: &abciv1.ResponseInfo{
+				Timeouts: abciv1.TimeoutsInfo{
+					TimeoutPropose: 1,
+					TimeoutCommit:  2,
+				},
+			},
+		}
+
+		client := &RemoteABCIClientV1{
+			ABCIApplicationClient: mockClient,
+		}
+
+		want := abciv2.TimeoutInfo{
+			TimeoutPropose: 1,
+			TimeoutCommit:  2,
+		}
+		got, err := client.Info(&abciv2.RequestInfo{})
+		require.NoError(t, err)
+		assert.Equal(t, want, got.TimeoutInfo)
+	})
+}
+
+// mockABCIApplicationClient is a mock implementation of ABCIApplicationClient
+type mockABCIApplicationClient struct {
+	abciv1.ABCIApplicationClient
+	infoResp *abciv1.ResponseInfo
+}
+
+func (m *mockABCIApplicationClient) Info(ctx context.Context, req *abciv1.RequestInfo, opts ...grpc.CallOption) (*abciv1.ResponseInfo, error) {
+	return m.infoResp, nil
 }
