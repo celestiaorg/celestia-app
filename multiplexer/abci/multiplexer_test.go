657bb031199d28422320496cd3e5747539063e5d
Rootul P <rootulp@gmail.com>
2025-06-06 10:47:58 -0400

    fix(multiplexer): preserve home args before `start` command (#4933)
    Closes https://github.com/celestiaorg/celestia-app/issues/4926
    
    Previously the multiplexer removed the first two arguments passed to
    celestia-appd. This is problematic for Everstake who start celestia-appd
    like this
    
    ```
    celestia-appd --home /foo start
    ```
    
    This PR fixes the issue by filtering the os.Args to remove
    `celestia-appd` and `start` but still preserve the additional flags
    regardless of the order they're provided.

--- multiplexer/abci/multiplexer_test.go
+++ multiplexer/abci/multiplexer_test.go
@@ -12,3 +12,43 @@ func TestOpenTraceWriter(t *testing.T) {
 		require.NoError(t, err)
 	})
 }
+
+func Test_getProgramArgs(t *testing.T) {
+	type testCase struct {
+		name  string
+		input []string
+		want  []string
+	}
+	tests := []testCase{
+		{
+			name:  "should return empty slice if no args are provided",
+			input: []string{},
+			want:  []string{},
+		},
+		{
+			name:  "should return empty slice if just celestia-appd is provided",
+			input: []string{"celestia-appd"},
+			want:  []string{},
+		},
+		{
+			name:  "should remove celestia-appd and start from input",
+			input: []string{"celestia-appd", "start", "--home", "foo"},
+			want:  []string{"--home", "foo"},
+		},
+		{
+			name:  "should preserve extra additional args",
+			input: []string{"celestia-appd", "start", "--home", "foo", "--grpc.enable", "--api.enable"},
+			want:  []string{"--home", "foo", "--grpc.enable", "--api.enable"},
+		},
+		{
+			// Reproduces https://github.com/celestiaorg/celestia-app/issues/4926
+			name:  "should preserve --home if included before start",
+			input: []string{"celestia-appd", "--home", "foo", "start"},
+			want:  []string{"--home", "foo"},
+		},
+	}
+	for _, test := range tests {
+		got := removeStart(test.input)
+		require.Equal(t, test.want, got)
+	}
+}
