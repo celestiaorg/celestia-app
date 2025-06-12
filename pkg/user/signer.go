e90fd46b45b7ba5bf01eb6741037feb7e85bf44c
Rootul Patel <rootulp@gmail.com>
2025-06-12 09:34:59 -0400

    revert(signer): pub key all the time

--- pkg/user/signer.go
+++ pkg/user/signer.go
@@ -295,7 +295,9 @@ func (s *Signer) getSignatureV2(sequence uint64, pubKey cryptotypes.PubKey, sign
 			Signature: signature,
 		},
 		Sequence: sequence,
-		PubKey:   pubKey,
+	}
+	if sequence == 0 {
+		sigV2.PubKey = pubKey
 	}
 	return sigV2
 }
