123af80532642b1b5ca6f03076d39f2e70f825d2
Callum Waters <cmwaters19@gmail.com>
2025-06-26 13:18:47 +0200

    fix: use go-square to unmarshal blob tx instead of tendermint

--- app/encoding/blob_tx_decoder.go
+++ app/encoding/blob_tx_decoder.go
@@ -2,12 +2,16 @@ package encoding
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	coretypes "github.com/tendermint/tendermint/types"
+	coretypes "github.com/celestiaorg/go-square/v2/tx"
 )
 
 func blobTxDecoder(decoder sdk.TxDecoder) sdk.TxDecoder {
 	return func(txBytes []byte) (sdk.Tx, error) {
-		if blobTx, isBlobTx := coretypes.UnmarshalBlobTx(txBytes); isBlobTx {
+		blobTx, isBlobTx, err := coretypes.UnmarshalBlobTx(txBytes)
+		if err != nil {
+			return nil, err
+		}
+		if isBlobTx {
 			return decoder(blobTx.Tx)
 		}
 		return decoder(txBytes)
