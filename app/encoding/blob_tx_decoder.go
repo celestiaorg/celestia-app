49cba8ffb9612115c8b91dafca3e1c62b4788d47
Callum Waters <cmwaters19@gmail.com>
2025-06-26 13:33:22 +0200

    make fix

--- app/encoding/blob_tx_decoder.go
+++ app/encoding/blob_tx_decoder.go
@@ -1,17 +1,17 @@
 package encoding
 
 import (
+	"github.com/celestiaorg/go-square/v2/tx"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	coretypes "github.com/celestiaorg/go-square/v2/tx"
 )
 
 func blobTxDecoder(decoder sdk.TxDecoder) sdk.TxDecoder {
 	return func(txBytes []byte) (sdk.Tx, error) {
-		blobTx, isBlobTx, err := coretypes.UnmarshalBlobTx(txBytes)
-		if err != nil {
-			return nil, err
-		}
+		blobTx, isBlobTx, err := tx.UnmarshalBlobTx(txBytes)
 		if isBlobTx {
+			if err != nil {
+				return nil, err
+			}
 			return decoder(blobTx.Tx)
 		}
 		return decoder(txBytes)
