633e135f6641b2fd7e177464adb4794521781895
Alessandro De Blasis <alex@deblasis.net>
2025-06-27 10:17:34 +0200

    refactor: rely on flag again
    Signed-off-by: Alessandro De Blasis <alex@deblasis.net>

--- cmd/celestia-appd/cmd/init.go
+++ cmd/celestia-appd/cmd/init.go
@@ -31,8 +31,8 @@ const (
 	FlagOverwrite = "overwrite"
 	// FlagRecover defines a flag to initialize the private validator key from a specific seed.
 	FlagRecover = "recover"
-	// FlagYes defines a flag to automatically answer yes to prompts.
-	FlagYes = "yes"
+	// FlagConfirmKnownChain defines a flag that automatically answers yes to prompts for known/public chainIDs
+	FlagConfirmKnownChain = "confirm-known-chain"
 )
 
 // InitCmd returns a command that creates the config files and genesis.json for
@@ -109,7 +109,7 @@ func initCmd(basicManager module.BasicManager, defaultNodeHome string) *cobra.Co
 			if isKnownChainID(chainID) {
 				fmt.Println("Warning: You are initializing a public chain. This is a very rare edge case and will likely result in a fork. Please be sure you know what you are doing.")
 
-				confirm, _ := cmd.Flags().GetBool(FlagYes)
+				confirm, _ := cmd.Flags().GetBool(FlagConfirmKnownChain)
 
 				if !confirm {
 					inBuf := bufio.NewReader(cmd.InOrStdin())
@@ -185,7 +185,7 @@ func initCmd(basicManager module.BasicManager, defaultNodeHome string) *cobra.Co
 	cmd.Flags().Bool(FlagRecover, false, "provide seed phrase to recover existing key instead of creating")
 	cmd.Flags().String(flags.FlagChainID, "", "genesis file chain-id, if left blank will be randomly created")
 	cmd.Flags().Int64(flags.FlagInitHeight, 1, "specify the initial block height at genesis")
-	cmd.Flags().BoolP(FlagYes, "y", false, "automatically answer yes to genesis download for known chainID")
+	cmd.Flags().BoolP(FlagConfirmKnownChain, "y", false, "automatically answer yes to genesis download for known chainID")
 
 	return cmd
 }
