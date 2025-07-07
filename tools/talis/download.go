f4f91ef945abe38cff0963749ba21d563a7fbe69
sweexordious <chamirachid1@gmail.com>
2025-07-07 13:40:30 +0100

    chore: create directories when downloading talis traces

--- tools/talis/download.go
+++ tools/talis/download.go
@@ -60,6 +60,12 @@ func downloadCmd() *cobra.Command {
 				go func() {
 					defer wg.Done()
 					localPath := filepath.Join(rootDir, "data/", node.Name)
+
+					if err := os.MkdirAll(localPath, 0755); err != nil {
+						fmt.Printf("failed to create directory %s: %v\n", localPath, err)
+						return
+					}
+
 					err := sftpDownload(remotePath, localPath, "root", node.PublicIP, SSHKeyPath)
 					if err != nil {
 						fmt.Printf("failed to download from %s: %v\n", node.PublicIP, err)
