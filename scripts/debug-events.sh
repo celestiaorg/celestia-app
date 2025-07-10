9f73c578381780846e7fb4b8cfaf4c1c66e1de29
Rootul Patel <rootulp@gmail.com>
2025-07-10 16:34:35 -0400

    v4 script

--- scripts/debug-events.sh
+++ scripts/debug-events.sh
@@ -3,15 +3,15 @@ VALIDATOR_ADDRESS=$(celestia-appd keys show validator --address)
 echo "Got validator address: $VALIDATOR_ADDRESS"
 
 echo "Sending a bank send transaction..."
-celestia-appd tx bank send $VALIDATOR_ADDRESS $VALIDATOR_ADDRESS 1utia --fees 20000utia --yes
+celestia-appd tx bank send $VALIDATOR_ADDRESS $VALIDATOR_ADDRESS 1utia --fees 20000utia --chain-id test --yes
 echo "Sent bank send transaction"
 
 # On v4.x
-# echo "Querying for events..."
-# celestia-appd query txs --query "message.sender='$VALIDATOR_ADDRESS'"
-# echo "Queried for events"
-
-# On v3.x
 echo "Querying for events..."
-celestia-appd query txs --events message.sender=$VALIDATOR_ADDRESS
+celestia-appd query txs --query "message.sender='$VALIDATOR_ADDRESS'"
 echo "Queried for events"
+
+# # On v3.x
+# echo "Querying for events..."
+# celestia-appd query txs --events message.sender=$VALIDATOR_ADDRESS
+# echo "Queried for events"
