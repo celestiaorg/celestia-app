31e18e8eae09b824712b21ab7141487f465b7789
Rootul Patel <rootulp@gmail.com>
2025-07-10 16:08:43 -0400

    it works on v3.x

--- scripts/debug-events.sh
+++ scripts/debug-events.sh
@@ -6,6 +6,12 @@ echo "Sending a bank send transaction..."
 celestia-appd tx bank send $VALIDATOR_ADDRESS $VALIDATOR_ADDRESS 1utia --fees 20000utia --yes
 echo "Sent bank send transaction"
 
+# On v4.x
+# echo "Querying for events..."
+# celestia-appd query txs --query "message.sender='$VALIDATOR_ADDRESS'"
+# echo "Queried for events"
+
+# On v3.x
 echo "Querying for events..."
-celestia-appd query txs --query "message.sender='$VALIDATOR_ADDRESS'"
+celestia-appd query txs --events message.sender=$VALIDATOR_ADDRESS
 echo "Queried for events"
