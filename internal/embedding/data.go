4139075eb19a9fee7979748b0f85564d790bef14
Rootul Patel <rootulp@gmail.com>
2025-07-22 10:15:57 -0400

    fix: merge conflicts

--- internal/embedding/data.go
+++ internal/embedding/data.go
@@ -9,14 +9,7 @@ import (
 
 // NOTE: This version must be updated at the same time as the version in the
 // Makefile.
-<<<<<<< HEAD
-const v3Version = "v3.10.4"
-=======
-const (
-	v3Version = "v3.10.5"
-	v4Version = "v4.0.7-mocha"
-)
->>>>>>> b235874 (fix: upgrade to v3.10.5 embedded app (#5295))
+const v3Version = "v3.10.5"
 
 // CelestiaAppV3 returns the compressed platform specific Celestia binary and
 // the version.
