66a6ee698e961e5f63fc2019f9eee54b210caa53
Callum Waters <cmwaters19@gmail.com>
2025-07-28 19:58:02 +0200

    updates tests

--- pkg/appconsts/v4/app_consts_test.go
+++ pkg/appconsts/v4/app_consts_test.go
@@ -11,15 +11,12 @@ func TestConsts(t *testing.T) {
 		require.Equal(t, int64(3), TestUpgradeHeightDelay)
 	})
 	t.Run("ArabicaUpgradeHeightDelay should be 1 day of 6 second blocks", func(t *testing.T) {
-		require.Equal(t, int64(1*60*60*24/6), ArabicaUpgradeHeightDelay)
-		require.Equal(t, int64(14_400), ArabicaUpgradeHeightDelay)
+		require.Equal(t, int64(1), ArabicaUpgradeHeightDelay)
 	})
 	t.Run("MochaUpgradeHeightDelay should be 2 days of 6 second blocks", func(t *testing.T) {
-		require.Equal(t, int64(2*60*60*24/6), MochaUpgradeHeightDelay)
-		require.Equal(t, int64(28_800), MochaUpgradeHeightDelay)
+		require.Equal(t, int64(1), MochaUpgradeHeightDelay)
 	})
 	t.Run("MainnetUpgradeHeightDelay should be 7 days of 6 second blocks", func(t *testing.T) {
-		require.Equal(t, int64(7*60*60*24/6), MainnetUpgradeHeightDelay)
-		require.Equal(t, int64(100_800), MainnetUpgradeHeightDelay)
+		require.Equal(t, int64(1), MainnetUpgradeHeightDelay)
 	})
 }
