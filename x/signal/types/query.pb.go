0985c00e11c56c2a4f7b70723120aea28c592d4f
Callum Waters <cmwaters19@gmail.com>
2025-07-31 12:55:38 +0200

    feat: query validators who have not signalled

--- x/signal/types/query.pb.go
+++ x/signal/types/query.pb.go
@@ -216,43 +216,138 @@ func (m *QueryGetUpgradeResponse) GetUpgrade() *Upgrade {
 	return nil
 }
 
+type QueryMissingValidatorsRequest struct {
+	Version uint64 `protobuf:"varint,1,opt,name=version,proto3" json:"version,omitempty"`
+}
+
+func (m *QueryMissingValidatorsRequest) Reset()         { *m = QueryMissingValidatorsRequest{} }
+func (m *QueryMissingValidatorsRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryMissingValidatorsRequest) ProtoMessage()    {}
+func (*QueryMissingValidatorsRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_7af24246367e432c, []int{4}
+}
+func (m *QueryMissingValidatorsRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryMissingValidatorsRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryMissingValidatorsRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryMissingValidatorsRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryMissingValidatorsRequest.Merge(m, src)
+}
+func (m *QueryMissingValidatorsRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryMissingValidatorsRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryMissingValidatorsRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryMissingValidatorsRequest proto.InternalMessageInfo
+
+func (m *QueryMissingValidatorsRequest) GetVersion() uint64 {
+	if m != nil {
+		return m.Version
+	}
+	return 0
+}
+
+type QueryMissingValidatorsResponse struct {
+	MissingValidators []string `protobuf:"bytes,1,rep,name=missing_validators,json=missingValidators,proto3" json:"missing_validators,omitempty"`
+}
+
+func (m *QueryMissingValidatorsResponse) Reset()         { *m = QueryMissingValidatorsResponse{} }
+func (m *QueryMissingValidatorsResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryMissingValidatorsResponse) ProtoMessage()    {}
+func (*QueryMissingValidatorsResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_7af24246367e432c, []int{5}
+}
+func (m *QueryMissingValidatorsResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryMissingValidatorsResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryMissingValidatorsResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryMissingValidatorsResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryMissingValidatorsResponse.Merge(m, src)
+}
+func (m *QueryMissingValidatorsResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryMissingValidatorsResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryMissingValidatorsResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryMissingValidatorsResponse proto.InternalMessageInfo
+
+func (m *QueryMissingValidatorsResponse) GetMissingValidators() []string {
+	if m != nil {
+		return m.MissingValidators
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*QueryVersionTallyRequest)(nil), "celestia.signal.v1.QueryVersionTallyRequest")
 	proto.RegisterType((*QueryVersionTallyResponse)(nil), "celestia.signal.v1.QueryVersionTallyResponse")
 	proto.RegisterType((*QueryGetUpgradeRequest)(nil), "celestia.signal.v1.QueryGetUpgradeRequest")
 	proto.RegisterType((*QueryGetUpgradeResponse)(nil), "celestia.signal.v1.QueryGetUpgradeResponse")
+	proto.RegisterType((*QueryMissingValidatorsRequest)(nil), "celestia.signal.v1.QueryMissingValidatorsRequest")
+	proto.RegisterType((*QueryMissingValidatorsResponse)(nil), "celestia.signal.v1.QueryMissingValidatorsResponse")
 }
 
 func init() { proto.RegisterFile("celestia/signal/v1/query.proto", fileDescriptor_7af24246367e432c) }
 
 var fileDescriptor_7af24246367e432c = []byte{
-	// 411 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0x4f, 0x4f, 0xe2, 0x40,
-	0x18, 0xc6, 0x19, 0xf6, 0x0f, 0xc9, 0x40, 0x76, 0x37, 0x93, 0xcd, 0x6e, 0xb7, 0x4b, 0x1a, 0xb6,
-	0x97, 0xdd, 0xac, 0xd0, 0x09, 0xa8, 0x5f, 0xc0, 0x8b, 0x17, 0x0f, 0x48, 0x94, 0x83, 0x17, 0x32,
-	0xc0, 0xa4, 0x34, 0xa9, 0x9d, 0xa1, 0x33, 0xad, 0x12, 0xe3, 0x41, 0xbf, 0x80, 0x26, 0xc6, 0xa3,
-	0xdf, 0xc7, 0x23, 0x89, 0x17, 0x8f, 0x06, 0xfc, 0x20, 0x86, 0x99, 0x16, 0x30, 0x85, 0x84, 0x5b,
-	0xfb, 0xbe, 0xbf, 0xf7, 0x99, 0xe7, 0x7d, 0x66, 0xa0, 0xd5, 0xa3, 0x3e, 0x15, 0xd2, 0x23, 0x58,
-	0x78, 0x6e, 0x40, 0x7c, 0x1c, 0xd7, 0xf1, 0x30, 0xa2, 0xe1, 0xc8, 0xe1, 0x21, 0x93, 0x0c, 0xa1,
-	0xb4, 0xef, 0xe8, 0xbe, 0x13, 0xd7, 0xcd, 0xb2, 0xcb, 0x98, 0xeb, 0x53, 0x4c, 0xb8, 0x87, 0x49,
-	0x10, 0x30, 0x49, 0xa4, 0xc7, 0x02, 0xa1, 0x27, 0xcc, 0xca, 0x0a, 0xc5, 0x88, 0xbb, 0x21, 0xe9,
-	0x53, 0x4d, 0xd8, 0x3b, 0xd0, 0x38, 0x9c, 0x1d, 0xd1, 0xa6, 0xa1, 0xf0, 0x58, 0x70, 0x44, 0x7c,
-	0x7f, 0xd4, 0xa2, 0xc3, 0x88, 0x0a, 0x89, 0x0c, 0x58, 0x88, 0x75, 0xd9, 0x00, 0x15, 0xf0, 0xef,
-	0x63, 0x2b, 0xfd, 0xb5, 0xef, 0x01, 0xfc, 0xb5, 0x62, 0x4c, 0x70, 0x16, 0x08, 0x8a, 0xfe, 0xc0,
-	0x52, 0xcc, 0xa4, 0x17, 0xb8, 0x1d, 0xce, 0xce, 0x68, 0x98, 0x0c, 0x17, 0x75, 0xad, 0x39, 0x2b,
-	0xa1, 0xbf, 0xf0, 0xab, 0x1c, 0x84, 0x54, 0x0c, 0x98, 0xdf, 0x4f, 0xa8, 0xbc, 0xa2, 0xbe, 0xcc,
-	0xcb, 0x1a, 0xac, 0x42, 0x24, 0x99, 0x24, 0x7e, 0xe7, 0x9d, 0xe2, 0x07, 0xc5, 0x7e, 0x53, 0x9d,
-	0xf6, 0x42, 0xd6, 0x36, 0xe0, 0x0f, 0x65, 0x6b, 0x9f, 0xca, 0x63, 0xbd, 0x66, 0xb2, 0x8b, 0xdd,
-	0x84, 0x3f, 0x33, 0x9d, 0xc4, 0xee, 0x2e, 0x2c, 0x24, 0x99, 0x28, 0xa7, 0xc5, 0xc6, 0x6f, 0x27,
-	0x1b, 0xb4, 0x93, 0x4e, 0xa5, 0x6c, 0xe3, 0x21, 0x0f, 0x3f, 0x29, 0x49, 0x74, 0x03, 0x60, 0x69,
-	0x39, 0x08, 0x54, 0x5d, 0x25, 0xb0, 0x2e, 0x66, 0xb3, 0xb6, 0x21, 0xad, 0xed, 0xda, 0xf6, 0xf5,
-	0xd3, 0xeb, 0x5d, 0xbe, 0x8c, 0xcc, 0xa5, 0x3b, 0x95, 0x33, 0x02, 0x5f, 0x24, 0xd7, 0x73, 0x89,
-	0xae, 0x00, 0x84, 0x8b, 0x4d, 0xd1, 0xff, 0xb5, 0x27, 0x64, 0x82, 0x32, 0xb7, 0x36, 0x62, 0x13,
-	0x2f, 0xa6, 0xf2, 0xf2, 0x1d, 0xa1, 0xec, 0xfb, 0xda, 0x3b, 0x78, 0x9c, 0x58, 0x60, 0x3c, 0xb1,
-	0xc0, 0xcb, 0xc4, 0x02, 0xb7, 0x53, 0x2b, 0x37, 0x9e, 0x5a, 0xb9, 0xe7, 0xa9, 0x95, 0x3b, 0x69,
-	0xb8, 0x9e, 0x1c, 0x44, 0x5d, 0xa7, 0xc7, 0x4e, 0x71, 0x7a, 0x18, 0x0b, 0xdd, 0xf9, 0x77, 0x8d,
-	0x70, 0x8e, 0xcf, 0x53, 0x49, 0x39, 0xe2, 0x54, 0x74, 0x3f, 0xab, 0xe7, 0xba, 0xfd, 0x16, 0x00,
-	0x00, 0xff, 0xff, 0x5b, 0x0c, 0x46, 0x0b, 0x24, 0x03, 0x00, 0x00,
+	// 492 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x94, 0x53, 0x4d, 0x6f, 0xd3, 0x40,
+	0x10, 0xcd, 0x36, 0x40, 0xc5, 0xb4, 0xe2, 0x63, 0x54, 0x81, 0x31, 0xc1, 0x0a, 0x16, 0x12, 0x15,
+	0x34, 0xb6, 0x12, 0xe0, 0xc0, 0x95, 0x4b, 0x2f, 0x20, 0x4a, 0x04, 0x39, 0x70, 0x89, 0xb6, 0xcd,
+	0xca, 0x59, 0xc9, 0xf1, 0xba, 0xde, 0x8d, 0x21, 0x42, 0x1c, 0xe0, 0x0f, 0x80, 0x84, 0xf8, 0x05,
+	0xfc, 0x14, 0x2e, 0x1c, 0x2b, 0x71, 0xe1, 0x88, 0x12, 0x7e, 0x08, 0xca, 0xee, 0x3a, 0x09, 0x38,
+	0x16, 0xe5, 0x96, 0xcc, 0xbc, 0xf7, 0xe6, 0xcd, 0x9b, 0x35, 0x78, 0x47, 0x2c, 0x66, 0x52, 0x71,
+	0x1a, 0x4a, 0x1e, 0x25, 0x34, 0x0e, 0xf3, 0x76, 0x78, 0x3c, 0x66, 0xd9, 0x24, 0x48, 0x33, 0xa1,
+	0x04, 0x62, 0xd1, 0x0f, 0x4c, 0x3f, 0xc8, 0xdb, 0x6e, 0x23, 0x12, 0x22, 0x8a, 0x59, 0x48, 0x53,
+	0x1e, 0xd2, 0x24, 0x11, 0x8a, 0x2a, 0x2e, 0x12, 0x69, 0x18, 0x6e, 0x73, 0x8d, 0xe2, 0x38, 0x8d,
+	0x32, 0x3a, 0x60, 0x06, 0xe1, 0xdf, 0x07, 0xe7, 0xd9, 0x7c, 0x44, 0x8f, 0x65, 0x92, 0x8b, 0xe4,
+	0x39, 0x8d, 0xe3, 0x49, 0x97, 0x1d, 0x8f, 0x99, 0x54, 0xe8, 0xc0, 0x66, 0x6e, 0xca, 0x0e, 0x69,
+	0x92, 0xdd, 0x33, 0xdd, 0xe2, 0xaf, 0xff, 0x99, 0xc0, 0xb5, 0x35, 0x34, 0x99, 0x8a, 0x44, 0x32,
+	0xbc, 0x09, 0xdb, 0xb9, 0x50, 0x3c, 0x89, 0xfa, 0xa9, 0x78, 0xc5, 0x32, 0x4b, 0xde, 0x32, 0xb5,
+	0x83, 0x79, 0x09, 0x6f, 0xc3, 0x45, 0x35, 0xcc, 0x98, 0x1c, 0x8a, 0x78, 0x60, 0x51, 0x1b, 0x1a,
+	0x75, 0x61, 0x51, 0x36, 0xc0, 0x3d, 0x40, 0x25, 0x14, 0x8d, 0xfb, 0x7f, 0x28, 0xd6, 0x35, 0xf6,
+	0x92, 0xee, 0xf4, 0x96, 0xb2, 0xbe, 0x03, 0x57, 0xb4, 0xad, 0x7d, 0xa6, 0x5e, 0x98, 0x35, 0xed,
+	0x2e, 0xfe, 0x01, 0x5c, 0x2d, 0x75, 0xac, 0xdd, 0x07, 0xb0, 0x69, 0x33, 0xd1, 0x4e, 0xb7, 0x3a,
+	0xd7, 0x83, 0x72, 0xd0, 0x41, 0xc1, 0x2a, 0xb0, 0xfe, 0x43, 0xb8, 0xa1, 0x15, 0x9f, 0x70, 0x29,
+	0x79, 0x12, 0xf5, 0x68, 0xcc, 0x07, 0x54, 0x89, 0x4c, 0xfe, 0x3b, 0xbe, 0xa7, 0xe0, 0x55, 0x51,
+	0xad, 0xa7, 0x16, 0xe0, 0xc8, 0x34, 0xfb, 0xf9, 0xa2, 0xeb, 0x90, 0x66, 0x7d, 0xf7, 0x7c, 0xf7,
+	0xf2, 0xe8, 0x6f, 0x5a, 0xe7, 0x6b, 0x1d, 0xce, 0x6a, 0x45, 0xfc, 0x40, 0x60, 0x7b, 0xf5, 0x28,
+	0xb8, 0xb7, 0x6e, 0x99, 0xaa, 0x93, 0xbb, 0xad, 0x53, 0xa2, 0x8d, 0x4d, 0xdf, 0x7f, 0xff, 0xfd,
+	0xd7, 0xa7, 0x8d, 0x06, 0xba, 0x2b, 0xef, 0x4b, 0xcd, 0x11, 0xe1, 0x1b, 0xbb, 0xeb, 0x5b, 0x7c,
+	0x47, 0x00, 0x96, 0xa9, 0xe3, 0x9d, 0xca, 0x09, 0xa5, 0xa3, 0xb9, 0x77, 0x4f, 0x85, 0xb5, 0x5e,
+	0x5c, 0xed, 0x65, 0x07, 0xb1, 0xfc, 0xd6, 0xf1, 0x0b, 0x81, 0x9d, 0x7d, 0xa6, 0x4a, 0x79, 0x63,
+	0xbb, 0x72, 0x42, 0xd5, 0x59, 0xdd, 0xce, 0xff, 0x50, 0xac, 0xb7, 0x5b, 0xda, 0x9b, 0x87, 0x8d,
+	0x15, 0x6f, 0xf6, 0x8a, 0xcb, 0xa4, 0x1e, 0x3d, 0xfe, 0x36, 0xf5, 0xc8, 0xc9, 0xd4, 0x23, 0x3f,
+	0xa7, 0x1e, 0xf9, 0x38, 0xf3, 0x6a, 0x27, 0x33, 0xaf, 0xf6, 0x63, 0xe6, 0xd5, 0x5e, 0x76, 0x22,
+	0xae, 0x86, 0xe3, 0xc3, 0xe0, 0x48, 0x8c, 0xc2, 0x62, 0xba, 0xc8, 0xa2, 0xc5, 0xef, 0x16, 0x4d,
+	0xd3, 0xf0, 0x75, 0x21, 0xae, 0x26, 0x29, 0x93, 0x87, 0xe7, 0xf4, 0x07, 0x7e, 0xef, 0x77, 0x00,
+	0x00, 0x00, 0xff, 0xff, 0x61, 0x69, 0x00, 0xee, 0x56, 0x04, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -273,6 +368,9 @@ type QueryClient interface {
 	// GetUpgrade enables a client to query for upgrade information if an upgrade
 	// is pending. The response will be empty if no upgrade is pending.
 	GetUpgrade(ctx context.Context, in *QueryGetUpgradeRequest, opts ...grpc.CallOption) (*QueryGetUpgradeResponse, error)
+	// GetMissingValidators enables a client to query for the validators that
+	// have not yet signalled for a particular version
+	GetMissingValidators(ctx context.Context, in *QueryMissingValidatorsRequest, opts ...grpc.CallOption) (*QueryMissingValidatorsResponse, error)
 }
 
 type queryClient struct {
@@ -301,6 +399,15 @@ func (c *queryClient) GetUpgrade(ctx context.Context, in *QueryGetUpgradeRequest
 	return out, nil
 }
 
+func (c *queryClient) GetMissingValidators(ctx context.Context, in *QueryMissingValidatorsRequest, opts ...grpc.CallOption) (*QueryMissingValidatorsResponse, error) {
+	out := new(QueryMissingValidatorsResponse)
+	err := c.cc.Invoke(ctx, "/celestia.signal.v1.Query/GetMissingValidators", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// VersionTally enables a client to query for the tally of voting power that
@@ -309,6 +416,9 @@ type QueryServer interface {
 	// GetUpgrade enables a client to query for upgrade information if an upgrade
 	// is pending. The response will be empty if no upgrade is pending.
 	GetUpgrade(context.Context, *QueryGetUpgradeRequest) (*QueryGetUpgradeResponse, error)
+	// GetMissingValidators enables a client to query for the validators that
+	// have not yet signalled for a particular version
+	GetMissingValidators(context.Context, *QueryMissingValidatorsRequest) (*QueryMissingValidatorsResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -320,6 +430,9 @@ func (*UnimplementedQueryServer) VersionTally(ctx context.Context, req *QueryVer
 }
 func (*UnimplementedQueryServer) GetUpgrade(ctx context.Context, req *QueryGetUpgradeRequest) (*QueryGetUpgradeResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetUpgrade not implemented")
+}
+func (*UnimplementedQueryServer) GetMissingValidators(ctx context.Context, req *QueryMissingValidatorsRequest) (*QueryMissingValidatorsResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMissingValidators not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -362,6 +475,24 @@ func _Query_GetUpgrade_Handler(srv interface{}, ctx context.Context, dec func(in
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_GetMissingValidators_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryMissingValidatorsRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).GetMissingValidators(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/celestia.signal.v1.Query/GetMissingValidators",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).GetMissingValidators(ctx, req.(*QueryMissingValidatorsRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var Query_serviceDesc = _Query_serviceDesc
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "celestia.signal.v1.Query",
@@ -374,6 +505,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "GetUpgrade",
 			Handler:    _Query_GetUpgrade_Handler,
+		},
+		{
+			MethodName: "GetMissingValidators",
+			Handler:    _Query_GetMissingValidators_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -504,6 +639,66 @@ func (m *QueryGetUpgradeResponse) MarshalToSizedBuffer(dAtA []byte) (int, error)
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryMissingValidatorsRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryMissingValidatorsRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryMissingValidatorsRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Version != 0 {
+		i = encodeVarintQuery(dAtA, i, uint64(m.Version))
+		i--
+		dAtA[i] = 0x8
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryMissingValidatorsResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryMissingValidatorsResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryMissingValidatorsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.MissingValidators) > 0 {
+		for iNdEx := len(m.MissingValidators) - 1; iNdEx >= 0; iNdEx-- {
+			i -= len(m.MissingValidators[iNdEx])
+			copy(dAtA[i:], m.MissingValidators[iNdEx])
+			i = encodeVarintQuery(dAtA, i, uint64(len(m.MissingValidators[iNdEx])))
+			i--
+			dAtA[i] = 0xa
+		}
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -563,6 +758,33 @@ func (m *QueryGetUpgradeResponse) Size() (n int) {
 	if m.Upgrade != nil {
 		l = m.Upgrade.Size()
 		n += 1 + l + sovQuery(uint64(l))
+	}
+	return n
+}
+
+func (m *QueryMissingValidatorsRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if m.Version != 0 {
+		n += 1 + sovQuery(uint64(m.Version))
+	}
+	return n
+}
+
+func (m *QueryMissingValidatorsResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if len(m.MissingValidators) > 0 {
+		for _, s := range m.MissingValidators {
+			l = len(s)
+			n += 1 + l + sovQuery(uint64(l))
+		}
 	}
 	return n
 }
@@ -863,6 +1085,157 @@ func (m *QueryGetUpgradeResponse) Unmarshal(dAtA []byte) error {
 			if err := m.Upgrade.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryMissingValidatorsRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryMissingValidatorsRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryMissingValidatorsRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Version", wireType)
+			}
+			m.Version = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Version |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryMissingValidatorsResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryMissingValidatorsResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryMissingValidatorsResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field MissingValidators", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.MissingValidators = append(m.MissingValidators, string(dAtA[iNdEx:postIndex]))
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
