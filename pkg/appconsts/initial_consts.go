429c9c5da5096e1c5f4bc01d43727c2c9e19a013
Rootul Patel <rootulp@gmail.com>
2025-08-11 20:37:59 -0400

    refactor: move from initial_consts to app_consts

--- pkg/appconsts/initial_consts.go
+++ pkg/appconsts/initial_consts.go
@@ -1,9 +1,5 @@
 package appconsts
 
-import (
-	"time"
-)
-
 // The following defaults correspond to initial parameters of the network that can be changed, not via app versions
 // but other means such as on-chain governance, or the node's local config
 const (
@@ -20,13 +16,6 @@ const (
 	// The min gas price acts as a filter. Transactions below that limit will not pass
 	// a node's `CheckTx` and thus not be proposed by that node.
 	DefaultMinGasPrice = 0.004 // utia
-
-	// UnbondingTime is the time a validator must wait to unbond in a proof of
-	// stake system. Any validator within this time can be subject to slashing
-	// under conditions of misbehavior.
-	//
-	// Modified from 3 weeks to 14 days + 1 hour in CIP-037.
-	UnbondingTime = 337 * time.Hour // (14 days + 1 hour)
 
 	// DefaultNetworkMinGasPrice is used by x/minfee to prevent transactions from being
 	// included in a block if they specify a gas price lower than this.
