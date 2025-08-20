c516714e31b6baae87cc8dc563b62e15b9dda1e1
evan-forbes <evan.samuel.forbes@gmail.com>
2025-08-20 04:25:17 -0500

    chore: revert some timeouts now that pre-emmptive block building was temporarily disabled

--- pkg/user/e2e_test.go
+++ pkg/user/e2e_test.go
@@ -30,7 +30,7 @@ func TestConcurrentTxSubmission(t *testing.T) {
 			// Setup network
 			tmConfig := testnode.DefaultTendermintConfig()
 			tmConfig.Mempool.Type = mempool
-			tmConfig.Consensus.TimeoutCommit = 1700 * time.Millisecond
+			tmConfig.Consensus.TimeoutCommit = 5 * time.Second
 			ctx, _, _ := testnode.NewNetwork(t, testnode.DefaultConfig().WithTendermintConfig(tmConfig))
 			_, err := ctx.WaitForHeight(1)
 			require.NoError(t, err)
