795b3c12e8b5fc86eb1af72ab32ce3fadbc4d856
sweexordious <chamirachid1@gmail.com>
2025-09-03 20:47:18 +0400

    Revert "TEMP"
    This reverts commit 29253289ca95d131f7acfad87c25e3b4ab493646.

--- test/util/testnode/config.go
+++ test/util/testnode/config.go
@@ -149,9 +149,9 @@ func DefaultTendermintConfig() *tmconfig.Config {
 	tmCfg := app.DefaultConsensusConfig()
 
 	// Set all the ports to random open ones.
-	tmCfg.RPC.ListenAddress = fmt.Sprintf("tcp://127.0.0.1:0", GetDeterministicPort())
-	tmCfg.P2P.ListenAddress = fmt.Sprintf("tcp://127.0.0.1:0", GetDeterministicPort())
-	tmCfg.RPC.GRPCListenAddress = fmt.Sprintf("tcp://127.0.0.1:0", GetDeterministicPort())
+	tmCfg.RPC.ListenAddress = fmt.Sprintf("tcp://127.0.0.1:%d", GetDeterministicPort())
+	tmCfg.P2P.ListenAddress = fmt.Sprintf("tcp://127.0.0.1:%d", GetDeterministicPort())
+	tmCfg.RPC.GRPCListenAddress = fmt.Sprintf("tcp://127.0.0.1:%d", GetDeterministicPort())
 
 	tmCfg.TxIndex.Indexer = "kv"
 
@@ -201,8 +201,8 @@ func CustomAppCreator(appOptions ...func(*baseapp.BaseApp)) srvtypes.AppCreator 
 func DefaultAppConfig() *srvconfig.Config {
 	appCfg := app.DefaultAppConfig()
 	appCfg.GRPC.Enable = true
-	appCfg.GRPC.Address = fmt.Sprintf("127.0.0.1:0", GetDeterministicPort())
+	appCfg.GRPC.Address = fmt.Sprintf("127.0.0.1:%d", GetDeterministicPort())
 	appCfg.API.Enable = true
-	appCfg.API.Address = fmt.Sprintf("tcp://127.0.0.1:0", GetDeterministicPort())
+	appCfg.API.Address = fmt.Sprintf("tcp://127.0.0.1:%d", GetDeterministicPort())
 	return appCfg
 }
