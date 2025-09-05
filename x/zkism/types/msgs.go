42928e5da52b948f34ef48641a9fab2b918c9e3d
Damian Nolan <damiannolan@gmail.com>
2025-09-05 11:43:38 +0200

    chore: add length validation on vkey commitment

--- x/zkism/types/msgs.go
+++ x/zkism/types/msgs.go
@@ -12,16 +12,20 @@ var _ sdk.HasValidateBasic = (*MsgCreateZKExecutionISM)(nil)
 
 // ValidateBasic implements stateless validation for the HasValidateBasic interface.
 func (msg *MsgCreateZKExecutionISM) ValidateBasic() error {
-	if len(msg.StateRoot) != 32 {
-		return errors.New("state root must be exactly 32 bytes")
-	}
-
 	if _, err := share.NewNamespaceFromBytes(msg.Namespace); err != nil {
 		return fmt.Errorf("failed to parse namespace from bytes: %x", msg.Namespace)
 	}
 
 	if len(msg.SequencerPublicKey) != 32 {
 		return errors.New("public key must be exactly 32 bytes")
+	}
+
+	if len(msg.StateRoot) != 32 {
+		return errors.New("state root must be exactly 32 bytes")
+	}
+
+	if len(msg.VkeyCommitment) != 32 {
+		return errors.New("vkey commitment must be exactly 32 bytes")
 	}
 
 	return nil
