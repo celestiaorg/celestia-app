celestiaorg/celestia-app

commit 4830c646e741029548b2e4e888711a0711f6acc5

refactor: remove unused params (#5675)
On `main`, my editor surfaces these unused parameters as lint warnings
so I removed them in this PR.

pkg/user/tx_client.go
@@ -408,7 +408,7 @@ func (client *TxClient) BroadcastTx(ctx context.Context, msgs []sdktypes.Msg, op
 }
 
 func (client *TxClient) broadcastTxAndIncrementSequence(ctx context.Context, conn *grpc.ClientConn, txBytes []byte, signer string) (*sdktypes.TxResponse, error) {
-	resp, err := client.broadcastTx(ctx, conn, txBytes, signer)
+	resp, err := client.broadcastTx(ctx, conn, txBytes)
 	if err != nil {
 		return nil, err
 	}
@@ -428,7 +428,7 @@ func (client *TxClient) broadcastTxAndIncrementSequence(ctx context.Context, con
 
 // broadcastTx resubmits a transaction that was evicted from the mempool.
 // Unlike the initial broadcast, it doesn't increment the signer's sequence number.
-func (client *TxClient) broadcastTx(ctx context.Context, conn *grpc.ClientConn, txBytes []byte, signer string) (*sdktypes.TxResponse, error) {
+func (client *TxClient) broadcastTx(ctx context.Context, conn *grpc.ClientConn, txBytes []byte) (*sdktypes.TxResponse, error) {
 	txClient := sdktx.NewServiceClient(conn)
 	resp, err := txClient.BroadcastTx(
 		ctx,
@@ -628,12 +628,12 @@ func (client *TxClient) ConfirmTx(ctx context.Context, txHash string) (*TxRespon
 			client.deleteFromTxTracker(txHash)
 			return txResponse, nil
 		case core.TxStatusEvicted:
-			_, signer, exists := client.GetTxFromTxTracker(txHash)
+			_, _, exists := client.GetTxFromTxTracker(txHash)
 			if !exists {
 				return nil, fmt.Errorf("tx: %s not found in txTracker; likely failed during broadcast", txHash)
 			}
 			// Resubmit straight away in the event of eviction and keep polling until tx is committed
-			_, err := client.broadcastTx(ctx, client.conns[0], client.txTracker[txHash].txBytes, signer)
+			_, err := client.broadcastTx(ctx, client.conns[0], client.txTracker[txHash].txBytes)
 			if err != nil {
 				return nil, fmt.Errorf("resubmission for evicted tx with hash %s failed: %w", txHash, err)
 			}

app/test/claim_test.go
@@ -49,7 +49,7 @@ func TestClaimRewardsAfterFullUndelegation(t *testing.T) {
 	verifyDelegationDoesNotExist(t, &cctx, stakingClient, delegatorAddress, validatorAddress)
 	verifyDelegationRewardsExist(t, &cctx, distributionClient, delegatorAddress, validatorAddress)
 
-	claimRewards(t, &cctx, txClient, delegatorAddress, validatorAddress, amount)
+	claimRewards(t, &cctx, txClient, delegatorAddress, validatorAddress)
 }
 
 func getDelegatorAddress(t *testing.T, cctx *testnode.Context, accounts []string) string {
@@ -135,7 +135,7 @@ func verifyDelegationRewardsExist(t *testing.T, cctx *testnode.Context, distribu
 	require.True(t, resp.Rewards.AmountOf(appconsts.BondDenom).GT(math.LegacyOneDec()))
 }
 
-func claimRewards(t *testing.T, cctx *testnode.Context, txClient *user.TxClient, delegatorAddress string, validatorAddress string, amount types.Coin) {
+func claimRewards(t *testing.T, cctx *testnode.Context, txClient *user.TxClient, delegatorAddress string, validatorAddress string) {
 	withdrawRewardsMsg := &distributiontypes.MsgWithdrawDelegatorReward{
 		DelegatorAddress: delegatorAddress,
 		ValidatorAddress: validatorAddress,
@@ -151,13 +151,13 @@ func claimRewards(t *testing.T, cctx *testnode.Context, txClient *user.TxClient,
 	require.NotNil(t, getTxResp.TxResponse)
 	require.Equal(t, abci.CodeTypeOK, getTxResp.TxResponse.Code)
 
-	event, err := getWithdrawRewardsEvent(t, getTxResp.TxResponse.Events)
+	event, err := getWithdrawRewardsEvent(getTxResp.TxResponse.Events)
 	require.NoError(t, err)
 	require.Equal(t, delegatorAddress, event.DelegatorAddress)
 	require.Equal(t, validatorAddress, event.ValidatorAddress)
 }
 
-func getWithdrawRewardsEvent(t *testing.T, events []abci.Event) (WithdrawRewardsEvent, error) {
+func getWithdrawRewardsEvent(events []abci.Event) (WithdrawRewardsEvent, error) {
 	for _, event := range events {
 		if event.Type == distributiontypes.EventTypeWithdrawRewards {
 			var delegatorAddress string

test/util/testnode/config.go
@@ -20,8 +20,7 @@ import (
 )
 
 const (
-	kibibyte                    = 1024      // bytes
-	mebibyte                    = 1_048_576 // bytes
+	kibibyte                    = 1024 // bytes
 	DefaultValidatorAccountName = "validator"
 	DefaultInitialBalance       = genesis.DefaultInitialBalance
 	// TimeoutCommit is a flag that can be used to override the timeout_commit.

commit 929a3984e7a352643933f3b3160e9b0496bec45d

fix: unused params regression (#5694)
CI on main has been failing since
https://github.com/celestiaorg/celestia-app/pull/5675/files merged. The
PR that merged right before it started using the `signer` in that
method: https://github.com/celestiaorg/celestia-app/pull/5582/files

pkg/user/tx_client.go
@@ -411,7 +411,7 @@ func (client *TxClient) BroadcastTx(ctx context.Context, msgs []sdktypes.Msg, op
 }
 
 func (client *TxClient) broadcastTxAndIncrementSequence(ctx context.Context, conn *grpc.ClientConn, txBytes []byte, signer string) (*sdktypes.TxResponse, error) {
-	resp, err := client.broadcastTx(ctx, conn, txBytes)
+	resp, err := client.broadcastTx(ctx, conn, txBytes, signer)
 	if err != nil {
 		return nil, err
 	}
@@ -431,7 +431,7 @@ func (client *TxClient) broadcastTxAndIncrementSequence(ctx context.Context, con
 
 // broadcastTx resubmits a transaction that was evicted from the mempool.
 // Unlike the initial broadcast, it doesn't increment the signer's sequence number.
-func (client *TxClient) broadcastTx(ctx context.Context, conn *grpc.ClientConn, txBytes []byte) (*sdktypes.TxResponse, error) {
+func (client *TxClient) broadcastTx(ctx context.Context, conn *grpc.ClientConn, txBytes []byte, signer string) (*sdktypes.TxResponse, error) {
 	txClient := sdktx.NewServiceClient(conn)
 	resp, err := txClient.BroadcastTx(
 		ctx,
@@ -631,12 +631,12 @@ func (client *TxClient) ConfirmTx(ctx context.Context, txHash string) (*TxRespon
 			client.deleteFromTxTracker(txHash)
 			return txResponse, nil
 		case core.TxStatusEvicted:
-			_, _, exists := client.GetTxFromTxTracker(txHash)
+			_, signer, exists := client.GetTxFromTxTracker(txHash)
 			if !exists {
 				return nil, fmt.Errorf("tx: %s not found in txTracker; likely failed during broadcast", txHash)
 			}
 			// Resubmit straight away in the event of eviction and keep polling until tx is committed
-			_, err := client.broadcastTx(ctx, client.conns[0], client.txTracker[txHash].txBytes)
+			_, err := client.broadcastTx(ctx, client.conns[0], client.txTracker[txHash].txBytes, signer)
 			if err != nil {
 				return nil, fmt.Errorf("resubmission for evicted tx with hash %s failed: %w", txHash, err)
 			}