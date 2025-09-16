9a4052cd51c96a9a26073e15266374720551be39
evan-forbes <evan.samuel.forbes@gmail.com>
2025-09-16 17:40:07 -0500

    chore: weird merge issue

--- pkg/user/parallel_tx_submission_test.go
+++ pkg/user/parallel_tx_submission_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/celestiaorg/celestia-app/v6/app/grpc/tx"
 	"github.com/celestiaorg/celestia-app/v6/pkg/user"
 	"github.com/celestiaorg/celestia-app/v6/test/util/grpctest"
-	"github.com/celestiaorg/go-square/v2/share"
+	"github.com/celestiaorg/go-square/v3/share"
 	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cometbft/cometbft/rpc/core"
 	"github.com/cosmos/cosmos-sdk/crypto/hd"
