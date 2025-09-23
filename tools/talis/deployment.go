6874c5dd09d4dea5aaf3c484d097e6571bf1caae
keeghcet <keeghcet@outlook.com>
2025-09-23 22:18:29 +0800

    refactor: use strings.builder (#5679)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    The modernize has added an analyzer for using `strings.Builder` for
    string construction. `strings.Builder` has fewer memory allocations and
    better performance.
    
    More info: https://github.com/golang/go/issues/75190
    
    Signed-off-by: keeghcet <keeghcet@outlook.com>

--- tools/talis/deployment.go
+++ tools/talis/deployment.go
@@ -207,11 +207,12 @@ func deployPayloadDirect(
 		errs = append(errs, e)
 	}
 	if len(errs) > 0 {
-		sb := "deployment errors:\n"
+		var sb strings.Builder
+		sb.WriteString("deployment errors:\n")
 		for _, e := range errs {
-			sb += "- " + e.Error() + "\n"
+			sb.WriteString("- " + e.Error() + "\n")
 		}
-		return errors.New(sb)
+		return errors.New(sb.String())
 	}
 	return nil
 }
@@ -294,11 +295,12 @@ func deployPayloadViaS3(
 		errs = append(errs, e)
 	}
 	if len(errs) > 0 {
-		sb := "deployment errors:\n"
+		var sb strings.Builder
+		sb.WriteString("deployment errors:\n")
 		for _, e := range errs {
-			sb += "- " + e.Error() + "\n"
+			sb.WriteString("- " + e.Error() + "\n")
 		}
-		return errors.New(sb)
+		return errors.New(sb.String())
 	}
 	return nil
 }
