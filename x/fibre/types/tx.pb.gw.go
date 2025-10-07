f30d6ea67f4e9913ff3f7cd9f84cb14203c5e525
Rootul Patel <rootulp@gmail.com>
2025-10-07 15:20:53 -0400

    fix: method path

--- x/fibre/types/tx.pb.gw.go
+++ x/fibre/types/tx.pb.gw.go
@@ -398,7 +398,7 @@ var (
 
 	pattern_Msg_PayForFibre_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fibre", "v1", "pay-for-fibre"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Msg_PaymentPromiseTimeout_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fibre", "v1", "payment-timeout"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Msg_PaymentPromiseTimeout_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"fibre", "v1", "payment-promise-timeout"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
