e1381eb4421933c0ed94ec908a580fabcbe715c8
evan-forbes <evan.samuel.forbes@gmail.com>
2025-10-12 23:29:43 -0500

    feat: add the ability to query the sequence from the mempool's checkTxState

--- multiplexer/abci/abci.go
+++ multiplexer/abci/abci.go
@@ -172,6 +172,14 @@ func (m *Multiplexer) VerifyVoteExtension(_ context.Context, req *abci.RequestVe
 	return app.VerifyVoteExtension(req)
 }
 
+func (m *Multiplexer) QuerySequence(ctx context.Context, req *abci.RequestQuerySequence) (*abci.ResponseQuerySequence, error) {
+	app, err := m.getApp()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get app for version %d: %w", m.appVersion, err)
+	}
+	return app.QuerySequence(ctx, req)
+}
+
 // checkHaltConditions returns an error if the node should halt based on a halt-height or
 // halt-time configured in app.toml.
 func (m *Multiplexer) checkHaltConditions(req *abci.RequestFinalizeBlock) error {
