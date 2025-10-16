2e9e29c43f4dbd24e068932f491c87cb52d19968
sweexordious <chamirachid1@gmail.com>
2025-10-16 22:33:11 +0400

    chore: propose block time

--- tools/cpu_requirements/main.go
+++ tools/cpu_requirements/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"github.com/celestiaorg/celestia-app/v6/app"
 	"github.com/celestiaorg/celestia-app/v6/app/encoding"
@@ -10,11 +11,34 @@ import (
 	"github.com/celestiaorg/celestia-app/v6/test/util/random"
 	"github.com/celestiaorg/celestia-app/v6/test/util/testfactory"
 	"github.com/celestiaorg/go-square/v3/share"
+	dbm "github.com/cometbft/cometbft-db"
 	"github.com/cometbft/cometbft/abci/types"
+	abcimocks "github.com/cometbft/cometbft/abci/types/mocks"
+	propagationtypes "github.com/cometbft/cometbft/consensus/propagation/types"
+	"github.com/cometbft/cometbft/crypto/ed25519"
+	"github.com/cometbft/cometbft/libs/log"
+	mpmocks "github.com/cometbft/cometbft/mempool/mocks"
+	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/cometbft/cometbft/proxy"
+	sm "github.com/cometbft/cometbft/state"
+	"github.com/cometbft/cometbft/state/mocks"
+	"github.com/cometbft/cometbft/store"
+	comettypes "github.com/cometbft/cometbft/types"
+	"github.com/stretchr/testify/mock"
 	"os"
 	"runtime"
 	"strings"
 	"time"
+)
+
+// Reference times in milliseconds (to be filled with actual values)
+const (
+	referencePrepareProposalMs = 0.0
+	referenceProcessProposalMs = 0.0
+	referenceFinalizeBlockMs   = 0.0
+	referenceProposeBlockMs    = 0.0
+	referenceEncodeBlockMs     = 0.0
+	referenceDecodeBlockMs     = 0.0
 )
 
 type CPUInfo struct {
@@ -28,18 +52,19 @@ type CPUInfo struct {
 func main() {
 	if runtime.GOOS != "linux" {
 		fmt.Println("Error: This script only runs on Linux")
-		os.Exit(1)
+		//os.Exit(1)
 	}
 
-	info, err := getCPUInfo()
-	if err != nil {
-		fmt.Printf("Error getting CPU info: %v\n", err)
-		os.Exit(1)
-	}
+	info := &CPUInfo{}
+	//info, err := getCPUInfo()
+	//if err != nil {
+	//fmt.Printf("Error getting CPU info: %v\n", err)
+	//os.Exit(1)
+	//}
 
-	displayCPUInfo(info)
+	//displayCPUInfo(info)
 
-	app, txs, err := generatePayForBlobTransactions(140, 1024*1024)
+	app, txs, err := generatePayForBlobTransactions(127, 1024*1024)
 	if err != nil {
 		fmt.Printf("Error generating transactions: %v\n", err)
 		os.Exit(1)
@@ -62,8 +87,24 @@ func main() {
 		fmt.Printf("Error running FinalizeBlock: %v\n", err)
 		os.Exit(1)
 	}
+	proposeBlockTime, encodeBlockTime, decodeBlockTime, err := runProposeBlock(app, txs)
+	if err != nil {
+		fmt.Printf("Error running ProposeBlock: %v\n", err)
+		os.Exit(1)
+	}
 
-	fmt.Println("Done")
+	// Display performance results with comparison to reference times
+	displayPerformanceResults(
+		prepareProposalTime,
+		processProposalTime,
+		finalizeBlockTime,
+		proposeBlockTime,
+		encodeBlockTime,
+		decodeBlockTime,
+		info,
+	)
+
+	fmt.Println("\nDone")
 }
 
 func runPrepareProposal(testApp *app.App, txs [][]byte) (time.Duration, error) {
@@ -81,7 +122,6 @@ func runPrepareProposal(testApp *app.App, txs [][]byte) (time.Duration, error) {
 		return 0, fmt.Errorf("no transactions returned")
 	}
 	elapsed := time.Since(start)
-	fmt.Printf("PrepareProposal took %vms\n", elapsed.Milliseconds())
 	return elapsed, nil
 }
 
@@ -111,7 +151,6 @@ func runProcessProposal(testApp *app.App, txs [][]byte) (time.Duration, error) {
 		return 0, fmt.Errorf("error running ProcessProposal: %w", err)
 	}
 	elapsed := time.Since(start)
-	fmt.Printf("ProcessProposal took %vms\n", elapsed.Milliseconds())
 	return elapsed, nil
 }
 
@@ -141,38 +180,160 @@ func runFinalizeBlock(testApp *app.App, txs [][]byte) (time.Duration, error) {
 		return 0, fmt.Errorf("error running FinalizeBlock: %w", err)
 	}
 	elapsed := time.Since(start)
-	fmt.Printf("FinalizeBlock took %vms\n", elapsed.Milliseconds())
 	return elapsed, nil
 }
 
-func runProposeBlock(testApp *app.App, txs [][]byte) (time.Duration, error) {
-	prepareProposalReq := types.RequestPrepareProposal{
-		Txs:    txs,
-		Height: testApp.LastBlockHeight() + 1,
+func runProposeBlock(testApp *app.App, txs [][]byte) (proposeBlockTime, encodeBlockTime, decodeBlockTime time.Duration, err error) {
+	app := &abcimocks.Application{}
+	app.On("PrepareProposal", mock.Anything, mock.Anything).Return(&types.ResponsePrepareProposal{
+		Txs: txs,
+	}, nil)
+	cc := proxy.NewLocalClientCreator(app)
+	proxyApp := proxy.NewAppConns(cc, proxy.NopMetrics())
+	err = proxyApp.Start()
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("error starting proxy app: %w", err)
+	}
+	defer proxyApp.Stop()
+
+	evpool := &mocks.EvidencePool{}
+	evpool.On("PendingEvidence", mock.Anything).Return([]comettypes.Evidence{}, int64(0))
+
+	cachedTxs := comettypes.CachedTxFromTxs(comettypes.ToTxs(txs))
+	mp := &mpmocks.Mempool{}
+	mp.On("ReapMaxBytesMaxGas", mock.Anything, mock.Anything).Return(cachedTxs)
+
+	nVals := 1
+	vals := make([]comettypes.GenesisValidator, nVals)
+	privVals := make(map[string]comettypes.PrivValidator, nVals)
+	for i := 0; i < nVals; i++ {
+		secret := []byte(fmt.Sprintf("test%d", i))
+		pk := ed25519.GenPrivKeyFromSecret(secret)
+		valAddr := pk.PubKey().Address()
+		vals[i] = comettypes.GenesisValidator{
+			Address: valAddr,
+			PubKey:  pk.PubKey(),
+			Power:   1000,
+			Name:    fmt.Sprintf("test%d", i),
+		}
+		privVals[valAddr.String()] = comettypes.NewMockPVWithParams(pk, false, false)
+	}
+	genDoc := comettypes.GenesisDoc{
+		ChainID:         testApp.ChainID(),
+		Validators:      vals,
+		AppHash:         nil,
+		ConsensusParams: comettypes.DefaultConsensusParams(),
+	}
+	genDoc.ConsensusParams.Block.MaxBytes = comettypes.DefaultMaxBlockSizeBytes
+	state, _ := sm.MakeGenesisState(&genDoc)
+	stateDB := dbm.NewMemDB()
+	stateStore := sm.NewStore(stateDB, sm.StoreOptions{
+		DiscardABCIResponses: false,
+	})
+	if err := stateStore.Save(state); err != nil {
+		return 0, 0, 0, err
 	}
 
-	prepareProposalResp, err := testApp.PrepareProposal(&prepareProposalReq)
-	if err != nil {
-		return 0, fmt.Errorf("error running PrepareProposal: %w", err)
+	for i := 1; i < int(testApp.LastBlockHeight()); i++ {
+		state.LastBlockHeight++
+		state.LastValidators = state.Validators.Copy()
+		if err := stateStore.Save(state); err != nil {
+			return 0, 0, 0, err
+		}
 	}
-	if len(prepareProposalResp.Txs) == 0 {
-		return 0, fmt.Errorf("no transactions returned")
+	pa, _ := state.Validators.GetByIndex(0)
+	commit, _, err := makeValidCommit(testApp.ChainID(), 1, comettypes.BlockID{}, state.Validators, privVals)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("error creating valid commit: %w", err)
 	}
 
-	finalizeBlockReq := types.RequestFinalizeBlock{
-		Time:   testutil.GenesisTime.Add(time.Duration(6 * time.Second)),
-		Height: testApp.LastBlockHeight() + 1,
-		Hash:   testApp.LastCommitID().Hash,
-		Txs:    txs,
-	}
-	start := time.Now()
-	_, err = testApp.FinalizeBlock(&finalizeBlockReq)
+	blockStore := store.NewBlockStore(dbm.NewMemDB())
+	blockExec := sm.NewBlockExecutor(
+		stateStore,
+		log.NewNopLogger(),
+		proxyApp.Consensus(),
+		mp,
+		evpool,
+		blockStore,
+	)
+
+	ctx := context.Background()
+	createProposalBlockStart := time.Now()
+	_, ps, err := blockExec.CreateProposalBlock(ctx, 1, state, commit, pa)
 	if err != nil {
-		return 0, fmt.Errorf("error running FinalizeBlock: %w", err)
+		return 0, 0, 0, fmt.Errorf("error creating proposal block: %w", err)
 	}
-	elapsed := time.Since(start)
-	fmt.Printf("FinalizeBlock took %vms\n", elapsed.Milliseconds())
-	return elapsed, nil
+	proposeBlockTime = time.Since(createProposalBlockStart)
+
+	encodeBlockStart := time.Now()
+	pps, lastLen, err := comettypes.Encode(ps, comettypes.BlockPartSizeBytes)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("error encoding proposal block: %w", err)
+	}
+	encodeBlockTime = time.Since(encodeBlockStart)
+	cps := propagationtypes.NewCombinedSetFromCompactBlock(&propagationtypes.CompactBlock{
+		BpHash:   pps.Hash(),
+		Proposal: comettypes.Proposal{BlockID: comettypes.BlockID{PartSetHeader: comettypes.PartSetHeader{Total: ps.Total(), Hash: ps.Hash()}}},
+		LastLen:  uint32(lastLen),
+	})
+	for _, id := range pps.BitArray().GetTrueIndices() {
+		part := pps.GetPart(id)
+		added, err := cps.AddPart(&propagationtypes.RecoveryPart{
+			Height: 11,
+			Round:  11,
+			Index:  ps.Total() + part.Index,
+			Data:   part.Bytes,
+			Proof:  &part.Proof,
+		}, part.Proof)
+		if err != nil {
+			return 0, 0, 0, fmt.Errorf("error adding part to proposal block: %w", err)
+		}
+		if !added {
+			return 0, 0, 0, fmt.Errorf("part was not added to proposal block")
+		}
+	}
+
+	decodeBlockStart := time.Now()
+	err = cps.Decode()
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("error decoding proposal block: %w", err)
+	}
+	decodeBlockTime = time.Since(decodeBlockStart)
+	return
+}
+
+func makeValidCommit(
+	chainID string,
+	height int64,
+	blockID comettypes.BlockID,
+	vals *comettypes.ValidatorSet,
+	privVals map[string]comettypes.PrivValidator,
+) (*comettypes.ExtendedCommit, []*comettypes.Vote, error) {
+	sigs := make([]comettypes.ExtendedCommitSig, vals.Size())
+	votes := make([]*comettypes.Vote, vals.Size())
+	for i := 0; i < vals.Size(); i++ {
+		_, val := vals.GetByIndex(int32(i))
+		vote, err := comettypes.MakeVote(
+			privVals[val.Address.String()],
+			chainID,
+			int32(i),
+			height,
+			0,
+			cmtproto.PrecommitType,
+			blockID,
+			time.Now(),
+		)
+		if err != nil {
+			return nil, nil, err
+		}
+		sigs[i] = vote.ExtendedCommitSig()
+		votes[i] = vote
+	}
+	return &comettypes.ExtendedCommit{
+		Height:             height,
+		BlockID:            blockID,
+		ExtendedSignatures: sigs,
+	}, votes, nil
 }
 
 // getCPUInfo reads and parses /proc/cpuinfo to extract CPU details
@@ -252,6 +413,117 @@ func displayCPUInfo(info *CPUInfo) {
 	fmt.Printf("GFNI Support:      %v\n", info.HasGFNI)
 	fmt.Printf("SHA-NI Support:    %v\n", info.HasSHANI)
 	fmt.Println("==========================")
+}
+
+// displayPerformanceResults shows the timing results in a user-friendly format
+func displayPerformanceResults(
+	prepareProposalTime, processProposalTime, finalizeBlockTime,
+	proposeBlockTime, encodeBlockTime, decodeBlockTime time.Duration,
+	cpuInfo *CPUInfo,
+) {
+	fmt.Println("\n=== Performance Test Results ===")
+
+	// Convert to milliseconds for easier reading
+	prepareMs := prepareProposalTime.Milliseconds()
+	processMs := processProposalTime.Milliseconds()
+	finalizeMs := finalizeBlockTime.Milliseconds()
+	proposeMs := proposeBlockTime.Milliseconds()
+	encodeMs := encodeBlockTime.Milliseconds()
+	decodeMs := decodeBlockTime.Milliseconds()
+
+	// Track if any operation is slower than reference
+	anySlower := false
+
+	// Helper function to compare and display
+	compareAndDisplay := func(name, description string, actualMs int64, referenceMs float64) bool {
+		fmt.Printf("\n%s:\n", name)
+		fmt.Printf("  What it does: %s\n", description)
+		fmt.Printf("  Your time: %d ms\n", actualMs)
+
+		if referenceMs > 0 {
+			fmt.Printf("  Reference time: %.0f ms\n", referenceMs)
+			if float64(actualMs) > referenceMs {
+				fmt.Printf("  Status: SLOWER than reference (%.1fx slower)\n", float64(actualMs)/referenceMs)
+				return true
+			} else {
+				fmt.Printf("  Status: FASTER than reference (%.1fx faster)\n", referenceMs/float64(actualMs))
+			}
+		} else {
+			fmt.Printf("  Status: No reference time set\n")
+		}
+		return false
+	}
+
+	// Display each operation
+	anySlower = compareAndDisplay(
+		"Prepare Proposal",
+		"Getting transactions ready to be included in a block",
+		prepareMs,
+		referencePrepareProposalMs,
+	) || anySlower
+
+	anySlower = compareAndDisplay(
+		"Process Proposal",
+		"Validating a proposed block before accepting it",
+		processMs,
+		referenceProcessProposalMs,
+	) || anySlower
+
+	anySlower = compareAndDisplay(
+		"Finalize Block",
+		"Committing the block to the blockchain",
+		finalizeMs,
+		referenceFinalizeBlockMs,
+	) || anySlower
+
+	anySlower = compareAndDisplay(
+		"Propose Block",
+		"Creating a new block proposal",
+		proposeMs,
+		referenceProposeBlockMs,
+	) || anySlower
+
+	anySlower = compareAndDisplay(
+		"Encode Block",
+		"Converting the block to transmittable format",
+		encodeMs,
+		referenceEncodeBlockMs,
+	) || anySlower
+
+	anySlower = compareAndDisplay(
+		"Decode Block",
+		"Converting received block data back to usable format",
+		decodeMs,
+		referenceDecodeBlockMs,
+	) || anySlower
+
+	fmt.Println("\n=== Final Assessment ===")
+
+	// Check if any reference times are set
+	hasReferenceTimes := referencePrepareProposalMs > 0 || referenceProcessProposalMs > 0 ||
+		referenceFinalizeBlockMs > 0 || referenceProposeBlockMs > 0 ||
+		referenceEncodeBlockMs > 0 || referenceDecodeBlockMs > 0
+
+	if !hasReferenceTimes {
+		fmt.Println("No reference times have been set yet.")
+		fmt.Println("Please update the reference constants in the code with your target values.")
+	} else if anySlower {
+		fmt.Println("\nWARNING: Your system is slower than the reference requirements!")
+		fmt.Println("\nRECOMMENDATION:")
+		fmt.Println("To meet the performance requirements, you need to upgrade your hardware to:")
+		fmt.Println("  - 32 CPU cores (or more)")
+		fmt.Println("  - CPUs that support GFNI (Galois Field New Instructions)")
+		fmt.Println("  - CPUs that support SHA-NI (SHA New Instructions)")
+		fmt.Println("\nYour current system:")
+		fmt.Printf("  - CPU cores: %d\n", cpuInfo.Cores)
+		fmt.Printf("  - GFNI support: %v\n", cpuInfo.HasGFNI)
+		fmt.Printf("  - SHA-NI support: %v\n", cpuInfo.HasSHANI)
+	} else {
+		fmt.Println("\nCONGRATULATIONS! Your system meets or exceeds the performance requirements.")
+		fmt.Println("Your hardware is capable of running the validator node efficiently.")
+	}
+
+	fmt.Println("================================")
 }
 
 func generatePayForBlobTransactions(count, size int) (*app.App, [][]byte, error) {
