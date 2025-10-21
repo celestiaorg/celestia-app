e7055b5fcef1fa6d0f88abc1aaf15e4ec37832b9
evan-forbes <evan.samuel.forbes@gmail.com>
2025-10-21 12:59:12 -0500

    chore: linter

--- tools/chainbuilder/main.go
+++ tools/chainbuilder/main.go
@@ -326,10 +326,7 @@ func Run(ctx context.Context, cfg BuilderConfig, dir string) error {
 				return fmt.Errorf("failed to convert data from protobuf: %w", err)
 			}
 
-			block, err := state.MakeBlock(height, data, commit, nil, validatorAddr)
-			if err != nil {
-				return fmt.Errorf("failed to make block: %w", err)
-			}
+			block := state.MakeBlock(height, data, commit, nil, validatorAddr)
 			blockParts, err := block.MakePartSet(types.BlockPartSizeBytes)
 			if err != nil {
 				return fmt.Errorf("failed to make part set: %w", err)
