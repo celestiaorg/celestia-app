d4aae86f1204e39b28bbb1e9d7735ca1ed87ac14
Hlib Kanunnikov <hlibwondertan@gmail.com>
2025-10-30 20:49:48 +0100

    feat(fibre): `client.Put` sends PFF (#49)
    As PFF is now available, adding the missing submission piece to the
    `Put` method.

--- fibre/client_put.go
+++ fibre/client_put.go
@@ -2,32 +2,29 @@ package fibre
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/celestiaorg/celestia-app/v6/x/fibre/types"
 	"github.com/celestiaorg/go-square/v3/share"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
-// PFFConfirmation contains information about the PayForFibre transaction.
-type PFFConfirmation struct {
-	// TxHash is the transaction hash of the PayForFibre message.
-	TxHash string
-	// Height is the block height where the PayForFibre transaction was included.
-	Height uint64
-}
-
 // PutResult contains the result of a [Client.Put] operation.
 type PutResult struct {
-	// Commitment is the commitment to the blob.
+	// Commitment is the commitment to the [Blob].
 	Commitment Commitment
-	// ValidatorSignatures are secp256k1 signatures over the [PaymentPromise] sign bytes.
+	// ValidatorSignatures are ed25519 signatures over the [PaymentPromise] sign bytes.
 	ValidatorSignatures [][]byte
-	// TTL is the time-to-live for the blob.
+	// TTL is the time-to-live for the [Blob].
 	TTL time.Time
-	// PFFConfirmation contains the transaction hash and height of the PayForFibre message.
-	PFFConfirmation PFFConfirmation
+	// TxHash is the transaction hash of the [types.MsgPayForFibre].
+	TxHash string
+	// Height is the block height where the [types.MsgPayForFibre] transaction was included.
+	Height uint64
 }
 
 // Put uploads given data to the Fibre network.
@@ -51,7 +48,7 @@ func (c *Client) Put(ctx context.Context, ns share.Namespace, data []byte) (resu
 	}
 
 	commitment := blob.Commitment()
-	span.AddEvent("encoded", trace.WithAttributes(
+	span.AddEvent("blob_encoded", trace.WithAttributes(
 		attribute.String("commitment", commitment.String()),
 		attribute.Int("row_size", blob.RowSize()),
 	))
@@ -62,13 +59,44 @@ func (c *Client) Put(ctx context.Context, ns share.Namespace, data []byte) (resu
 		span.SetStatus(codes.Error, "failed to upload blob")
 		return result, err
 	}
+	span.AddEvent("blob_uploaded", trace.WithAttributes(
+		attribute.Int("sigs_amount", len(signedPromise.ValidatorSignatures)),
+	))
 
-	// TODO(@Wondertan): Submit and await inclusion of MsgPayForFibre transaction through txClient.
-
-	span.SetStatus(codes.Ok, "")
-	result = PutResult{
-		Commitment:          commitment,
+	// broadcast PayForFibre transaction
+	signerAddr := c.txClient.DefaultAddress()
+	msg := &types.MsgPayForFibre{
+		Signer:              signerAddr.String(),
+		PaymentPromise:      *signedPromise.ToProto(),
 		ValidatorSignatures: signedPromise.ValidatorSignatures,
 	}
-	return result, nil
+
+	broadcastResp, err := c.txClient.BroadcastTx(ctx, []sdk.Msg{msg})
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "failed to broadcast PayForFibre transaction")
+		return result, fmt.Errorf("broadcasting PayForFibre transaction: %w", err)
+	}
+	span.AddEvent("pff_broadcasted", trace.WithAttributes(
+		attribute.String("pff_hash", broadcastResp.TxHash),
+	))
+
+	// confirm transaction inclusion
+	txResp, err := c.txClient.ConfirmTx(ctx, broadcastResp.TxHash)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, "failed to confirm PayForFibre transaction")
+		return result, fmt.Errorf("confirming PayForFibre transaction: %w", err)
+	}
+	span.AddEvent("pff_confirmed", trace.WithAttributes(
+		attribute.Int64("height", txResp.Height),
+	))
+
+	span.SetStatus(codes.Ok, "")
+	return PutResult{
+		Commitment:          commitment,
+		ValidatorSignatures: signedPromise.ValidatorSignatures,
+		TxHash:              txResp.TxHash,
+		Height:              uint64(txResp.Height),
+	}, nil
 }
