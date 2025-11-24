2a3affa812cc97e4769509e75d01cdaa8d64b265
sweexordious <chamirachid1@gmail.com>
2025-11-24 11:00:04 +0400

    chore: update the timeouts

--- cmd/celestia-appd/cmd/override_consensus_config_test.go
+++ cmd/celestia-appd/cmd/override_consensus_config_test.go
@@ -2,18 +2,17 @@ package cmd
 
 import (
 	"context"
-	"os"
-	"path/filepath"
-	"testing"
-	"time"
-
 	"cosmossdk.io/log"
 	"github.com/celestiaorg/celestia-app/v6/app"
+	"github.com/celestiaorg/celestia-app/v6/pkg/appconsts"
 	tmcfg "github.com/cometbft/cometbft/config"
 	"github.com/cosmos/cosmos-sdk/server"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"os"
+	"path/filepath"
+	"testing"
 )
 
 func TestOverrideConsensusConfig_Integration(t *testing.T) {
@@ -55,10 +54,10 @@ func TestOverrideConsensusConfig_Integration(t *testing.T) {
 	// Get the modified config from the server context
 	modifiedCfg := server.GetServerContextFromCmd(cmd).Config
 
-	assert.Equal(t, 3500*time.Millisecond, modifiedCfg.Consensus.TimeoutPropose)
-	assert.Equal(t, time.Second, modifiedCfg.Consensus.TimeoutPrevote)
-	assert.Equal(t, 500*time.Millisecond, modifiedCfg.Consensus.TimeoutPrevoteDelta)
-	assert.Equal(t, time.Second, modifiedCfg.Consensus.TimeoutPrecommit)
-	assert.Equal(t, 500*time.Millisecond, modifiedCfg.Consensus.TimeoutPrecommitDelta)
-	assert.Equal(t, 4200*time.Millisecond, modifiedCfg.Consensus.TimeoutCommit)
+	assert.Equal(t, appconsts.TimeoutPropose, modifiedCfg.Consensus.TimeoutPropose)
+	assert.Equal(t, appconsts.TimeoutPrevote, modifiedCfg.Consensus.TimeoutPrevote)
+	assert.Equal(t, appconsts.TimeoutPrevoteDelta, modifiedCfg.Consensus.TimeoutPrevoteDelta)
+	assert.Equal(t, appconsts.TimeoutPrecommit, modifiedCfg.Consensus.TimeoutPrecommit)
+	assert.Equal(t, appconsts.TimeoutPrecommitDelta, modifiedCfg.Consensus.TimeoutPrecommitDelta)
+	assert.Equal(t, appconsts.TimeoutCommit, modifiedCfg.Consensus.TimeoutCommit)
 }
