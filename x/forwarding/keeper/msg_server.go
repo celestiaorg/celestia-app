96f6f6928e677a3fe1f95de1a7c67e49a8190eb5
Damian Nolan <damiannolan@gmail.com>
2026-03-18 14:50:28 +0100

    chore: propagate events

--- x/forwarding/keeper/msg_server.go
+++ x/forwarding/keeper/msg_server.go
@@ -97,7 +97,9 @@ func (m msgServer) processTokens(
 		result := m.forwardSingleToken(cacheCtx, forwardAddr, signerAddr, balance, msg.DestDomain, destRecipient, msg.MaxIgpFee)
 		if result.Success {
 			writeCache()
+			ctx.EventManager().EmitEvents(cacheCtx.EventManager().Events())
 		}
+
 		results = append(results, result)
 
 		EmitTokenForwardedEvent(ctx, msg.ForwardAddr, result)
