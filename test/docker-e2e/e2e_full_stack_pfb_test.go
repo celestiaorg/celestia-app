f7caca66ee5dd62daa530bab7f9de0464b0c293c
Nina Barbakadze <barbakadzeninaa@gmail.com>
2026-04-01 18:36:24 +0200

    fix: remove da node related logic from full stack pfb test and bump tastora version

--- test/docker-e2e/e2e_full_stack_pfb_test.go
+++ test/docker-e2e/e2e_full_stack_pfb_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/celestiaorg/celestia-app/v8/pkg/user"
 	"github.com/celestiaorg/celestia-app/v8/test/util/testfactory"
 	"github.com/celestiaorg/celestia-app/v8/x/blob/types"
-	"github.com/celestiaorg/go-square/v3/share"
+	"github.com/celestiaorg/go-square/v3/share"x
 	tastoracontainertypes "github.com/celestiaorg/tastora/framework/docker/container"
 	tastoradockertypes "github.com/celestiaorg/tastora/framework/docker/cosmos"
 	da "github.com/celestiaorg/tastora/framework/docker/dataavailability"
@@ -34,7 +34,6 @@ const (
 // TestE2EFullStackPFB is an E2E test which tests the basic functionality of the entire stack.
 // This test does the following:
 // - deploys celestia-app
-// - deploys a celestia-node full node
 // - deploys a celestia-node bridge node
 // - deploys a celestia-node light node
 // - submits multiple PFBs
@@ -88,17 +87,13 @@ func (s *CelestiaTestSuite) TestE2EFullStackPFB() {
 	t.Log("Full stack blob test completed successfully")
 }
 
-// DeployDANetwork deploys a data availability network with bridge, full, and light nodes
+// DeployDANetwork deploys a data availability network with bridge and light nodes
 func (s *CelestiaTestSuite) DeployDANetwork(ctx context.Context, celestia *tastoradockertypes.Chain, dockerClient tastoratypes.TastoraDockerClient, networkID string) *da.Network {
 	t := s.T()
 
 	// Create node configurations
 	bridgeNodeConfig := da.NewNodeBuilder().
 		WithNodeType(tastoratypes.BridgeNode).
-		Build()
-
-	fullNodeConfig := da.NewNodeBuilder().
-		WithNodeType(tastoratypes.FullNode).
 		Build()
 
 	lightNodeConfig := da.NewNodeBuilder().
@@ -118,7 +113,7 @@ func (s *CelestiaTestSuite) DeployDANetwork(ctx context.Context, celestia *tasto
 		WithDockerClient(dockerClient).
 		WithDockerNetworkID(networkID).
 		WithImage(daImage).
-		WithNodes(bridgeNodeConfig, lightNodeConfig, fullNodeConfig).
+		WithNodes(bridgeNodeConfig, lightNodeConfig).
 		Build(ctx)
 	s.Require().NoError(err, "failed to create DA network")
 
@@ -148,36 +143,13 @@ func (s *CelestiaTestSuite) DeployDANetwork(ctx context.Context, celestia *tasto
 		s.Require().NoError(err, "failed to start bridge node")
 	}
 
-	// get P2P info from bridge node for full nodes
+	// get P2P info from bridge node for light nodes
 	var bridgeP2PAddr string
 	if len(bridgeNodes) > 0 {
 		p2pInfo, err := bridgeNodes[0].GetP2PInfo(ctx)
 		s.Require().NoError(err, "failed to get bridge node p2p info")
 		bridgeP2PAddr, err = p2pInfo.GetP2PAddress()
 		s.Require().NoError(err, "failed to get bridge node p2p address")
-	}
-
-	// start full nodes
-	fullNodes := daNetwork.GetFullNodes()
-	for _, node := range fullNodes {
-		err := node.Start(ctx,
-			da.WithChainID(celestia.GetChainID()),
-			da.WithAdditionalStartArguments("--p2p.network", celestia.GetChainID(), "--core.ip", coreNodeHostname, "--rpc.addr", "0.0.0.0"),
-			da.WithEnvironmentVariables(map[string]string{
-				"CELESTIA_CUSTOM": tastoratypes.BuildCelestiaCustomEnvVar(celestia.GetChainID(), genesisHash, bridgeP2PAddr),
-				"P2P_NETWORK":     celestia.GetChainID(),
-			}),
-		)
-		s.Require().NoError(err, "failed to start full node")
-	}
-
-	// get P2P info from full node for light nodes
-	var fullP2PAddr string
-	if len(fullNodes) > 0 {
-		p2pInfo, err := fullNodes[0].GetP2PInfo(ctx)
-		s.Require().NoError(err, "failed to get full node p2p info")
-		fullP2PAddr, err = p2pInfo.GetP2PAddress()
-		s.Require().NoError(err, "failed to get full node p2p address")
 	}
 
 	// start light nodes
@@ -187,7 +159,7 @@ func (s *CelestiaTestSuite) DeployDANetwork(ctx context.Context, celestia *tasto
 			da.WithChainID(celestia.GetChainID()),
 			da.WithAdditionalStartArguments("--p2p.network", celestia.GetChainID(), "--rpc.addr", "0.0.0.0"),
 			da.WithEnvironmentVariables(map[string]string{
-				"CELESTIA_CUSTOM": tastoratypes.BuildCelestiaCustomEnvVar(celestia.GetChainID(), genesisHash, fullP2PAddr),
+				"CELESTIA_CUSTOM": tastoratypes.BuildCelestiaCustomEnvVar(celestia.GetChainID(), genesisHash, bridgeP2PAddr),
 				"P2P_NETWORK":     celestia.GetChainID(),
 			}),
 		)
@@ -371,11 +343,6 @@ func removeDANetwork(ctx context.Context, daNetwork *da.Network) error {
 	for _, node := range daNetwork.GetBridgeNodes() {
 		if err := node.Remove(ctx); err != nil {
 			errs = append(errs, fmt.Errorf("failed to remove bridge node: %w", err))
-		}
-	}
-	for _, node := range daNetwork.GetFullNodes() {
-		if err := node.Remove(ctx); err != nil {
-			errs = append(errs, fmt.Errorf("failed to remove full node: %w", err))
 		}
 	}
 	for _, node := range daNetwork.GetLightNodes() {
