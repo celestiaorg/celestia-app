3708415ec371810613f9f1b0d30532b9d5b62b0e
sweexordious <chamirachid1@gmail.com>
2026-04-06 21:53:46 +0100

    chore: update Celestia V8 references to `v8.0.1-mocha` across Dockerfile, Makefile, and release scripts

--- internal/embedding/data.go
+++ internal/embedding/data.go
@@ -15,8 +15,7 @@ const (
 	v5Version = "v5.0.12"
 	v6Version = "v6.4.4"
 	v7Version = "v7.0.2-mocha"
-	// TODO(@rach-id): use the version deployed on mainnet
-	v8Version = "v8.0.0"
+	v8Version = "v8.0.1-mocha"
 )
 
 // CelestiaAppV3 returns the compressed platform specific Celestia binary and
