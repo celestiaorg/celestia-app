430d6df27d0a92831cd6914ea007d771f15312cc
nina / ნინა <barbakadzeninaa@gmail.com>
2026-05-28 19:51:52 +0400

    chore: bump the embedded v8 binary version (#7307)
    (cherry picked from commit 5076dad85d7b6c15c60a0b8de443783c16cd79e6)

--- internal/embedding/data.go
+++ internal/embedding/data.go
@@ -15,7 +15,7 @@ const (
 	v5Version = "v5.0.12"
 	v6Version = "v6.4.4"
 	v7Version = "v7.0.2-mocha"
-	v8Version = "v8.0.3"
+	v8Version = "v8.0.6-arabica"
 )
 
 // CelestiaAppV3 returns the compressed platform specific Celestia binary and
