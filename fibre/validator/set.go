9b4739c4f1f35e3224794a7690329f4c62384101
reflecttypefor <reflecttypefor@outlook.com>
2026-06-19 04:56:29 +0800

    chore(fibre): fix row indices typo (#7431)
    <!--
    Please read and fill out this form before submitting your PR.
    
    Please make sure you have reviewed our contributors guide before
    submitting your
    first PR.
    -->
    
    ## Overview
    
    <!-- 
    Please provide an explanation of the PR, including the appropriate
    context,
    background, goal, and rationale. If there is an issue with this
    information,
    please provide a tl;dr and link the issue. 
    -->
    
    fix row indices typo
    
    Signed-off-by: reflecttypefor <reflecttypefor@outlook.com>
    Co-authored-by: nina / ნინა <barbakadzeninaa@gmail.com>

--- fibre/validator/set.go
+++ fibre/validator/set.go
@@ -83,12 +83,12 @@ func (s Set) Assign(commitment rsema1d.Commitment, totalRows, originalRows, minR
 
 	// shuffle all totalRows indices with Fisher-Yates algorithm
 	// NOTE: std library Shuffle implements Fisher-Yates algorithm
-	rowsIndicies := make([]int, totalRows)
+	rowsIndices := make([]int, totalRows)
 	for i := range totalRows {
-		rowsIndicies[i] = i
+		rowsIndices[i] = i
 	}
 	rng.Shuffle(totalRows, func(i, j int) {
-		rowsIndicies[i], rowsIndicies[j] = rowsIndicies[j], rowsIndicies[i]
+		rowsIndices[i], rowsIndices[j] = rowsIndices[j], rowsIndices[i]
 	})
 
 	// assign rows to validators, wrapping around with modulo if total assigned exceeds totalRows
@@ -98,7 +98,7 @@ func (s Set) Assign(commitment rsema1d.Commitment, totalRows, originalRows, minR
 		rows := make([]int, rowsPerValidator[i])
 		for j := range rows {
 			// modulo ensures wrap-around when minRows causes over-assignment
-			rows[j] = rowsIndicies[(offset+j)%totalRows]
+			rows[j] = rowsIndices[(offset+j)%totalRows]
 		}
 		shardMap[validator] = rows
 		offset += rowsPerValidator[i]
